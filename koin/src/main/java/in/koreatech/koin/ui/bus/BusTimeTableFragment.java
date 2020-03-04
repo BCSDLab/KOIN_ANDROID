4697383a36646878012873aa2719e8eaf8666cc8
Yunjae-Na <yunjaena@gmail.com>
2020-03-05 00:42:48 +0900

    Change comment
    Change comment

--- koin/src/main/java/in/koreatech/koin/ui/bus/BusTimeTableFragment.java
+++ koin/src/main/java/in/koreatech/koin/ui/bus/BusTimeTableFragment.java
@@ -152,10 +152,11 @@ public class BusTimeTableFragment extends BusBaseFragment implements BusTimeTabl
         fragmentTransaction.commit();
     }
 
-    // TODO -> 코로나 바이러스 끝난 후 변경 필요
+
     @OnClick(R.id.bus_timetable_bustype_daesung)
     public void onClickBustypeDaesung() {
         FragmentTransaction fragmentTransaction = this.fragmentManger.beginTransaction();
+        // TODO -> 코로나 바이러스 끝난 후 변경 필요
         fragmentTransaction.replace(R.id.bus_timetable_fragment, new BusTimeTableUnitoYawooriDaesungTemporaryFragment());
         // fragmentTransaction.replace(R.id.bus_timetable_fragment, new BusTimeTableUnitoYawooriDaesungFragment());
         busTimetableCheonanStartEndSpinner.setVisibility(View.GONE);
