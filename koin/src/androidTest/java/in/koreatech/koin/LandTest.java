bbaf17204ddb5aa056c8a6ecb1a85a4e7d2e47a9
주동민 <dongmin960903@gmail.com>
2020-04-17 01:00:09 +0900

    Add  basic land ui test

---
+++ koin/src/androidTest/java/in/koreatech/koin/LandTest.java
@@ -0,0 +1,83 @@
+package in.koreatech.koin;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.espresso.matcher.RootMatchers;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import in.koreatech.koin.ui.land.LandActivity;
+import in.koreatech.koin.ui.land.LandDetailActivity;
+import in.koreatech.koin.ui.land.adapter.LandRecyclerAdapter;
+import in.koreatech.koin.ui.main.MainActivity;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+
+public class LandTest {
+    @Rule
+    public IntentsTestRule<LandActivity> activityRule =
+            new IntentsTestRule<>(LandActivity.class);
+
+    @Test
+    public void testcaseForAppBarTitle(){
+        onView(withId(R.id.koin_base_app_bar_dark)).check(matches(hasDescendant(withText(R.string.navigation_item_land))));
+    }
+
+
+    @Test
+    public void testCaseForRecyclerScroll(){
+        RecyclerView recyclerView = activityRule.getActivity().findViewById(R.id.land_recyclerlayout);
+        int itemCount = recyclerView.getAdapter().getItemCount();
+
+        onView(withId(R.id.land_recyclerlayout)).inRoot(RootMatchers.withDecorView(
+                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(RecyclerViewActions.scrollToPosition(itemCount-1));
+    }
+    @Test
+    public void testCaseForRecyclerClick(){
+        onView(withId(R.id.land_recyclerlayout)).inRoot(RootMatchers.withDecorView(
+                Matchers.is(activityRule.getActivity().getWindow().getDecorView())))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        String key = "Land_ID";
+
+        //RecyclerView recyclerView = activityRule.getActivity().findViewById(R.id.land_recyclerlayout);
+        //int itemCount = recyclerView.getAdapter().geTgetItemCount();
+
+        intended(hasExtra(key, 11));
+        intended(toPackage("in.koreatech.koin"));
+        intended(hasComponent("in.koreatech.koin.ui.land.LandDetailActivity"));
+
+        intended(allOf(
+                hasExtra("Land_ID", 11),
+                toPackage("in.koreatech.koin"),
+                hasComponent("in.koreatech.koin.ui.land.LandDetailActivity")));
+    }
+
+}
