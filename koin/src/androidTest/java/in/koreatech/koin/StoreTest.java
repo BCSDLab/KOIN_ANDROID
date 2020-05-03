ef114c25348fadf6771c92e043d69dec62edfe28
주동민 <dongmin960903@gmail.com>
2020-05-03 16:58:48 +0900

    Revise textviewcolormatcher

--- koin/src/androidTest/java/in/koreatech/koin/StoreTest.java
+++ koin/src/androidTest/java/in/koreatech/koin/StoreTest.java
@@ -29,8 +29,11 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static in.koreatech.koin.matcher.RecyclerViewMatcher.atPositionOnView;
+import static in.koreatech.koin.matcher.TextColorMatcher.textViewTextColorMatcher;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -65,35 +68,20 @@ public class StoreTest {
     }
 
     /**
-     * 리사이클러뷰 스크롤 테스트
+     * 주변상점 상세화면 이동 테스트
      */
     @Test
-    public void testCaseForRecyclerScroll(){
-        onView(withId(R.id.store_root_scroll_view)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(ViewActions.swipeDown());
+    public void testcaseForMoveDetail() {
+        onView(withId(R.id.store_root_scroll_view)).perform(ViewActions.swipeDown());
 
-        RecyclerView recyclerView = activityRule.getActivity().findViewById(R.id.store_recyclerview);
-        int itemCount = recyclerView.getAdapter().getItemCount();
+        onView(atPositionOnView(R.id.store_recyclerview, 1, R.id.store_name_textview))
+                .perform(click());
 
-        onView(withId(R.id.store_recyclerview)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(RecyclerViewActions.scrollToPosition(itemCount-1));
+        onView(withId(R.id.store_detail_title_textview)).check(matches(isDisplayed()));
     }
 
-    /**
-     * 리사이클러뷰의 아이템 클릭 테스트
-     * 리사이클러뷰의 2번째 항목인 가나 콜벤 클릭 시 음식점 상세화면의 음식점 이름이 가나콜밴인지 확인
-     */
-    @Test
-    public void testCaseForRecyclerClick(){     //리사이클러뷰의 항목을 클릭하면 해당 항목의 상세화면으로 이동하는지 확인
-        onView(withId(R.id.store_root_scroll_view)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(ViewActions.swipeDown());
 
-        onView(withId(R.id.store_recyclerview)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView())))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(1,click()));
-        onView(withId(R.id.store_detail_title_textview)).check(matches(withText("가나콜밴")));
 
-    }
 
     /**
      * 카테고리에 속한 버튼 클릭 시 클릭한 버튼이 주황색으로 변화는지 확인
@@ -101,9 +89,7 @@ public class StoreTest {
     @Test
     public void testcaseForCategoryButtonClick(){
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        onView(withId(R.id.store_root_scroll_view)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(ViewActions.swipeDown());
-
+        onView(withId(R.id.store_root_scroll_view)).perform(ViewActions.swipeDown());
         int [] storeCategoryId = {R.id.store_category_chicken_textview, R.id.store_category_pizza_textview,R.id.store_category_sweet_pork_textview, R.id.store_category_dosirak_textview
                 , R.id.store_category_sweet_pork_feet_textview, R.id.store_category_chinese_textview, R.id.store_category_normal_textview, R.id.store_category_hair_textview, R.id.store_category_etc_textview};
 
@@ -120,9 +106,7 @@ public class StoreTest {
     @Test
     public void testcaseForChickenCategory(){
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        onView(withId(R.id.store_root_scroll_view)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(ViewActions.swipeDown());
-
+        onView(withId(R.id.store_root_scroll_view)).perform(ViewActions.swipeDown());
         //치킨 글자가 주황색으로 변경되는지 확인
         onView(withId(R.id.store_category_chicken_textview)).perform(click());
         onView(withId(R.id.store_category_chicken_textview)).check(matches(textViewTextColorMatcher(context.getResources().getColor(R.color.colorAccent))));
@@ -140,9 +124,7 @@ public class StoreTest {
     @Test
     public void testcaseForPizzaCategory(){     //피자 카테고리 클릭 후 아래 리사이클러뷰가 바뀌는지 확인
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        onView(withId(R.id.store_root_scroll_view)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(ViewActions.swipeDown());
-
+        onView(withId(R.id.store_root_scroll_view)).perform(ViewActions.swipeDown());
         //피자 글자가 주황색으로 변경되는지 확인
         onView(withId(R.id.store_category_pizza_textview)).perform(click());
         onView(withId(R.id.store_category_pizza_textview)).check(matches(textViewTextColorMatcher(context.getResources().getColor(R.color.colorAccent))));
@@ -160,9 +142,7 @@ public class StoreTest {
     @Test
     public void testcaseForSweetPorkCategory(){
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        onView(withId(R.id.store_root_scroll_view)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(ViewActions.swipeDown());
-
+        onView(withId(R.id.store_root_scroll_view)).perform(ViewActions.swipeDown());
         //탕수육 글자가 주황색으로 변경되는지 확인
        onView(withId(R.id.store_category_sweet_pork_textview)).perform(click());
         onView(withId(R.id.store_category_sweet_pork_textview)).check(matches(textViewTextColorMatcher(context.getResources().getColor(R.color.colorAccent))));
@@ -180,8 +160,7 @@ public class StoreTest {
     @Test
     public void testcaseForDosirakCategory(){
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        onView(withId(R.id.store_root_scroll_view)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(ViewActions.swipeDown());
+        onView(withId(R.id.store_root_scroll_view)).perform(ViewActions.swipeDown());
         //도시락 글자가 주황색으로 변경되는지 확인
         onView(withId(R.id.store_category_dosirak_textview)).perform(click());
         onView(withId(R.id.store_category_dosirak_textview)).check(matches(textViewTextColorMatcher(context.getResources().getColor(R.color.colorAccent))));
@@ -200,9 +179,7 @@ public class StoreTest {
     @Test
     public void testcaseForSweetPorkFeetCategory(){
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        onView(withId(R.id.store_root_scroll_view)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(ViewActions.swipeDown());
-
+        onView(withId(R.id.store_root_scroll_view)).perform(ViewActions.swipeDown());
         //족발 글자가 주황색으로 변경되는지 확인
         onView(withId(R.id.store_category_sweet_pork_feet_textview)).perform(click());
         onView(withId(R.id.store_category_sweet_pork_feet_textview)).check(matches(textViewTextColorMatcher(context.getResources().getColor(R.color.colorAccent))));
@@ -220,9 +197,7 @@ public class StoreTest {
     @Test
     public void testcaseForChineseCategory(){
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        onView(withId(R.id.store_root_scroll_view)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(ViewActions.swipeDown());
-
+        onView(withId(R.id.store_root_scroll_view)).perform(ViewActions.swipeDown());
         //중국집 글자가 주황색으로 변경되는지 확인
         onView(withId(R.id.store_category_chinese_textview)).perform(click());
         onView(withId(R.id.store_category_chinese_textview)).check(matches(textViewTextColorMatcher(context.getResources().getColor(R.color.colorAccent))));
@@ -240,9 +215,7 @@ public class StoreTest {
     @Test
     public void testcaseForNormalCategory(){
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        onView(withId(R.id.store_root_scroll_view)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(ViewActions.swipeDown());
-
+        onView(withId(R.id.store_root_scroll_view)).perform(ViewActions.swipeDown());
         //일반음식점 글자가 주황색으로 변경되는지 확인
         onView(withId(R.id.store_category_normal_textview)).perform(click());
         onView(withId(R.id.store_category_normal_textview)).check(matches(textViewTextColorMatcher(context.getResources().getColor(R.color.colorAccent))));
@@ -260,9 +233,7 @@ public class StoreTest {
     @Test
     public void testcaseForHairCategory(){
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        onView(withId(R.id.store_root_scroll_view)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(ViewActions.swipeDown());
-
+        onView(withId(R.id.store_root_scroll_view)).perform(ViewActions.swipeDown());
         //미용실 글자가 주황색으로 변경되는지 확인
         onView(withId(R.id.store_category_hair_textview)).perform(click());
         onView(withId(R.id.store_category_hair_textview)).check(matches(textViewTextColorMatcher(context.getResources().getColor(R.color.colorAccent))));
@@ -280,9 +251,7 @@ public class StoreTest {
     @Test
     public void testcaseForEtcCategory(){
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        onView(withId(R.id.store_root_scroll_view)).inRoot(RootMatchers.withDecorView(
-                Matchers.is(activityRule.getActivity().getWindow().getDecorView()))).perform(ViewActions.swipeDown());
-
+        onView(withId(R.id.store_root_scroll_view)).perform(ViewActions.swipeDown());
         //기타 글자가 주황색으로 변경되는지 확인
         onView(withId(R.id.store_category_etc_textview)).perform(click());
         onView(withId(R.id.store_category_etc_textview)).check(matches(textViewTextColorMatcher(context.getResources().getColor(R.color.colorAccent))));
@@ -295,21 +264,5 @@ public class StoreTest {
 
     }
 
-    /**
-     * TextView 글자 색 Matcher 함수
-     * @param matcherColor
-     * @return
-     */
-    public static Matcher<View> textViewTextColorMatcher(final int matcherColor) {
-        return new BoundedMatcher<View, TextView>(TextView.class) {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("with text color: " + matcherColor);
-            }
-            @Override
-            protected boolean matchesSafely(TextView textView) {
-                return matcherColor == textView.getCurrentTextColor();
-            }
-        };
-    }
+
 }
