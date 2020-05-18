749770914a9c0e9eddab2f8a17932925193eac08
주동민 <dongmin960903@gmail.com>
2020-05-18 22:27:43 +0900

    Revise spacing code

--- koin/src/androidTest/java/in/koreatech/koin/LostFoundInstrumentedTest/LostFoundCommentTest.java
+++ koin/src/androidTest/java/in/koreatech/koin/LostFoundInstrumentedTest/LostFoundCommentTest.java
@@ -1,6 +1,5 @@
 package in.koreatech.koin.LostFoundInstrumentedTest;
 
-import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
@@ -9,8 +8,6 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import in.koreatech.koin.R;
-import in.koreatech.koin.ui.lostfound.LostFoundCommentActivity;
-import in.koreatech.koin.ui.lostfound.LostFoundEditActivity;
 import in.koreatech.koin.ui.lostfound.LostFoundMainActivity;
 
 import static androidx.test.espresso.Espresso.onView;
@@ -21,7 +18,6 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static in.koreatech.koin.matcher.RecyclerViewMatcher.atPositionOnView;
@@ -30,8 +26,8 @@ import static org.hamcrest.Matchers.allOf;
 
 public class LostFoundCommentTest {
     @Rule
-    public ActivityTestRule<LostFoundMainActivity> activityRule =
-            new ActivityTestRule<>(LostFoundMainActivity.class);
+    public ActivityTestRule<LostFoundMainActivity> activityRule = new ActivityTestRule<>(LostFoundMainActivity.class);
+
     /**
      * 앱바 이름 확인
      */
@@ -44,28 +40,26 @@ public class LostFoundCommentTest {
      * 1. 아무것도 입력하지 않았을경우 토스트 메시지 확인
      */
     @Test
-    public void testcaseForBlinkTextView(){
-        onView(withId(R.id.lostfound_main_recyclerview))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));    //첫번째 분실물게시글 클릭
+    public void testcaseForBlinkTextView() {
+        onView(withId(R.id.lostfound_main_recyclerview)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));       //첫번째 분실물게시글 클릭
 
-        onView(withId(R.id.lostfound_detail_comment_button)).perform(click());                //등록 버튼 클릭
+        onView(withId(R.id.lostfound_detail_comment_button)).perform(click());                                                          //등록 버튼 클릭
 
-        onView(withId(R.id.lostfound_comment_register_button)).perform(click());                //등록 버튼 클릭
-        onView(withText("내용을 입력해주세요")).inRoot(withMessage("내용을 입력해주세요"))       //토스트메시지 도출
-                .check(matches(isDisplayed()));
+        onView(withId(R.id.lostfound_comment_register_button)).perform(click());                                                        //등록 버튼 클릭
+        onView(withText(R.string.board_content_check)).inRoot(withMessage("내용을 입력해주세요")).check(matches(isDisplayed()));         //토스트메시지 도출
+
     }
 
     /**
      * 댓글을 입력하고 취소버튼을 누를 경우 입력한 글이 지워지는지 확인
      */
     @Test
-    public void testcaseForCancelButtonClick(){
-        onView(withId(R.id.lostfound_main_recyclerview))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));    //첫번째 분실물게시글 클릭
+    public void testcaseForCancelButtonClick() {
+        onView(withId(R.id.lostfound_main_recyclerview)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));       //첫번째 분실물게시글 클릭
 
-        onView(withId(R.id.lostfound_detail_comment_button)).perform(click());                //등록 버튼 클릭
+        onView(withId(R.id.lostfound_detail_comment_button)).perform(click());                                                          //등록 버튼 클릭
 
-        onView(withId(R.id.lostfound_comment_content_edittext)).perform(typeText("I found it"), closeSoftKeyboard());   //"I found it" 입력
+        onView(withId(R.id.lostfound_comment_content_edittext)).perform(typeText("I found it"), closeSoftKeyboard());    //"I found it" 입력
         onView(withId(R.id.lostfound_comment_cancel_button)).perform(click());                                                          //취소 버튼 클릭
         onView(withId(R.id.lostfound_comment_content_edittext)).check(matches(withText("")));                                           //입력한 글자가 지워졌는지 확인
     }
@@ -74,57 +68,55 @@ public class LostFoundCommentTest {
      * 댓글을 작성하고 토스트 메시지 확인
      */
     @Test
-    public void testcaseForCreateComment(){
-        onView(withId(R.id.lostfound_main_recyclerview))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));    //첫번째 분실물게시글 클릭
+    public void testcaseForCreateComment() {
+        onView(withId(R.id.lostfound_main_recyclerview)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));       //첫번째 분실물게시글 클릭
 
-        onView(withId(R.id.lostfound_detail_comment_button)).perform(click());                //등록 버튼 클릭
+        onView(withId(R.id.lostfound_detail_comment_button)).perform(click());                                                          //등록 버튼 클릭
 
 
-        onView(withId(R.id.lostfound_comment_content_edittext)).perform(typeText("I found it"), closeSoftKeyboard());   //"I found it"입력
+        onView(withId(R.id.lostfound_comment_content_edittext)).perform(typeText("I found it"), closeSoftKeyboard());    //"I found it"입력
         onView(withId(R.id.lostfound_comment_register_button)).perform(click());                                                        //등록 버튼 클릭
-        onView(withText(R.string.lost_and_found_created)).inRoot(withMessage("생성되었습니다.")).check(matches(isDisplayed()));           //토스트메시지 확인
+        onView(withText(R.string.lost_and_found_created)).inRoot(withMessage("생성되었습니다.")).check(matches(isDisplayed()));          //토스트메시지 확인
     }
+
     /**
      * 댓글을 수정하고 토스트 메시지 확인
      */
     @Test
-    public void testcaseForEditComment(){
-        onView(withId(R.id.lostfound_main_recyclerview))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));    //첫번째 분실물게시글 클릭
+    public void testcaseForEditComment() {
+        onView(withId(R.id.lostfound_main_recyclerview)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));       //첫번째 분실물게시글 클릭
 
-        onView(withId(R.id.lostfound_detail_comment_button)).perform(click());                //등록 버튼 클릭
+        onView(withId(R.id.lostfound_detail_comment_button)).perform(click());                                                          //등록 버튼 클릭
 
-        onView(withId(R.id.lostfound_comment_content_edittext)).perform(typeText("I found it"), closeSoftKeyboard());   //"I found it"입력
-        onView(withId(R.id.lostfound_comment_register_button)).perform(click());            //등록 버튼 클릭
-        onView(withText(R.string.lost_and_found_created)).inRoot(withMessage("생성되었습니다.")).check(matches(isDisplayed()));        //토스트메시지 확인
+        onView(withId(R.id.lostfound_comment_content_edittext)).perform(typeText("I found it"), closeSoftKeyboard());    //"I found it"입력
+        onView(withId(R.id.lostfound_comment_register_button)).perform(click());                                                        //등록 버튼 클릭
+        onView(withText(R.string.lost_and_found_created)).inRoot(withMessage("생성되었습니다.")).check(matches(isDisplayed()));          //토스트메시지 확인
 
         onView(atPositionOnView(R.id.lostfound_comment_content_recyclerview, 0, R.id.lostfound_comment_edit)).perform(click()); //첫번째 댓글의 수정버튼 클릭
         onView(withId(R.id.lostfound_comment_content_edittext)).perform(clearText(), closeSoftKeyboard());
-        onView(withId(R.id.lostfound_comment_content_edittext)).perform(typeText("I found it2"), closeSoftKeyboard());  //"I found it2"입력
+        onView(withId(R.id.lostfound_comment_content_edittext)).perform(typeText("I found it2"), closeSoftKeyboard());   //"I found it2"입력
         onView(withId(R.id.lostfound_comment_register_button)).perform(click());                                                        //등록 버튼 클릭
-        onView(withText(R.string.lost_and_found_edited)).inRoot(withMessage("수정되었습니다.")).check(matches(isDisplayed()));         //토스트메시지 확인
+        onView(withText(R.string.lost_and_found_edited)).inRoot(withMessage("수정되었습니다.")).check(matches(isDisplayed()));           //토스트메시지 확인
     }
 
     /**
      * 댓글 삭제 확인
      */
     @Test
-    public void testcaseForDeleteComment(){
-        onView(withId(R.id.lostfound_main_recyclerview))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));    //첫번째 분실물게시글 클릭
+    public void testcaseForDeleteComment() {
+        onView(withId(R.id.lostfound_main_recyclerview)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));       //첫번째 분실물게시글 클릭
 
-        onView(withId(R.id.lostfound_detail_comment_button)).perform(click());                //등록 버튼 클릭
+        onView(withId(R.id.lostfound_detail_comment_button)).perform(click());                                                          //등록 버튼 클릭
 
 
         onView(withId(R.id.lostfound_comment_content_edittext)).perform(typeText("I found it"), closeSoftKeyboard());   //"I found it"입력
-        onView(withId(R.id.lostfound_comment_register_button)).perform(click());                                                        //등록 버튼 클릭
-        onView(withText(R.string.lost_and_found_created)).inRoot(withMessage("생성되었습니다.")).check(matches(isDisplayed()));           //토스트메시지 확인
+        onView(withId(R.id.lostfound_comment_register_button)).perform(click());                                                       //등록 버튼 클릭
+        onView(withText(R.string.lost_and_found_created)).inRoot(withMessage("생성되었습니다.")).check(matches(isDisplayed()));         //토스트메시지 확인
 
         onView(atPositionOnView(R.id.lostfound_comment_content_recyclerview, 0, R.id.lostfound_comment_remove)).perform(click()); //첫번째 댓글의 삭제버튼 클릭
 
-        onView(allOf(withText("댓글을 삭제할까요?"))).check(matches(isDisplayed()));                            //스낵바 도출
-        onView(withText("YES")).perform(click());                                                               //YES 버튼 클릭
+        onView(allOf(withText("댓글을 삭제할까요?"))).check(matches(isDisplayed()));                                                    //스낵바 도출
+        onView(withText("YES")).perform(click());                                                                                      //YES 버튼 클릭
 
 
         onView(withText(R.string.lost_and_found_deleted)).inRoot(withMessage("삭제되었습니다.")).check(matches(isDisplayed()));        //토스트메시지 확인
--- koin/src/androidTest/java/in/koreatech/koin/LostFoundInstrumentedTest/LostFoundMainTest.java
+++ koin/src/androidTest/java/in/koreatech/koin/LostFoundInstrumentedTest/LostFoundMainTest.java
@@ -1,21 +1,12 @@
 package in.koreatech.koin.LostFoundInstrumentedTest;
 
-import android.os.IBinder;
-import android.view.WindowManager;
-
-import androidx.test.espresso.Root;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 
-import org.hamcrest.Description;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 
 import in.koreatech.koin.R;
-import in.koreatech.koin.ui.lostfound.LostFoundEditActivity;
 import in.koreatech.koin.ui.lostfound.LostFoundMainActivity;
 
 import static androidx.test.espresso.Espresso.onView;
@@ -26,7 +17,6 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static in.koreatech.koin.matcher.TextColorMatcher.textViewTextColorMatcher;
@@ -35,8 +25,8 @@ import static org.hamcrest.Matchers.allOf;
 
 public class LostFoundMainTest {
     @Rule
-    public ActivityTestRule<LostFoundMainActivity> activityRule =
-            new ActivityTestRule<>(LostFoundMainActivity.class);
+    public ActivityTestRule<LostFoundMainActivity> activityRule = new ActivityTestRule<>(LostFoundMainActivity.class);
+
     /**
      * 앱바 이름 확인
      */
@@ -49,58 +39,56 @@ public class LostFoundMainTest {
      * 게시판 생성 확인
      */
     @Test
-    public void testcaseForBlinkTextView(){
-        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());                //분실물 게시글 작성버튼 클릭
+    public void testcaseForBlinkTextView() {
+        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());                                                            //분실물 게시글 작성버튼 클릭
 
-        onView(withId((R.id.lostfound_create_title_edittext))).perform(typeText("I lost phone"), closeSoftKeyboard()); //제목입력
-        onView(withId(R.id.lostfound_create_content_ediitext)).perform(typeText("call me"), closeSoftKeyboard());      //내용 입력
+        onView(withId((R.id.lostfound_create_title_edittext))).perform(typeText("I lost phone"), closeSoftKeyboard());   //제목입력
+        onView(withId(R.id.lostfound_create_content_ediitext)).perform(typeText("call me"), closeSoftKeyboard());        //내용 입력
 
-        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());    //작성 완료 버튼 클릭
+        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());                                                            //작성 완료 버튼 클릭
 
-        onView(withId(R.id.lostfound_detail_title_textview)).check(matches(isDisplayed()));     //입력한 제목이 생성되었는지 확인
-
-
+        onView(withId(R.id.lostfound_detail_title_textview)).check(matches(isDisplayed()));                                             //입력한 제목이 생성되었는지 확인
     }
 
     /**
      * 게시판 삭제 확인
      */
     @Test
-    public void testcasdForBackButton(){
-        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());                //분실물 게시글 작성버튼 클릭
+    public void testcasdForBackButton() {
+        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());                                                                                              //분실물 게시글 작성버튼 클릭
 
-        onView(withId((R.id.lostfound_create_title_edittext))).perform(typeText("I lost phone"), closeSoftKeyboard()); //제목입력
-        onView(withId(R.id.lostfound_create_content_ediitext)).perform(typeText("call me"), closeSoftKeyboard());      //내용 입력
+        onView(withId((R.id.lostfound_create_title_edittext))).perform(typeText("I lost phone"), closeSoftKeyboard());                                     //제목입력
+        onView(withId(R.id.lostfound_create_content_ediitext)).perform(typeText("call me"), closeSoftKeyboard());                                          //내용 입력
 
-        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());    //작성 완료 버튼 클릭
+        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());                                                                                              //작성 완료 버튼 클릭
 
         onView(withId(R.id.lostfound_detail_delete_button)).check(matches(textViewTextColorMatcher(activityRule.getActivity().getResources().getColor(R.color.red1))));   //삭제 버튼 색상 확인
-        onView(withId(R.id.lostfound_detail_delete_button)).perform(click());   //삭제 버튼 클릭
+        onView(withId(R.id.lostfound_detail_delete_button)).perform(click());                                                                                             //삭제 버튼 클릭
 
 
-        onView(allOf(withText("글을 삭제할까요?"))).check(matches(isDisplayed()));                            //스낵바 도출
-        onView(withText("YES")).perform(click());                                                            //YES 버튼 클릭
+        onView(allOf(withText(R.string.lost_and_found_remove_question))).check(matches(isDisplayed()));                                                                   //스낵바 도출
+        onView(withText("YES")).perform(click());                                                                                                                         //YES 버튼 클릭
     }
 
     /**
      * 게시판 수정 확인
      */
     @Test
-    public void testcaseForTextViewChange(){
-        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());                //분실물 게시글 작성버튼 클릭
+    public void testcaseForTextViewChange() {
+        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());                                                            //분실물 게시글 작성버튼 클릭
 
-        onView(withId((R.id.lostfound_create_title_edittext))).perform(typeText("I lost phone"), closeSoftKeyboard()); //제목입력
-        onView(withId(R.id.lostfound_create_content_ediitext)).perform(typeText("call me"), closeSoftKeyboard());      //내용 입력
+        onView(withId((R.id.lostfound_create_title_edittext))).perform(typeText("I lost phone"), closeSoftKeyboard());  //제목입력
+        onView(withId(R.id.lostfound_create_content_ediitext)).perform(typeText("call me"), closeSoftKeyboard());       //내용 입력
 
-        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());    //작성 완료 버튼 클릭
+        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());                                                            //작성 완료 버튼 클릭
 
         onView(withId(R.id.lostfound_detail_edit_button)).perform(click());
 
-        onView(withId((R.id.lostfound_create_title_edittext))).perform(clearText()); //제목글자 삭제
-        onView(withId((R.id.lostfound_create_title_edittext))).perform(typeText("I lost bag"), closeSoftKeyboard()); //"I lost bag"입력
-        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());                //분실물 게시글 작성버튼 클릭
+        onView(withId((R.id.lostfound_create_title_edittext))).perform(clearText());                                                    //제목글자 삭제
+        onView(withId((R.id.lostfound_create_title_edittext))).perform(typeText("I lost bag"), closeSoftKeyboard());     //"I lost bag"입력
+        onView(withId(R.id.base_appbar_dark_right_button)).perform(click());                                                            //분실물 게시글 작성버튼 클릭
 
-        onView(withText("수정되었습니다.")).inRoot(withMessage("수정되었습니다."))       //토스트메시지 확인
-                .check(matches(isDisplayed()));
+        onView(withText(R.string.lost_and_found_edited)).inRoot(withMessage("수정되었습니다.")).check(matches(isDisplayed()));           //토스트메시지 확인
+
     }
 }
