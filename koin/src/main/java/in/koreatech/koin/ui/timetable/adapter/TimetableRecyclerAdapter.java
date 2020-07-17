afb35b5897da9cddce9ecdd7e0ef7e19a82ae9ea
주동민 <dongmin960903@gmail.com>
2020-07-17 17:42:41 +0900

    Add lottie animation in timetableRecyclerview item

--- koin/src/main/java/in/koreatech/koin/ui/timetable/adapter/TimetableRecyclerAdapter.java
+++ koin/src/main/java/in/koreatech/koin/ui/timetable/adapter/TimetableRecyclerAdapter.java
@@ -1,16 +1,18 @@
 package in.koreatech.koin.ui.timetable.adapter;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 import java.util.ArrayList;
 
@@ -75,20 +77,20 @@ public class TimetableRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.
 
     public class ItemViewHolder extends RecyclerView.ViewHolder {
 
-        RelativeLayout mTmetableRecyclerviewItemRelativelayout;
-        TextView mLectureTitle;
-        TextView mLectureInformationOne;
-        TextView mLectureInfromationTwo;
-        ImageButton mAddLectureButton;
+        RelativeLayout timetableRecyclerviewItemRelativelayout;
+        TextView lectureTitle;
+        TextView lectureInformationOne;
+        TextView lectureInfromationTwo;
+        LottieAnimationView addLectureButton;
 
 
         public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
-            mTmetableRecyclerviewItemRelativelayout = itemView.findViewById(R.id.timetable_recyclerview_item_relativelayout);
-            mLectureTitle = itemView.findViewById(R.id.lecture_title);
-            mLectureInformationOne = itemView.findViewById(R.id.lecture_information1);
-            mLectureInfromationTwo = itemView.findViewById(R.id.lecture_infromation2);
-            mAddLectureButton = itemView.findViewById(R.id.add_lecture_button);
+            timetableRecyclerviewItemRelativelayout = itemView.findViewById(R.id.timetable_recyclerview_item_relativelayout);
+            lectureTitle = itemView.findViewById(R.id.lecture_title);
+            lectureInformationOne = itemView.findViewById(R.id.lecture_information1);
+            lectureInfromationTwo = itemView.findViewById(R.id.lecture_infromation2);
+            addLectureButton = itemView.findViewById(R.id.add_lecture_button);
         }
     }
 
@@ -113,18 +115,19 @@ public class TimetableRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.
         StringBuilder stringBuilderTwo = new StringBuilder();
         Lecture lecture = this.lectureArrayList.get(position);
         if (lecture.isItemClicked)
-            holder.mTmetableRecyclerviewItemRelativelayout.setBackgroundColor(this.context.getResources().getColor(R.color.white4));
+            holder.timetableRecyclerviewItemRelativelayout.setBackgroundColor(this.context.getResources().getColor(R.color.white4));
         else
-            holder.mTmetableRecyclerviewItemRelativelayout.setBackgroundColor(this.context.getResources().getColor(R.color.white));
+            holder.timetableRecyclerviewItemRelativelayout.setBackgroundColor(this.context.getResources().getColor(R.color.white));
 
         if (lecture.isAddButtonClicked)
-            holder.mAddLectureButton.setBackground(this.context.getDrawable(R.drawable.ic_delete_lecture_item_button));
+            holder.addLectureButton.setAnimation("delete.json");
         else
-            holder.mAddLectureButton.setBackground(this.context.getDrawable(R.drawable.ic_add_lecture_item_button));
-        holder.mLectureTitle.setText(lecture.getName());
+            holder.addLectureButton.setAnimation("add.json");
+
+        holder.lectureTitle.setText(lecture.getName());
 
         stringBuilder.append(SeparateTime.getSpertateTimeToString(lecture.getClassTime())).append("/ 정원 ").append(lecture.getRegularNumber());
-        holder.mLectureInformationOne.setText(stringBuilder.toString());
+        holder.lectureInformationOne.setText(stringBuilder.toString());
 
         stringBuilderTwo.append(lecture.getDepartment()).append("/").append(lecture.getGrades()).append("학점").append("/").append(lecture.getCode()).append("/").append(lecture.getProfessor());
         if (lecture.getIsElearning().equals("Y"))
@@ -132,12 +135,39 @@ public class TimetableRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.
         if (lecture.getIsEnglish().equals("Y"))
             stringBuilderTwo.append("/").append("영강");
 
-        holder.mLectureInfromationTwo.setText(stringBuilderTwo.toString());
+        holder.lectureInfromationTwo.setText(stringBuilderTwo.toString());
 
         if (recyclerViewClickListener != null) {
-            holder.mAddLectureButton.setOnClickListener(v -> recyclerViewClickListener.onClick(v, position));
-            holder.mTmetableRecyclerviewItemRelativelayout.setOnClickListener(v -> recyclerViewClickListener.onClick(v, position));
+            holder.addLectureButton.setOnClickListener(v -> {
+                if (lecture.isAddButtonClicked) {
+                    holder.addLectureButton.playAnimation();
+                } else {
+                    holder.addLectureButton.playAnimation();
+                }
+            });
+            holder.timetableRecyclerviewItemRelativelayout.setOnClickListener(v -> recyclerViewClickListener.onClick(v, position));
         }
+        holder.addLectureButton.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                recyclerViewClickListener.onClick(holder.addLectureButton, position);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
 
 
