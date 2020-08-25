9bb54abe5dff57dcf94ff4b74aa0ddd38a545490
Seungmin Yang <tmdals099@gmail.com>
2020-08-25 15:21:08 +0900

    Fixed that bus timer is not working in ViewPager

--- koin/src/main/java/in/koreatech/koin/ui/main/MainActivity.java
+++ koin/src/main/java/in/koreatech/koin/ui/main/MainActivity.java
@@ -28,22 +28,32 @@ import in.koreatech.koin.core.utils.StatusBarUtil;
 import in.koreatech.koin.core.viewpager.ScaleViewPager;
 import in.koreatech.koin.data.network.interactor.CityBusRestInteractor;
 import in.koreatech.koin.data.network.interactor.TermRestInteractor;
+import in.koreatech.koin.ui.bus.TimerRenewListener;
 import in.koreatech.koin.ui.main.enums.DiningKinds;
 import in.koreatech.koin.ui.main.presenter.MainActivityContact;
 import in.koreatech.koin.ui.main.presenter.MainActivityPresenter;
 import in.koreatech.koin.ui.navigation.KoinNavigationDrawerActivity;
 import in.koreatech.koin.ui.store.StoreActivity;
+import in.koreatech.koin.util.BusTimerUtil;
 import in.koreatech.koin.util.FirebasePerformanceUtil;
+import in.koreatech.koin.util.TimeUtil;
+import in.koreatech.koin.util.TimerListener;
 
-public class MainActivity extends ActivityBase implements MainActivityContact.View {
+public class MainActivity extends ActivityBase implements MainActivityContact.View, TimerRenewListener, SwipeRefreshLayout.OnRefreshListener {
     private static String TAG = MainActivity.class.getSimpleName();
+    public static final int REFRESH_TIME = 60; // 1분 갱신
+
     private MainActivityContact.Presenter presenter = null;
     private Unbinder unbinder;
 
     private int departureState = 0; // 0 : 한기대 1 : 야우리 2 : 천안역
     private int arrivalState = 1; // 0 : 한기대 1 : 야우리 2 : 천안역
+    private int term;
 
-    private FirebasePerformanceUtil firebasePerformanceUtil;
+    private BusTimerUtil citySoonBusTimerUtil;
+    private BusTimerUtil daesungBusSoonBusTimerUtil;
+    private BusTimerUtil shuttleBusSoonBusTimerUtil;
+    private BusTimerUtil refreshBusTimerUtil;
 
     @BindView(R.id.toolbar)
     MaterialCardView toolbar;
@@ -85,13 +95,36 @@ public class MainActivity extends ActivityBase implements MainActivityContact.Vi
         init();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (refreshBusTimerUtil != null) {
+            refreshBusTimerUtil.setEndTime(REFRESH_TIME); // 5분마다 갱신
+            refreshBusTimerUtil.startTimer();
+        }
+    }
+
     private void init() {
         unbinder = ButterKnife.bind(this);
         setPresenter();
         StatusBarUtil.applyTopPaddingStatusBarHeight(toolbarLayout, getResources());
 
-        this.firebasePerformanceUtil = new FirebasePerformanceUtil("Bus_Activity");
-        this.firebasePerformanceUtil.start();
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+        citySoonBusTimerUtil = new BusTimerUtil(10);
+        daesungBusSoonBusTimerUtil = new BusTimerUtil(11);
+        shuttleBusSoonBusTimerUtil = new BusTimerUtil(12);
+        refreshBusTimerUtil = new BusTimerUtil(13);
+
+        citySoonBusTimerUtil.setTimerRenewListener(this);
+        daesungBusSoonBusTimerUtil.setTimerRenewListener(this);
+        shuttleBusSoonBusTimerUtil.setTimerRenewListener(this);
+        refreshBusTimerUtil.setTimerRenewListener(this);
+
+        citySoonBusTimerUtil.setTimerListener(value -> busPagerAdapter.updateCityBusTimeText(citySoonBusTimerUtil.getStrTime()));
+        daesungBusSoonBusTimerUtil.setTimerListener(value ->
+                busPagerAdapter.updateDaesungBusTimeText(daesungBusSoonBusTimerUtil.getStrTime()));
+        shuttleBusSoonBusTimerUtil.setTimerListener(value -> busPagerAdapter.updateShuttleBusTimeText(shuttleBusSoonBusTimerUtil.getStrTime()));
 
         initBusPager();
         initStoreRecyclerView();
@@ -122,6 +155,8 @@ public class MainActivity extends ActivityBase implements MainActivityContact.Vi
         });
     }
 
+
+
     private void initDining() {
         selectDiningKind(textViewDiningKinds.get(DiningKinds.KOREAN.getPosition()));
     }
@@ -150,24 +185,26 @@ public class MainActivity extends ActivityBase implements MainActivityContact.Vi
     @Override
     protected void onResume() {
         super.onResume();
-        if (this.presenter != null) {
-            showLoading();
-            this.presenter.getCityBus(this.departureState, this.arrivalState);
-            this.presenter.getDaesungBus(this.departureState, this.arrivalState);
-            this.presenter.getTermInfo();
-        }
+        onRefresh();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
+        if(citySoonBusTimerUtil != null) citySoonBusTimerUtil.stopTimer();
+        if(daesungBusSoonBusTimerUtil != null) daesungBusSoonBusTimerUtil.stopTimer();
+        if(shuttleBusSoonBusTimerUtil != null) shuttleBusSoonBusTimerUtil.stopTimer();
+        if(refreshBusTimerUtil != null) refreshBusTimerUtil.stopTimer();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         unbinder.unbind();
+        if(citySoonBusTimerUtil != null) citySoonBusTimerUtil.stopTimer();
+        if(daesungBusSoonBusTimerUtil != null) daesungBusSoonBusTimerUtil.stopTimer();
+        if(shuttleBusSoonBusTimerUtil != null) shuttleBusSoonBusTimerUtil.stopTimer();
+        if(refreshBusTimerUtil != null) refreshBusTimerUtil.stopTimer();
     }
 
     @Override
@@ -182,56 +219,116 @@ public class MainActivity extends ActivityBase implements MainActivityContact.Vi
 
     @Override
     public void updateShuttleBusTime(int current) {
-        busPagerAdapter.updateShuttleBusTime(current);
+        if (current >= 0) {
+            shuttleBusSoonBusTimerUtil.setEndTime(current);
+            busPagerAdapter.updateShuttleBusTimeText(shuttleBusSoonBusTimerUtil.getStrTime());
+            shuttleBusSoonBusTimerUtil.startTimer();
+        } else {
+            shuttleBusSoonBusTimerUtil.stopTimer();
+            busPagerAdapter.updateShuttleBusTimeText(getString(R.string.bus_no_information));
+        }
+
     }
 
     @Override
     public void updateCityBusTime(int current) {
-        busPagerAdapter.updateCityBusTime(current);
-    }
+        if (current > 0) {
+            //citybusSoonDepartureTimeTextview.setVisibility(View.VISIBLE);
+            //citybusSoonDepartureTimeTextview.setText("(" + TimeUtil.getAddTimeSecond(current) + ")분 출발");
+            this.citySoonBusTimerUtil.setEndTime(current);
+            busPagerAdapter.updateCityBusTimeText(citySoonBusTimerUtil.getStrTime());
+            this.citySoonBusTimerUtil.startTimer();
 
-    @Override
-    public void updateDaesungBusTime(int current) {
-        busPagerAdapter.updateDaesungBusTime(current);
-    }
-
-    @Override
-    public void updateShuttleBusDepartInfo(String current) {
-        busPagerAdapter.updateShuttleBusDepartInfo(current);
+        } else {
+            //citybusSoonArrivalTimeTextview.setText(R.string.bus_no_information);
+            //citybusSoonDepartureTimeTextview.setVisibility(View.INVISIBLE);
+            this.citySoonBusTimerUtil.stopTimer();
+        }
     }
 
     @Override
     public void updateCityBusDepartInfo(int current) {
-        busPagerAdapter.updateCityBusDepartInfo(current);
+        busPagerAdapter.updateCityBusDepartInfoText(current);
+    }
+
+    @Override
+    public void updateDaesungBusTime(int current) {
+        if (current > 0) {
+            daesungBusSoonBusTimerUtil.setEndTime(current);
+            busPagerAdapter.updateDaesungBusTimeText(daesungBusSoonBusTimerUtil.getStrTime());
+            daesungBusSoonBusTimerUtil.startTimer();
+        } else {
+            daesungBusSoonBusTimerUtil.stopTimer();
+            busPagerAdapter.updateDaesungBusDepartInfoText(getString(R.string.bus_no_information));
+        }
+    }
+
+    @Override
+    public void updateShuttleBusDepartInfo(String current) {
+        busPagerAdapter.updateShuttleBusDepartInfoText(current);
     }
 
     @Override
     public void updateDaesungBusDepartInfo(String current) {
-        busPagerAdapter.updateDaesungBusDepartInfo(current);
+        busPagerAdapter.updateDaesungBusDepartInfoText(current);
     }
 
     @Override
     public void updateFailDaesungBusDepartInfo() {
-
+        daesungBusSoonBusTimerUtil.stopTimer();
+        busPagerAdapter.updateDaesungBusTimeText(getString(R.string.bus_no_information));
+        busPagerAdapter.updateDaesungBusDepartInfoText("");
     }
 
     @Override
     public void updateFailShuttleBusDepartInfo() {
-
+        daesungBusSoonBusTimerUtil.stopTimer();
+        busPagerAdapter.updateShuttleBusTimeText(getString(R.string.bus_no_information));
+        busPagerAdapter.updateShuttleBusDepartInfoText("");
     }
 
     @Override
     public void updateFailCityBusDepartInfo() {
-
+        daesungBusSoonBusTimerUtil.stopTimer();
+        busPagerAdapter.updateCityBusTimeText(getString(R.string.bus_no_information));
+        busPagerAdapter.updateCityBusDepartInfoText(0);
     }
 
     @Override
     public void updateShuttleBusInfo(int term) {
-
+        this.term = term;
+        presenter.getShuttleBus(this.departureState, this.arrivalState, term);
     }
 
     @Override
     public void setPresenter(MainActivityContact.Presenter presenter) {
         this.presenter = presenter;
+    }
+
+    @Override
+    public void refreshTimer(int code) {
+        if (code == 10)
+            presenter.getCityBus(this.departureState, this.arrivalState);
+        else if (code == 11)
+            presenter.getDaesungBus(this.departureState, this.arrivalState);
+        else if (code == 12)
+            presenter.getTermInfo();
+        else if (code == 13) {
+            presenter.getCityBus(this.departureState, this.arrivalState);
+            presenter.getDaesungBus(this.departureState, this.arrivalState);
+            presenter.getTermInfo();
+            refreshBusTimerUtil.setEndTime(REFRESH_TIME);
+            refreshBusTimerUtil.startTimer();
+        }
+    }
+
+    @Override
+    public void onRefresh() {
+        if (this.presenter != null) {
+            showLoading();
+            this.presenter.getCityBus(this.departureState, this.arrivalState);
+            this.presenter.getDaesungBus(this.departureState, this.arrivalState);
+            this.presenter.getTermInfo();
+        }
     }
 }
