fe7eb5cddd86488c423a2c551ceb2049d30a83f2
Seungmin Yang <tmdals099@gmail.com>
2020-08-31 22:42:17 +0900

    Fixed not working bus timer
    Only Daesung bus timer tested.

--- koin/src/main/java/in/koreatech/koin/ui/main/BusPagerAdapter.java
+++ koin/src/main/java/in/koreatech/koin/ui/main/BusPagerAdapter.java
@@ -1,8 +1,11 @@
 package in.koreatech.koin.ui.main;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -10,12 +13,16 @@ import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.PagerAdapter;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import in.koreatech.koin.R;
+import in.koreatech.koin.util.timer.CountTimer;
 
-public class BusPagerAdapter extends PagerAdapter {
+public class BusPagerAdapter extends PagerAdapter implements CountTimer.OnTimerListener {
 
     interface OnSwitchClickListener {
         void onSwitchClick();
@@ -44,9 +51,23 @@ public class BusPagerAdapter extends PagerAdapter {
     private String daesungInfo = "";
     private int citybusInfo = 0;
 
+    private List<TextView> textViewsBus;
+
     @Override
     public int getCount() {
         return Integer.MAX_VALUE;
+    }
+
+    public BusPagerAdapter(Context context) {
+        textViewsBus = new ArrayList<>();
+        textViewsBus.add(new TextView(context));
+        textViewsBus.add(new TextView(context));
+        textViewsBus.add(new TextView(context));
+
+
+        textViewsBus.get(0).setText("학교셔틀");
+        textViewsBus.get(1).setText("대성고속");
+        textViewsBus.get(2).setText("시내버스");
     }
 
     @NonNull
@@ -125,18 +146,24 @@ public class BusPagerAdapter extends PagerAdapter {
     }
 
     public void updateShuttleBusTimeText(String value) {
-        for (View view : itemViewsShuttle)
-            ((TextView) view.findViewById(R.id.text_view_remaining_time)).setText(value);
+        for (View view : itemViewsShuttle) {
+            TextView textView = view.findViewById(R.id.text_view_remaining_time);
+            textView.post(() -> textView.setText(value));
+        }
     }
 
     public void updateCityBusTimeText(String value) {
-        for (View view : itemViewsCityBus)
-            ((TextView) view.findViewById(R.id.text_view_remaining_time)).setText(value);
+        for (View view : itemViewsCityBus) {
+            TextView textView = view.findViewById(R.id.text_view_remaining_time);
+            textView.post(() -> textView.setText(value));
+        }
     }
 
     public void updateDaesungBusTimeText(String value) {
-        for (View view : itemViewsDaesung)
-            ((TextView) view.findViewById(R.id.text_view_remaining_time)).setText(value);
+        for (View view : itemViewsDaesung) {
+            TextView textView = view.findViewById(R.id.text_view_remaining_time);
+            textView.post(() -> textView.setText(value));
+        }
     }
 
     public void updateShuttleBusDepartInfoText(String value) {
@@ -180,5 +207,40 @@ public class BusPagerAdapter extends PagerAdapter {
 
     public void setOnCardClickListener(OnCardClickListener onCardClickListener) {
         this.onCardClickListener = onCardClickListener;
+    }
+
+
+    @Override
+    public void onCountEvent(String name, long millisUntilFinished) {
+        switch (name) {
+            case MainActivity.SHUTTLE_SOON_BUS:
+                updateShuttleBusTimeText(timerMillisecondFormat(millisUntilFinished));
+                break;
+            case MainActivity.DAESUNG_SOON_BUS:
+                updateDaesungBusTimeText(timerMillisecondFormat(millisUntilFinished));
+                break;
+            case MainActivity.CITY_SOON_BUS:
+                updateCityBusTimeText(timerMillisecondFormat(millisUntilFinished));
+                break;
+        }
+    }
+
+    private String timerMillisecondFormat(long millisecond) {
+        String timeString = "";
+        long hour = (millisecond / (1000 * 60 * 60)) % 24;
+        long min = (millisecond / (1000 * 60)) % 60;
+        long sec = (millisecond / 1000) % 60;
+
+        if (hour > 0)
+            timeString = String.format(Locale.KOREA, "%d시간 %d분 %d초 남음", hour, min, sec);
+        else {
+            if (min > 1)
+                timeString = String.format(Locale.KOREA, "%d분 %d초 남음", min, sec);
+            else {
+                timeString = String.format("약 %d분 남음", min);
+                if (min == 0) timeString = "곧 도착 예정";
+            }
+        }
+        return timeString;
     }
 }
