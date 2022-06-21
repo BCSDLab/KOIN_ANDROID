aa43349a73e2e23eb52f2bb6c836e51bf4a95a09
Yang-Seungmin <tmdals099@gmail.com>
2022-06-21 14:21:40 +0900

    Add base class

---
+++ core/src/main/java/in/koreatech/koin/core/fragment/DataBindingFragment.kt
@@ -0,0 +1,26 @@
+package `in`.koreatech.koin.core.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class DataBindingFragment<T : ViewDataBinding> : BaseFragment() {
+    @get:LayoutRes
+    abstract val layoutId: Int
+
+    lateinit var binding: T
+        private set
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        return binding.root
+    }
+}
