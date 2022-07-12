8777554fa5952a5d9eed2b3697332b266208f306
Yang-Seungmin <tmdals099@gmail.com>
2022-07-12 20:34:11 +0900

    Remove 100ms delay

--- domain/src/main/java/in/koreatech/koin/domain/model/version/Version.kt
+++ domain/src/main/java/in/koreatech/koin/domain/model/version/Version.kt
@@ -1,5 +1,7 @@
 package `in`.koreatech.koin.domain.model.version
 
+import `in`.koreatech.koin.domain.state.version.VersionUpdatePriority
+
 data class Version(
     val currentVersion: String,
     val latestVersion: String,
