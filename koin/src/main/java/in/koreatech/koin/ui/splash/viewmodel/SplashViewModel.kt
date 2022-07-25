3682d15941a6d82ac4abf2f228ed7cbe8d984636
Yang-Seungmin <tmdals099@gmail.com>
2022-07-25 14:49:35 +0900

    Move checkToken to viewmodel

--- koin/src/main/java/in/koreatech/koin/ui/splash/viewmodel/SplashViewModel.kt
+++ koin/src/main/java/in/koreatech/koin/ui/splash/viewmodel/SplashViewModel.kt
@@ -3,6 +3,7 @@ package `in`.koreatech.koin.ui.splash.viewmodel
 import `in`.koreatech.koin.core.viewmodel.BaseViewModel
 import `in`.koreatech.koin.domain.model.user.User
 import `in`.koreatech.koin.domain.model.version.Version
+import `in`.koreatech.koin.domain.state.version.VersionUpdatePriority
 import `in`.koreatech.koin.domain.usecase.token.IsTokenSavedInDeviceUseCase
 import `in`.koreatech.koin.domain.usecase.user.GetUserInfoUseCase
 import `in`.koreatech.koin.domain.usecase.version.GetVersionInformationUseCase
@@ -22,24 +23,33 @@ class SplashViewModel @Inject constructor(
 ) : BaseViewModel() {
 
     private val _updateCheckResult = MutableLiveData<Result<Version>?>(null)
-    val updateCheckResult : LiveData<Result<Version>?> get() = _updateCheckResult
+    val updateCheckResult: LiveData<Result<Version>?> get() = _updateCheckResult
 
     private val _tokenIsSaved = MutableLiveData<Boolean>(null)
-    val tokenIsSaved : LiveData<Boolean> get() = _tokenIsSaved
+    val tokenIsSaved: LiveData<Boolean> get() = _tokenIsSaved
 
     private val _tokenIsValid = MutableLiveData<Result<User>?>(null)
-    val tokenIsValid : LiveData<Result<User>?> get() = _tokenIsValid
+    val tokenIsValid: LiveData<Result<User>?> get() = _tokenIsValid
 
     fun checkUpdate() {
         viewModelScope.launch {
-            _updateCheckResult.value = getVersionInformationUseCase()
+            try {
+                _updateCheckResult.value = getVersionInformationUseCase().also {
+                    if (!(it.getOrThrow().versionUpdatePriority is VersionUpdatePriority.High ||
+                        it.getOrThrow().versionUpdatePriority is VersionUpdatePriority.Medium)) {
+                        checkToken()
+                    }
+                }
+            } catch (t: Throwable) {
+                checkToken()
+            }
         }
     }
 
     fun checkToken() {
         viewModelScope.launch {
             _tokenIsSaved.value = isTokenSavedInDeviceUseCase().also {
-                if(it) _tokenIsValid.value = getUserInfoUseCase()
+                if (it) _tokenIsValid.value = getUserInfoUseCase()
             }
         }
     }
