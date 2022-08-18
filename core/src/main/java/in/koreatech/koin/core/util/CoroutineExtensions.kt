a60ebaeb0b21086734d4f9c61d46cae8aa1ea232
Yang-Seungmin <tmdals099@gmail.com>
2022-08-18 10:20:16 +0900

    Fix ignore cancellation function

--- core/src/main/java/in/koreatech/koin/core/util/CoroutineExtensions.kt
+++
@@ -1,8 +0,0 @@
-package `in`.koreatech.koin.core.util
-
-import kotlin.coroutines.cancellation.CancellationException
-
-inline fun <R> ignoreCancelledResult(block: () -> Result<R>) = block().let { result ->
-    if(result.exceptionOrNull() is CancellationException) null
-    else result
-}
