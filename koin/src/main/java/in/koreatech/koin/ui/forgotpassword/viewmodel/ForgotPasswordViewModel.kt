3a50a26b464b1b022ecf2843f760aa03a4b86817
Yang-Seungmin <tmdals099@gmail.com>
2022-08-23 22:56:50 +0900

    Change SingleLiveEvent

--- koin/src/main/java/in/koreatech/koin/ui/forgotpassword/viewmodel/ForgotPasswordViewModel.kt
+++ koin/src/main/java/in/koreatech/koin/ui/forgotpassword/viewmodel/ForgotPasswordViewModel.kt
@@ -1,27 +1,31 @@
 package `in`.koreatech.koin.ui.forgotpassword.viewmodel
 
 import `in`.koreatech.koin.core.viewmodel.BaseViewModel
+import `in`.koreatech.koin.core.viewmodel.SingleLiveEvent
 import `in`.koreatech.koin.domain.usecase.user.RequestFindPasswordEmailUseCase
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ForgotPasswordViewModel @Inject constructor(
     private val requestFindPasswordEmailUseCase: RequestFindPasswordEmailUseCase
 ) : BaseViewModel() {
-    private val _passwordResetEmailRequestState = MutableLiveData<Result<Unit>?>(null)
-    val passwordResetEmailRequestState : LiveData<Result<Unit>?> get() = _passwordResetEmailRequestState
+    private val _passwordResetEmailRequested = SingleLiveEvent<Unit>()
+    val passwordResetEmailRequested: LiveData<Unit> get() = _passwordResetEmailRequested
+
+    private val _passwordResetEmailRequestError = SingleLiveEvent<Throwable>()
+    val passwordResetEmailRequestError: LiveData<Throwable> get() = _passwordResetEmailRequestError
 
     fun requestFindPasswordEmail(portalAccount: String) {
-        if(isLoading.value == false) {
-            _isLoading.value = true
-            viewModelScope.launch {
-                _passwordResetEmailRequestState.value = requestFindPasswordEmailUseCase(portalAccount)
-                _isLoading.value = false
+        if (isLoading.value == false) {
+            viewModelScope.launchWithLoading {
+                requestFindPasswordEmailUseCase(portalAccount).onSuccess {
+                    _passwordResetEmailRequested.call()
+                }.onFailure {
+                    _passwordResetEmailRequestError.value = it
+                }
             }
         }
     }
