7e09d1ef5af8b7debbaffecfb455c58bf3040f51
Yang-Seungmin <tmdals099@gmail.com>
2022-10-11 23:55:35 +0900

    Add serializedname

--- data/src/main/java/in/koreatech/koin/data/response/VersionResponse.kt
+++ data/src/main/java/in/koreatech/koin/data/response/VersionResponse.kt
@@ -3,9 +3,9 @@ package `in`.koreatech.koin.data.response
 import com.google.gson.annotations.SerializedName
 
 data class VersionResponse(
-    val id: Int,
-    val version: String,
-    val type: String,
+    @SerializedName("id") val id: Int,
+    @SerializedName("version") val version: String,
+    @SerializedName("type") val type: String,
     @SerializedName("created_at") val createdAt: String,
     @SerializedName("updated_at") val updatedAt: String
 )
