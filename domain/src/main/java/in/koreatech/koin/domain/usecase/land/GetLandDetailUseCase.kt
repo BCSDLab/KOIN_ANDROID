bd53648c2dda195275acc341efc4e3f544af7ac9
Kim Jaeyong <jy3479@naver.com>
2022-10-21 23:31:32 +0900

    fix run catching

--- domain/src/main/java/in/koreatech/koin/domain/usecase/land/GetLandDetailUseCase.kt
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/land/GetLandDetailUseCase.kt
@@ -2,14 +2,19 @@ package `in`.koreatech.koin.domain.usecase.land
 
 import `in`.koreatech.koin.domain.model.land.LandDetail
 import `in`.koreatech.koin.domain.repository.LandRepository
+import java.util.concurrent.CancellationException
 import javax.inject.Inject
 
 class GetLandDetailUseCase @Inject constructor(
     private val landRepository: LandRepository
 ) {
     suspend operator fun invoke(id: Int): Result<LandDetail> {
-        return kotlin.runCatching {
-            landRepository.getLandDetail(id)
+        return try {
+            Result.success(landRepository.getLandDetail(id))
+        } catch (e: CancellationException) {
+            throw e
+        } catch (e: Exception) {
+            Result.failure(e)
         }
     }
 }
--- domain/src/main/java/in/koreatech/koin/domain/usecase/land/GetLandListUseCase.kt
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/land/GetLandListUseCase.kt
@@ -2,14 +2,19 @@ package `in`.koreatech.koin.domain.usecase.land
 
 import `in`.koreatech.koin.domain.model.land.Land
 import `in`.koreatech.koin.domain.repository.LandRepository
+import java.util.concurrent.CancellationException
 import javax.inject.Inject
 
 class GetLandListUseCase @Inject constructor(
     private val landRepository: LandRepository
 ) {
     suspend operator fun invoke(): Result<List<Land>> {
-        return kotlin.runCatching {
-            landRepository.getLandList()
+        return try {
+            Result.success(landRepository.getLandList())
+        } catch (e: CancellationException) {
+            throw e
+        } catch (e: Exception) {
+            Result.failure(e)
         }
     }
 }
