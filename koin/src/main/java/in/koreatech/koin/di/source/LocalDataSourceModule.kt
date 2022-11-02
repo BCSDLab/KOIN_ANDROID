83fd37991e556400c2e796388dadaa41254a1454
Seungmin Yang <tmdals099@gmail.com>
2022-11-02 13:13:40 +0900

    Fix get dept from deptcode

--- koin/src/main/java/in/koreatech/koin/di/source/LocalDataSourceModule.kt
+++ koin/src/main/java/in/koreatech/koin/di/source/LocalDataSourceModule.kt
@@ -1,5 +1,6 @@
 package `in`.koreatech.koin.di.source
 
+import `in`.koreatech.koin.data.source.local.DeptLocalDataSource
 import `in`.koreatech.koin.data.source.local.SignupTermsLocalDataSource
 import `in`.koreatech.koin.data.source.local.TokenLocalDataSource
 import `in`.koreatech.koin.data.source.local.VersionLocalDataSource
@@ -36,5 +37,13 @@ object LocalDataSourceModule {
         @ApplicationContext applicationContext: Context
     ) : VersionLocalDataSource {
         return VersionLocalDataSource(applicationContext)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDeptLocalDataSource(
+        @ApplicationContext applicationContext: Context
+    ) : DeptLocalDataSource {
+        return DeptLocalDataSource(applicationContext)
     }
 }
