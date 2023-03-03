b5f1fa602b1beced7dba5a024d8eb0a4b18df0c7
Seungmin Yang <tmdals099@gmail.com>
2023-03-03 21:31:04 +0900

    Fix dining date error

--- domain/src/main/java/in/koreatech/koin/domain/model/dining/DiningType.kt
+++ domain/src/main/java/in/koreatech/koin/domain/model/dining/DiningType.kt
@@ -4,8 +4,19 @@ sealed class DiningType(
     val typeCode: Int,
     val typeEnglish: String,
     val typeKorean: String
-) {
+) : Comparable<DiningType> {
     object Breakfast : DiningType(0, "BREAKFAST", "아침")
     object Lunch : DiningType(1, "LUNCH", "점심")
     object Dinner : DiningType(2, "DINNER", "저녁")
+
+    override fun compareTo(other: DiningType): Int {
+        return typeCode.compareTo(other.typeCode)
+    }
+}
+
+fun String.toDiningType() = when(this) {
+    "BREAKFAST" -> DiningType.Breakfast
+    "LUNCH" -> DiningType.Lunch
+    "DINNER" -> DiningType.Dinner
+    else -> throw IllegalArgumentException("Invalid dining type string")
 }
