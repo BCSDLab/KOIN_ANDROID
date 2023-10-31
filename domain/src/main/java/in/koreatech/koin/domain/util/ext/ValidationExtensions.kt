dbf34047ba3a4bb8de86ee136e8c12caa8b5a0ff
junsu1023 <12jangdokg55@gmail.com>
2023-11-01 01:40:02 +0900

    modify ValidationExtensions

--- domain/src/main/java/in/koreatech/koin/domain/util/ext/ValidationExtensions.kt
+++ domain/src/main/java/in/koreatech/koin/domain/util/ext/ValidationExtensions.kt
@@ -3,7 +3,7 @@ package `in`.koreatech.koin.domain.util.ext
 import `in`.koreatech.koin.domain.util.EmailUtil
 import `in`.koreatech.koin.domain.util.PasswordUtil
 
-fun String.isValidEmail() = EmailUtil.isEmailValidate(this)
+fun String.isValidEmail() = EmailUtil.isEmailValidate(this.split("@")[0])
 fun String.isValidPassword() = PasswordUtil.isPasswordValidate(this)
 fun String.isNotValidEmail() = !isValidEmail()
 fun String.isNotValidPassword() = !isValidPassword()
