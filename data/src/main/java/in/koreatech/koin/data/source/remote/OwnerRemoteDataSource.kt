dea0638106ebe8a656382307c3032955ce417c52
junsu1023 <12jangdokg55@gmail.com>
2023-11-01 00:10:57 +0900

    add OwnerRemoteDataSource

--- data/src/main/java/in/koreatech/koin/data/source/remote/OwnerRemoteDataSource.kt
+++ data/src/main/java/in/koreatech/koin/data/source/remote/OwnerRemoteDataSource.kt
@@ -1,15 +1,13 @@
 package `in`.koreatech.koin.data.source.remote
 
 import `in`.koreatech.koin.data.api.OwnerApi
-import `in`.koreatech.koin.data.request.owner.OwnerRequest
+import `in`.koreatech.koin.data.request.owner.OwnerLoginRequest
 import `in`.koreatech.koin.data.request.owner.OwnerVerificationCodeRequest
 import `in`.koreatech.koin.data.request.owner.OwnerVerificationEmailRequest
 import `in`.koreatech.koin.data.response.owner.OwnerResponse
+import `in`.koreatech.koin.data.response.user.RegisterResponse
 
 class OwnerRemoteDataSource(private val ownerApi: OwnerApi) {
-    suspend fun postRegister(ownerRequest: OwnerRequest): OwnerResponse {
-        return ownerApi.postRegister(ownerRequest)
-    }
 
     suspend fun postVerificationCode(ownerVerificationCode: OwnerVerificationCodeRequest): OwnerResponse {
         return ownerApi.postVerificationCode(ownerVerificationCode)
@@ -17,5 +15,11 @@ class OwnerRemoteDataSource(private val ownerApi: OwnerApi) {
 
     suspend fun postVerificationEmail(ownerVerificationEmail: OwnerVerificationEmailRequest): OwnerResponse {
         return ownerApi.postVerificationEmail(ownerVerificationEmail)
+    }
+
+    suspend fun sendRegisterEmail(
+        ownerLoginRequest: OwnerLoginRequest
+    ): RegisterResponse {
+        return ownerApi.postRegister(ownerLoginRequest)
     }
 }
