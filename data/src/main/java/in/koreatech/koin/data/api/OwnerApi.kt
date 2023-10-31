eb0f4c3588d41cc7c08b47f6a7b6ddb9d48e9baa
junsu1023 <12jangdokg55@gmail.com>
2023-11-01 00:10:21 +0900

    add OwnerApi

--- data/src/main/java/in/koreatech/koin/data/api/OwnerApi.kt
+++ data/src/main/java/in/koreatech/koin/data/api/OwnerApi.kt
@@ -1,16 +1,18 @@
 package `in`.koreatech.koin.data.api
 
 import `in`.koreatech.koin.data.constant.URLConstant
+import `in`.koreatech.koin.data.request.owner.OwnerLoginRequest
 import `in`.koreatech.koin.data.request.owner.OwnerRequest
 import `in`.koreatech.koin.data.request.owner.OwnerVerificationCodeRequest
 import `in`.koreatech.koin.data.request.owner.OwnerVerificationEmailRequest
 import `in`.koreatech.koin.data.response.owner.OwnerResponse
+import `in`.koreatech.koin.data.response.user.RegisterResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface OwnerApi {
     @POST(URLConstant.OWNER.REGISTER)
-    suspend fun postRegister(@Body ownerRequest: OwnerRequest): OwnerResponse
+    suspend fun postRegister(@Body ownerLoginRequest: OwnerLoginRequest): RegisterResponse
 
     @POST(URLConstant.OWNER.CODE)
     suspend fun postVerificationCode(@Body ownerVerificationCode: OwnerVerificationCodeRequest): OwnerResponse
