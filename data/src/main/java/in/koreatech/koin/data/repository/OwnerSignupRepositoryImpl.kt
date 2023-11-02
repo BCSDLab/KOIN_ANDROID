3bc8bdd9b55796487ef7a6f3944d364059176df6
junsu1023 <12jangdokg55@gmail.com>
2023-11-03 01:14:21 +0900

    add 422error

--- data/src/main/java/in/koreatech/koin/data/repository/OwnerSignupRepositoryImpl.kt
+++ data/src/main/java/in/koreatech/koin/data/repository/OwnerSignupRepositoryImpl.kt
@@ -1,10 +1,13 @@
 package `in`.koreatech.koin.data.repository
 
-import `in`.koreatech.koin.data.request.owner.OwnerLoginRequest
+import `in`.koreatech.koin.data.request.owner.OwnerSignUpRequest
+import `in`.koreatech.koin.data.request.owner.OwnerVerificationEmailRequest
 import `in`.koreatech.koin.data.source.local.SignupTermsLocalDataSource
 import `in`.koreatech.koin.data.source.remote.OwnerRemoteDataSource
+import `in`.koreatech.koin.domain.error.signup.InCorrectEmailAddressException
 import `in`.koreatech.koin.domain.error.signup.SignupAlreadySentEmailException
 import `in`.koreatech.koin.domain.repository.OwnerSignupRepository
+import android.util.Log
 import retrofit2.HttpException
 import javax.inject.Inject
 
@@ -21,19 +24,19 @@ class OwnerSignupRepositoryImpl @Inject constructor(
     }
 
     override suspend fun requestEmailVerification(
-        email: String,
-        hashedPassword: String
+        email: String
     ): Result<Unit> {
         return try {
-            ownerRemoteDataSource.sendRegisterEmail(
-                OwnerLoginRequest(
-                    email = email,
-                    passwordHashed = hashedPassword
+            val response = ownerRemoteDataSource.postVerificationEmail (
+                OwnerVerificationEmailRequest(
+                    address = email
                 )
             )
+
             Result.success(Unit)
         } catch (e: HttpException) {
             if(e.code() == 409) Result.failure(SignupAlreadySentEmailException())
+            else if(e.code() == 422) Result.failure(InCorrectEmailAddressException())
             else Result.failure(e)
         } catch (t: Throwable) {
             Result.failure(t)
