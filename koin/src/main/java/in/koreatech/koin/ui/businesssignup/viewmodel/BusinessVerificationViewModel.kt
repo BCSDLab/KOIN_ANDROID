5982a278e52d34a0d19bc2540e674805b04b1929
junsu1023 <12jangdokg55@gmail.com>
2023-11-14 16:30:43 +0900

    add BusinessVerificationViewModel

---
+++ koin/src/main/java/in/koreatech/koin/ui/businesssignup/viewmodel/BusinessVerificationViewModel.kt
@@ -0,0 +1,39 @@
+package `in`.koreatech.koin.ui.businesssignup.viewmodel
+
+import `in`.koreatech.koin.core.viewmodel.BaseViewModel
+import `in`.koreatech.koin.core.viewmodel.SingleLiveEvent
+import `in`.koreatech.koin.domain.usecase.owner.OwnerVerificationCodeUseCase
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BusinessVerificationViewModel @Inject constructor(
+    private val ownerVerificationCodeUseCase: OwnerVerificationCodeUseCase
+): BaseViewModel() {
+    private val _businessVerificationContinuationState = SingleLiveEvent<Unit>()
+    val businessVerificationContinuationState: LiveData<Unit>
+        get() = _businessVerificationContinuationState
+
+    private val _businessVerificationContinuationError = SingleLiveEvent<Throwable>()
+    val businessVerificationContinuationError: LiveData<Throwable>
+        get() = _businessVerificationContinuationError
+
+    fun continueVerificationEmail(
+        email: String,
+        verificationCode: String
+    ) {
+        if(isLoading.value == false) {
+            viewModelScope.launchWithLoading {
+                ownerVerificationCodeUseCase(
+                    email, verificationCode
+                ).onSuccess {
+                    _businessVerificationContinuationState.value = it
+                }.onFailure {
+                    _businessVerificationContinuationError.value = it
+                }
+            }
+        }
+    }
+}
