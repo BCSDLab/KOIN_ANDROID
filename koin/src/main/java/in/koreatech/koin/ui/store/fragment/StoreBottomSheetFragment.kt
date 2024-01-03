cafc36d2f97eca73109be32acdddb5ab4e12d4f7
junsu1023 <12jangdokg55@gmail.com>
2024-01-03 21:14:00 +0900

    add StoreBottomSheetFragment

---
+++ koin/src/main/java/in/koreatech/koin/ui/store/fragment/StoreBottomSheetFragment.kt
@@ -0,0 +1,59 @@
+package `in`.koreatech.koin.ui.store.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import `in`.koreatech.koin.databinding.SelectStoreBottomSheetBinding
+import `in`.koreatech.koin.ui.store.viewmodel.StoreViewModel
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class StoreBottomSheetFragment : BottomSheetDialogFragment() {
+    private var _binding: SelectStoreBottomSheetBinding? = null
+    private val binding: SelectStoreBottomSheetBinding get() = _binding!!
+    private val viewModel: StoreViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return SelectStoreBottomSheetBinding.inflate(inflater, container, false).also {
+            _binding = it
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.clickSelectButton(false, "")
+
+                viewModel.store.collectLatest {
+                    binding.storeNameText.text = it?.name ?: ""
+                    binding.storePhoneText.text = it?.phoneNumber ?: ""
+                }
+            }
+        }
+
+        binding.storeSelectButton.setOnClickListener {
+            viewModel.clickSelectButton(true, binding.storeNameText.text.toString())
+            dismiss()
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
