08dfe37917198ed3fefe88dc54ba87b6545bc2d1
junsu1023 <12jangdokg55@gmail.com>
2024-01-03 21:17:08 +0900

    modify StoreRepositoryImpl

--- data/src/main/java/in/koreatech/koin/data/repository/StoreRepositoryImpl.kt
+++ data/src/main/java/in/koreatech/koin/data/repository/StoreRepositoryImpl.kt
@@ -1,11 +1,9 @@
 package `in`.koreatech.koin.data.repository
 
 import `in`.koreatech.koin.data.mapper.toStore
-import `in`.koreatech.koin.data.mapper.toStoreMenu
 import `in`.koreatech.koin.data.mapper.toStoreWithMenu
 import `in`.koreatech.koin.data.source.remote.StoreRemoteDataSource
 import `in`.koreatech.koin.domain.model.store.Store
-import `in`.koreatech.koin.domain.model.store.StoreMenu
 import `in`.koreatech.koin.domain.model.store.StoreWithMenu
 import `in`.koreatech.koin.domain.repository.StoreRepository
 import javax.inject.Inject
