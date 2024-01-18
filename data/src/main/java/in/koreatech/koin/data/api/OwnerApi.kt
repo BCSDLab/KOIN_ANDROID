7854c1a65b7cbea1d9cbd7fce1bd9c2796dec300
junsu1023 <12jangdokg55@gmail.com>
2024-01-18 21:29:03 +0900

    add api

--- data/src/main/java/in/koreatech/koin/data/api/OwnerApi.kt
+++ data/src/main/java/in/koreatech/koin/data/api/OwnerApi.kt
@@ -5,6 +5,7 @@ import `in`.koreatech.koin.data.request.owner.OwnerSignUpRequest
 import `in`.koreatech.koin.data.request.owner.OwnerVerificationCodeRequest
 import `in`.koreatech.koin.data.request.owner.OwnerVerificationEmailRequest
 import `in`.koreatech.koin.data.response.owner.OwnerResponse
+import `in`.koreatech.koin.data.response.owner.OwnerVerificationCodeResponse
 import `in`.koreatech.koin.data.response.user.RegisterResponse
 import retrofit2.Response
 import retrofit2.http.Body
@@ -14,8 +15,11 @@ interface OwnerApi {
     @POST(URLConstant.OWNER.REGISTER)
     suspend fun postRegister(@Body ownerLoginRequest: OwnerSignUpRequest): RegisterResponse
 
+//    @POST(URLConstant.OWNER.CODE)
+//    suspend fun postVerificationCode(@Body ownerVerificationCode: OwnerVerificationCodeRequest): OwnerResponse
+
     @POST(URLConstant.OWNER.CODE)
-    suspend fun postVerificationCode(@Body ownerVerificationCode: OwnerVerificationCodeRequest): OwnerResponse
+    suspend fun postVerificationCode(@Body ownerVerificationCode: OwnerVerificationCodeRequest): OwnerVerificationCodeResponse
 
     @POST(URLConstant.OWNER.EMAIL)
     suspend fun postVerificationEmail(@Body ownerVerificationEmail: OwnerVerificationEmailRequest): Response<Unit>
---
+++ data/src/main/java/in/koreatech/koin/data/api/UploadUrlApi.kt
@@ -0,0 +1,12 @@
+package `in`.koreatech.koin.data.api
+
+import `in`.koreatech.koin.data.constant.URLConstant
+import `in`.koreatech.koin.data.request.upload.UploadUrlRequest
+import `in`.koreatech.koin.data.response.upload.UploadUrlResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface UploadUrlApi {
+    @POST(URLConstant.UPLOAD.url)
+    suspend fun postUploadUrl(@Body uploadUrlRequest: UploadUrlRequest): UploadUrlResponse
+}
