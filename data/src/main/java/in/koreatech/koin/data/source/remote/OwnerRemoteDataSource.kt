59b243bc64962db8d23043a2d77c65ace3ec88d9
junsu1023 <12jangdokg55@gmail.com>
2024-01-18 21:30:24 +0900

    add source

--- data/src/main/java/in/koreatech/koin/data/source/remote/OwnerRemoteDataSource.kt
+++ data/src/main/java/in/koreatech/koin/data/source/remote/OwnerRemoteDataSource.kt
@@ -5,11 +5,16 @@ import `in`.koreatech.koin.data.request.owner.OwnerSignUpRequest
 import `in`.koreatech.koin.data.request.owner.OwnerVerificationCodeRequest
 import `in`.koreatech.koin.data.request.owner.OwnerVerificationEmailRequest
 import `in`.koreatech.koin.data.response.owner.OwnerResponse
+import `in`.koreatech.koin.data.response.owner.OwnerVerificationCodeResponse
 import `in`.koreatech.koin.data.response.user.RegisterResponse
 import retrofit2.Response
 
 class OwnerRemoteDataSource(private val ownerApi: OwnerApi) {
-    suspend fun postVerificationCode(ownerVerificationCode: OwnerVerificationCodeRequest): OwnerResponse {
+//    suspend fun postVerificationCode(ownerVerificationCode: OwnerVerificationCodeRequest): OwnerResponse {
+//        return ownerApi.postVerificationCode(ownerVerificationCode)
+//    }
+
+    suspend fun postVerificationCode(ownerVerificationCode: OwnerVerificationCodeRequest): OwnerVerificationCodeResponse {
         return ownerApi.postVerificationCode(ownerVerificationCode)
     }
 
