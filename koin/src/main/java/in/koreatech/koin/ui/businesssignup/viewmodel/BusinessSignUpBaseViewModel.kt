a1ecf40098db9e4fcad9283d9bc419ed3a56fd5b
junsu1023 <12jangdokg55@gmail.com>
2024-01-24 20:49:34 +0900

    add BusinessSignUpBasicInfoViewModel, BusinessSignUpBaseViewModel

---
+++ koin/src/main/java/in/koreatech/koin/ui/businesssignup/viewmodel/BusinessSignUpBaseViewModel.kt
@@ -0,0 +1,19 @@
+package `in`.koreatech.koin.ui.businesssignup.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import `in`.koreatech.koin.core.viewmodel.BaseViewModel
+import kotlinx.coroutines.launch
+
+class BusinessSignUpBaseViewModel: BaseViewModel() {
+    private val _toBeShownFragment = MutableLiveData<String>()
+    val toBeShownFragment: LiveData<String>
+        get() = _toBeShownFragment
+
+    fun setFragmentTag(tag: String) {
+        viewModelScope.launch {
+            _toBeShownFragment.value = tag
+        }
+    }
+}
