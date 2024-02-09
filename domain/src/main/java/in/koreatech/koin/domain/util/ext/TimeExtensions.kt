a78fcece26f340cb895cebe7ad5eb8be06f315c6
Jokwanhee <jonotch1@naver.com>
2024-02-10 02:18:15 +0900

    #158 Fix StoreCategory

--- domain/src/main/java/in/koreatech/koin/domain/util/ext/TimeExtensions.kt
+++ domain/src/main/java/in/koreatech/koin/domain/util/ext/TimeExtensions.kt
@@ -6,6 +6,16 @@ import java.time.ZoneId
 
 val localTimeNow: LocalTime get() = LocalTime.now(ZoneId.of("Asia/Seoul"))
 val localDateTimeNow: LocalDateTime get() = LocalDateTime.now(ZoneId.of("Asia/Seoul"))
+val localDayOfWeekName get() = when(localDateTimeNow.dayOfWeek.value) {
+    1 -> "MONDAY"
+    2 -> "TUESDAY"
+    3 -> "WEDNESDAY"
+    4 -> "THURSDAY"
+    5 -> "FRIDAY"
+    6 -> "SATURDAY"
+    7 -> "SUNDAY"
+    else -> {}
+}
 
 val Int.second get() = this * 1000L
 val Int.minute get() = this * 60 * 1000L
