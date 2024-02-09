24455e05cb0de4fff4212d52b4ed2f7dc72de287
Jokwanhee <jonotch1@naver.com>
2024-02-10 02:16:14 +0900

    #158 Fix Store Respone dto in Data Layer

--- data/src/main/java/in/koreatech/koin/data/repository/StoreRepositoryImpl.kt
+++ data/src/main/java/in/koreatech/koin/data/repository/StoreRepositoryImpl.kt
@@ -1,11 +1,8 @@
 package `in`.koreatech.koin.data.repository
 
 import `in`.koreatech.koin.data.mapper.toStore
-import `in`.koreatech.koin.data.mapper.toStoreMenu
-import `in`.koreatech.koin.data.mapper.toStoreWithMenu
 import `in`.koreatech.koin.data.source.remote.StoreRemoteDataSource
 import `in`.koreatech.koin.domain.model.store.Store
-import `in`.koreatech.koin.domain.model.store.StoreMenu
 import `in`.koreatech.koin.domain.model.store.StoreWithMenu
 import `in`.koreatech.koin.domain.repository.StoreRepository
 import javax.inject.Inject
@@ -24,7 +21,8 @@ class StoreRepositoryImpl @Inject constructor(
     }
 
     override suspend fun getStoreWithMenu(storeId: Int): StoreWithMenu {
-        return storeRemoteDataSource.getStoreMenu(storeId).toStoreWithMenu()
+//        return storeRemoteDataSource.getStoreMenu(storeId).toStoreWithMenu()
+        return TODO()
     }
 
     override suspend fun invalidateStores() {
