90b4e0e7a845b38e2a952c3b03d9b26e4870ff79
Jokwanhee <jonotch1@naver.com>
2024-02-13 01:09:53 +0900

    #160 Fix PR comment : Inject Coroutine Dispatcher

--- data/src/main/java/in/koreatech/koin/data/source/local/TokenLocalDataSource.kt
+++ data/src/main/java/in/koreatech/koin/data/source/local/TokenLocalDataSource.kt
@@ -1,16 +1,20 @@
 package `in`.koreatech.koin.data.source.local
 
 import android.content.Context
+import android.util.Log
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.job
 import kotlinx.coroutines.withContext
 
 
 class TokenLocalDataSource @Inject constructor(
     @ApplicationContext applicationContext: Context,
+    private val dispatchersIO: CoroutineDispatcher,
 ) {
     var masterKey = MasterKey.Builder(applicationContext, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
         .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
@@ -26,7 +30,7 @@ class TokenLocalDataSource @Inject constructor(
 
     suspend fun saveAccessToken(
         accessToken: String,
-    ) = withContext(Dispatchers.IO) {
+    ) = withContext(dispatchersIO) {
         with(sharedPreferences.edit()) {
             putString(SHARED_PREF_KEY, accessToken)
             apply()
@@ -35,29 +39,29 @@ class TokenLocalDataSource @Inject constructor(
 
     suspend fun saveRefreshToken(
         refreshToken: String,
-    ) = withContext(Dispatchers.IO) {
+    ) = withContext(dispatchersIO) {
         with(sharedPreferences.edit()) {
             putString(SHARED_PREF_REFRESH_KEY, refreshToken)
             apply()
         }
     }
 
-    suspend fun getAccessToken(): String? = withContext(Dispatchers.IO) {
+    suspend fun getAccessToken(): String? = withContext(dispatchersIO) {
         sharedPreferences.getString(SHARED_PREF_KEY, null)
     }
 
-    suspend fun getRefreshToken(): String? = withContext(Dispatchers.IO) {
+    suspend fun getRefreshToken(): String? = withContext(dispatchersIO) {
         sharedPreferences.getString(SHARED_PREF_REFRESH_KEY, null)
     }
 
-    suspend fun removeAccessToken() = withContext(Dispatchers.IO) {
+    suspend fun removeAccessToken() = withContext(dispatchersIO) {
         with(sharedPreferences.edit()) {
             remove(SHARED_PREF_KEY)
             apply()
         }
     }
 
-    suspend fun removeRefreshToken() = withContext(Dispatchers.IO) {
+    suspend fun removeRefreshToken() = withContext(dispatchersIO) {
         with(sharedPreferences.edit()) {
             remove(SHARED_PREF_REFRESH_KEY)
             apply()
