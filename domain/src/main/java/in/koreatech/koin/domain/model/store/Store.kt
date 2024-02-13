108456df77f0d5f10b7be6add2005531a4a875b8
Jokwanhee <jonotch1@naver.com>
2024-02-14 01:09:16 +0900

    #158 Add shop open time

--- domain/src/main/java/in/koreatech/koin/domain/model/store/Store.kt
+++ domain/src/main/java/in/koreatech/koin/domain/model/store/Store.kt
@@ -1,5 +1,11 @@
 package `in`.koreatech.koin.domain.model.store
 
+import `in`.koreatech.koin.domain.util.ext.HHMM
+import `in`.koreatech.koin.domain.util.ext.isEqualOrBigger
+import `in`.koreatech.koin.domain.util.ext.isEqualOrSmaller
+import `in`.koreatech.koin.domain.util.ext.localTimeNow
+import java.time.LocalTime
+
 data class Store(
     val uid: Int,
     val name: String,
@@ -15,5 +21,23 @@ data class Store(
         val closed: Boolean,
         val openTime: String,
         val closeTime: String,
-    )
+    ) {
+        fun openStore(): Boolean {
+            return if(openTime.isNotEmpty() && closeTime.isNotEmpty()) {
+                val openTime = LocalTime.parse(if(openTime == "24:00") "00:00" else openTime)
+                val closeTime = LocalTime.parse(if(closeTime == "24:00") "00:00" else closeTime)
+                val currentTime = LocalTime.parse(localTimeNow.HHMM)
+
+                if (openTime.isBefore(closeTime)) { // 17:00(오픈 시간) < 23:00(종료 시간)
+                    currentTime.isEqualOrBigger(openTime) && currentTime.isEqualOrSmaller(closeTime)
+                } else if (openTime.isAfter(closeTime)) { // 17:00(오픈 시간) > 02:00(종료 시간)
+                    currentTime.isEqualOrBigger(openTime) || currentTime.isEqualOrSmaller(closeTime)
+                } else { // 00:00 ~ 00:00 (하루종일 영업)
+                    true
+                }
+            } else {
+                false
+            }
+        }
+    }
 }
--- domain/src/main/java/in/koreatech/koin/domain/util/ext/TimeExtensions.kt
+++ domain/src/main/java/in/koreatech/koin/domain/util/ext/TimeExtensions.kt
@@ -3,6 +3,7 @@ package `in`.koreatech.koin.domain.util.ext
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 val localTimeNow: LocalTime get() = LocalTime.now(ZoneId.of("Asia/Seoul"))
 val localDateTimeNow: LocalDateTime get() = LocalDateTime.now(ZoneId.of("Asia/Seoul"))
@@ -16,6 +17,16 @@ val localDayOfWeekName get() = when(localDateTimeNow.dayOfWeek.value) {
     7 -> "SUNDAY"
     else -> {}
 }
+
+val LocalTime.HHMM get() = this.format(DateTimeFormatter.ofPattern("HH:mm"))
+
+// this >= time (17:00 >= 15:00)
+fun LocalTime.isEqualOrBigger(time: LocalTime): Boolean =
+    this.isAfter(time) || this.equals(time)
+
+// this <= time (17:00 <= 22:00)
+fun LocalTime.isEqualOrSmaller(time: LocalTime): Boolean =
+    this.isBefore(time) || this.equals(time)
 
 val Int.second get() = this * 1000L
 val Int.minute get() = this * 60 * 1000L
