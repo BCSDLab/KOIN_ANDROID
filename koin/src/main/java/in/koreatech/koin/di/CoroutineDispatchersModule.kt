29419a5e3debb6340b7cc33bd0b12be9245efc8f
Jokwanhee <jonotch1@naver.com>
2024-02-13 13:04:33 +0900

    #160 Fix PR comment : seperate of dispatcher

--- koin/src/main/java/in/koreatech/koin/di/CoroutineDispatchersModule.kt
+++ koin/src/main/java/in/koreatech/koin/di/CoroutineDispatchersModule.kt
@@ -4,7 +4,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import `in`.koreatech.koin.core.qualifier.DefaultDispatcher
 import `in`.koreatech.koin.core.qualifier.IoDispatcher
+import `in`.koreatech.koin.core.qualifier.MainDispatcher
+import `in`.koreatech.koin.core.qualifier.UnconfinedDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
@@ -14,4 +17,16 @@ object CoroutineDispatchersModule {
     @IoDispatcher
     @Provides
     fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @UnconfinedDispatcher
+    @Provides
+    fun providesUnconfinedDispatcher(): CoroutineDispatcher = Dispatchers.Unconfined
+
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
 }
