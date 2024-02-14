753067aeab0f513fd9d8ce2484b84a8eb833b0b8
Jokwanhee <jonotch1@naver.com>
2024-02-14 18:17:05 +0900

    #158 Add store sorted by open

--- domain/src/main/java/in/koreatech/koin/domain/usecase/store/GetRecommendStoresUseCase.kt
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/store/GetRecommendStoresUseCase.kt
@@ -5,22 +5,26 @@ import `in`.koreatech.koin.domain.model.store.Store
 import `in`.koreatech.koin.domain.model.store.StoreCategory
 import `in`.koreatech.koin.domain.model.store.StoreWithMenu
 import `in`.koreatech.koin.domain.repository.StoreRepository
+import `in`.koreatech.koin.domain.util.ext.sortedOpenStore
 import javax.inject.Inject
 
 class GetRecommendStoresUseCase @Inject constructor(
-    private val storeRepository: StoreRepository
+    private val storeRepository: StoreRepository,
 ) {
     suspend operator fun invoke(
-        store: StoreWithMenu
+        store: StoreWithMenu,
     ): List<Store> {
         return storeRepository.getStores()
             .filter {
-                val shopRandomCategoryId = store.shopCategories?.filter { it.id != StoreCategory.All.code }?.randomOrNull()?.id
+                val shopRandomCategoryId =
+                    store.shopCategories?.filter { it.id != StoreCategory.All.code }
+                        ?.randomOrNull()?.id
                 it.categoryIds.find {
                     it?.code == shopRandomCategoryId
                 }?.code == shopRandomCategoryId && !it.open.closed && it.uid != store.uid
             }
             .shuffled()
             .take(STORE_RECOMMEND_STORES)
+            .sortedOpenStore()
     }
 }
