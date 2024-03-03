edfce456d2c30e922442cd36161475c55da4e845
nayunjae <yunjae.na@wantedlab.com>
2024-03-04 01:03:46 +0900

    fix store options

--- koin/src/main/java/in/koreatech/koin/ui/store/adapter/StoreDetailMenuRecyclerAdapter.kt
+++ koin/src/main/java/in/koreatech/koin/ui/store/adapter/StoreDetailMenuRecyclerAdapter.kt
@@ -16,14 +16,37 @@ class StoreDetailMenuRecyclerAdapter :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(shopMenu: ShopMenus) {
             binding.storeDetailMenuNameTextview.text = shopMenu.name
-            if (shopMenu.singlePrice != null) {
-                binding.storeDetailMenuPriceTextview.text = binding.root.context.getString(
-                    R.string.store_delivery_price,
-                    shopMenu.singlePrice
-                )
+            when {
+                shopMenu.isSingle && shopMenu.singlePrice != null -> {
+                    binding.storeDetailMenuPriceTextview.text = binding.root.context.getString(
+                        R.string.store_delivery_price,
+                        shopMenu.singlePrice
+                    )
+                }
+
+                shopMenu.optionPrices?.isNotEmpty() == true -> {
+                    val menus = shopMenu.optionPrices?.fold("") { acc, menu ->
+                        acc + "${getOptionPriceText(menu)}\n"
+                    }?.trim()
+
+                    binding.storeDetailMenuPriceTextview.text = menus
+                }
             }
         }
+
+        private fun getOptionPriceText(menu: ShopMenus.ShopMenuOptions) = when {
+            menu.price != null -> {
+                val priceText = binding.root.context.getString(
+                    R.string.store_delivery_price,
+                    menu.price
+                )
+                "${menu.option} - $priceText"
+            }
+
+            else -> menu.option
+        }
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
