4b0335ef33a570598feaa002b5799b1f9bd8c261
nazero <skdud4081@gmail.com>
2024-04-01 19:47:59 +0900

    Modify gradle

--- business/build.gradle.kts
+++ business/build.gradle.kts
@@ -1,12 +1,21 @@
+import `in`.koreatech.convention.kapt
+
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.koin.compose)
     alias(libs.plugins.koin.application)
+    alias(libs.plugins.hilt)
+    id("kotlin-kapt")
+
 }
 
 android {
     namespace = "in.koreatech.business"
 
+    packagingOptions {
+        resources.pickFirsts.add("META-INF/gradle/incremental.annotation.processors")
+    }
+    //  2 files found with path 'META-INF/gradle/incremental.annotation.processors' from inputs: 오류 해결
 
     buildTypes {
         release {
@@ -24,14 +33,14 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.lifecycle.runtime.ktx)
-    implementation(libs.androidx.navigation.compose)
-    implementation(project(":core"))
+    implementation(libs.compose.navigation)
     implementation(libs.orbit.compose)
     implementation(libs.orbit.core)
     implementation(libs.orbit.viewmodel)
     implementation(libs.hilt.android)
-    implementation(libs.hilt.navigation)
-    implementation(project(":domain"))
-    implementation(project(":koin"))
-    implementation(project(":data"))
+    implementation(libs.hilt.compose)
+    kapt(libs.hilt.compiler)
+    implementation(project(mapOf("path" to ":domain")))
+    implementation(project(mapOf("path" to ":data")))
+    implementation(project(mapOf("path" to ":core")))
 }
