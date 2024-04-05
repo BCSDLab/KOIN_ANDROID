06f10842f9b24387a8f054362333bb79d0fff950
Strone <sangbbang31@gmail.com>
2024-04-05 20:46:01 +0900

    Make type strings constant

--- domain/src/main/java/in/koreatech/koin/domain/model/dining/DiningType.kt
+++ domain/src/main/java/in/koreatech/koin/domain/model/dining/DiningType.kt
@@ -1,13 +1,17 @@
 package `in`.koreatech.koin.domain.model.dining
 
+import `in`.koreatech.koin.domain.constant.BREAKFAST
+import `in`.koreatech.koin.domain.constant.DINNER
+import `in`.koreatech.koin.domain.constant.LUNCH
+
 sealed class DiningType(
     val typeCode: Int,
     val typeEnglish: String,
     val typeKorean: String
 ) : Comparable<DiningType> {
-    object Breakfast : DiningType(0, "BREAKFAST", "아침")
-    object Lunch : DiningType(1, "LUNCH", "점심")
-    object Dinner : DiningType(2, "DINNER", "저녁")
+    object Breakfast : DiningType(0, BREAKFAST, "아침")
+    object Lunch : DiningType(1, LUNCH, "점심")
+    object Dinner : DiningType(2, DINNER, "저녁")
 
     override fun compareTo(other: DiningType): Int {
         return typeCode.compareTo(other.typeCode)
@@ -15,8 +19,8 @@ sealed class DiningType(
 }
 
 fun String.toDiningType() = when(this) {
-    "BREAKFAST" -> DiningType.Breakfast
-    "LUNCH" -> DiningType.Lunch
-    "DINNER" -> DiningType.Dinner
+    BREAKFAST -> DiningType.Breakfast
+    LUNCH -> DiningType.Lunch
+    DINNER -> DiningType.Dinner
     else -> throw IllegalArgumentException("Invalid dining type string")
 }
