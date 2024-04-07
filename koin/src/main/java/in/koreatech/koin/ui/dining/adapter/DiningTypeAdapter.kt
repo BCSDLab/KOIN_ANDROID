71e7696c6e2664b4e6f82c398a6364ce47a1de68
Strone <sangbbang31@gmail.com>
2024-04-07 15:50:40 +0900

    Set dining item values

--- koin/src/main/java/in/koreatech/koin/ui/dining/adapter/DiningTypeAdapter.kt
+++ koin/src/main/java/in/koreatech/koin/ui/dining/adapter/DiningTypeAdapter.kt
@@ -1,5 +1,6 @@
 package `in`.koreatech.koin.ui.dining.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
@@ -9,9 +10,7 @@ import com.bumptech.glide.Glide
 import `in`.koreatech.koin.databinding.ItemDiningTypeBinding
 import `in`.koreatech.koin.domain.model.dining.Dining
 
-class DiningTypeAdapter(
-
-) : ListAdapter<Dining, RecyclerView.ViewHolder>(diffCallback) {
+class DiningTypeAdapter : ListAdapter<Dining, RecyclerView.ViewHolder>(diffCallback) {
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         (holder as DiningViewHolder).bind(getItem(position))
@@ -22,18 +21,26 @@ class DiningTypeAdapter(
     }
 
     class DiningViewHolder(private val binding: ItemDiningTypeBinding) : RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SetTextI18n")
         fun bind(dining: Dining) {
             with(binding) {
                 textViewDiningCorner.text = dining.place
-                textViewKcal.text = dining.kcal
-                textViewCashPrice.text = dining.priceCash
-                textViewCardPrice.text = dining.priceCard
-
-                Glide.with(root.context)
-                    .load(dining.imageUrl)
-                    .into(imageViewDining)
-
+                textViewKcal.text = dining.kcal + "kcal"
+                textViewCashPrice.text = dining.priceCash + "원"
+                textViewCardPrice.text = dining.priceCard + "원"
                 textViewDiningMenuItems.text = dining.menu.joinToString("\n")
+
+                if(dining.imageUrl.isNotEmpty()) {
+                    Glide.with(root.context)
+                        .load(dining.imageUrl)
+                        .into(imageViewDining)
+                }
+
+                if(dining.isSoldOut) {
+                    groupSoldOut.visibility = android.view.View.VISIBLE
+                } else {
+                    groupSoldOut.visibility = android.view.View.INVISIBLE
+                }
             }
         }
     }
