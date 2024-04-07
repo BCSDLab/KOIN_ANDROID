8d16999acb0103d1b441549ea92199012b5e2fa2
nazero <skdud4081@gmail.com>
2024-04-08 00:17:11 +0900

    Add account auth state

---
+++ business/src/main/java/in/koreatech/business/feature_signup/accountsetup/AccountAuthState.kt
@@ -0,0 +1,13 @@
+package `in`.koreatech.business.feature_signup.accountsetup
+
+import `in`.koreatech.koin.domain.state.signup.SignupContinuationState
+
+data class AccountAuthState(
+    val id: String = "",
+    val password: String = "",
+    val passwordConfirm: String = "",
+    val email: String = "",
+    val signupContinuationState: SignupContinuationState = SignupContinuationState.RequestedEmailValidation,
+    val signUpContinuationError:Throwable? = null,
+    val isLoading: Boolean = false
+)
