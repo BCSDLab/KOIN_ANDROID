018ac94ad46e689d08b8f85039d2508c91b0740b
nazero <skdud4081@gmail.com>
2024-04-07 23:33:55 +0900

    Add send signup email usecase

---
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/business/SendSignupEmailUseCase.kt
@@ -0,0 +1,27 @@
+package `in`.koreatech.koin.domain.usecase.owner
+
+import `in`.koreatech.koin.domain.repository.OwnerSignupRepository
+import `in`.koreatech.koin.domain.state.signup.SignupContinuationState
+import `in`.koreatech.koin.domain.util.ext.isNotBusinessValidEmail
+import `in`.koreatech.koin.domain.util.ext.isNotValidEmail
+import `in`.koreatech.koin.domain.util.ext.isNotValidPassword
+import javax.inject.Inject
+
+class SendSignupEmailUseCase @Inject constructor(
+    private val ownerSignupRepository: OwnerSignupRepository
+) {
+    suspend operator fun invoke(
+        email: String,
+        password: String,
+        passwordConfirm: String,
+    ): Result<SignupContinuationState> {
+        return when {
+            password != passwordConfirm -> Result.success(SignupContinuationState.PasswordNotMatching)
+            email.isNotBusinessValidEmail() -> Result.success(SignupContinuationState.EmailIsNotValidate)
+            else -> ownerSignupRepository.requestEmailVerification(
+                email = email
+            ).map { SignupContinuationState.RequestedEmailValidation }
+        }
+    }
+
+}
