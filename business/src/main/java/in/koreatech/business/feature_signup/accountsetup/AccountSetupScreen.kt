d56314f7a3c2d7d5b6c2d74a5e294b97bdd599eb
nazero <skdud4081@gmail.com>
2024-04-08 00:25:55 +0900

    Modify account setup logic

--- business/src/main/java/in/koreatech/business/feature_signup/accountsetup/AccountSetupScreen.kt
+++ business/src/main/java/in/koreatech/business/feature_signup/accountsetup/AccountSetupScreen.kt
@@ -1,7 +1,6 @@
 package `in`.koreatech.business.feature_signup.accountsetup
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,10 +15,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -31,6 +26,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import `in`.koreatech.business.R
 import `in`.koreatech.business.feature_signup.textfield.LinedTextField
 import `in`.koreatech.business.ui.theme.ColorActiveButton
@@ -38,22 +34,21 @@ import `in`.koreatech.business.ui.theme.ColorDisabledButton
 import `in`.koreatech.business.ui.theme.ColorEmphasis
 import `in`.koreatech.business.ui.theme.ColorHelper
 import `in`.koreatech.business.ui.theme.ColorUnachieved
-
+import `in`.koreatech.koin.domain.state.signup.SignupContinuationState
+import org.orbitmvi.orbit.compose.collectAsState
+import org.orbitmvi.orbit.compose.collectSideEffect
 
 @Composable
 fun AccountSetupScreen(
     modifier: Modifier = Modifier,
+    viewModel: AccountSetupViewModel = hiltViewModel(),
     onBackClicked: () -> Unit = {},
     onNextClicked: (String) -> Unit = {},
 ) {
-    var id by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    var passwordConfirm by remember { mutableStateOf("") }
-    var emailConfirm by remember { mutableStateOf("") }
+    val state = viewModel.collectAsState().value
+
     Column(
-        modifier = modifier
-            .background(Color.White)
-            .fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
     ) {
         IconButton(
             modifier = Modifier.padding(vertical = 24.dp),
@@ -113,8 +108,8 @@ fun AccountSetupScreen(
             Spacer(modifier = Modifier.height(32.dp))
 
             LinedTextField(
-                value = id,
-                onValueChange = { id = it },
+                value = state.id,
+                onValueChange = { viewModel.onIdChanged(it) },
                 modifier = Modifier.fillMaxWidth(),
                 label = stringResource(id = R.string.id),
                 textStyle = TextStyle.Default.copy(fontSize = 15.sp),
@@ -122,8 +117,8 @@ fun AccountSetupScreen(
             )
 
             LinedTextField(
-                value = password,
-                onValueChange = { password = it },
+                value = state.password,
+                onValueChange = { viewModel.onPasswordChanged(it) },
                 modifier = Modifier.fillMaxWidth(),
                 label = stringResource(id = R.string.password),
                 textStyle = TextStyle.Default.copy(fontSize = 15.sp),
@@ -133,23 +128,26 @@ fun AccountSetupScreen(
             )
 
             LinedTextField(
-                value = passwordConfirm,
-                onValueChange = { passwordConfirm = it },
+                value = state.passwordConfirm,
+                onValueChange = { viewModel.onPasswordConfirmChanged(it) },
                 modifier = Modifier.fillMaxWidth(),
                 label = stringResource(id = R.string.password_confirm),
                 textStyle = TextStyle.Default.copy(fontSize = 15.sp),
                 lineColor = ColorHelper,
                 isPassword = true,
                 errorText = stringResource(id = R.string.password_mismatch),
+                isError = state.signupContinuationState == SignupContinuationState.PasswordNotMatching,
             )
 
             LinedTextField(
-                value = emailConfirm,
-                onValueChange = { emailConfirm = it },
+                value = state.email,
+                onValueChange = { viewModel.onEmailChanged(it) },
                 modifier = Modifier.fillMaxWidth(),
                 label = stringResource(id = R.string.email_confirm),
                 textStyle = TextStyle.Default.copy(fontSize = 15.sp),
                 lineColor = ColorHelper,
+                errorText = stringResource(id = R.string.email_not_validate),
+                isError = state.signupContinuationState == SignupContinuationState.EmailIsNotValidate,
             )
 
             Spacer(modifier = Modifier.height(78.dp))
@@ -158,20 +156,35 @@ fun AccountSetupScreen(
                 .fillMaxWidth()
                 .height(44.dp),
                 shape = RectangleShape,
+                enabled = state.id.isNotEmpty() && state.password.isNotEmpty() && state.passwordConfirm.isNotEmpty() && state.email.isNotEmpty(),
                 colors = ButtonDefaults.buttonColors(
                     containerColor = ColorActiveButton,
                     contentColor = Color.White,
                     disabledContainerColor = ColorDisabledButton,
                     disabledContentColor = Color.White,
                 ),
-                onClick = { onNextClicked(emailConfirm) }) {
+                onClick = {
+                    if (state.signupContinuationState == SignupContinuationState.CheckComplete)
+                        viewModel.onNextButtonClicked()
+                    viewModel.postEmailVerification(
+                        state.email,
+                        state.password,
+                        state.passwordConfirm
+                    )
+                }) {
                 Text(
                     text = stringResource(id = R.string.email_authentication),
                     fontSize = 15.sp,
                     color = Color.White,
-
-                    )
+                )
             }
+        }
+    }
+
+    viewModel.collectSideEffect {
+        when (it) {
+            is AccountAuthSideEffect.NavigateToNextScreen -> onNextClicked(it.email)
+            AccountAuthSideEffect.NavigateToBackScreen -> onBackClicked()
         }
     }
 }
