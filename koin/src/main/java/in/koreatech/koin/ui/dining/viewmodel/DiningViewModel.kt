87f642dd4af5c7f800229778e8adaac14b836072
Strone <sangbbang31@gmail.com>
2024-04-08 15:27:33 +0900

    Delete unused class

--- koin/src/main/java/in/koreatech/koin/ui/dining/viewmodel/DiningViewModel.kt
+++ koin/src/main/java/in/koreatech/koin/ui/dining/viewmodel/DiningViewModel.kt
@@ -1,20 +1,15 @@
 package `in`.koreatech.koin.ui.dining.viewmodel
 
-import android.util.Log
-import `in`.koreatech.koin.core.viewmodel.BaseViewModel
-import `in`.koreatech.koin.domain.model.dining.Dining
-import `in`.koreatech.koin.domain.model.dining.DiningType
-import `in`.koreatech.koin.domain.usecase.dining.CheckCorrectDateRangeUseCase
-import `in`.koreatech.koin.domain.usecase.dining.GetDiningUseCase
-import `in`.koreatech.koin.domain.util.DiningUtil
-import `in`.koreatech.koin.domain.util.TimeUtil
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import `in`.koreatech.koin.core.viewmodel.BaseViewModel
 import `in`.koreatech.koin.domain.constant.BREAKFAST
 import `in`.koreatech.koin.domain.constant.DINNER
 import `in`.koreatech.koin.domain.constant.LUNCH
+import `in`.koreatech.koin.domain.model.dining.Dining
+import `in`.koreatech.koin.domain.usecase.dining.GetDiningUseCase
+import `in`.koreatech.koin.domain.util.DiningUtil
+import `in`.koreatech.koin.domain.util.TimeUtil
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import java.util.Date
@@ -23,7 +18,6 @@ import javax.inject.Inject
 @HiltViewModel
 class DiningViewModel @Inject constructor(
     private val getDiningUseCase: GetDiningUseCase,
-    private val checkCorrectDateRangeUseCase: CheckCorrectDateRangeUseCase
 ) : BaseViewModel() {
 
     private val _selectedDate = MutableStateFlow(TimeUtil.dateFormatToYYMMDD(DiningUtil.getCurrentDate()))
