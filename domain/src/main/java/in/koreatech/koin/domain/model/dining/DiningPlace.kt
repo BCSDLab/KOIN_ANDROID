2c6f5467b29d1d14a15b9ee5a26298de37e3efd3
wateralsie <tnals08019@gmail.com>
2024-04-16 11:32:58 +0900

    #212 Add dining place enum class

---
+++ domain/src/main/java/in/koreatech/koin/domain/model/dining/DiningPlace.kt
@@ -0,0 +1,10 @@
+package `in`.koreatech.koin.domain.model.dining
+
+enum class DiningPlace(
+    val place: String
+) {
+    CornerA("A코너"),
+    CornerB("B코너"),
+    CornerC("C코너"),
+    Nungsu("능수관")
+}
