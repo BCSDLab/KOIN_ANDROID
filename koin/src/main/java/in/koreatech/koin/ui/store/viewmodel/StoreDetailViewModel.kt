5b835290d079928aea93d48a36086ba94c70673b
nazero <skdud4081@gmail.com>
2024-04-17 02:10:47 +0900

    Add categories value

--- koin/src/main/java/in/koreatech/koin/ui/store/viewmodel/StoreDetailViewModel.kt
+++ koin/src/main/java/in/koreatech/koin/ui/store/viewmodel/StoreDetailViewModel.kt
@@ -7,6 +7,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import `in`.koreatech.koin.core.viewmodel.BaseViewModel
 import `in`.koreatech.koin.domain.model.store.ShopMenus
 import `in`.koreatech.koin.domain.model.store.Store
+import `in`.koreatech.koin.domain.model.store.StoreMenu
 import `in`.koreatech.koin.domain.model.store.StoreWithMenu
 import `in`.koreatech.koin.domain.usecase.store.GetRecommendStoresUseCase
 import `in`.koreatech.koin.domain.usecase.store.GetShopMenusUseCase
@@ -21,6 +22,8 @@ class StoreDetailViewModel @Inject constructor(
 ) : BaseViewModel() {
     val store: LiveData<StoreWithMenu> get() = _store
     private val _store = MutableLiveData<StoreWithMenu>()
+    val categories: LiveData<StoreMenu> get() = _categories
+    private var _categories = MutableLiveData<StoreMenu>()
     val storeMenu: LiveData<List<ShopMenus>> get() = _storeMenu
     private val _storeMenu = MutableLiveData<List<ShopMenus>>()
     val recommendStores: LiveData<List<Store>?> get() = _recommendStores
@@ -35,6 +38,7 @@ class StoreDetailViewModel @Inject constructor(
 
     fun getShopMenus(storeId: Int) = viewModelScope.launchWithLoading {
         getShopMenusUseCase(storeId).also { shop ->
+            _categories.value = shop
             _storeMenu.value = if (shop.menuCategories?.isEmpty() == true) {
                 emptyList()
             } else {
