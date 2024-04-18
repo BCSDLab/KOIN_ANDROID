b82c28e413d7bb810e8464bf6ef5dea21692525a
nazero <skdud4081@gmail.com>
2024-04-19 02:45:01 +0900

    Add business auth viewmodel

--- business/src/main/java/in/koreatech/business/feature/signup/businessauth/BusinessAuthViewModel.kt
+++ business/src/main/java/in/koreatech/business/feature/signup/businessauth/BusinessAuthViewModel.kt
@@ -5,6 +5,7 @@ import `in`.koreatech.koin.core.viewmodel.BaseViewModel
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.postSideEffect
+import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
@@ -12,8 +13,55 @@ import javax.inject.Inject
 class BusinessAuthViewModel @Inject constructor(
 
 ) : ContainerHost<BusinessAuthState, BusinessAuthSideEffect>, BaseViewModel() {
-    override val container = container<BusinessAuthState, BusinessAuthSideEffect>(BusinessAuthState())
-    fun onShowDialog() = intent {
-        postSideEffect(BusinessAuthSideEffect.ShowDialog)
+    override val container =
+        container<BusinessAuthState, BusinessAuthSideEffect>(BusinessAuthState())
+
+    fun onNameChanged(name: String) = intent {
+        reduce {
+            state.copy(name = name)
+        }
     }
+
+    fun onStoreNameChanged(storeName: String) = intent {
+        reduce {
+            state.copy(storeName = storeName)
+        }
+    }
+
+    fun onStoreNumberChanged(storeNumber: String) = intent {
+        reduce {
+            state.copy(storeNumber = storeNumber)
+        }
+    }
+
+    fun onPhoneNumberChanged(phoneNumber: String) = intent {
+        reduce {
+            state.copy(phoneNumber = phoneNumber)
+        }
+    }
+
+    fun onImageUrisChanged(selectedImageUris: MutableList<String>) = intent {
+        reduce {
+            state.copy(selectedImageUris = selectedImageUris)
+        }
+    }
+
+    fun onDialogVisibilityChanged(dialogVisibility: Boolean) = intent {
+        reduce {
+            state.copy(dialogVisibility = dialogVisibility)
+        }
+    }
+
+    fun onNavigateToSearchStore() = intent {
+        postSideEffect(BusinessAuthSideEffect.NavigateToSearchStore)
+    }
+
+    fun onNavigateToBackScreen() = intent {
+        postSideEffect(BusinessAuthSideEffect.NavigateToBackScreen)
+    }
+
+    fun onNavigateToNextScreen() = intent {
+        postSideEffect(BusinessAuthSideEffect.NavigateToNextScreen)
+    }
+
 }
