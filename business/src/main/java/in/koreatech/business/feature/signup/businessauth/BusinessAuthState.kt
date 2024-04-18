4fa20890d1d99419a013545299dacccfba76fc78
nazero <skdud4081@gmail.com>
2024-04-19 02:43:10 +0900

    Add business auth state

--- business/src/main/java/in/koreatech/business/feature/signup/businessauth/BusinessAuthState.kt
+++ business/src/main/java/in/koreatech/business/feature/signup/businessauth/BusinessAuthState.kt
@@ -1,5 +1,11 @@
 package `in`.koreatech.business.feature.signup.businessauth
 
 data class BusinessAuthState(
-    val count: Int = 0
+    val name: String = "",
+    val storeName: String = "",
+    val storeNumber: String = "",
+    val phoneNumber: String = "",
+    val openAlertDialog: Boolean = false,
+    val selectedImageUris :MutableList<String> = mutableListOf(),
+    val dialogVisibility:Boolean = false
 )
