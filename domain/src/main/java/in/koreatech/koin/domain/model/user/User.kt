77fa350162acd339acbb18e8bf8a916e03ebca11
Jokwanhee <jonotch1@naver.com>
2024-04-21 00:28:43 +0900

    Delete unnecessary properties

--- domain/src/main/java/in/koreatech/koin/domain/model/user/User.kt
+++ domain/src/main/java/in/koreatech/koin/domain/model/user/User.kt
@@ -2,23 +2,13 @@ package `in`.koreatech.koin.domain.model.user
 
 sealed class User {
     data class Student(
-//        val id: Int,
-//        val isGraduated: Boolean,
-//        val enabled: Boolean,
         val anonymousNickname: String?,
         val email: String?,
-//        val portalAccount: String,
-//        val identity: UserIdentity,
         val name: String?,
         val studentNumber: String?,
-//        val profileImageUrl: String?,
         val gender: Gender,
         val nickname: String?,
         val phoneNumber: String?,
-//        val accountNonExpired: Boolean,
-//        val accountNonLocked: Boolean,
-//        val credentialsNonExpired: Boolean,
-//        val username: String?,
         val major: String?
     ): User()
 
