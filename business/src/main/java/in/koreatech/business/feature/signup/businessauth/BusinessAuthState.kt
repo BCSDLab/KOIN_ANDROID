77c8486d72fc716f73038cacac71929377a77492
nazero <skdud4081@gmail.com>
2024-04-23 03:47:33 +0900

    Add BusinessAuthState value

--- business/src/main/java/in/koreatech/business/feature/signup/businessauth/BusinessAuthState.kt
+++ business/src/main/java/in/koreatech/business/feature/signup/businessauth/BusinessAuthState.kt
@@ -1,11 +1,19 @@
 package `in`.koreatech.business.feature.signup.businessauth
 
+import `in`.koreatech.koin.domain.model.store.AttachStore
+import `in`.koreatech.koin.domain.model.store.StoreUrl
+import java.io.InputStream
+
 data class BusinessAuthState(
     val name: String = "",
     val storeName: String = "",
     val storeNumber: String = "",
     val phoneNumber: String = "",
     val openAlertDialog: Boolean = false,
-    val selectedImageUris :MutableList<String> = mutableListOf(),
-    val dialogVisibility:Boolean = false
+    val selectedImages :MutableList<AttachStore> = mutableListOf(),
+    val dialogVisibility:Boolean = false,
+    val fileInfo: MutableList<StoreUrl> = mutableListOf(),
+    val inputStream: MutableList<InputStream> = mutableListOf(),
+    val continuation :Boolean = false,
+    val error :Throwable? = null,
 )
