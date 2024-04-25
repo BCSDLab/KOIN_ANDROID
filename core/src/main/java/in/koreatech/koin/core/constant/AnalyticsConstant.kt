5b2965d136ee9a41ce9338c474282c63f537c838
Strone <sangbbang31@gmail.com>
2024-04-25 19:15:38 +0900

    Add Activity analytics event log function

---
+++ core/src/main/java/in/koreatech/koin/core/constant/AnalyticsConstant.kt
@@ -0,0 +1,19 @@
+package `in`.koreatech.koin.core.constant
+
+object AnalyticsConstant {
+
+    object Domain {
+        const val BUSINESS = "BUSINESS"
+        const val CAMPUS = "CAMPUS"
+        const val USER = "USER"
+    }
+
+    object Category {
+        const val CLICK = "click"
+        const val SCROLL = "scroll"
+    }
+
+    object Label {
+
+    }
+}
