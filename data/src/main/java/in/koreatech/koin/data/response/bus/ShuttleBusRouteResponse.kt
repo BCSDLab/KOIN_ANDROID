1f6f0bed790e14aa3e681c45805f3f7d5f3b399e
Strone <sangbbang31@gmail.com>
2024-05-02 14:54:54 +0900

    Rollback timetable api response

--- data/src/main/java/in/koreatech/koin/data/response/bus/ShuttleBusRouteResponse.kt
+++ data/src/main/java/in/koreatech/koin/data/response/bus/ShuttleBusRouteResponse.kt
@@ -3,11 +3,11 @@ package `in`.koreatech.koin.data.response.bus
 import com.google.gson.annotations.SerializedName
 
 data class ShuttleBusTimetableResponse(
-    @SerializedName("bus_timetable") val routes: List<ShuttleBusRouteResponse>,
+    @SerializedName("bus_timetables") val routes: List<ShuttleBusRouteResponse>,
     @SerializedName("updated_at") val updatedAt: String
 )
 
 data class ExpressBusTimetableResponse(
-    @SerializedName("bus_timetable") val routes: List<ExpressBusRouteResponse>,
+    @SerializedName("bus_timetables") val routes: List<ExpressBusRouteResponse>,
     @SerializedName("updated_at") val updatedAt: String
 )
