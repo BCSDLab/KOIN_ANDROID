6264a2c9df87363206f9bef145ee40e0cf378a92
hsgo2430 <hsgo2430@naver.com>
2024-05-15 14:02:18 +0900

    1차 피드백 적용

--- business/src/main/java/in/koreatech/business/BusinessMainActivity.kt
+++ business/src/main/java/in/koreatech/business/BusinessMainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import `in`.koreatech.business.feature.signin.navigator.SignInNavigator
@@ -21,14 +26,26 @@ class BusinessMainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KOIN_ANDROIDTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    //ChangePassword()
-                    //SignupNavigator(modifier = Modifier.fillMaxSize())
-                    SignInNavigator()
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = stringResource(R.string.koin_for_business),
+                                    style = MaterialTheme.typography.h3,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            }
+                        )
+                    }
+                )
+                { innerPadding ->
+                    Surface(
+                        modifier = Modifier.padding(innerPadding),
+                        color = MaterialTheme.colors.background
+                    ) {
+                        SignInNavigator()
+                    }
                 }
             }
         }
