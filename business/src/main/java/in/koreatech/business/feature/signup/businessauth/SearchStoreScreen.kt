cfcdd5b69f9261d216603378a5172c4fa4891fd7
nazero <skdud4081@gmail.com>
2024-05-15 23:14:30 +0900

    Modify SearchStoreScreen

--- business/src/main/java/in/koreatech/business/feature/signup/businessauth/SearchStoreScreen.kt
+++ business/src/main/java/in/koreatech/business/feature/signup/businessauth/SearchStoreScreen.kt
@@ -1,11 +1,15 @@
 package `in`.koreatech.business.feature.signup.businessauth
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,7 +19,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -30,8 +36,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -50,6 +58,10 @@ import `in`.koreatech.business.ui.theme.ColorHelper
 import `in`.koreatech.business.ui.theme.ColorPrimary
 import `in`.koreatech.business.ui.theme.ColorSearch
 import `in`.koreatech.business.ui.theme.ColorSecondary
+import `in`.koreatech.business.ui.theme.ColorTextField
+import `in`.koreatech.business.ui.theme.ColorUnarchived
+import `in`.koreatech.business.ui.theme.Gray1
+import `in`.koreatech.business.ui.theme.Gray2
 import `in`.koreatech.koin.domain.model.store.Store
 import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.compose.collectAsState
@@ -60,60 +72,94 @@ fun SearchStoreScreen(
     modifier: Modifier = Modifier,
     viewModel: SearchStoreViewModel = hiltViewModel(),
     businessAuthViewModel: BusinessAuthViewModel = hiltViewModel(),
-    onBackClicked: () -> Unit = {}
+    onBackButtonClicked: () -> Unit = {}
 ) {
     val state = viewModel.collectAsState().value
-
     Column(
         modifier = modifier,
-    ) {
-        IconButton(
-            modifier = Modifier.padding(vertical = 24.dp),
-            onClick = { onBackClicked() }
-        ) {
-            Icon(
-                modifier = Modifier.padding(start = 10.dp),
-                painter = painterResource(id = R.drawable.ic_arrow_back),
-                contentDescription = stringResource(id = R.string.back_icon),
-            )
-        }
-        Column(
-            modifier = Modifier
-                .padding(horizontal = 32.dp)
-                .background(Color.White),
-            verticalArrangement = Arrangement.Center,
-        ) {
 
-            Text(
-                text = stringResource(id = R.string.master_sign_up),
-                fontSize = 24.sp,
-                fontWeight = Bold,
-            )
-            Spacer(modifier = Modifier.height(8.dp))
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 12.dp)
+        ) {
+            IconButton(
+                onClick = { viewModel.onNavigateToBackScreen() },
+                modifier = Modifier.align(Alignment.CenterStart)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_arrow_back),
+                    contentDescription = stringResource(id = R.string.back_icon),
+                )
+            }
+
             Text(
                 text = stringResource(id = R.string.search_store),
                 fontSize = 18.sp,
                 fontWeight = Bold,
+                modifier = Modifier.align(Alignment.Center)
             )
         }
 
+
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 24.dp),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    modifier = Modifier,
+                    color = ColorPrimary,
+                    fontWeight = Bold,
+                    text = stringResource(id = R.string.business_auth),
+                )
+                Text(
+                    text = stringResource(id = R.string.three_third),
+                    color = ColorPrimary,
+                    fontWeight = Bold,
+                )
+            }
+
+            Canvas(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+
+                drawLine(
+                    color = ColorPrimary,
+                    start = Offset(-40f, 0f),
+                    end = Offset((size.width + 40), size.height),
+                    strokeWidth = 4.dp.toPx(),
+                    cap = StrokeCap.Round
+                )
+            }
+        }
         Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 16.dp)
         ) {
-            Spacer(modifier = Modifier.height(42.dp))
+            Spacer(modifier = Modifier.height(10.dp))
             TextField(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(50.dp),
-                value = state.search, onValueChange = { viewModel.onSearchChanged(it) },
+                    .height(45.dp),
+                shape = RoundedCornerShape(6.dp),
+                value = state.search, onValueChange = { viewModel.onSearchChanged(it)
+                                                      viewModel.onSearchStore()},
                 textStyle = TextStyle(fontSize = 14.sp),
 
                 colors = TextFieldDefaults.textFieldColors(
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    focusedLabelColor = ColorSearch,
+                    focusedIndicatorColor = ColorTextField,
+                    unfocusedIndicatorColor = ColorTextField,
+                    focusedLabelColor = ColorTextField,
                 ),
                 placeholder = {
                     Text(
@@ -132,16 +178,20 @@ fun SearchStoreScreen(
                     }
                 }
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            StoreList(state.stores, viewModel,businessAuthViewModel)
+            Spacer(modifier = Modifier.height(5.dp))
+            StoreList(state.stores, viewModel, businessAuthViewModel)
+
         }
+
 
         viewModel.collectSideEffect {
             when (it) {
                 is SearchStoreSideEffect.SearchStore -> {
+                    viewModel.searchStore()
                 }
+
                 SearchStoreSideEffect.NavigateToBackScreen -> {
-                    onBackClicked()
+                    onBackButtonClicked()
                 }
             }
         }
@@ -152,26 +202,36 @@ fun SearchStoreScreen(
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun StoreList(item: List<Store>, viewModel: SearchStoreViewModel= hiltViewModel(), authViewModel: BusinessAuthViewModel= hiltViewModel() ) {
-    val state= viewModel.collectAsState().value
+fun StoreList(
+    item: List<Store>,
+    viewModel: SearchStoreViewModel = hiltViewModel(),
+    businessAuthViewModel: BusinessAuthViewModel = hiltViewModel()
+) {
+    val state = viewModel.collectAsState().value
+    val selectedStoreState = businessAuthViewModel.collectAsState().value
     val sheetState = rememberModalBottomSheetState(
         initialValue = ModalBottomSheetValue.Hidden
     )
     val scope = rememberCoroutineScope()
 
-    Scaffold() { contentPadding ->
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .height(200.dp)
+
+    ) {
         LazyColumn(
             modifier = Modifier
+                .weight(1f)
+                .padding(vertical = 8.dp)
                 .fillMaxSize()
-                .padding(contentPadding),
-            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             items(item.size) { index ->
                 Row(
                     modifier = Modifier
                         .padding(vertical = 8.dp)
                         .fillMaxWidth()
-                        .height(59.dp)
+                        .height(55.dp)
                         .clickable {
                             scope.launch {
                                 sheetState.show()
@@ -182,7 +242,8 @@ fun StoreList(item: List<Store>, viewModel: SearchStoreViewModel= hiltViewModel(
                             BorderStroke(
                                 width = if (state.itemIndex == index) 1.5.dp else 1.dp,
                                 color = if (state.itemIndex == index) ColorPrimary else ColorHelper
-                            )
+                            ),
+                            shape = RoundedCornerShape(6.dp),
                         ),
                 ) {
                     Row(
@@ -197,92 +258,52 @@ fun StoreList(item: List<Store>, viewModel: SearchStoreViewModel= hiltViewModel(
                             text = item[index].name,
                             fontSize = 15.sp,
                             color = Color.Black,
-                            fontWeight = Bold
-                        )
+
+                            )
                         Spacer(modifier = Modifier.width(74.dp))
                         Text(
                             buildAnnotatedString {
-                                withStyle(style = SpanStyle(color = ColorSecondary)) {
+                                withStyle(style = SpanStyle(color = ColorPrimary)) {
                                     append(text = stringResource(id = R.string.delivery))
                                 }
                                 append(" ")
-                                withStyle(style = SpanStyle(color = ColorSecondary)) {
+                                withStyle(style = SpanStyle(color = ColorPrimary)) {
                                     append(text = stringResource(id = R.string.card_payment))
                                 }
                                 append(" ")
-                                withStyle(style = SpanStyle(color = ColorSecondary)) {
+                                withStyle(style = SpanStyle(color = ColorPrimary)) {
                                     append(text = stringResource(id = R.string.account_transfer))
                                 }
                             },
-                            fontSize = 15.sp
+                            fontSize = 12.sp
                         )
                     }
                 }
             }
         }
+        Spacer(modifier =Modifier.height(70.dp))
 
-        if(state.itemIndex > -1)
-            ModalBottomSheetLayout(
-                modifier = Modifier,
-                sheetState = sheetState,
-                sheetElevation = 8.dp,
-                sheetContent = {
-                    StoreBottomSheet(item[state.itemIndex], viewModel, authViewModel)
-                }) {
-            }
-    }
-}
-
-@Composable
-fun StoreBottomSheet(store: Store, viewModel: SearchStoreViewModel= hiltViewModel(), authViewModel: BusinessAuthViewModel= hiltViewModel() ) {
-   val state=authViewModel.collectAsState().value
-    Row(
-        modifier = Modifier
-            .background(Color.White)
+        Button(modifier = Modifier
             .fillMaxWidth()
-            .height(118.dp)
-            .padding(20.dp),
-
-        ) {
-        Column(modifier = Modifier) {
-            Text(
-                modifier = Modifier.padding(bottom = 8.dp),
-                text = store.name,
-                fontSize = 18.sp,
-                color = Color.Black,
-                fontWeight = Bold,
-            )
-            Row(
-                modifier = Modifier,
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = stringResource(id = R.string.phone_number),
-                    fontSize = 14.sp,
-                    color = ColorActiveButton,
-                    fontWeight = Bold
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(
-                    text =store.phone , fontSize = 14.sp, color = Color.Black,
-                    fontWeight = Bold
-                )
-            }
-        }
-        Button(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 20.dp)
-                .height(55.dp),
+            .height(44.dp),
+            shape = RectangleShape,
+            enabled = state.itemIndex > -1,
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = ColorPrimary,
+                contentColor = Color.White,
+                disabledBackgroundColor = Gray2,
+                disabledContentColor = Gray1,
+            ),
             onClick = {
                 viewModel.onNavigateToBackScreen()
-                authViewModel.onShopIdChanged(store.uid)
-                authViewModel.onShopNameChanged(store.name)},
-            shape = RectangleShape,
-        ) {
-            Text(text = stringResource(id = R.string.select), fontWeight = Bold)
+                selectedStoreState.shopId?.let { businessAuthViewModel.onShopIdChanged(state.itemIndex) }
+                businessAuthViewModel.onShopNameChanged(state.stores[state.itemIndex].name)
+            }) {
+            Text(
+                text = stringResource(id = R.string.next),
+                fontSize = 13.sp,
+                fontWeight = Bold,
+            )
         }
     }
-
 }
