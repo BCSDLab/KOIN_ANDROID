0ecfcb159aa4e4abbab99fff265247f9a68bd703
nazero <skdud4081@gmail.com>
2024-05-18 01:12:56 +0900

    Delete email auth screen

--- business/src/main/java/in/koreatech/business/feature/signup/accountauth/EmailAuthSideEffect.kt
+++
@@ -1,6 +0,0 @@
-package `in`.koreatech.business.feature.signup.accountauth
-
-sealed class EmailAuthSideEffect {
-    data object NavigateToBackScreen : EmailAuthSideEffect()
-    data class NavigateToNextScreen(val email:String) : EmailAuthSideEffect()
-}
