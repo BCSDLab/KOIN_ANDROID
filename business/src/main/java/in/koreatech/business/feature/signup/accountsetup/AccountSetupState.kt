5246786ae06b6f37b7e2d2c6215aa6b5f00dc039
nazero <skdud4081@gmail.com>
2024-05-17 17:11:00 +0900

    Modify business signup logic

--- business/src/main/java/in/koreatech/business/feature/signup/accountsetup/AccountSetupState.kt
+++ business/src/main/java/in/koreatech/business/feature/signup/accountsetup/AccountSetupState.kt
@@ -7,11 +7,12 @@ data class AccountSetupState(
     val password: String = "",
     val passwordConfirm: String = "",
     val phoneNumber: String = "",
+    val authCode: String = "",
     val signupContinuationState: SignupContinuationState = SignupContinuationState.RequestedEmailValidation,
     val signUpContinuationError:Throwable? = null,
     val isLoading: Boolean = false
 ){
     val isButtonEnabled: Boolean
-        get() = id.isNotEmpty() && password.isNotEmpty() && passwordConfirm.isNotEmpty() && phoneNumber.isNotEmpty()
+        get() = id.isNotEmpty() && password.isNotEmpty() && passwordConfirm.isNotEmpty() && phoneNumber.isNotEmpty() && authCode.isNotEmpty()
 
 }
