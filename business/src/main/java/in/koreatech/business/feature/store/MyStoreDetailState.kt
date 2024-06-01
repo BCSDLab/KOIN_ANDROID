02f14c587ad8f4fe519d74895057b0aa37af20ab
nazero <skdud4081@gmail.com>
2024-06-01 23:24:51 +0900

    Add MyStoreDetailState

---
+++ business/src/main/java/in/koreatech/business/feature/store/MyStoreDetailState.kt
@@ -0,0 +1,7 @@
+package `in`.koreatech.business.feature.store
+
+import `in`.koreatech.koin.domain.model.store.StoreWithMenu
+
+data class MyStoreDetailState (
+    val storeInfo: StoreWithMenu? = null,
+)
