525dfac5e871cfaa9b6ce0cbad6593ad6765dc6a
hsgo2430 <hsgo2430@naver.com>
2024-06-05 12:53:55 +0900

    가게 상세 정보 등록 기능 구현

--- business/src/main/java/in/koreatech/business/feature/insertstore/insertdetailinfo/InsertDetailInfoScreenState.kt
+++ business/src/main/java/in/koreatech/business/feature/insertstore/insertdetailinfo/InsertDetailInfoScreenState.kt
@@ -2,6 +2,9 @@ package `in`.koreatech.business.feature.insertstore.insertdetailinfo
 
 import android.net.Uri
 import android.os.Parcelable
+import com.chargemap.compose.numberpicker.FullHours
+import com.chargemap.compose.numberpicker.Hours
+import `in`.koreatech.koin.domain.model.owner.insertstore.OperatingTime
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -9,5 +12,24 @@ data class InsertDetailInfoScreenState (
     val storeCategory: Int = -1,
     val storeName: String = "",
     val storeAddress: String = "",
-    val storeImage: Uri = Uri.EMPTY
+    val storeImage: Uri = Uri.EMPTY,
+    val storePhoneNumber: String = "",
+    val storeDeliveryFee: String ="",
+    val storeOtherInfo: String = "",
+    val isDeliveryOk: Boolean = false,
+    val isCardOk: Boolean = false,
+    val isBankOk: Boolean = false,
+    val operatingTimeList: List<OperatingTimeState> = listOf(
+        OperatingTimeState("00:00", false, "월", "00:00"),
+        OperatingTimeState("00:00", false, "화", "00:00"),
+        OperatingTimeState("00:00", false, "수", "00:00"),
+        OperatingTimeState("00:00", false, "목", "00:00"),
+        OperatingTimeState("00:00", false, "금", "00:00"),
+        OperatingTimeState("00:00", false, "토", "00:00"),
+        OperatingTimeState("00:00", false, "일", "00:00"),
+    ),
+    val isDetailInfoValid: Boolean = false,
+    val showDialog: Boolean = false,
+    val isOpenTimeSetting:Boolean = false,
+    val dayOfWeekIndex: Int = -1
 ): Parcelable
