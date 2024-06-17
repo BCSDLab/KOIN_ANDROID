866955cc388ef5b119ae8c24d77be288ed0bce0d
nazero <skdud4081@gmail.com>
2024-06-18 00:53:50 +0900

    Add initEventItem, toggleEventItem functions to MyStoreDetailViewModel

--- business/src/main/java/in/koreatech/business/feature/store/MyStoreDetailViewModel.kt
+++ business/src/main/java/in/koreatech/business/feature/store/MyStoreDetailViewModel.kt
@@ -2,19 +2,32 @@ package `in`.koreatech.business.feature.store
 
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import `in`.koreatech.koin.domain.usecase.store.GetStoreWithMenuUseCase
-import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.ContainerHost
-import org.orbitmvi.orbit.viewmodel.container
 import org.orbitmvi.orbit.syntax.simple.intent
-import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.syntax.simple.reduce
+import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
 @HiltViewModel
 class MyStoreDetailViewModel @Inject constructor(
 ) : ContainerHost<MyStoreDetailState, MyStoreDetailSideEffect>, ViewModel() {
-    override val container = container<MyStoreDetailState, MyStoreDetailSideEffect>(MyStoreDetailState())
+    override val container =
+        container<MyStoreDetailState, MyStoreDetailSideEffect>(MyStoreDetailState())
+
+    fun initEventItem() = intent {
+        reduce {
+            state.copy(isEventExpanded = state.isEventExpanded.mapIndexed { _, _ -> false })
+        }
+    }
+
+    fun toggleEventItem(index: Int) = intent {
+        reduce {
+            state.copy(
+                isEventExpanded = state.isEventExpanded.mapIndexed { i, isExpanded ->
+                    if (i == index) !isExpanded else isExpanded
+                }
+            )
+        }
+    }
 }
