fc26067cb15496ab4f00a6c44498cc8896e20355
wateralsie <tnals08019@gmail.com>
2024-06-26 19:39:17 +0900

    Add dining notice ui

---
+++ koin/src/main/java/in/koreatech/koin/ui/dining/DiningNoticeActivity.kt
@@ -0,0 +1,21 @@
+package `in`.koreatech.koin.ui.dining
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import `in`.koreatech.koin.R
+
+class DiningNoticeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dining_notice)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
