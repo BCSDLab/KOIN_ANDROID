24dc7b7c659e60a9194892e4ba8f2f207f830fec
nazero <skdud4081@gmail.com>
2024-06-30 17:02:54 +0900

    Modify wrong code

--- data/src/main/java/in/koreatech/koin/data/api/auth/OwnerAuthApi.kt
+++ data/src/main/java/in/koreatech/koin/data/api/auth/OwnerAuthApi.kt
@@ -7,14 +7,15 @@ import `in`.koreatech.koin.data.response.store.StoreMenuResponse
 import `in`.koreatech.koin.data.response.store.StoreResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface OwnerAuthApi {
     @GET(URLConstant.SHOPS.OWNERSHOPS)
     suspend fun getMyShopList(): StoreResponse
     @GET(URLConstant.SHOPS.OWNERSHOPS + "/{id}")
     suspend fun getOwnerShopInfo(@Path("id") uid: Int): StoreItemWithMenusResponse
-    @GET(URLConstant.SHOPS.OWNERSHOPS + "/{id}" + "/menus")
-    suspend fun getOwnerShopMenus(@Path("id") uid: Int): StoreMenuResponse
-    @GET(URLConstant.SHOPS.OWNERSHOPS + "/{id}" + "/events")
-    suspend fun getOwnerShopEvents(@Path("id") uid: Int): StoreDetailEventResponse
+    @GET(URLConstant.SHOPS.OWNERSHOPS + "/menus")
+    suspend fun getOwnerShopMenus(@Query("shopId") uid: Int): StoreMenuResponse
+    @GET(URLConstant.SHOPS.OWNERSHOPS + "/{shopId}" + "/event")
+    suspend fun getOwnerShopEvents(@Path("shopId") uid: Int): StoreDetailEventResponse
 }
