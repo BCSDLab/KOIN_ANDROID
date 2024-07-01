b8658fdbec932985e39a431f025e664c64670daf
nazero <skdud4081@gmail.com>
2024-07-01 10:24:10 +0900

    Add store detail menu and event handling

--- business/src/main/java/in/koreatech/business/feature/store/MyStoreDetailViewModel.kt
+++ business/src/main/java/in/koreatech/business/feature/store/MyStoreDetailViewModel.kt
@@ -4,8 +4,10 @@ package `in`.koreatech.business.feature.store
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import `in`.koreatech.koin.domain.usecase.business.GetOwnerShopEventsUseCase
 import `in`.koreatech.koin.domain.usecase.business.GetOwnerShopInfoUseCase
 import `in`.koreatech.koin.domain.usecase.business.GetOwnerShopListUseCase
+import `in`.koreatech.koin.domain.usecase.business.GetOwnerShopMenusUseCase
 import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
@@ -16,10 +18,16 @@ import javax.inject.Inject
 @HiltViewModel
 class MyStoreDetailViewModel @Inject constructor(
     private val getOwnerShopInfoUseCase: GetOwnerShopInfoUseCase,
-    private val getOwnerShopListUseCase: GetOwnerShopListUseCase
+    private val getOwnerShopListUseCase: GetOwnerShopListUseCase,
+    private val getOwnerShopEventsUseCase: GetOwnerShopEventsUseCase,
+    private val getOwnerShopMenusUseCase: GetOwnerShopMenusUseCase,
 ) : ContainerHost<MyStoreDetailState, MyStoreDetailSideEffect>, ViewModel() {
     override val container =
         container<MyStoreDetailState, MyStoreDetailSideEffect>(MyStoreDetailState())
+
+    init {
+        initOwnerShopList()
+    }
 
     fun initEventItem() = intent {
         reduce {
@@ -39,13 +47,47 @@ class MyStoreDetailViewModel @Inject constructor(
         }
     }
 
-    fun getOwnerShopList() = intent {
+    fun initOwnerShopList() = intent {
         viewModelScope.launch {
             getOwnerShopListUseCase().let { storeList ->
+                if (storeList.isNotEmpty()) {
+                    reduce {
+                        state.copy(
+                            storeList = storeList,
+                        )
+                    }
+                } else {
+                    reduce {
+                        state.copy(
+                            storeList = emptyList(),
+                        )
+                    }
+                }
+            }
+            intent {
+                reduce { state.copy(storeId = state.storeList.first().uid) }
+                getOwnerShopInfo(state.storeId)
+                getShopEvents()
+                getShopMenus()
+            }
+        }
+    }
+
+    fun getShopMenus() = intent {
+        viewModelScope.launch {
+            getOwnerShopMenusUseCase(state.storeId).also {
                 reduce {
-                    state.copy(
-                        storeList = storeList, storeId = storeList.first().uid
-                    )
+                    state.copy(storeMenu = it.menuCategories)
+                }
+            }
+        }
+    }
+
+    fun getShopEvents() = intent{
+        viewModelScope.launch {
+            getOwnerShopEventsUseCase(state.storeId).also {
+                reduce {
+                    state.copy(storeEvent = it)
                 }
             }
         }
