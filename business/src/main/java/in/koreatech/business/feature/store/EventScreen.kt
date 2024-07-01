6594ad6e6a1c5576112942d0a2ded991f1e56d9a
nazero <skdud4081@gmail.com>
2024-07-01 15:06:55 +0900

    Modify EventScreen

--- business/src/main/java/in/koreatech/business/feature/store/EventScreen.kt
+++ business/src/main/java/in/koreatech/business/feature/store/EventScreen.kt
@@ -36,10 +36,16 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import `in`.koreatech.business.R
 import `in`.koreatech.business.ui.theme.ColorTextField
 import `in`.koreatech.business.ui.theme.Gray1
 import `in`.koreatech.business.ui.theme.Gray6
+import `in`.koreatech.koin.domain.model.store.ShopEvent
+import `in`.koreatech.koin.domain.model.store.ShopEvents
+import `in`.koreatech.koin.domain.model.store.StoreEvent
+import `in`.koreatech.koin.domain.util.StoreUtil.generateOpenCloseTimeString
 import org.orbitmvi.orbit.compose.collectAsState
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -111,11 +117,13 @@ fun EventScreen(verticalOffset: Boolean, currentPage: Int) {
                 rememberPagerState { state.storeEvent.events[index].thumbnailImages?.size ?: 1 }
             if (state.isEventExpanded[index]) {
                 EventExpandedItem(
-                    state.storeEvent.events[index].title,
+                    state.storeEvent.events[index],
                     pagerState,
                     onCollapse = { viewModel.toggleEventItem(index) })
             } else {
-                EventItem(state.storeEvent.events[index].title, onClicked = { viewModel.toggleEventItem(index) })
+                EventItem(
+                    state.storeEvent.events[index],
+                    onClicked = { viewModel.toggleEventItem(index) })
             }
 
             Divider(
@@ -125,12 +133,11 @@ fun EventScreen(verticalOffset: Boolean, currentPage: Int) {
                     .height(1.dp)
             )
         }
-
     }
 }
 
 @Composable
-fun EventItem(item: String, onClicked: () -> Unit = {}) {
+fun EventItem(item: ShopEvent, onClicked: () -> Unit = {}) {
     Row(
         modifier = Modifier
             .clickable(onClick = { onClicked() })
@@ -139,19 +146,21 @@ fun EventItem(item: String, onClicked: () -> Unit = {}) {
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        Image(
-            modifier = Modifier
-                .width(68.dp)
-                .height(68.dp),
-            painter = painterResource(id = R.drawable.ic_koin_logo),
-            contentDescription = stringResource(R.string.event_default_image),
-        )
+        item.thumbnailImages?.get(0)?.let {
+            Image(
+                modifier = Modifier
+                    .width(68.dp)
+                    .height(68.dp),
+                painter = painterResource(id=R.drawable.no_event_image),
+                contentDescription = stringResource(R.string.event_default_image),
+            )
+        }
         Column() {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(text = item, fontWeight = FontWeight(500))
+                Text(text = item.title, fontWeight = FontWeight(500))
                 Row {
                     Text(
                         text = stringResource(R.string.view_all),
@@ -160,21 +169,24 @@ fun EventItem(item: String, onClicked: () -> Unit = {}) {
                         color = Gray6
                     )
                     Image(
-
                         painter = painterResource(id = R.drawable.ic_arrow_down),
                         contentDescription = stringResource(R.string.view_all)
                     )
                 }
             }
-            Text(text = "", fontSize = 12.sp, color = Gray6)
-            Text(text = "", fontSize = 10.sp, color = Gray6)
+            Text(text = item.content, fontSize = 12.sp, color = Gray6)
+            Text(
+                text = generateOpenCloseTimeString(item.startDate, item.endDate),
+                fontSize = 10.sp,
+                color = Gray6
+            )
         }
     }
 }
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun EventExpandedItem(item: String, pagerState: PagerState, onCollapse: () -> Unit = {}) {
+fun EventExpandedItem(item: ShopEvent, pagerState: PagerState, onCollapse: () -> Unit = {}) {
     Column(
         modifier = Modifier
             .padding(horizontal = 20.dp, vertical = 10.dp)
@@ -191,7 +203,8 @@ fun EventExpandedItem(item: String, pagerState: PagerState, onCollapse: () -> Un
         ) {
             Image(
                 modifier = Modifier.fillMaxSize(),
-                painter = painterResource(id = R.drawable.no_event_image),
+                painter = rememberAsyncImagePainter(model = item.thumbnailImages?.getOrNull(it))
+                ,
                 contentDescription = stringResource(R.string.event_default_image),
             )
         }
@@ -200,7 +213,7 @@ fun EventExpandedItem(item: String, pagerState: PagerState, onCollapse: () -> Un
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(text = item, fontWeight = FontWeight(500))
+                Text(text = item.title, fontWeight = FontWeight(500))
                 Row {
                     Text(
                         text = stringResource(R.string.fold),
@@ -217,8 +230,8 @@ fun EventExpandedItem(item: String, pagerState: PagerState, onCollapse: () -> Un
                     )
                 }
             }
-            Text(text = "", fontSize = 12.sp, color = Gray1)
-            Text(text = "", fontSize = 10.sp, color = Gray6)
+            Text(text = item.content, fontSize = 12.sp, color = Gray1)
+            Text(text =generateOpenCloseTimeString(item.startDate, item.endDate) , fontSize = 10.sp, color = Gray6)
         }
     }
 }
