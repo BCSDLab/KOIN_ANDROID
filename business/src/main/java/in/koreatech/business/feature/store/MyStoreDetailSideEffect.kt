63fb89e905659e02556dd6ef5ffc22d7237d6201
nazero <skdud4081@gmail.com>
2024-07-06 22:15:43 +0900

    Add navigation and toast SideEffect

--- business/src/main/java/in/koreatech/business/feature/store/MyStoreDetailSideEffect.kt
+++ business/src/main/java/in/koreatech/business/feature/store/MyStoreDetailSideEffect.kt
@@ -1,7 +1,8 @@
 package `in`.koreatech.business.feature.store
 
 sealed class MyStoreDetailSideEffect {
-    data object ShowDialog : MyStoreDetailSideEffect()
     data object NavigateToUploadEventScreen : MyStoreDetailSideEffect()
+    data object NavigateToModifyScreen : MyStoreDetailSideEffect()
     data class ShowErrorMessage(val errorMessage: String) : MyStoreDetailSideEffect()
+    data object ShowErrorModifyEventToast : MyStoreDetailSideEffect()
 }
