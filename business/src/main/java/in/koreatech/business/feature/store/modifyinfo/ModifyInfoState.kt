e53997b89d1cb155c65ce3d50349aed90d197c7c
nazero <skdud4081@gmail.com>
2024-07-09 11:28:21 +0900

    Add ModifyInfoState

---
+++ business/src/main/java/in/koreatech/business/feature/store/modifyinfo/ModifyInfoState.kt
@@ -0,0 +1,19 @@
+package `in`.koreatech.business.feature.store.modifyinfo
+
+import com.chargemap.compose.numberpicker.FullHours
+import `in`.koreatech.business.feature.insertstore.insertdetailinfo.operatingTime.OperatingTimeState
+
+data class ModifyInfoState(
+    val operatingTimeList: List<OperatingTimeState> = listOf(
+        OperatingTimeState("00:00", false, "월", "00:00", "MONDAY"),
+        OperatingTimeState("00:00", false, "화", "00:00", "TUESDAY"),
+        OperatingTimeState("00:00", false, "수", "00:00", "WEDNESDAY"),
+        OperatingTimeState("00:00", false, "목", "00:00", "THURSDAY"),
+        OperatingTimeState("00:00", false, "금", "00:00", "FRIDAY"),
+        OperatingTimeState("00:00", false, "토", "00:00", "SATURDAY"),
+        OperatingTimeState("00:00", false, "일", "00:00", "SUNDAY"),
+    ),
+    val dialogTimeState: Pair<FullHours, FullHours> = Pair(FullHours(0, 0), FullHours(0, 0)),
+    val showDialog: Boolean = false,
+    val dayOfWeekIndex: Int = -1,
+)
