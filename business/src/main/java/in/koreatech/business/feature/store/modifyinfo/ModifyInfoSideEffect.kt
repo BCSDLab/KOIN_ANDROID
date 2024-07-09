5ef66e74c7a782c148c17d0132744fa319bfd859
nazero <skdud4081@gmail.com>
2024-07-09 15:23:18 +0900

    Add NavigateToSettingOperatingTime SideEffect

--- business/src/main/java/in/koreatech/business/feature/store/modifyinfo/ModifyInfoSideEffect.kt
+++ business/src/main/java/in/koreatech/business/feature/store/modifyinfo/ModifyInfoSideEffect.kt
@@ -2,4 +2,5 @@ package `in`.koreatech.business.feature.store.modifyinfo
 
 sealed class ModifyInfoSideEffect {
     data object NavigateToBackScreen :  ModifyInfoSideEffect()
+    data object NavigateToSettingOperatingTime :  ModifyInfoSideEffect()
 }
