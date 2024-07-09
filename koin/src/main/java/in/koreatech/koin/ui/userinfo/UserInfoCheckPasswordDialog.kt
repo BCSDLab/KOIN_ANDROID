91c22ab03a901e0212302ee21800bcbd05abb6fa
DohyeokKim <hyeokdokim@gmail.com>
2024-07-09 16:36:24 +0900

    Change UserInfoCheckPasswordDialog to show error message

--- koin/src/main/java/in/koreatech/koin/ui/userinfo/UserInfoCheckPasswordDialog.kt
+++ koin/src/main/java/in/koreatech/koin/ui/userinfo/UserInfoCheckPasswordDialog.kt
@@ -2,6 +2,8 @@ package `in`.koreatech.koin.ui.userinfo
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
@@ -10,11 +12,10 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import `in`.koreatech.koin.R
-import `in`.koreatech.koin.core.toast.ToastUtil
+import `in`.koreatech.koin.common.UiStatus
 import `in`.koreatech.koin.core.util.dataBinding
 import `in`.koreatech.koin.databinding.DialogUserInfoCheckPasswordBinding
 import `in`.koreatech.koin.ui.userinfo.viewmodel.UserInfoCheckPasswordViewModel
-import `in`.koreatech.koin.util.ext.observeLiveData
 import `in`.koreatech.koin.util.ext.setWidthPercent
 import kotlinx.coroutines.launch
 
@@ -24,7 +25,29 @@ class UserInfoCheckPasswordDialog : DialogFragment(R.layout.dialog_user_info_che
     private val binding by dataBinding<DialogUserInfoCheckPasswordBinding>()
     private val viewModel by viewModels<UserInfoCheckPasswordViewModel>()
 
+    private val passwordWatcher by lazy {
+        object : TextWatcher {
+            var prevPwd: String = ""
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                prevPwd = s.toString()
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!binding.btnCheckPassword.isEnabled && prevPwd != s.toString()) {
+                    this.prevPwd = s.toString()
+                    hideErrors()
+                    viewModel.onPwdTextChanged()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         setWidthPercent(90)
 
         initView()
@@ -39,21 +62,51 @@ class UserInfoCheckPasswordDialog : DialogFragment(R.layout.dialog_user_info_che
             ivDialogClose.setOnClickListener {
                 dismiss()
             }
+            tietPassword.addTextChangedListener(passwordWatcher)
         }
     }
 
     private fun initObservers() {
-        observeLiveData(viewModel.errorToast) {
-            ToastUtil.getInstance().makeShort(it)
-        }
-
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.passwordValidEvent.collect {
-                    dismiss()
-                    startActivity(Intent(requireContext(), UserInfoEditActivity::class.java))
+                launch {
+                    viewModel.verifyStatus.collect {
+                        when (it.status) {
+                            is UiStatus.Init -> Unit
+                            is UiStatus.Loading -> Unit
+                            is UiStatus.Success -> {
+                                hideErrors()
+                                startActivity(Intent(requireContext(), UserInfoEditActivity::class.java))
+                            }
+
+                            is UiStatus.Failed -> {
+                                if (!it.isEdited) {
+                                    showErrors()
+                                }
+                            }
+                        }
+                    }
                 }
+
             }
         }
+    }
+
+    private fun showErrors() = with(binding) {
+        groupWarning.visibility = View.VISIBLE
+        btnCheckPassword.isEnabled = false
+        tilPassword.error = " "
+    }
+
+    private fun hideErrors() = with(binding) {
+        groupWarning.visibility = View.GONE
+        btnCheckPassword.isEnabled = true
+        tilPassword.error = null
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        binding.tietPassword.removeTextChangedListener(passwordWatcher)
     }
 }
--- koin/src/main/java/in/koreatech/koin/ui/userinfo/viewmodel/UserInfoCheckPasswordViewModel.kt
+++ koin/src/main/java/in/koreatech/koin/ui/userinfo/viewmodel/UserInfoCheckPasswordViewModel.kt
@@ -2,10 +2,15 @@ package `in`.koreatech.koin.ui.userinfo.viewmodel
 
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import `in`.koreatech.koin.common.UiStatus
 import `in`.koreatech.koin.core.viewmodel.BaseViewModel
 import `in`.koreatech.koin.domain.usecase.user.VerifyUserPasswordUseCase
-import `in`.koreatech.koin.util.EventFlow
-import `in`.koreatech.koin.util.MutableEventFlow
+import `in`.koreatech.koin.ui.userinfo.PwdVerificationState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,14 +18,24 @@ class UserInfoCheckPasswordViewModel @Inject constructor(
     private val verifyUserPasswordUseCase: VerifyUserPasswordUseCase
 ) : BaseViewModel() {
 
-    private val _passwordValidEvent: MutableEventFlow<Unit> = MutableEventFlow()
-    val passwordValidEvent: EventFlow<Unit> get() = _passwordValidEvent
+    private val _verifyStatus = MutableStateFlow(PwdVerificationState(UiStatus.Init, isEdited = false))
+    val verifyStatus: StateFlow<PwdVerificationState> = _verifyStatus.asStateFlow()
 
     fun verifyPassword(password: String) {
-        viewModelScope.launchWithLoading {
-            verifyUserPasswordUseCase(password)?.let {
-                updateErrorMessage(it.message)
-            } ?: _passwordValidEvent.emit(Unit)
+        viewModelScope.launch {
+            verifyUserPasswordUseCase(password).let { handler ->
+                if (handler == null) {
+                    _verifyStatus.update { it.copy(status = UiStatus.Success, isEdited = false) }
+                } else {
+                    _verifyStatus.update { it.copy(UiStatus.Failed(handler.message), isEdited = false) }
+                }
+            }
+        }
+    }
+
+    fun onPwdTextChanged() {
+        viewModelScope.launch {
+            _verifyStatus.update { it.copy(isEdited = true) }
         }
     }
 }
