03ea4c178afb1126d766377022dc652afebf662c
nazero <skdud4081@gmail.com>
2024-07-10 10:07:35 +0900

    Delete wrong code

--- business/src/main/java/in/koreatech/business/di/network/AuthNetworkModule.kt
+++ business/src/main/java/in/koreatech/business/di/network/AuthNetworkModule.kt
@@ -14,13 +14,11 @@ import `in`.koreatech.koin.core.qualifier.PreSignedUrl
 import `in`.koreatech.koin.core.qualifier.Refresh
 import `in`.koreatech.koin.core.qualifier.ServerUrl
 import `in`.koreatech.koin.data.api.PreSignedUrlApi
-import `in`.koreatech.koin.data.api.StoreApi
 import `in`.koreatech.koin.data.api.UploadUrlApi
 import `in`.koreatech.koin.data.api.UserApi
 import `in`.koreatech.koin.data.api.auth.OwnerAuthApi
 import `in`.koreatech.koin.data.api.auth.UserAuthApi
 import `in`.koreatech.koin.data.source.local.TokenLocalDataSource
-import `in`.koreatech.koin.data.source.remote.UserRemoteDataSource
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
@@ -116,18 +114,7 @@ object BusinessAuthNetworkModule {
     ): Interceptor {
         return Interceptor { chain: Interceptor.Chain ->
             runBlocking {
-                val ownerAccessToken =
-                    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpZCI6NTU0OCwiZXhwIjoxNzE5ODg2Nzc4fQ.5LjbuPxi1G0aQoxtvWjB3ajpd28k6kjjCp4I199fkt3C8cRa40zxcjJwS91X0pSi"
-               //     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpZCI6NTU0OCwiZXhwIjoxNzE5ODAwNTA3fQ.5JP2LEvsvGGLt_0XdiyS-XzP_9y79CXfs7MVqdin8YIwFF1v3W43RYRqsy5ILUAR"
-               //     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpZCI6NTU0OCwiZXhwIjoxNzE5NzQ0NjMwfQ.lFGkh9EYK4dNfNcm765XiJ40BgWUFEJEZ__G4917xj74BUvqlp-CqxFxBBvyLs8D"
-                //    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpZCI6NTU0OCwiZXhwIjoxNzE5NzM4MzAzfQ.YD-w5GLOziL8vxIH9snuzNPJmkweaEUGrgUoT3_Zf7VYkYiQ8IhNuAwbIwXi-59s"
-                 //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpZCI6NTU0OCwiZXhwIjoxNzE5NzMwNTY4fQ.fyZZEt8ZX4eTSUQ0FA8uxKvHAQLoXjKMwad8FcE-E-mwhVbpX9fAqh2Arxw0GtMx"
-                  //  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpZCI6NTU0OCwiZXhwIjoxNzE5NzI4Mjc5fQ.kfvy-qGmYvOD-FtAWEPZ4zNsKOoz-l-xGW7R4Wb42eWhuhgcPHcOLs_RixPJD6Rb"
-                    //"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpZCI6NTU0OCwiZXhwIjoxNzE5NzIwODUzfQ.GhRRGmlAT16kT3QRjS9dT4-ZKIih1od30-0Ca9Ee2DS6iVhFVHXZnwp5vSjJiMCs"
-                   // "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpZCI6NTU0OCwiZXhwIjoxNzE5NzIwNjE5fQ.ZMyWalzI9gTpLBUnIWj6XNotKsTuZRUSOC-cDainytso4QdBRSAjwvj9NA2x2dcy"
-                 //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpZCI6NTU0OCwiZXhwIjoxNzE5NjM3NjU3fQ.w_FsTfvI59yErcUEyBhseivHgohlfaV_SJRmJ9Z2YTT56EJfpKQR2HWap5I7_E_G"
-                  //  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpZCI6NTU0OCwiZXhwIjoxNzE5NjMxMzM3fQ.pBmetpcS99QZAwgR93DPY0s1kntW518WuoodRVNrAR1RNzPxOw7-b22Qe9-n1vrV"
-                    //"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpZCI6NTU0OCwiZXhwIjoxNzE5NTU1MTI1fQ.eYUnpAuN9cEAMbgveHOoIs2mZ_i4F3taALMRvMm4X9XnQgyYQQk1veHQhEYLSnvF"//tokenLocalDataSource.getOwnerAccessToken() ?: ""
+                val ownerAccessToken = tokenLocalDataSource.getOwnerAccessToken() ?: ""
                 val newRequest: Request = chain.request().newBuilder()
                     .addHeader("Authorization", "Bearer $ownerAccessToken")
                     .build()
@@ -192,8 +179,6 @@ object BusinessAuthNetworkModule {
         return retrofit.create(OwnerAuthApi::class.java)
     }
 }
-
-
 
 
 @Module
