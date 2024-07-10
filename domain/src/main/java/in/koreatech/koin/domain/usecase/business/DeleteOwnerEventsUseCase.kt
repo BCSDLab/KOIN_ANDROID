b27093fe2d58e3e165054d30ee628c1e4979d334
nazero <skdud4081@gmail.com>
2024-07-10 11:27:38 +0900

    Add error handling logic for delete shop event

--- domain/src/main/java/in/koreatech/koin/domain/usecase/business/DeleteOwnerEventsUseCase.kt
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/business/DeleteOwnerEventsUseCase.kt
@@ -1,21 +1,18 @@
 package `in`.koreatech.koin.domain.usecase.business
 
-import `in`.koreatech.koin.domain.constant.ERROR_OWNERSHOP_NOT_SET
 import `in`.koreatech.koin.domain.error.owner.OwnerErrorHandler
-import `in`.koreatech.koin.domain.model.error.ErrorHandler
-import `in`.koreatech.koin.domain.model.store.StoreWithMenu
 import `in`.koreatech.koin.domain.repository.OwnerShopRepository
 import javax.inject.Inject
 
 class DeleteOwnerEventsUseCase @Inject constructor(
     private val storeRepository: OwnerShopRepository,
+    private val ownerErrorHandler: OwnerErrorHandler
 ) {
-    suspend operator fun invoke(shopId:Int, eventId: Int) {
+    suspend operator fun invoke(shopId: Int, eventId: Int) {
         try {
             storeRepository.deleteOwnerShopEvent(shopId, eventId)
-        }
-        catch (throwable: Throwable) {
-            throw throwable
+        } catch (throwable: Throwable) {
+            ownerErrorHandler.handleDeleteOwnerShopEventError(throwable)
         }
     }
 }
