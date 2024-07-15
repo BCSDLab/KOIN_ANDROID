67ed84e995cad4db183021d1c26f5ba35a286b76
nazero <skdud4081@gmail.com>
2024-07-15 17:11:29 +0900

    Delete operatingTimeList and Add storeInfo state

--- business/src/main/java/in/koreatech/business/feature/store/modifyinfo/ModifyInfoState.kt
+++ business/src/main/java/in/koreatech/business/feature/store/modifyinfo/ModifyInfoState.kt
@@ -1,17 +1,10 @@
 package `in`.koreatech.business.feature.store.modifyinfo
 
 import com.chargemap.compose.numberpicker.FullHours
+import `in`.koreatech.koin.domain.model.owner.StoreDetailInfo
 
 data class ModifyInfoState(
-    val operatingTimeList: List<StoreOperatingTime> = listOf(
-        StoreOperatingTime(OperatingTime(FullHours(0, 0), FullHours(0, 0)), false, "월", "MONDAY"),
-        StoreOperatingTime(OperatingTime(FullHours(0, 0), FullHours(0, 0)), false, "화", "TUESDAY"),
-        StoreOperatingTime(OperatingTime(FullHours(0, 0), FullHours(0, 0)), false, "수", "WEDNESDAY"),
-        StoreOperatingTime(OperatingTime(FullHours(0, 0), FullHours(0, 0)), false, "목", "THURSDAY"),
-        StoreOperatingTime(OperatingTime(FullHours(0, 0), FullHours(0, 0)), false, "금", "FRIDAY"),
-        StoreOperatingTime(OperatingTime(FullHours(0, 0), FullHours(0, 0)), false, "토", "SATURDAY"),
-        StoreOperatingTime(OperatingTime(FullHours(0, 0), FullHours(0, 0)), false, "일", "SUNDAY"),
-    ),
+    val storeInfo: StoreDetailInfo? = null,
     val dialogTimeState: OperatingTime = OperatingTime(FullHours(0, 0), FullHours(0, 0)),
     val showDialog: Boolean = false,
     val dayOfWeekIndex: Int = -1,
