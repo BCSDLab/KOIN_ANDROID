88cb7d9eaa0a848750efdd3a11b583e21ea51958
nazero <skdud4081@gmail.com>
2024-07-15 18:27:16 +0900

    Add modifyInfoViewModel to ModifyInfoNavigator

--- business/src/main/java/in/koreatech/business/feature/store/modifyinfo/navigator/ModifyInfoNavigator.kt
+++ business/src/main/java/in/koreatech/business/feature/store/modifyinfo/navigator/ModifyInfoNavigator.kt
@@ -2,10 +2,12 @@ package `in`.koreatech.business.feature.store.modifyinfo.navigator
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import `in`.koreatech.business.feature.store.modifyinfo.ModifyInfoScreen
+import `in`.koreatech.business.feature.store.modifyinfo.ModifyInfoViewModel
 import `in`.koreatech.business.feature.store.modifyinfo.ModifyOperatingTimeScreen
 
 
@@ -14,6 +16,7 @@ fun ModifyInfoNavigator(
     modifier: Modifier,
 ) {
     val navController = rememberNavController()
+    val modifyInfoViewModel = hiltViewModel<ModifyInfoViewModel>()
     NavHost(
         navController = navController,
         startDestination = ModifyInfoRoute.MODIFY_INFO.name,
@@ -24,6 +27,7 @@ fun ModifyInfoNavigator(
             route = ModifyInfoRoute.MODIFY_INFO.name,
         ) {
             ModifyInfoScreen(
+                viewModel = modifyInfoViewModel,
                 onSettingOperatingClicked = { navController.navigate(ModifyInfoRoute.SETTING_OPERATING_TIME.name) }
             )
         }
@@ -32,6 +36,7 @@ fun ModifyInfoNavigator(
             route = ModifyInfoRoute.SETTING_OPERATING_TIME.name,
         ) {
             ModifyOperatingTimeScreen(
+                viewModel = modifyInfoViewModel,
                 onBackClicked = { navController.popBackStack() }
             )
         }
