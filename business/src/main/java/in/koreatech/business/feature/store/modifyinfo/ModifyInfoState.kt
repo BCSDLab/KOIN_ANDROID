960ad790d151c5f8f2fa56d772a61f18e2c277cf
nazero <skdud4081@gmail.com>
2024-07-15 18:28:25 +0900

    Change storeInfo from nullable to non-nullable.

--- business/src/main/java/in/koreatech/business/feature/store/modifyinfo/ModifyInfoState.kt
+++ business/src/main/java/in/koreatech/business/feature/store/modifyinfo/ModifyInfoState.kt
@@ -4,7 +4,21 @@ import com.chargemap.compose.numberpicker.FullHours
 import `in`.koreatech.koin.domain.model.owner.StoreDetailInfo
 
 data class ModifyInfoState(
-    val storeInfo: StoreDetailInfo? = null,
+    val storeInfo: StoreDetailInfo = StoreDetailInfo(
+        address = "" ,
+        categoryIds = listOf(),
+        isDeliveryOk = false,
+        deliveryPrice = 0,
+        description = "",
+        imageUrls = listOf(),
+        name = "",
+        operatingTime = listOf(),
+        isBankOk = false,
+        isCardOk = false,
+        phone = "",
+        bank = "",
+        accountNumber = "",
+    ),
     val dialogTimeState: OperatingTime = OperatingTime(FullHours(0, 0), FullHours(0, 0)),
     val showDialog: Boolean = false,
     val dayOfWeekIndex: Int = -1,
