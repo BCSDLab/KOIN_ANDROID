09e5583849d363eff7dec11ab831315cb7e75ef7
nazero <skdud4081@gmail.com>
2024-07-16 14:52:51 +0900

    Change usecase name AttachStoreFileUseCase ->GetPresignedUrlUseCase

--- business/src/main/java/in/koreatech/business/feature/signup/businessauth/BusinessAuthViewModel.kt
+++ business/src/main/java/in/koreatech/business/feature/signup/businessauth/BusinessAuthViewModel.kt
@@ -9,10 +9,9 @@ import `in`.koreatech.koin.data.mapper.strToOwnerRegisterUrl
 import `in`.koreatech.koin.domain.model.store.AttachStore
 import `in`.koreatech.koin.domain.model.store.StoreUrl
 import `in`.koreatech.koin.domain.usecase.business.UploadFileUseCase
-import `in`.koreatech.koin.domain.usecase.owner.AttachStoreFileUseCase
+import `in`.koreatech.koin.domain.usecase.owner.GetPresignedUrlUseCase
 import `in`.koreatech.koin.domain.usecase.owner.OwnerRegisterUseCase
 import `in`.koreatech.koin.domain.util.ext.formatBusinessNumber
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
@@ -24,7 +23,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class BusinessAuthViewModel @Inject constructor(
-    private val getPresignedUrlUseCase: AttachStoreFileUseCase,
+    private val getPresignedUrlUseCase: GetPresignedUrlUseCase,
     private val uploadFilesUseCase: UploadFileUseCase,
     private val ownerRegisterUseCase: OwnerRegisterUseCase
 ) : ContainerHost<BusinessAuthState, BusinessAuthSideEffect>, ViewModel() {
