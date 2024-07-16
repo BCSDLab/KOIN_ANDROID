b1a94c70fde6f8d768e4fe99965a136cfc286df2
nazero <skdud4081@gmail.com>
2024-07-16 16:35:51 +0900

    Modify GetOwnerShopInfoUseCase to use Result

--- domain/src/main/java/in/koreatech/koin/domain/usecase/business/GetOwnerShopInfoUseCase.kt
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/business/GetOwnerShopInfoUseCase.kt
@@ -2,26 +2,24 @@ package `in`.koreatech.koin.domain.usecase.business
 
 import `in`.koreatech.koin.domain.constant.ERROR_OWNERSHOP_NOT_SET
 import `in`.koreatech.koin.domain.error.owner.OwnerErrorHandler
-import `in`.koreatech.koin.domain.model.error.ErrorHandler
 import `in`.koreatech.koin.domain.model.owner.StoreDetailInfo
-import `in`.koreatech.koin.domain.model.store.StoreWithMenu
 import `in`.koreatech.koin.domain.repository.OwnerShopRepository
 import javax.inject.Inject
 
 class GetOwnerShopInfoUseCase @Inject constructor(
     private val storeRepository: OwnerShopRepository,
-    private val errorHandler: OwnerErrorHandler,
 ) {
     suspend operator fun invoke(
         storeId: Int
-    ): Pair<StoreDetailInfo?, ErrorHandler?> {
+    ): Result<StoreDetailInfo?> {
         return try {
             if (storeId < 0) {
                 throw IllegalArgumentException(ERROR_OWNERSHOP_NOT_SET)
             }
-            storeRepository.getOwnerShopInfo(storeId) to null
+            Result.success(storeRepository.getOwnerShopInfo(storeId))
+
         } catch (throwable: Throwable) {
-            null to errorHandler.handleGetOwnerInfoError(throwable)
+            Result.failure(throwable)
         }
     }
 }
