1bd7396a15decbd9f2d414a63e6ea858a18b9c2f
nazero <skdud4081@gmail.com>
2024-07-16 14:52:51 +0900

    Change usecase name AttachStoreFileUseCase ->GetPresignedUrlUseCase

--- koin/src/main/java/in/koreatech/koin/ui/businesssignup/viewmodel/BusinessCertificationViewModel.kt
+++ koin/src/main/java/in/koreatech/koin/ui/businesssignup/viewmodel/BusinessCertificationViewModel.kt
@@ -4,7 +4,7 @@ import android.net.Uri
 import `in`.koreatech.koin.core.viewmodel.BaseViewModel
 import `in`.koreatech.koin.core.viewmodel.SingleLiveEvent
 import `in`.koreatech.koin.domain.model.store.AttachStore
-import `in`.koreatech.koin.domain.usecase.owner.AttachStoreFileUseCase
+import `in`.koreatech.koin.domain.usecase.owner.GetPresignedUrlUseCase
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
@@ -21,7 +21,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class BusinessCertificationViewModel @Inject constructor(
-    private val attachStoreFileUseCase: AttachStoreFileUseCase,
+    private val getPresignedUrlUseCase: GetPresignedUrlUseCase,
     private val uploadPreSignedUrlUseCase: UploadPreSignedUrlUseCase
 ): BaseViewModel() {
     private val _saveImageList = MutableLiveData<List<AttachStore>>(emptyList())
@@ -68,7 +68,7 @@ class BusinessCertificationViewModel @Inject constructor(
         fileName: String
     ) {
         viewModelScope.launch {
-            attachStoreFileUseCase(
+            getPresignedUrlUseCase(
                 fileSize, fileType, fileName
             ).onSuccess {
                 _shopImageUrlAndSize.value = StoreUrl(uri.toString(), it.first, fileName, fileType, it.second, fileSize)
