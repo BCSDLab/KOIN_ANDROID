6250a2009bdee452afaaf9b5859968e263d81ec8
nazero <skdud4081@gmail.com>
2024-07-19 12:04:14 +0900

    Add ShowToastMessage SideEffect

--- business/src/main/java/in/koreatech/business/feature/store/modifyinfo/ModifyInfoSideEffect.kt
+++ business/src/main/java/in/koreatech/business/feature/store/modifyinfo/ModifyInfoSideEffect.kt
@@ -1,6 +1,7 @@
 package `in`.koreatech.business.feature.store.modifyinfo
 
 sealed class ModifyInfoSideEffect {
-    data object NavigateToBackScreen :  ModifyInfoSideEffect()
-    data object NavigateToSettingOperatingTime :  ModifyInfoSideEffect()
+    data object NavigateToBackScreen : ModifyInfoSideEffect()
+    data object NavigateToSettingOperatingTime : ModifyInfoSideEffect()
+    data object ShowToastMessage: ModifyInfoSideEffect()
 }
