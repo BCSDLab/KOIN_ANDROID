203e9cb376b0d132e2da7d7384f745e6b3588b21
nazero <skdud4081@gmail.com>
2024-07-22 11:18:17 +0900

    Modify imagePickerHandler to use withContext

--- business/src/main/java/in/koreatech/business/feature/ImagePickerHandler.kt
+++ business/src/main/java/in/koreatech/business/feature/ImagePickerHandler.kt
@@ -9,7 +9,9 @@ import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 @Composable
@@ -31,28 +33,31 @@ fun launchImagePicker(
                 var fileName = ""
                 var fileSize = 0L
                 val inputStream = contentResolver.openInputStream(it)
-                if (it.scheme.equals("content")) {
-                    val cursor = contentResolver.query(it, null, null, null, null)
-                    cursor.use {
-                        if (cursor != null && cursor.moveToFirst()) {
-                            val fileNameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-                            val fileSizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
+                withContext(Dispatchers.IO) {
+                    if (it.scheme.equals("content")) {
+                        val cursor = contentResolver.query(it, null, null, null, null)
+                        cursor.use {
+                            if (cursor != null && cursor.moveToFirst()) {
+                                val fileNameIndex =
+                                    cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                                val fileSizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
 
-                            if (fileNameIndex != -1 && fileSizeIndex != -1) {
-                                fileName = cursor.getString(fileNameIndex)
-                                fileSize = cursor.getLong(fileSizeIndex)
+                                if (fileNameIndex != -1 && fileSizeIndex != -1) {
+                                    fileName = cursor.getString(fileNameIndex)
+                                    fileSize = cursor.getLong(fileSizeIndex)
+                                }
                             }
                         }
-                    }
-                    if (inputStream != null) {
-                        getPreSignedUrl(
-                            Pair(
-                                Pair(fileSize, "image/" + fileName.split(".")[1]),
-                                Pair(fileName, it.toString())
+                        if (inputStream != null) {
+                            getPreSignedUrl(
+                                Pair(
+                                    Pair(fileSize, "image/" + fileName.split(".")[1]),
+                                    Pair(fileName, it.toString())
+                                )
                             )
-                        )
+                        }
+                        inputStream?.close()
                     }
-                    inputStream?.close()
                 }
             }
         }
