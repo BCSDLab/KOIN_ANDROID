d91d998c0d2c2c34043841451c7e7d41b3385d62
Strone <sangbbang31@gmail.com>
2024-07-22 21:48:14 +0900

    Add dining notification onboarding bottom dialog

---
+++ koin/src/main/java/in/koreatech/koin/ui/dining/DiningNotificationOnBoardingFragment.kt
@@ -0,0 +1,43 @@
+package `in`.koreatech.koin.ui.dining
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
+import `in`.koreatech.koin.R
+import `in`.koreatech.koin.databinding.DiningNotificationOnBoardingBottomSheetBinding
+
+@AndroidEntryPoint
+class DiningNotificationOnBoardingFragment : BottomSheetDialogFragment() {
+    private var binding: DiningNotificationOnBoardingBottomSheetBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.CustomBottomSheetDialog)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return DiningNotificationOnBoardingBottomSheetBinding.inflate(inflater, container, false).also {
+            binding = it
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.textButtonClose?.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
