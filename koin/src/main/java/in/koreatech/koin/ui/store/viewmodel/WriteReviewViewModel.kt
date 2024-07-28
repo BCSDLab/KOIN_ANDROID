5707155f01dc5b1a49ba70148f72f7b879868d2e
nazero <skdud4081@gmail.com>
2024-07-28 11:32:33 +0900

    Add review image upload logic

--- koin/src/main/java/in/koreatech/koin/ui/store/viewmodel/WriteReviewViewModel.kt
+++ koin/src/main/java/in/koreatech/koin/ui/store/viewmodel/WriteReviewViewModel.kt
@@ -1,12 +1,14 @@
 package `in`.koreatech.koin.ui.store.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import `in`.koreatech.koin.core.viewmodel.BaseViewModel
 import `in`.koreatech.koin.domain.model.store.Review
+import `in`.koreatech.koin.domain.usecase.business.UploadFileUseCase
+import `in`.koreatech.koin.domain.usecase.presignedurl.GetMarketPreSignedUrlUseCase
 import `in`.koreatech.koin.domain.usecase.store.WriteReviewUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -14,19 +16,75 @@ import javax.inject.Inject
 @HiltViewModel
 class WriteReviewViewModel @Inject constructor(
     private val writeReviewUseCase: WriteReviewUseCase,
-): BaseViewModel() {
+    private val getMarketPreSignedUrlUseCase: GetMarketPreSignedUrlUseCase,
+    private val uploadFilesUseCase: UploadFileUseCase
+) : BaseViewModel() {
     private val _review = MutableStateFlow<Review?>(null)
     val review = _review.asStateFlow()
+    private val _menuList = MutableStateFlow<List<String>>(emptyList())
+    val menuList: StateFlow<List<String>> get() = _menuList
 
+    private val _menuImageUrls = MutableStateFlow<List<String>>(emptyList())
+    val menuImageUrls: StateFlow<List<String>> get() = _menuImageUrls.asStateFlow()
 
     fun writeReview(storeId: Int, content: Review) {
         viewModelScope.launch {
             writeReviewUseCase(storeId, content).also {
                 _review.value = content
-                Log.e("WriteReviewViewModel", "writeReview: $content")
             }
         }
 
     }
 
+    fun getPreSignedUrl(
+        fileSize: Long,
+        fileType: String,
+        fileName: String,
+        imageUri: String
+    ) {
+        viewModelScope.launch {
+            getMarketPreSignedUrlUseCase(
+                fileSize, fileType, fileName
+            ).onSuccess {
+                _menuImageUrls.value = emptyList()
+                uploadImage(
+                    preSignedUrl = it.second,
+                    fileUrl = it.first,
+                    mediaType = fileType,
+                    mediaSize = fileSize,
+                    imageUri = imageUri
+                )
+            }.onFailure {
+                _menuImageUrls.value = emptyList()
+            }
+
+        }
+    }
+
+    private fun uploadImage(
+        preSignedUrl: String,
+        fileUrl: String,
+        mediaType: String,
+        mediaSize: Long,
+        imageUri: String
+    ) {
+        viewModelScope.launch {
+            uploadFilesUseCase(
+                preSignedUrl,
+                mediaType,
+                mediaSize,
+                imageUri
+            ).onSuccess {
+                _menuImageUrls.value += fileUrl
+            }.onFailure {
+                _menuImageUrls.value = emptyList()
+            }
+        }
+    }
+
+    fun deleteMenuImage(position: Int) {
+        val list = _menuImageUrls.value.toMutableList()
+        list.removeAt(position)
+        _menuImageUrls.value = list
+    }
 }
