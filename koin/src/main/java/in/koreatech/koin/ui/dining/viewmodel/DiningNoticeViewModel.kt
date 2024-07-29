51f6849e955f64545dc3fa4b007ffc7633d6c698
wateralsie <tnals08019@gmail.com>
2024-07-29 13:47:48 +0900

    Add navigation to DiningNoticeActivity

---
+++ koin/src/main/java/in/koreatech/koin/ui/dining/viewmodel/DiningNoticeViewModel.kt
@@ -0,0 +1,7 @@
+package `in`.koreatech.koin.ui.dining.viewmodel
+
+import `in`.koreatech.koin.core.viewmodel.BaseViewModel
+import javax.inject.Inject
+
+class DiningNoticeViewModel @Inject constructor(): BaseViewModel() {
+}
