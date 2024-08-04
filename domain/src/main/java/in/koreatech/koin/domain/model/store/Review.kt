5a46fbded11248bd74a1f90e0fefb8e59b547200
nazero <skdud4081@gmail.com>
2024-08-04 21:48:36 +0900

    Modify review content nullable

--- domain/src/main/java/in/koreatech/koin/domain/model/store/Review.kt
+++ domain/src/main/java/in/koreatech/koin/domain/model/store/Review.kt
@@ -2,7 +2,7 @@ package `in`.koreatech.koin.domain.model.store
 
 data class Review(
     val rating: Int,
-    val content: String,
+    val content: String?,
     val imageUrls: List<String>?,
     val menuNames: List<String>?,
 )
