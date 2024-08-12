acb82995d18130f1d72d3a5d659ac7161714f748
Strone <sangbbang31@gmail.com>
2024-08-12 17:09:26 +0900

    Add toolbar states

--- koin/src/main/java/in/koreatech/koin/ui/article/ArticleActivity.kt
+++ koin/src/main/java/in/koreatech/koin/ui/article/ArticleActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import `in`.koreatech.koin.R
 import `in`.koreatech.koin.core.activity.ActivityBase
+import `in`.koreatech.koin.core.util.dataBinding
+import `in`.koreatech.koin.databinding.ActivityArticleBinding
 
 @AndroidEntryPoint
 class ArticleActivity : ActivityBase() {
 
-    override val screenTitle: String = "공지사항"
+    private val binding by dataBinding<ActivityArticleBinding>()
+    private val navController: NavController by lazy { findNavController(R.id.nav_host_article_fragment) }
+    override val screenTitle: String = applicationContext.getString(R.string.title_article)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,30 @@ class ArticleActivity : ActivityBase() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        navController.addOnDestinationChangedListener { _, dest, _ ->
+            when (dest.id) {
+                R.id.articleListFragment -> setToolbar(ArticleToolbarState.ARTICLE_LIST)
+                R.id.articleDetailFragment -> setToolbar(ArticleToolbarState.ARTICLE_DETAIL)
+                R.id.articleSearchFragment -> setToolbar(ArticleToolbarState.ARTICLE_SEARCH)
+                R.id.articleKeywordFragment -> setToolbar(ArticleToolbarState.ARTICLE_KEYWORD)
+            }
+        }
     }
 
+    private fun setToolbar(state: ArticleToolbarState) {
+        binding.toolbarArticleList.apply {
+            title = getString(state.title)
+            state.menuRes?.let { inflateMenu(it) }
+            setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.action_search_article -> {
+                        navController.navigate(R.id.action_articleListFragment_to_articleSearchFragment)
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+    }
 }
