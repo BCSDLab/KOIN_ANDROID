aff352fc1dc4964387c315fa5488c6e959845968
nazero <skdud4081@gmail.com>
2024-08-22 15:19:35 +0900

    Modify LinedTextField

--- business/src/main/java/in/koreatech/business/feature/textfield/LinedTextField.kt
+++ business/src/main/java/in/koreatech/business/feature/textfield/LinedTextField.kt
@@ -1,36 +1,28 @@
 package `in`.koreatech.business.feature.textfield
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import `in`.koreatech.business.ui.theme.ColorError
 import `in`.koreatech.business.ui.theme.ColorHelper
 import `in`.koreatech.business.ui.theme.ColorPrimary
 import `in`.koreatech.business.ui.theme.ColorSecondary
@@ -63,7 +55,7 @@ fun LinedTextField(
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(41.dp)
+                        .height(46.dp)
                         .border(
                             width = 1.dp,
                             color = if (isError) ColorSecondary else if (focused) ColorPrimary else ColorTextField,
@@ -75,8 +67,8 @@ fun LinedTextField(
                     Box(
                         modifier = Modifier.padding(start = 12.dp),
                     ) {
-                        if(value.isEmpty())
-                            Text(text = label, fontSize = 14.sp, color = ColorHelper, )
+                        if (value.isEmpty())
+                            Text(text = label, fontSize = 14.sp, color = ColorHelper)
                         innerTextField()
                     }
 
@@ -84,12 +76,17 @@ fun LinedTextField(
 
                 Box {
                     Text(
-                        modifier = Modifier,
+                        modifier = Modifier.padding(start = 8.dp),
                         text = helperText,
                         fontSize = 11.sp,
                         color = ColorHelper,
                     )
-                    if (isError) Text(text = errorText, fontSize = 11.sp, color = ColorSecondary)
+                    if (isError) Text(
+                        modifier = Modifier.padding(start = 8.dp),
+                        text = errorText,
+                        fontSize = 11.sp,
+                        color = ColorSecondary
+                    )
 
                 }
 
