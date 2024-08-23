6dca1ca5b50ffe62ceb8b57bc4e5db56ee7c7f84
nazero <skdud4081@gmail.com>
2024-08-23 22:59:18 +0900

    Modify change password ui

--- business/src/main/java/in/koreatech/business/feature_changepassword/changepassword/ChangePasswordScreen.kt
+++ business/src/main/java/in/koreatech/business/feature_changepassword/changepassword/ChangePasswordScreen.kt
@@ -9,15 +9,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -30,6 +33,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewFontScale
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -94,126 +99,117 @@ fun ChangePasswordScreen(
 ) {
 
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 12.dp),
     ) {
-        Column {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 12.dp)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            IconButton(
+                onClick = { onBackPressed() },
+                modifier = Modifier.align(Alignment.CenterStart)
             ) {
-                IconButton(
-                    onClick = { onBackPressed() },
-                    modifier = Modifier.align(Alignment.CenterStart)
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_back),
-                        contentDescription = stringResource(id = R.string.back_icon),
-                    )
-                }
-
-                Text(
-                    text = stringResource(id = R.string.password_find),
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.align(Alignment.Center)
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_back),
+                    contentDescription = stringResource(id = R.string.back_icon),
                 )
             }
 
-            Spacer(modifier = Modifier.height(20.dp))
-
-            Column(
+            Text(
+                text = stringResource(id = R.string.password_find),
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 24.dp, end = 24.dp, top = 32.dp, bottom = 24.dp),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Row(
                 modifier = Modifier
-                    .padding(horizontal = 24.dp),
-                verticalArrangement = Arrangement.Center,
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text(
-                        modifier = Modifier,
-                        color = ColorPrimary,
-                        fontWeight = FontWeight.Bold,
-                        text = stringResource(R.string.change_password_step)
-                    )
-                    Text(
-                        text = stringResource(id = R.string.two_half),
-                        color = ColorPrimary,
-                        fontWeight = FontWeight.Bold,
-                    )
-                }
-
-                Canvas(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                ) {
-
-                    drawLine(
-                        color = ColorPrimary,
-                        start = Offset(0f, 0f),
-                        end = Offset(size.width, size.height),
-                        strokeWidth = 4.dp.toPx(),
-                        cap = StrokeCap.Round
-                    )
-                }
-
-                Spacer(modifier = Modifier.height(25.dp))
-
                 Text(
-                    text = stringResource(R.string.new_password),
-                    color = Color.Black,
-                    fontWeight = FontWeight.Bold,
-                    modifier = modifier.padding(start = 8.dp, bottom = 8.dp),
+                    modifier = Modifier,
+                    color = ColorPrimary,
+                    style = MaterialTheme.typography.h6,
+                    text = stringResource(R.string.change_password_step)
                 )
-
-
-                LinedTextField(
-                    value = password,
-                    onValueChange = onPasswordChange,
-                    isPassword = true,
-                    helperText = stringResource(R.string.password_condition),
-                    label = stringResource(R.string.input_new_password)
-                )
-
-                Spacer(modifier = Modifier.height(20.dp))
-
                 Text(
-                    text = stringResource(R.string.confirm_password),
-                    color = Color.Black,
+                    text = stringResource(id = R.string.two_half),
+                    color = ColorPrimary,
+                    style = MaterialTheme.typography.h6,
+                    letterSpacing = 4.sp,
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Canvas(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+
+                drawLine(
+                    color = ColorPrimary,
+                    start = Offset(0f, 0f),
+                    end = Offset(size.width, size.height),
+                    strokeWidth = 4.dp.toPx(),
+                    cap = StrokeCap.Round
+                )
+            }
+
+            Spacer(modifier = Modifier.height(25.dp))
+            Text(
+                text = stringResource(R.string.new_password),
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                modifier = modifier.padding(start = 8.dp, bottom = 8.dp),
+            )
+            LinedTextField(
+                value = password,
+                onValueChange = onPasswordChange,
+                isPassword = true,
+                helperText = stringResource(R.string.password_condition),
+                label = stringResource(R.string.input_new_password)
+            )
+
+            Text(
+                text = stringResource(R.string.confirm_password),
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                modifier = modifier.padding(start = 8.dp, bottom = 8.dp, top = 4.dp),
+            )
+            LinedTextField(
+                value = passwordChecked,
+                onValueChange = onPasswordCheckedChange,
+                isPassword = true,
+                isError = notCoincidePassword,
+                errorText = stringResource(R.string.password_not_coincide),
+                label = stringResource(R.string.input_new_password_again)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                onClick = onChangePasswordClick,
+                colors = if (fillAllPasswords) ButtonDefaults.buttonColors(ColorPrimary)
+                else ButtonDefaults.buttonColors(Gray5),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.complete),
+                    fontSize = 15.sp,
                     fontWeight = FontWeight.Bold,
-                    modifier = modifier.padding(start = 8.dp, bottom = 8.dp),
+                    color = if (fillAllPasswords) Color.White else Color.Gray
                 )
-
-                LinedTextField(
-                    value = passwordChecked,
-                    onValueChange = onPasswordCheckedChange,
-                    isPassword = true,
-                    isError = notCoincidePassword,
-                    errorText = stringResource(R.string.password_not_coincide),
-                    label = stringResource(R.string.input_new_password_again)
-                )
-
-                Button(
-                    onClick = onChangePasswordClick,
-                    colors = if (fillAllPasswords) ButtonDefaults.buttonColors(ColorPrimary)
-                    else ButtonDefaults.buttonColors(Gray5),
-                    modifier = modifier
-                        .padding(bottom = 80.dp, top = 217.5.dp)
-                        .fillMaxWidth()
-                        .height(44.dp)
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.complete),
-                        fontSize = 15.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = if (fillAllPasswords) Color.White else Color.Gray
-                    )
-                }
             }
         }
     }
+
 }
 
 @Composable
@@ -266,7 +262,9 @@ fun WaringNotCoincidePW(
 
 @Preview
 @Composable
-fun PreviewChangePassswordScreen() {
+@PreviewScreenSizes
+@PreviewFontScale
+fun PreviewChangePasswordScreen() {
     ChangePasswordScreen(
         password = "",
         passwordChecked = "",
