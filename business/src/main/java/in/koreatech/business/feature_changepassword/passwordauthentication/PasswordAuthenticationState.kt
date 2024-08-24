ac3f25c1ddf39c4fa41b3cac4a189fe7197b7de3
nazero <skdud4081@gmail.com>
2024-08-24 18:36:20 +0900

    Add PasswordAuthenticationState

--- business/src/main/java/in/koreatech/business/feature_changepassword/passwordauthentication/PasswordAuthenticationState.kt
+++ business/src/main/java/in/koreatech/business/feature_changepassword/passwordauthentication/PasswordAuthenticationState.kt
@@ -1,10 +1,15 @@
 package `in`.koreatech.business.feature_changepassword.passwordauthentication
 
+import `in`.koreatech.koin.domain.state.business.changepw.ChangePasswordContinuationState
+
 data class PasswordAuthenticationState (
     val authenticationBtnIsClicked: Boolean = false,
     val phoneNumber: String = "",
     val authenticationCode: String = "",
-)
+    val accountContinuationState: ChangePasswordContinuationState=ChangePasswordContinuationState.RequestedSmsValidation,
+    val smsAuthContinuationState: ChangePasswordContinuationState=ChangePasswordContinuationState.RequestedSmsValidation,
+    val authErrorMessage:String?= null,
+    )
 
 fun PasswordAuthenticationState.phoneNumberIsEmpty() = phoneNumber.isEmpty()
 
