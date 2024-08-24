28d51decbbf7a316831fb4cd8b3d527a8c429813
nazero <skdud4081@gmail.com>
2024-08-24 18:42:20 +0900

    Add ChangePasswordState

--- business/src/main/java/in/koreatech/business/feature_changepassword/changepassword/ChangePasswordState.kt
+++ business/src/main/java/in/koreatech/business/feature_changepassword/changepassword/ChangePasswordState.kt
@@ -1,6 +1,9 @@
 package `in`.koreatech.business.feature_changepassword.changepassword
 
 import android.os.Parcelable
+import `in`.koreatech.koin.domain.state.business.changepw.ChangePasswordContinuationState
+import `in`.koreatech.koin.domain.state.business.changepw.ChangePasswordExceptionState
+import `in`.koreatech.koin.domain.state.signup.SignupContinuationState
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -9,8 +12,12 @@ data class ChangePasswordState(
     val passwordChecked: String = "",
     val phoneNumber: String = "",
     val notCoincidePW: Boolean = false,
-    val fillAllPasswords: Boolean = false
-): Parcelable
+    val fillAllPasswords: Boolean = false,
+): Parcelable{
+    val isButtonEnabled: Boolean
+        get() = password.isNotEmpty() && passwordChecked.isNotEmpty() && !notCoincidePW
+
+}
 
 fun ChangePasswordState.passwordIsBlank() = password.isBlank()
 fun ChangePasswordState.passwordCheckedIsBlank() = passwordChecked.isBlank()
