6947d17d43b07eff46613a7db9445cc7d5976e4b
DohyeokKim <hyeokdokim@gmail.com>
2024-08-27 17:46:56 +0900

    Apply nullable field on SignUpRequest

--- domain/src/main/java/in/koreatech/koin/domain/repository/SignupRepository.kt
+++ domain/src/main/java/in/koreatech/koin/domain/repository/SignupRepository.kt
@@ -4,18 +4,18 @@ import `in`.koreatech.koin.domain.model.user.Gender
 import `in`.koreatech.koin.domain.model.user.Graduated
 
 interface SignupRepository {
-    suspend fun getPrivacyTermText() : String
-    suspend fun getKoinTermText() : String
+    suspend fun getPrivacyTermText(): String
+    suspend fun getKoinTermText(): String
 
     suspend fun requestEmailVerification(
-            portalAccount: String,
-            gender: Gender,
-            isGraduated: Graduated,
-            major: String,
-            name:String,
-            nickName: String,
-            password: String,
-            phoneNumber: String,
-            studentNumber: String,
+        portalAccount: String,
+        gender: Gender?,
+        isGraduated: Graduated?,
+        major: String?,
+        name: String?,
+        nickName: String?,
+        password: String,
+        phoneNumber: String?,
+        studentNumber: String?,
     ): Result<Unit>
 }
--- domain/src/main/java/in/koreatech/koin/domain/usecase/signup/SignupRequestEmailVerificationUseCase.kt
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/signup/SignupRequestEmailVerificationUseCase.kt
@@ -4,45 +4,36 @@ import `in`.koreatech.koin.domain.model.user.Gender
 import `in`.koreatech.koin.domain.model.user.Graduated
 import `in`.koreatech.koin.domain.repository.SignupRepository
 import `in`.koreatech.koin.domain.state.signup.SignupContinuationState
-import `in`.koreatech.koin.domain.util.ext.formatPhoneNumber
-import `in`.koreatech.koin.domain.util.ext.isNotValidEmail
-import `in`.koreatech.koin.domain.util.ext.isNotValidPassword
-import `in`.koreatech.koin.domain.util.ext.toSHA256
 import javax.inject.Inject
 
 class SignupRequestEmailVerificationUseCase @Inject constructor(
     private val signupRepository: SignupRepository
 ) {
     suspend operator fun invoke(
-            portalAccount: String,
-            gender: Gender?,
-            isGraduated: Graduated?,
-            major: String,
-            name:String,
-            nickName: String,
-            password: String,
-            phoneNumber: String,
-            studentNumber: String,
-            isCheckNickname: Boolean
+        portalAccount: String,
+        gender: Gender?,
+        isGraduated: Graduated?,
+        major: String?,
+        name: String?,
+        nickName: String?,
+        password: String,
+        phoneNumber: String?,
+        studentNumber: String?,
+        isCheckNickname: Boolean
     ): Result<SignupContinuationState> {
         return when {
-            name.isEmpty() -> Result.success(SignupContinuationState.InitName)
-            !isCheckNickname -> Result.success(SignupContinuationState.CheckNickName)
-            gender == null -> Result.success(SignupContinuationState.CheckGender)
-            phoneNumber.isEmpty() || (phoneNumber.length != 11) -> Result.success(SignupContinuationState.InitPhoneNumber)
-            studentNumber.isEmpty() -> Result.success(SignupContinuationState.InitStudentId)
-            major.isEmpty() -> Result.success(SignupContinuationState.CheckDept)
-            isGraduated == null -> Result.success(SignupContinuationState.CheckGraduate)
+            !nickName.isNullOrEmpty() && !isCheckNickname -> Result.success(SignupContinuationState.CheckNickName)
+            !phoneNumber.isNullOrEmpty() && (phoneNumber.length != 11) -> Result.success(SignupContinuationState.InitPhoneNumber)
             else -> signupRepository.requestEmailVerification(
                 portalAccount = portalAccount,
                 gender = gender,
                 isGraduated = isGraduated,
-                major = major,
-                name = name,
-                nickName = nickName,
+                major = if(major.isNullOrBlank()) null else major,
+                name = if(name.isNullOrBlank()) null else name,
+                nickName = if(nickName.isNullOrBlank()) null else nickName,
                 password = password,
-                phoneNumber = phoneNumber,
-                studentNumber = studentNumber,
+                phoneNumber = if(phoneNumber.isNullOrBlank()) null else phoneNumber,
+                studentNumber = if(studentNumber.isNullOrBlank()) null else studentNumber,
             ).map {
                 SignupContinuationState.RequestedEmailValidation
             }
