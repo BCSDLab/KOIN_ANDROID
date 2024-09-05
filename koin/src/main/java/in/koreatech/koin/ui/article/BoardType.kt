fd090aadfb809a800856fd3131342a3282e98041
Thirfir <sangbbang31@gmail.com>
2024-09-05 10:48:07 +0900

    "코인공지" disable

--- koin/src/main/java/in/koreatech/koin/ui/article/BoardType.kt
+++ koin/src/main/java/in/koreatech/koin/ui/article/BoardType.kt
@@ -16,7 +16,7 @@ enum class BoardType(
     SCHOLARSHIP(6, R.string.article_scholarship, R.string.article_scholarship_simple),
     SCHOOL(7,  R.string.article_school, R.string.article_school_simple),
     RECRUIT(8, R.string.article_recruit, R.string.article_recruit_simple),
-    KOIN(9, R.string.article_koin, R.string.article_koin),
+    // KOIN(9, R.string.article_koin, R.string.article_koin),
     // QNA(10, R.string.article_qna),               // NOT USED
     // PROMOTION(11, R.string.article_promotion),   // NOT USED
     // IPP(10, R.string.article_ipp),                // API 준비중
