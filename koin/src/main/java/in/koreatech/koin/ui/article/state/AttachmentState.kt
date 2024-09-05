d943906e63fe63075255a7a9f10d0129afaf223f
Thirfir <sangbbang31@gmail.com>
2024-09-06 01:43:06 +0900

    파일 크기 추출 추가

--- koin/src/main/java/in/koreatech/koin/ui/article/state/AttachmentState.kt
+++ koin/src/main/java/in/koreatech/koin/ui/article/state/AttachmentState.kt
@@ -3,17 +3,35 @@ package `in`.koreatech.koin.ui.article.state
 import `in`.koreatech.koin.domain.model.article.Attachment
 
 data class AttachmentState(
-    val title: String,
-    val url: String
-) {
-
-    fun toAttachment() = Attachment(
-        name = title,
-        url = url
-    )
-}
+    val name: String,
+    val url: String,
+    val size: String
+)
 
 fun Attachment.toAttachmentState() = AttachmentState(
-    title = name,
-    url = url
+    name = name.removeFileSize(),
+    url = url,
+    size = name.extractFileSize() ?: "-"
 )
+
+/** 파일 크기 추출
+ * 본교 파일 이름은 "수강 신청 안내(129 KB)" 이런 형태임
+ */
+private fun String.extractFileSize(): String? {
+    val regex = Regex("""\d+\s?[KMG]?B""")
+    return regex.findAll(this).lastOrNull()?.value
+}
+
+private fun String.removeFileSize(): String {
+    val regex = Regex("""\d+\s?[KMG]?B""")
+    val match = regex.findAll(this).lastOrNull()?.value
+    return if (match != null) {
+        try {
+            this.removeRange(this.indexOf(match) - 1, this.length)
+        } catch (e: Exception) {
+            this
+        }
+    } else {
+        this
+    }
+}
