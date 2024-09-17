b7c56fb94bd89cc34af8fcdcce948c4b5c4b9330
DohyeokKim <hyeokdokim@gmail.com>
2024-09-17 14:45:11 +0900

    Add TermActivity for koin terms and privacy policy

--- data/src/main/java/in/koreatech/koin/data/repository/SignupRepositoryImpl.kt
+++ data/src/main/java/in/koreatech/koin/data/repository/SignupRepositoryImpl.kt
@@ -3,11 +3,13 @@ package `in`.koreatech.koin.data.repository
 import `in`.koreatech.koin.data.mapper.toBoolean
 import `in`.koreatech.koin.data.mapper.toInt
 import `in`.koreatech.koin.data.mapper.toPhoneNumber
+import `in`.koreatech.koin.data.mapper.toTerm
 import `in`.koreatech.koin.data.request.user.StudentInfoRequest
 import `in`.koreatech.koin.data.source.local.SignupTermsLocalDataSource
 import `in`.koreatech.koin.data.source.remote.UserRemoteDataSource
 import `in`.koreatech.koin.data.util.getErrorResponse
 import `in`.koreatech.koin.domain.error.signup.SignupAlreadySentEmailException
+import `in`.koreatech.koin.domain.model.term.Term
 import `in`.koreatech.koin.domain.model.user.Gender
 import `in`.koreatech.koin.domain.model.user.Graduated
 import `in`.koreatech.koin.domain.repository.SignupRepository
@@ -25,6 +27,14 @@ class SignupRepositoryImpl @Inject constructor(
 
     override suspend fun getKoinTermText(): String {
         return signupTermsLocalDataSource.getKoinTermText()
+    }
+
+    override suspend fun getPrivacyTerm(): Term {
+        return signupTermsLocalDataSource.getPrivacyTerm().toTerm()
+    }
+
+    override suspend fun getKoinTerm(): Term {
+        return signupTermsLocalDataSource.getKoinTerms().toTerm()
     }
 
     override suspend fun requestEmailVerification(
--- data/src/main/java/in/koreatech/koin/data/source/local/SignupTermsLocalDataSource.kt
+++ data/src/main/java/in/koreatech/koin/data/source/local/SignupTermsLocalDataSource.kt
@@ -1,15 +1,13 @@
 package `in`.koreatech.koin.data.source.local
 
 import android.content.Context
+import android.util.Log
 import dagger.hilt.android.qualifiers.ApplicationContext
-import javax.inject.Inject
+import `in`.koreatech.koin.data.entity.term.TermEntity
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.nio.channels.AsynchronousFileChannel
+import kotlinx.serialization.json.Json
+import javax.inject.Inject
 
 class SignupTermsLocalDataSource @Inject constructor(
     @ApplicationContext private val applicationContext: Context
@@ -26,8 +24,22 @@ class SignupTermsLocalDataSource @Inject constructor(
         }
     }
 
+    suspend fun getPrivacyTerm(): TermEntity = withContext(Dispatchers.IO) {
+        applicationContext.assets.open(PRIVACY_TERMS_JSON_FILE_NAME).bufferedReader().use { it.readText() }.let {
+            Json.decodeFromString<TermEntity>(it)
+        }
+    }
+
+    suspend fun getKoinTerms(): TermEntity = withContext(Dispatchers.IO) {
+        applicationContext.assets.open(KOIN_TERMS_JSON_FILE_NAME).bufferedReader().use { it.readText() }.let {
+            Json.decodeFromString<TermEntity>(it)
+        }
+    }
+
     companion object {
         const val PRIVACY_TERMS_TEXT_FILE_NAME = "Terms_personal_information.txt"
+        const val PRIVACY_TERMS_JSON_FILE_NAME = "Terms_personal_information.json"
         const val KOIN_TERMS_TEXT_FILE_NAME = "Terms_koin_sign_up.txt"
+        const val KOIN_TERMS_JSON_FILE_NAME = "Terms_koin_sign_up.json"
     }
 }
