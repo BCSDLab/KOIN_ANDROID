74dcfddf970183a63d820bab2089a4526591dda5
hsgo2430 <hsgo2430@naver.com>
2024-09-29 12:52:18 +0900

    2차 코멘드 적용(뷰모델 분리)

--- business/src/main/java/in/koreatech/business/main/BusinessMainActivity.kt
+++ business/src/main/java/in/koreatech/business/main/BusinessMainActivity.kt
@@ -1,9 +1,10 @@
-package `in`.koreatech.business
+package `in`.koreatech.business.main
 
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -14,71 +15,57 @@ import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import `in`.koreatech.business.navigation.KoinBusinessNavHost
 import `in`.koreatech.business.navigation.MYSTORESCREEN
-import `in`.koreatech.business.navigation.REGISTERMENUSCREEN
 import `in`.koreatech.business.navigation.REGISTERSTORESCREEN
 import `in`.koreatech.business.navigation.SIGNINSCREEN
 import `in`.koreatech.business.ui.theme.KOIN_ANDROIDTheme
 import `in`.koreatech.koin.domain.repository.OwnerShopRepository
 import `in`.koreatech.koin.domain.repository.TokenRepository
 import `in`.koreatech.koin.domain.repository.UserRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class BusinessMainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var userRepository: UserRepository
-
-    @Inject
-    lateinit var ownerShopRepository: OwnerShopRepository
-
     lateinit var destination: String
 
+    private val viewModel by viewModels<BusinessMainActivityViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        checkingToken()
+        initViewModel()
         setContent {
-            KOIN_ANDROIDTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    KoinBusinessNavHost(
-                        startDestination = destination
-                    )
+                KOIN_ANDROIDTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colors.background
+                    ) {
+                        KoinBusinessNavHost(
+                            startDestination = destination
+                        )
+                    }
                 }
             }
-        }
     }
-
-    private fun checkingToken(){
-        destination = if(userRepository.ownerTokenIsValid()){
-            if(ownerShopRepository.getOwnerStoreSize()){
-                REGISTERSTORESCREEN
-            } else{
-                MYSTORESCREEN
-            }
-        } else SIGNINSCREEN
+    private fun initViewModel() = with(viewModel) {
+        destinationString.observe(this@BusinessMainActivity
+        ) {
+            destination = destinationString.value.toString()
+        }
     }
 }
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+Text(
+    text = "Hello $name!",
+    modifier = modifier
+)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    KOIN_ANDROIDTheme {
-        Greeting("Android")
-    }
+KOIN_ANDROIDTheme {
+    Greeting("Android")
+}
 }
