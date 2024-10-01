5c6561875c6cbc2f72323cd4751fe40f19f1b056
Jokwanhee <jonotch1@naver.com>
2024-10-01 16:07:45 +0900

    [#387] 로깅 연결

--- koin/src/main/java/in/koreatech/koin/ui/forceupdate/ForceUpdateDialog.kt
+++ koin/src/main/java/in/koreatech/koin/ui/forceupdate/ForceUpdateDialog.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import `in`.koreatech.koin.R
 import `in`.koreatech.koin.core.activity.BaseDialogFragment
+import `in`.koreatech.koin.core.analytics.EventLogger
 import `in`.koreatech.koin.databinding.DialogForceUpdateBinding
 import `in`.koreatech.koin.util.ext.navigateToPlayStore
 
@@ -12,6 +13,8 @@ class ForceUpdateDialog : BaseDialogFragment<DialogForceUpdateBinding>(
     DialogForceUpdateBinding::inflate
 ) {
     companion object {
+        private const val action = "force_update"
+        private const val clickCategory = "click"
         const val TAG = "ForceUpdateDialog"
 
         fun newInstance(): ForceUpdateDialog {
@@ -36,9 +39,21 @@ class ForceUpdateDialog : BaseDialogFragment<DialogForceUpdateBinding>(
 
     private fun initEvent() {
         binding.btnConfirm.setOnClickListener {
+            EventLogger.logCustomEvent(
+                action = action,
+                category = clickCategory,
+                label = "already_update_popup",
+                value = "닫기"
+            )
             dismiss()
         }
         binding.btnStore.setOnClickListener {
+            EventLogger.logCustomEvent(
+                action = action,
+                category = clickCategory,
+                label = "already_update_popup",
+                value = "스토어로_가기"
+            )
             requireContext().navigateToPlayStore()
         }
     }
