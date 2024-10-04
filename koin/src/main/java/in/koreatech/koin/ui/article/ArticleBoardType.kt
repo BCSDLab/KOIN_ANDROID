4d38857e9652f780c7ce6c0b95edf0ae796a8380
Thirfir <sangbbang31@gmail.com>
2024-10-05 00:09:30 +0900

    코인 탭 순서 변경

--- koin/src/main/java/in/koreatech/koin/ui/article/ArticleBoardType.kt
+++ koin/src/main/java/in/koreatech/koin/ui/article/ArticleBoardType.kt
@@ -15,11 +15,11 @@ enum class ArticleBoardType(
     ALL(4, R.string.article_all, R.string.article_all),
     NORMAL(5, R.string.article_normal, R.string.article_normal_simple),
     SCHOLARSHIP(6, R.string.article_scholarship, R.string.article_scholarship_simple),
-    SCHOOL(7,  R.string.article_school, R.string.article_school_simple),
+    SCHOOL(7, R.string.article_school, R.string.article_school_simple),
     RECRUIT(8, R.string.article_recruit, R.string.article_recruit_simple),
-    KOIN(9, R.string.article_koin, R.string.article_koin, false),
     // QNA(10, R.string.article_qna),               // NOT USED
     // PROMOTION(11, R.string.article_promotion),   // NOT USED
-     IPP(12, R.string.article_ipp, R.string.article_ipp_simple),
-     STUDENT(13, R.string.article_student, R.string.article_student_simple, false),
+    IPP(12, R.string.article_ipp, R.string.article_ipp_simple),
+    STUDENT(13, R.string.article_student, R.string.article_student_simple, false),
+    KOIN(9, R.string.article_koin, R.string.article_koin, false),
 }
