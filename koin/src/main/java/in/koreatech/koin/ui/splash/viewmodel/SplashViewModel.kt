874850f228284a7bd7558bacf926992c687421c6
DohyeokKim <hyeokdokim@gmail.com>
2024-10-07 23:15:04 +0900

    [#405] 버전 저장 로직 연결

--- koin/src/main/java/in/koreatech/koin/ui/splash/viewmodel/SplashViewModel.kt
+++ koin/src/main/java/in/koreatech/koin/ui/splash/viewmodel/SplashViewModel.kt
@@ -1,5 +1,10 @@
 package `in`.koreatech.koin.ui.splash.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import `in`.koreatech.koin.core.viewmodel.BaseViewModel
 import `in`.koreatech.koin.core.viewmodel.SingleLiveEvent
 import `in`.koreatech.koin.domain.model.version.Version
@@ -7,20 +12,16 @@ import `in`.koreatech.koin.domain.state.version.VersionUpdatePriority
 import `in`.koreatech.koin.domain.usecase.token.IsTokenSavedInDeviceUseCase
 import `in`.koreatech.koin.domain.usecase.user.GetUserInfoUseCase
 import `in`.koreatech.koin.domain.usecase.version.GetVersionInformationUseCase
-import `in`.koreatech.koin.ui.navigation.state.UserState
+import `in`.koreatech.koin.domain.usecase.version.UpdateLatestVersionUseCase
 import `in`.koreatech.koin.ui.splash.state.TokenState
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
-import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-import kotlinx.coroutines.launch
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
     private val getVersionInformationUseCase: GetVersionInformationUseCase,
     private val getUserInfoUseCase: GetUserInfoUseCase,
-    private val isTokenSavedInDeviceUseCase: IsTokenSavedInDeviceUseCase
+    private val isTokenSavedInDeviceUseCase: IsTokenSavedInDeviceUseCase,
+    private val updateLatestVersionUseCase: UpdateLatestVersionUseCase
 ) : BaseViewModel() {
 
     private val _version = MutableLiveData<Version>()
@@ -30,22 +31,22 @@ class SplashViewModel @Inject constructor(
     val checkVersionError: LiveData<Throwable> get() = _checkVersionError
 
     private val _tokenState = SingleLiveEvent<TokenState>()
-    val tokenState : LiveData<TokenState> get() = _tokenState
+    val tokenState: LiveData<TokenState> get() = _tokenState
 
     fun checkUpdate() {
         viewModelScope.launchIgnoreCancellation {
-                getVersionInformationUseCase()
-                    .onSuccess {
-                        _version.value = it
-                        if (!(it.versionUpdatePriority is VersionUpdatePriority.High ||
-                                    it.versionUpdatePriority is VersionUpdatePriority.Medium)
-                        ) {
-                            checkToken()
-                        }
-                    }.onFailure {
-                        _checkVersionError.value = it
+            getVersionInformationUseCase()
+                .onSuccess {
+                    _version.value = it
+                    if (!(it.versionUpdatePriority is VersionUpdatePriority.High ||
+                                it.versionUpdatePriority is VersionUpdatePriority.Medium)
+                    ) {
                         checkToken()
                     }
+                }.onFailure {
+                    _checkVersionError.value = it
+                    checkToken()
+                }
 
         }
     }
@@ -67,6 +68,11 @@ class SplashViewModel @Inject constructor(
     }
 
     fun updateLatestVersionName(versionCode: Int) {
-        //
+        viewModelScope.launchIgnoreCancellation {
+            updateLatestVersionUseCase(versionCode)
+                .onFailure {
+                    Log.d("SplashViewModel", "Fail to update latest version: $versionCode")
+                }
+        }
     }
 }
