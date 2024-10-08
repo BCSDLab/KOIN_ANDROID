0f4e71eafdb9f1cba4f42c3689d36b1284fa4c2b
DohyeokKim <hyeokdokim@gmail.com>
2024-10-08 16:39:02 +0900

    [#405] 최신버전 로딩 실패시 대응 로직 추가

--- domain/src/main/java/in/koreatech/koin/domain/usecase/version/UpdateLatestVersionUseCase.kt
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/version/UpdateLatestVersionUseCase.kt
@@ -13,10 +13,16 @@ class UpdateLatestVersionUseCase @Inject constructor(
      *  m: Minor
      *  p: Path
      *
+     *  @throws IllegalArgumentException 버전 코드가 5자리가 아닌 경우
+     *  @throws IllegalArgumentException 버전 코드가 기존 코드 보다 작은 경우
      *  @since 2024-10-07
      */
     suspend operator fun invoke(versionCode: Int) = runCatching {
         if (versionCode > 99999 || versionCode < 10000) throw IllegalArgumentException("VersionCode must be in 10000 to 99999| input: $versionCode")
+
+        versionRepository.getLatestVersionCode()?.let { latestVersionCode ->
+            if (versionCode < latestVersionCode) throw IllegalArgumentException("VersionCode must be higher than before VersionCode| input: $versionCode, origin: ${latestVersionCode}")
+        }
 
         var vc = versionCode
 
