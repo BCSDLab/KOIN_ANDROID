acb0222095b37445077a2535180e7f2b4ece98ac
Jokwanhee <jonotch1@naver.com>
2024-10-15 22:41:19 +0900

    Update v4.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
     }
 
     extra.apply {
-        set("versionName", "3.7.0")
-        set("versionCode", 30700)
+        set("versionName", "4.0.0")
+        set("versionCode", 40000)
     }
 
     dependencies {
