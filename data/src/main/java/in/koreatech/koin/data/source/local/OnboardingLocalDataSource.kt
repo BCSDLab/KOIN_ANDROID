7f559ad6fa426d8964404406f22fb76a21917383
Thirfir <sangbbang31@gmail.com>
2024-10-23 03:02:21 +0900

    refactor: DataStore DI 로직 변경

--- data/src/main/java/in/koreatech/koin/data/source/local/OnboardingLocalDataSource.kt
+++ data/src/main/java/in/koreatech/koin/data/source/local/OnboardingLocalDataSource.kt
@@ -1,27 +1,18 @@
 package `in`.koreatech.koin.data.source.local
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
-import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
-import java.io.IOException
 import javax.inject.Inject
 
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
-    name = "koin_data_store"
-)
-
 class OnboardingLocalDataSource @Inject constructor(
-    context: Context
+    private val dataStore: DataStore<Preferences>
 ) {
-
-    private val dataStore = context.dataStore
 
     suspend fun getShouldShowTooltip(onboardingType: String): Boolean {
         return dataStore.data.catch {
