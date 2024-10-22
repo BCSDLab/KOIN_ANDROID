535d4c6e5a55a8b63889fd0474ca836c3016f7f1
Thirfir <sangbbang31@gmail.com>
2024-10-23 02:59:55 +0900

    refactor: 온보딩 바텀시트 액션 모듈화 이용

--- core/onboarding/src/main/java/in/koreatech/koin/core/onboarding/OnboardingType.kt
+++ core/onboarding/src/main/java/in/koreatech/koin/core/onboarding/OnboardingType.kt
@@ -2,9 +2,14 @@ package `in`.koreatech.koin.core.onboarding
 
 import androidx.annotation.StringRes
 
+/**
+ * OnboardingType
+ * @property descriptionResId 툴팁 내용 리소스 아이디. 툴팁이 아닐 경우 0 할당
+ */
 enum class OnboardingType(
     @StringRes val descriptionResId: Int,
 ) {
     DINING_IMAGE(R.string.dining_image_tooltip),
+    DINING_NOTIFICATION(0),
     ARTICLE_KEYWORD(R.string.article_keyword_tooltip)
 }
