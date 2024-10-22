d864ea543c7f824f5e10b4734e8eff075a8e2ca6
Thirfir <sangbbang31@gmail.com>
2024-10-23 03:07:05 +0900

    fix: 함수명 포괄적으로 변경

--- domain/src/main/java/in/koreatech/koin/domain/repository/OnboardingRepository.kt
+++ domain/src/main/java/in/koreatech/koin/domain/repository/OnboardingRepository.kt
@@ -1,6 +1,6 @@
 package `in`.koreatech.koin.domain.repository
 
 interface OnboardingRepository {
-    suspend fun getShouldShowTooltip(onboardingType: String): Boolean
-    suspend fun updateShouldShowTooltip(onboardingType: String, shouldShow: Boolean)
+    suspend fun getShouldOnboarding(onboardingType: String): Boolean
+    suspend fun updateShouldOnboarding(onboardingType: String, shouldShow: Boolean)
 }
