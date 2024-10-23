70aa466f1729b48f62f1cd3fe07f3e0f3f204c7f
Jokwanhee <jonotch1@naver.com>
2024-10-23 09:45:53 +0900

    [add] 시간표 feature 모듈 생성 후 패키지 구성

--- koin/build.gradle.kts
+++ koin/build.gradle.kts
@@ -90,6 +90,7 @@ dependencies {
     implementation(project(":core:navigation"))
     implementation(project(":data"))
     implementation(project(":domain"))
+    implementation(project(":feature:timetable"))
 
     implementation(libs.guava)
 
