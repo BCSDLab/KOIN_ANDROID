7664f44a33b30ef6105f96a69a23b1732cba7e9b
Jokwanhee <jonotch1@naver.com>
2024-10-23 09:45:53 +0900

    [add] 시간표 feature 모듈 생성 후 패키지 구성

---
+++ koin/src/main/java/in/koreatech/koin/ui/timetablev2/TimetableActivity.kt
@@ -0,0 +1,35 @@
+package `in`.koreatech.koin.ui.timetablev2
+
+import android.os.Bundle
+import androidx.compose.material.MaterialTheme
+import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
+import `in`.koreatech.koin.databinding.ActivityTimetableBinding
+import `in`.koreatech.koin.feature.timetable.view.TimetableScreen
+import `in`.koreatech.koin.ui.navigation.KoinNavigationDrawerActivity
+import `in`.koreatech.koin.ui.navigation.state.MenuState
+
+class TimetableActivity : KoinNavigationDrawerActivity() {
+    override val screenTitle: String
+        get() = SCREEN_TITLE
+    override val menuState: MenuState
+        get() = MenuState.Timetable
+
+    private lateinit var binding: ActivityTimetableBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTimetableBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.composeView.setContent {
+            MaterialTheme {
+                TimetableScreen()
+            }
+        }
+    }
+
+    companion object {
+        private const val SCREEN_TITLE = "시간표"
+    }
+}
