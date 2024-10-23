b3511fbdeaaa341a0ee142741833e9a7e4051020
Thirfir <sangbbang31@gmail.com>
2024-10-23 12:05:34 +0900

    add: 식단 공유 툴팁

--- koin/src/main/java/in/koreatech/koin/ui/dining/adapter/DiningAdapter.kt
+++ koin/src/main/java/in/koreatech/koin/ui/dining/adapter/DiningAdapter.kt
@@ -6,6 +6,10 @@ import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.doOnAttach
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.findViewTreeLifecycleOwner
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -19,6 +23,9 @@ import `in`.koreatech.koin.core.analytics.EventAction
 import `in`.koreatech.koin.core.analytics.EventLogger
 import `in`.koreatech.koin.core.constant.AnalyticsConstant
 import `in`.koreatech.koin.core.dialog.ImageZoomableDialog
+import `in`.koreatech.koin.core.onboarding.ArrowDirection
+import `in`.koreatech.koin.core.onboarding.OnboardingManager
+import `in`.koreatech.koin.core.onboarding.OnboardingType
 import `in`.koreatech.koin.databinding.ItemDiningBinding
 import `in`.koreatech.koin.domain.constant.BREAKFAST
 import `in`.koreatech.koin.domain.model.dining.Dining
@@ -28,6 +35,7 @@ import `in`.koreatech.koin.domain.util.TimeUtil
 
 class DiningAdapter(
     private val onShareClick: (Dining) -> Unit,
+    private val onboardingManager: OnboardingManager
 ) : ListAdapter<Dining, RecyclerView.ViewHolder>(diffCallback) {
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
@@ -35,10 +43,25 @@ class DiningAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return DiningViewHolder(ItemDiningBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return DiningViewHolder(
+            ItemDiningBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
-    inner class DiningViewHolder(private val binding: ItemDiningBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class DiningViewHolder(private val binding: ItemDiningBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            itemView.doOnAttach {
+                if (bindingAdapterPosition == 0) {
+                    it.findViewTreeLifecycleOwner()?.registerViewHolderLifecycleObserver()
+                }
+            }
+        }
 
         fun bind(dining: Dining) {
             with(binding) {
@@ -49,7 +72,7 @@ class DiningAdapter(
                 setEmptyDataVisibility(dining)
                 initShareAction(dining)
 
-                if(dining.imageUrl.isNotEmpty()) {
+                if (dining.imageUrl.isNotEmpty()) {
                     lottieImageLoading.visibility = View.VISIBLE
                     cardViewDining.strokeWidth = 0
                     textViewNoPhoto.visibility = View.INVISIBLE
@@ -94,8 +117,12 @@ class DiningAdapter(
                     }
                 } else {
                     cardViewDining.strokeWidth =
-                        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1f, context.resources.displayMetrics).toInt()
-                    textViewNoPhoto.text = if(TimeUtil.isWeekend(dining.date)) context.getString(
+                        TypedValue.applyDimension(
+                            TypedValue.COMPLEX_UNIT_DIP,
+                            1f,
+                            context.resources.displayMetrics
+                        ).toInt()
+                    textViewNoPhoto.text = if (TimeUtil.isWeekend(dining.date)) context.getString(
                         R.string.photo_not_provided_on_weekend
                     ) else context.getString(R.string.no_photo)
 
@@ -111,13 +138,13 @@ class DiningAdapter(
                     }
                 }
 
-                if(dining.changedAt.isNotEmpty()) {
+                if (dining.changedAt.isNotEmpty()) {
                     textViewDiningChanged.visibility = View.VISIBLE
                 } else {
                     textViewDiningChanged.visibility = View.INVISIBLE
                 }
 
-                if(dining.soldOutAt.isNotEmpty()) {
+                if (dining.soldOutAt.isNotEmpty()) {
                     groupSoldOut.visibility = View.VISIBLE
                     textViewDiningSoldOut.visibility = View.VISIBLE
                     textViewDiningChanged.visibility = View.INVISIBLE
@@ -126,6 +153,21 @@ class DiningAdapter(
                     textViewDiningSoldOut.visibility = View.INVISIBLE
                 }
             }
+        }
+        
+        private fun LifecycleOwner.registerViewHolderLifecycleObserver() {
+            lifecycle.addObserver(object : DefaultLifecycleObserver {
+                override fun onResume(owner: LifecycleOwner) {
+                    with(onboardingManager) {
+                        owner.showOnboardingTooltipIfNeeded(
+                            type = OnboardingType.DINING_SHARE,
+                            view = binding.textViewShare,
+                            arrowDirection = ArrowDirection.TOP
+                        )
+                    }
+                    super.onResume(owner)
+                }
+            })
         }
 
         private fun setDiningCard(context: Context, dining: Dining) {
@@ -154,14 +196,16 @@ class DiningAdapter(
         }
 
         private fun setDiningImageVisibility(context: Context, dining: Dining) {
-            when(dining.place) {
+            when (dining.place) {
                 context.getString(R.string.dining_nungsu),
-                context.getString(R.string.dining_2campus) -> binding.cardViewDining.visibility = View.GONE
+                context.getString(R.string.dining_2campus) -> binding.cardViewDining.visibility =
+                    View.GONE
+
                 else -> binding.cardViewDining.visibility = View.VISIBLE
             }
         }
 
-        private fun showDiningImage(context: Context, dining: Dining){
+        private fun showDiningImage(context: Context, dining: Dining) {
             with(binding) {
                 cardViewDining.visibility = View.VISIBLE
                 cardViewDining.strokeWidth = 0
@@ -188,10 +232,14 @@ class DiningAdapter(
         }
 
         private fun showEmptyDiningImage(context: Context, dining: Dining) {
-            with (binding) {
+            with(binding) {
                 cardViewDining.visibility = View.VISIBLE
                 cardViewDining.strokeWidth =
-                    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1f, context.resources.displayMetrics).toInt()
+                    TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        1f,
+                        context.resources.displayMetrics
+                    ).toInt()
                 textViewNoPhoto.visibility = View.VISIBLE
                 imageViewNoPhoto.visibility = View.VISIBLE
                 imageViewDining.visibility = View.INVISIBLE
@@ -213,23 +261,24 @@ class DiningAdapter(
                 dividerSlash.visibility = View.VISIBLE
                 textViewCardPrice.visibility = View.VISIBLE
 
-                if(dining.kcal.isEmpty() || dining.kcal == "0") {
+                if (dining.kcal.isEmpty() || dining.kcal == "0") {
                     textViewKcal.visibility = View.GONE
                     dividerDot.visibility = View.GONE
                 }
-                if(dining.priceCash.isEmpty() || dining.priceCash == "0") {
+                if (dining.priceCash.isEmpty() || dining.priceCash == "0") {
                     textViewCashPrice.visibility = View.GONE
                     dividerSlash.visibility = View.GONE
                 }
-                if(dining.priceCard.isEmpty() || dining.priceCard == "0") {
+                if (dining.priceCard.isEmpty() || dining.priceCard == "0") {
                     textViewCardPrice.visibility = View.GONE
                     dividerSlash.visibility = View.GONE
                 }
-                if((dining.priceCard.isEmpty() || dining.priceCard == "0") && (dining.priceCash.isEmpty() || dining.priceCash == "0")) {
+                if ((dining.priceCard.isEmpty() || dining.priceCard == "0") && (dining.priceCash.isEmpty() || dining.priceCash == "0")) {
                     dividerDot.visibility = View.GONE
                 }
             }
         }
+
         private fun setDiningData(context: Context, dining: Dining) {
             with(binding) {
                 textViewDiningCorner.text = dining.place
@@ -239,8 +288,10 @@ class DiningAdapter(
                     context.getString(R.string.price, dining.priceCash)
                 textViewCardPrice.text =
                     context.getString(R.string.price, dining.priceCard)
-                textViewDiningMenuItems1.text = dining.menu.subList(0, dining.menu.size / 2).joinToString("\n")
-                textViewDiningMenuItems2.text = dining.menu.subList(dining.menu.size / 2, dining.menu.size).joinToString("\n")
+                textViewDiningMenuItems1.text =
+                    dining.menu.subList(0, dining.menu.size / 2).joinToString("\n")
+                textViewDiningMenuItems2.text =
+                    dining.menu.subList(dining.menu.size / 2, dining.menu.size).joinToString("\n")
             }
         }
     }
