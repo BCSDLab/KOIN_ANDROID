f421d524c577a7a62cfefb8fa03650d8aef77520
Jokwanhee <jonotch1@naver.com>
2024-10-25 12:27:29 +0900

    [add] 요청 쿼리 data class

---
+++ domain/src/main/java/in/koreatech/koin/domain/model/timetable/request/TimetableLecturesUpdateQuery.kt
@@ -0,0 +1,16 @@
+package `in`.koreatech.koin.domain.model.timetable.request
+
+data class TimetableLecturesUpdateQueryRequest(
+    val timetableFrameId: Int,
+    val timetableLecture: List<TimetableLectureUpdateQueryRequest>,
+)
+
+data class TimetableLectureUpdateQueryRequest(
+    val lectureId: Int,
+    val classTitle: String,
+    val classTime: List<Int>,
+    val classPlace: String,
+    val professor: String,
+    val grades: String,
+    val memo: String,
+)
