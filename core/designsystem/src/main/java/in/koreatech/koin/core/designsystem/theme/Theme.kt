313226b53bc84eb9a6c4dc8e0bd9f421e37bf73f
DohyeokKim <hyeokdokim@gmail.com>
2024-10-25 20:33:23 +0900

    [#433] 색상 팔레트 적용

--- core/designsystem/src/main/java/in/koreatech/koin/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/in/koreatech/koin/core/designsystem/theme/Theme.kt
@@ -1,2 +1,57 @@
 package `in`.koreatech.koin.core.designsystem.theme
 
+import android.os.Build
+import androidx.annotation.ChecksSdkIntAtLeast
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
+
+val LocalKoinColorPalette = staticCompositionLocalOf {
+    KoinColorPalette()
+}
+
+val KoinLightColorScheme = lightColors()
+
+// 다크 테마 대응시 수정
+val KoinDarkColorScheme = lightColors()
+
+@Composable
+fun KoinTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicTheme: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val extendedColors =
+        if (darkTheme)
+            KoinDarkColorPalette
+        else
+            KoinLightColorPalette
+
+    val colorScheme = when {
+        dynamicTheme && isSupportDynamicTheme() -> {
+            if (darkTheme) KoinDarkColorScheme else KoinLightColorScheme
+        }
+
+        darkTheme -> KoinDarkColorScheme
+        else -> KoinLightColorScheme
+    }
+
+    CompositionLocalProvider(LocalKoinColorPalette provides extendedColors) {
+        MaterialTheme(
+            colors = colorScheme,
+            content = content
+        )
+    }
+}
+
+object KoinTheme {
+    val colors: KoinColorPalette
+        @Composable
+        get() = LocalKoinColorPalette.current
+}
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
+fun isSupportDynamicTheme(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
