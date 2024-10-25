26c01775b2b5a00a0ae875b5d1e709dacc620394
Jokwanhee <jonotch1@naver.com>
2024-10-25 12:28:23 +0900

    [del] 필요없는 코드

--- feature/timetable/src/main/java/in/koreatech/koin/feature/timetable/component/ExampleTimetableComponent.kt
+++ feature/timetable/src/main/java/in/koreatech/koin/feature/timetable/component/ExampleTimetableComponent.kt
@@ -1,25 +1,16 @@
 package `in`.koreatech.koin.feature.timetable.component
 
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import `in`.koreatech.koin.core.designsystem.theme.KoinPreview
-import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 
 @Composable
 fun ExampleTimetableComponent(
     modifier: Modifier = Modifier
 ) {
-        MaterialTheme {
-    KoinTheme {
-        KoinTheme.colors.danger100
-
-        }
-    }
 }
 
-@KoinPreview
+@Preview
 @Composable
 private fun ExampleTimetableComponentPreview() {
     ExampleTimetableComponent()
