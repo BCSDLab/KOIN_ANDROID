7fbb24a2607c2a64c44cc6b9c0f8aa2982d4699d
DohyeokKim <hyeokdokim@gmail.com>
2024-10-26 21:19:55 +0900

    [#433] timetable 모듈 m3 적용

--- feature/timetable/build.gradle.kts
+++ feature/timetable/build.gradle.kts
@@ -25,7 +25,8 @@ dependencies {
     implementation(libs.material)
 
     implementation(platform(libs.compose.bom))
-    implementation(libs.bundles.compose)
+    implementation(libs.bundles.compose.m3)
+
     debugImplementation(libs.bundles.compose.debug.test)
     androidTestImplementation(libs.compose.ui.test.manifest)
 }
