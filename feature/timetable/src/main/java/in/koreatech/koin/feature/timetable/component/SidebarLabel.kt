9086bc4dbaa8083d49583a253740d23532d76a46
Jokwanhee <jonotch1@naver.com>
2024-10-26 23:41:59 +0900

    [fix] materia3 컴파일 에러 해결

--- feature/timetable/src/main/java/in/koreatech/koin/feature/timetable/component/SidebarLabel.kt
+++ feature/timetable/src/main/java/in/koreatech/koin/feature/timetable/component/SidebarLabel.kt
@@ -2,7 +2,7 @@ package `in`.koreatech.koin.feature.timetable.component
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
