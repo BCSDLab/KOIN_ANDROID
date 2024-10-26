ed800d4f706ab2787a309e61a7b0ac5a66f3973c
DohyeokKim <hyeokdokim@gmail.com>
2024-10-26 18:56:33 +0900

    [#433] M3 테마로 변경

--- core/designsystem/src/main/java/in/koreatech/koin/core/designsystem/theme/Shape.kt
+++ core/designsystem/src/main/java/in/koreatech/koin/core/designsystem/theme/Shape.kt
@@ -1,11 +1,13 @@
 package `in`.koreatech.koin.core.designsystem.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
+import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 internal val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(5.dp),
-    large = RoundedCornerShape(6.dp)
+    extraSmall = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(12.dp),
+    large = RoundedCornerShape(14.dp),
+    extraLarge = RoundedCornerShape(18.dp)
 )
