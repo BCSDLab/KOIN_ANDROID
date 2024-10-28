ab1c46f7e677d4cb4fb5a927112f93bd54866086
Jokwanhee <jonotch1@naver.com>
2024-10-28 14:28:53 +0900

    [add] TimetableScreen 추가 및 액티비티 UI 연결

--- koin/src/main/java/in/koreatech/koin/ui/timetablev2/TimetableActivity.kt
+++ koin/src/main/java/in/koreatech/koin/ui/timetablev2/TimetableActivity.kt
@@ -1,11 +1,28 @@
 package `in`.koreatech.koin.ui.timetablev2
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.compose.material.BottomSheetValue
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material.rememberBottomSheetState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import `in`.koreatech.koin.R
+import `in`.koreatech.koin.core.analytics.EventAction
+import `in`.koreatech.koin.core.analytics.EventLogger
+import `in`.koreatech.koin.core.appbar.AppBarBase
+import `in`.koreatech.koin.core.constant.AnalyticsConstant
 import `in`.koreatech.koin.databinding.ActivityTimetableBinding
+import `in`.koreatech.koin.feature.timetable.section.TimetableBottomSheet
 import `in`.koreatech.koin.feature.timetable.view.TimetableScreen
 import `in`.koreatech.koin.ui.navigation.KoinNavigationDrawerActivity
 import `in`.koreatech.koin.ui.navigation.state.MenuState
+import kotlinx.coroutines.launch
 
 class TimetableActivity : KoinNavigationDrawerActivity() {
     override val screenTitle: String
@@ -20,10 +37,57 @@ class TimetableActivity : KoinNavigationDrawerActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityTimetableBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initView()
+        initEvent()
+    }
 
+    private fun initView() {
+        initComposeView()
+    }
+
+    @OptIn(ExperimentalMaterialApi::class)
+    private fun initComposeView() {
         binding.composeView.setContent {
+            var searchText by remember { mutableStateOf("") }
+
+            val sheetState = rememberBottomSheetState(
+                initialValue = BottomSheetValue.Collapsed
+            )
+            val scaffoldState = rememberBottomSheetScaffoldState(
+                bottomSheetState = sheetState
+            )
+            val scope = rememberCoroutineScope()
+
+            setAppbarEvent {
+                scope.launch {
+                    if (sheetState.isExpanded) {
+                        sheetState.collapse()
+                    } else {
+                        sheetState.expand()
+                    }
+                }
+            }
+
             MaterialTheme {
-                TimetableScreen()
+                TimetableScreen(
+                    searchText = searchText,
+                    sheetState = sheetState,
+                    scaffoldState = scaffoldState,
+                    onSearchTextChange = { searchText = it }
+                )
+            }
+        }
+    }
+
+    private fun initEvent() {
+        setAppbarEvent()
+    }
+
+    private fun setAppbarEvent(rightButtonClickable: () -> Unit = {}) {
+        binding.koinBaseAppbar.setOnClickListener {
+            when (it.id) {
+                AppBarBase.getLeftButtonId() -> onBackPressed()
+                AppBarBase.getRightButtonId() -> rightButtonClickable()
             }
         }
     }
