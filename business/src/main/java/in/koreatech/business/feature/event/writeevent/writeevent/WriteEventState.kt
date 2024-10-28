27363a15eacedd571249339bbfb6f026eec98e79
hsgo2430 <hsgo2430@naver.com>
2024-10-28 22:24:16 +0900

    feature: 이벤트 등록 UI 생성

---
+++ business/src/main/java/in/koreatech/business/feature/event/writeevent/writeevent/WriteEventState.kt
@@ -0,0 +1,18 @@
+package `in`.koreatech.business.feature.event.writeevent.writeevent
+
+import android.net.Uri
+
+data class WriteEventState(
+    val title: String = "",
+    val content: String = "",
+    val startYear: String = "",
+    val startMonth: String = "",
+    val startDay: String = "",
+    val endYear: String = "",
+    val endMonth: String = "",
+    val endDay: String = "",
+    val images: List<Uri> = emptyList(),
+    val showTitleInputAlert: Boolean = false,
+    val showContentInputAlert: Boolean = false,
+    val showDateInputAlert: Boolean = false,
+)
