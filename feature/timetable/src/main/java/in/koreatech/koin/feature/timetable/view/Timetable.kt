72f32425bf5e88c2cfc8de6583ca217a5cb107cc
Jokwanhee <jonotch1@naver.com>
2024-10-30 12:40:52 +0900

    [fix] Timetable UI 수정

--- feature/timetable/src/main/java/in/koreatech/koin/feature/timetable/view/Timetable.kt
+++ feature/timetable/src/main/java/in/koreatech/koin/feature/timetable/view/Timetable.kt
@@ -1,9 +1,8 @@
 package `in`.koreatech.koin.feature.timetable.view
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -11,17 +10,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import `in`.koreatech.koin.feature.timetable.component.TimetableEventTime
 import `in`.koreatech.koin.feature.timetable.component.TimetableEventType
-import `in`.koreatech.koin.feature.timetable.model.TimetableConstants
 import `in`.koreatech.koin.feature.timetable.model.TimetableEvent
 import `in`.koreatech.koin.feature.timetable.model.dummyEvent
 import `in`.koreatech.koin.feature.timetable.section.TimetableContent
-import `in`.koreatech.koin.feature.timetable.section.TimetableHeader
-import `in`.koreatech.koin.feature.timetable.section.TimetableSidebar
 import java.time.DayOfWeek
 
 
@@ -35,52 +30,34 @@ fun Timetable(
         (event: TimetableEvent, eventType: TimetableEventType, onEventTimeClick: (TimetableEvent) -> Unit) -> Unit = { event, eventType, onEventTimeClick ->
         TimetableEventTime(
             event = event,
+            modifier = Modifier.padding(bottom = (1.5).dp),
             eventType = eventType,
             onEventTimeClick = onEventTimeClick
         )
     },
     onEventClick: (TimetableEvent) -> Unit = {},
 ) {
-    val verticalScrollState = rememberScrollState()
-    val horizontalPadding = 10 + (0.4)
-    val widthPadding = String.format("%.2f", (horizontalPadding * 2) / 6).toFloat()
-
-    val width =
-        (LocalContext.current.resources.displayMetrics.widthPixels / LocalContext.current.resources.displayMetrics.density).dp / (TimetableConstants.days.size + 1) - widthPadding.dp
-
+    val verticalScrollState: ScrollState = rememberScrollState()
 
     Column(
         modifier = modifier
             .background(Color.White),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TimetableHeader(
+        TimetableContent(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = horizontalPadding.dp)
+                .verticalScroll(verticalScrollState)
+                .padding(vertical = 14.dp),
+            range = range,
+            horizontalPadding = 48.dp,
+            events = events,
+            clickEvent = clickEvent,
+            content = content,
+            onEventClick = onEventClick
         )
-        Row(
-            modifier = Modifier
-        ) {
-            TimetableSidebar(
-                modifier = Modifier
-                    .verticalScroll(verticalScrollState),
-                range = range,
-                hourWidth = width,
-            )
-            TimetableContent(
-                modifier = Modifier
-                    .verticalScroll(verticalScrollState),
-                range = range,
-                dayWidth = width,
-                events = events,
-                clickEvent = clickEvent,
-                content = content,
-                onEventClick = onEventClick
-            )
-        }
     }
 }
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -88,7 +65,7 @@ private fun TimetablePreview() {
     Timetable(
         range = 9,
         events = listOf(dummyEvent, dummyEvent.copy(dayOfWeek = DayOfWeek.MONDAY)),
-        clickEvent = listOf(dummyEvent)
+        clickEvent = listOf(dummyEvent, dummyEvent.copy(dayOfWeek = DayOfWeek.THURSDAY))
     )
 }
 
