63e6cb51bd4ad8765173ebcea681755f77e6afd2
Jokwanhee <jonotch1@naver.com>
2024-11-02 23:25:07 +0900

    [add] 연산 로직 함수 구현 및 테스트 코드 구현

--- domain/src/main/java/in/koreatech/koin/domain/model/timetable/response/TimetableLecture.kt
+++ domain/src/main/java/in/koreatech/koin/domain/model/timetable/response/TimetableLecture.kt
@@ -1,5 +1,8 @@
 package `in`.koreatech.koin.domain.model.timetable.response
 
+import java.time.DayOfWeek
+import java.time.LocalTime
+
 data class TimetableLecture(
     val id: Int,
     val lectureId: Int,
@@ -15,4 +18,39 @@ data class TimetableLecture(
     val target: String = "",
     val professor: String = "",
     val department: String = "",
-)
+) {
+    /**
+     * @reference : TimetableLectureTest.kt
+     */
+    fun findDayOfWeekAndTime(): Map<DayOfWeek?, List<LocalTime>> {
+        return classTime.groupBy { it / 100 }
+            .mapValues { entry ->
+                /**
+                 * @input : [0,1,100,101]
+                 */
+                entry.value.sorted().map { value ->
+                    val timeIndex = if (entry.key == 0) value else value % (entry.key * 100)
+                    LocalTime.of(9 + timeIndex / 2, (timeIndex % 2) * 30)
+                }
+                /**
+                 * @output : [09:00, 09:30], [09:00, 09:30]
+                 */
+            }
+            .mapKeys {
+                /**
+                 * @input : {0=[09:00, 09:30], 1=[09:00, 09:30]}
+                 */
+                when (it.key) {
+                    0 -> DayOfWeek.MONDAY
+                    1 -> DayOfWeek.TUESDAY
+                    2 -> DayOfWeek.WEDNESDAY
+                    3 -> DayOfWeek.THURSDAY
+                    4 -> DayOfWeek.FRIDAY
+                    else -> null
+                }
+                /**
+                 * @output : {MONDAY=[09:00, 09:30], TUESDAY=[09:00, 09:30]}
+                 */
+            }
+    }
+}
