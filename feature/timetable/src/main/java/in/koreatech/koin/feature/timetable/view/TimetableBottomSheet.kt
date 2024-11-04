4c43854b515e1ac62527e06c0950ec5e57c52abb
Jokwanhee <jonotch1@naver.com>
2024-11-05 01:09:35 +0900

    [fix] TimetableBottomSheet UI 구분

--- feature/timetable/src/main/java/in/koreatech/koin/feature/timetable/view/TimetableBottomSheet.kt
+++ feature/timetable/src/main/java/in/koreatech/koin/feature/timetable/view/TimetableBottomSheet.kt
@@ -2,10 +2,11 @@ package `in`.koreatech.koin.feature.timetable.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,17 +16,37 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 import `in`.koreatech.koin.domain.model.timetable.response.Lecture
-import `in`.koreatech.koin.feature.timetable.component.LectureBox
-import `in`.koreatech.koin.feature.timetable.component.TimetableSearchBox
 import `in`.koreatech.koin.feature.timetable.model.TimetableEvent
-import `in`.koreatech.koin.feature.timetable.model.defaultColors
 import `in`.koreatech.koin.feature.timetable.model.dummyLecture
+import `in`.koreatech.koin.feature.timetable.section.TimetableBottomSheetBasic
+import `in`.koreatech.koin.feature.timetable.section.TimetableBottomSheetCustom
 import `in`.koreatech.koin.feature.timetable.section.TimetableBottomSheetHeader
+import java.time.DayOfWeek
+import java.time.LocalTime
+
+enum class TimetableBottomSheetContentMode {
+    CUSTOM, BASIC
+}
+
+data class CustomEventData(
+    val schedule: String = "",
+    val professor: String? = "",
+    val data: List<CustomEventExtraData> = emptyList()
+)
+
+data class CustomEventExtraData(
+    val position: Int = 0,
+    val dayOfWeek: DayOfWeek = DayOfWeek.MONDAY,
+    val startTime: LocalTime = LocalTime.of(9, 0),
+    val endTime: LocalTime = LocalTime.of(10, 0),
+    val place: String = ""
+)
 
 @Composable
 fun TimetableBottomSheet(
     searchText: String,
     lectures: List<Lecture>,
+    bottomSheetContentMode: TimetableBottomSheetContentMode,
     modifier: Modifier = Modifier,
     selectedLecture: Lecture? = null,
     timetableEvents: List<TimetableEvent> = emptyList(),
@@ -57,33 +78,35 @@ fun TimetableBottomSheet(
         TimetableBottomSheetHeader(
             modifier = Modifier.padding(bottom = 4.dp),
             onComplete = onComplete,
+            mode = bottomSheetContentMode,
             onClickAddLectureMode = onClickAddLectureMode,
             onClickAddCustomLectureMode = onClickAddCustomLectureMode
         )
         HorizontalDivider(thickness = 1.dp, color = KoinTheme.colors.neutral300)
-        TimetableSearchBox(
-            modifier = Modifier.padding(vertical = 8.dp),
-            searchText = searchText,
-            onSearchTextChange = onSearchTextChange,
-            onClickSearchIcon = onClickSearchIcon,
-            onClickSettingIcon = onClickSettingIcon
-        )
-        HorizontalDivider(thickness = 2.dp, color = KoinTheme.colors.neutral300)
-        LazyColumn {
-            items(lectures.size) {
-                LectureBox(
-                    position = it,
-                    lecture = lectures[it],
+        Spacer(modifier = Modifier.height(8.dp))
+
+        when (bottomSheetContentMode) {
+            TimetableBottomSheetContentMode.BASIC -> {
+                TimetableBottomSheetBasic(
+                    searchText = searchText,
+                    lectures = lectures,
                     selectedLecture = selectedLecture,
                     timetableEvents = timetableEvents,
+                    onClickSettingIcon = onClickSettingIcon,
+                    onClickSearchIcon = onClickSearchIcon,
+                    onSearchTextChange = onSearchTextChange,
+                    onClickAddLecture = onClickAddLecture,
+                    onClickRemoveLecture = onClickRemoveLecture,
                     onClickLecture = onClickLecture,
                     onSelectedLecture = onSelectedLecture,
-                    onClickAddLecture = onClickAddLecture,
-                    onClickRemoveLecture = onClickRemoveLecture
                 )
-                HorizontalDivider(thickness = 1.dp, color = KoinTheme.colors.neutral300)
+            }
+
+            TimetableBottomSheetContentMode.CUSTOM -> {
+                TimetableBottomSheetCustom()
             }
         }
+
     }
 }
 
@@ -94,6 +117,20 @@ private fun TimetableBottomSheetPreview() {
         TimetableBottomSheet(
             searchText = "",
             lectures = listOf(dummyLecture, dummyLecture.copy(id = 2, name = "컴퓨터 개발")),
+            bottomSheetContentMode = TimetableBottomSheetContentMode.BASIC,
+            selectedLecture = null,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TimetableBottomSheetPreview_Custom() {
+    KoinTheme {
+        TimetableBottomSheet(
+            searchText = "",
+            lectures = listOf(dummyLecture, dummyLecture.copy(id = 2, name = "컴퓨터 개발")),
+            bottomSheetContentMode = TimetableBottomSheetContentMode.CUSTOM,
             selectedLecture = null,
         )
     }
