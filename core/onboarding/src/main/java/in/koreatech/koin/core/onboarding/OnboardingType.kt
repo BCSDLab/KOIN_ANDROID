b75df71fcb01f4c73e72318f08da1212b3afe6de
hsgo2430 <hsgo2430@naver.com>
2024-11-04 21:01:31 +0900

    feature: 툴팁 기능 추가

--- core/onboarding/src/main/java/in/koreatech/koin/core/onboarding/OnboardingType.kt
+++ core/onboarding/src/main/java/in/koreatech/koin/core/onboarding/OnboardingType.kt
@@ -12,5 +12,6 @@ enum class OnboardingType(
     DINING_IMAGE(R.string.dining_image_tooltip),
     DINING_NOTIFICATION(0),
     DINING_SHARE(0),
-    ARTICLE_KEYWORD(R.string.article_keyword_tooltip)
+    ARTICLE_KEYWORD(R.string.article_keyword_tooltip),
+    REVIEW_SORTING(R.string.store_review_sorting_tooltip)
 }
