73847764a1e85657de32744918846e751ae0a1a5
Thirfir <sangbbang31@gmail.com>
2024-11-06 02:06:14 +0900

    add: 시내버스 시간표 전체 뷰

---
+++ feature/bus/src/main/java/in/koreatech/bus/screen/timetable/composable/CityTimetableScreen.kt
@@ -0,0 +1,109 @@
+package `in`.koreatech.bus.screen.timetable.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import `in`.koreatech.bus.screen.timetable.type.CityBusNumberType
+import `in`.koreatech.bus.screen.timetable.type.CommonDirectionType
+import `in`.koreatech.bus.screen.timetable.type.DaytimeType
+import `in`.koreatech.bus.viewstate.ArrivalViewState
+import `in`.koreatech.bus.viewstate.CommonTimetableViewState
+import `in`.koreatech.koin.core.designsystem.component.chip.TextChipGroup
+import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
+
+@Composable
+internal fun CityTimetableScreen(
+    timetable: CommonTimetableViewState,
+    modifier: Modifier = Modifier,
+    onBusNumberChanged: (CityBusNumberType) -> Unit = {},
+    onDirectionChanged: (CommonDirectionType) -> Unit = {}
+) {
+
+    var selectedBusNumberTypeIndex by rememberSaveable { mutableIntStateOf(CityBusNumberType.N400.ordinal) }
+    var selectedDirectionTypeIndex by rememberSaveable { mutableIntStateOf(CommonDirectionType.TO_BYEONGCHEON.ordinal) }
+    val context = LocalContext.current
+
+    Column(
+        modifier = modifier
+    ) {
+        TextChipGroup(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp)
+                .padding(top = 8.dp, bottom = 4.dp),
+            titles = CityBusNumberType.entries.map { stringResource(it.titleRes) },
+            onChipSelected = { title ->
+                selectedBusNumberTypeIndex = CityBusNumberType.entries.find { context.getString(it.titleRes) == title }?.ordinal ?: 0
+            },
+            selectedChipIndexes = intArrayOf(selectedBusNumberTypeIndex)
+        )
+        TextChipGroup(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp)
+                .padding(top = 4.dp, bottom = 8.dp),
+            titles = CommonDirectionType.entries.map { stringResource(it.titleRes) },
+            onChipSelected = { title ->
+                selectedDirectionTypeIndex = CommonDirectionType.entries.find { context.getString(it.titleRes) == title }?.ordinal ?: 0
+            },
+            selectedChipIndexes = intArrayOf(selectedDirectionTypeIndex)
+        )
+
+        CommonTimetableView(
+            timetable = timetable,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+
+    LaunchedEffect(selectedBusNumberTypeIndex) {
+        onBusNumberChanged(CityBusNumberType.entries[selectedBusNumberTypeIndex])
+    }
+
+    LaunchedEffect(selectedDirectionTypeIndex) {
+        onDirectionChanged(CommonDirectionType.entries[selectedDirectionTypeIndex])
+    }
+}
+
+@Preview
+@Composable
+private fun CityTimetableScreenPreview() {
+    CityTimetableScreen(
+        modifier = Modifier.fillMaxSize().background(KoinTheme.colors.neutral100),
+        timetable = CommonTimetableViewState(
+            updatedAt = "2024-09-21",
+            arrivals = mapOf(
+                DaytimeType.AM to listOf(
+                    ArrivalViewState(
+                        arrivalTime = "09:00"
+                    ),
+                    ArrivalViewState(
+                        arrivalTime = "09:30"
+                    ),
+                    ArrivalViewState(
+                        arrivalTime = "10:00"
+                    ),
+                    ArrivalViewState(
+                        arrivalTime = "10:30"
+                    ),
+                ), DaytimeType.PM to listOf(
+                    ArrivalViewState(
+                        arrivalTime = "14:30"
+                    )
+                )
+            )
+        )
+    )
+}
