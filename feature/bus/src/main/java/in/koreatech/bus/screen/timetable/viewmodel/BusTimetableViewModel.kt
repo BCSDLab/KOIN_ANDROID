d99ae0cb7baa00042708d21c6e4463b5682cb81c
Thirfir <sangbbang31@gmail.com>
2024-11-05 23:04:59 +0900

    add: BusTimetableViewModel base

---
+++ feature/bus/src/main/java/in/koreatech/bus/screen/timetable/viewmodel/BusTimetableViewModel.kt
@@ -0,0 +1,12 @@
+package `in`.koreatech.bus.screen.timetable.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BusTimetableViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
