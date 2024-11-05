2a1675661d479bf5e9e4a3c3ff83c01bd91ad631
Thirfir <sangbbang31@gmail.com>
2024-11-06 02:51:17 +0900

    add: 버스 공지 헤드 아이템 뷰 추가

--- feature/bus/src/main/java/in/koreatech/bus/screen/timetable/viewmodel/BusTimetableViewModel.kt
+++ feature/bus/src/main/java/in/koreatech/bus/screen/timetable/viewmodel/BusTimetableViewModel.kt
@@ -244,4 +244,11 @@ class BusTimetableViewModel @Inject constructor(
             )
         )
     )
+    val headArticle = flow {
+        emit("[긴급] 9.27(금) 대학등교방향 천안셔틀버스 터미널 미정차 알림(천안역에서 승차바람)")
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = "[긴급] 9.27(금) 대학등교방향 천안셔틀버스 터미널 미정차 알림(천안역에서 승차바람)"
+    )
 }
