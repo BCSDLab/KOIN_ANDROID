a3e58120160712798f48ffd8367170b7f2d007fa
Thirfir <sangbbang31@gmail.com>
2024-11-06 21:56:13 +0900

    refactor: layout.xml 삭제 및 setContent

--- feature/bus/src/main/java/in/koreatech/bus/Bus2Activity.kt
+++ feature/bus/src/main/java/in/koreatech/bus/Bus2Activity.kt
@@ -1,6 +1,7 @@
 package `in`.koreatech.bus
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,8 +22,7 @@ class Bus2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_bus2)
-        findViewById<ComposeView>(R.id.compose_view_bus).setContent {
+        setContent {
             KoinTheme {
                 BusNavigation(
                     modifier = Modifier.fillMaxSize()
