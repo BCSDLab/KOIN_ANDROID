018378bb36d0fb2939a0588997fe7d1b6e6d659c
Thirfir <sangbbang31@gmail.com>
2024-11-06 20:25:21 +0900

    add: Singleton annotation

--- core/src/main/java/in/koreatech/koin/core/di/CoroutineDispatchersModule.kt
+++ core/src/main/java/in/koreatech/koin/core/di/CoroutineDispatchersModule.kt
@@ -10,23 +10,29 @@ import `in`.koreatech.koin.core.qualifier.MainDispatcher
 import `in`.koreatech.koin.core.qualifier.UnconfinedDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object CoroutineDispatchersModule {
+
     @IoDispatcher
+    @Singleton
     @Provides
     fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
 
     @UnconfinedDispatcher
+    @Singleton
     @Provides
     fun providesUnconfinedDispatcher(): CoroutineDispatcher = Dispatchers.Unconfined
 
     @DefaultDispatcher
+    @Singleton
     @Provides
     fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
 
     @MainDispatcher
+    @Singleton
     @Provides
     fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
 }
