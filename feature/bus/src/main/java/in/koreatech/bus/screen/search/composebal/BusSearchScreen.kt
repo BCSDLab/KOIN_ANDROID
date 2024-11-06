fed5808a61cde3727557b47885c52446e8379d8c
Thirfir <sangbbang31@gmail.com>
2024-11-06 23:55:01 +0900

    add: ConstraintLayout 적용한 버스 조회 뷰

--- feature/bus/src/main/java/in/koreatech/bus/screen/search/composebal/BusSearchScreen.kt
+++ feature/bus/src/main/java/in/koreatech/bus/screen/search/composebal/BusSearchScreen.kt
@@ -3,9 +3,13 @@ package `in`.koreatech.bus.screen.search.composebal
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -16,14 +20,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import `in`.koreatech.bus.screen.search.viewmodel.BusSearchViewModel
+import `in`.koreatech.koin.core.designsystem.component.tab.KoinSurface
 import `in`.koreatech.koin.core.designsystem.component.topbar.KoinTopAppBar
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 import `in`.koreatech.koin.feature.bus.R
@@ -47,54 +55,123 @@ fun BusSearchScreen(
             onNavigationIconClick = onNavigationIconClick
         )
 
-        Text(
-            modifier = Modifier.padding(top = 16.dp),
-            text = stringResource(R.string.introduce_bus_search),
-            style = KoinTheme.typography.medium16,
-            color = KoinTheme.colors.neutral800
+        BusSearchContentView(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 16.dp)
+                .padding(horizontal = 24.dp),
+            departure = departure,
+            arrival = arrival,
+            onSwapIconClicked = viewModel::swapDepartureAndArrival
         )
+    }
+}
 
-        Text(
-            modifier = Modifier.padding(top = 2.dp),
-            text = stringResource(R.string.caution_possibly_inaccurate),
-            style = KoinTheme.typography.regular12,
-            color = KoinTheme.colors.neutral600
-        )
+@Composable
+private fun BusSearchContentView(
+    departure: String,
+    arrival: String,
+    modifier: Modifier = Modifier,
+    onSwapIconClicked: () -> Unit = {},
+) {
 
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(top = 46.dp),
-            verticalAlignment = Alignment.Bottom
-        ) {
-            BusSearchInput(
-                title = stringResource(R.string.departure),
-                place = departure,
-                placeholder = stringResource(R.string.select_departure),
-                modifier = Modifier.weight(1f)
+    KoinSurface {
+        Column(modifier = modifier) {
+            Text(
+                modifier = Modifier,
+                text = stringResource(R.string.introduce_bus_search),
+                style = KoinTheme.typography.medium16,
+                color = KoinTheme.colors.neutral800
             )
 
-            IconButton(
-                onClick = viewModel::swapDepartureAndArrival,
-                modifier = Modifier.padding(horizontal = 16.dp)
+            Text(
+                modifier = Modifier.padding(top = 2.dp),
+                text = stringResource(R.string.caution_possibly_inaccurate),
+                style = KoinTheme.typography.regular12,
+                color = KoinTheme.colors.neutral600
+            )
+
+            ConstraintLayout(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 46.dp)
+                    .height(IntrinsicSize.Min)
             ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_swap),
-                    contentDescription = stringResource(R.string.swap_content_description)
+                val (departureText, arrivalText, departureField, arrivalField, iconSwap) = createRefs()
+
+                Text(
+                    modifier = Modifier.constrainAs(departureText) {
+                        start.linkTo(departureField.start)
+                        top.linkTo(parent.top)
+                        end.linkTo(departureField.end)
+                    },
+                    text = stringResource(R.string.departure),
+                    style = KoinTheme.typography.medium16,
+                    color = KoinTheme.colors.primary500
+                )
+                Text(
+                    modifier = Modifier.constrainAs(arrivalText) {
+                        top.linkTo(parent.top)
+                        start.linkTo(arrivalField.start)
+                        end.linkTo(arrivalField.end)
+                    },
+                    text = stringResource(R.string.arrival),
+                    style = KoinTheme.typography.medium16,
+                    color = KoinTheme.colors.primary500
+                )
+
+                BusSearchInput(
+                    place = departure,
+                    placeholder = stringResource(R.string.select_departure),
+                    modifier = Modifier.padding(top = 10.dp).constrainAs(departureField) {
+                        top.linkTo(iconSwap.top)
+                        bottom.linkTo(iconSwap.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(iconSwap.start)
+
+                        width = Dimension.fillToConstraints
+                        height = Dimension.preferredWrapContent
+                    }
+                )
+
+                IconButton(
+                    onClick = onSwapIconClicked,
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                        .padding(horizontal = 16.dp, vertical = 12.dp)
+                        .size(32.dp)
+                        .constrainAs(iconSwap) {
+                            top.linkTo(departureText.bottom)
+                            start.linkTo(departureField.end)
+                            end.linkTo(arrivalField.start)
+                        }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_swap),
+                        contentDescription = stringResource(R.string.swap_content_description),
+                    )
+                }
+
+                BusSearchInput(
+                    place = arrival,
+                    placeholder = stringResource(R.string.select_arrival),
+                    modifier = Modifier.padding(top = 10.dp).constrainAs(arrivalField) {
+                        top.linkTo(iconSwap.top)
+                        bottom.linkTo(iconSwap.bottom)
+                        start.linkTo(iconSwap.end)
+                        end.linkTo(parent.end)
+
+                        width = Dimension.fillToConstraints
+                        height = Dimension.preferredWrapContent
+                    }
                 )
             }
-
-            BusSearchInput(
-                title = stringResource(R.string.arrival),
-                place = arrival,
-                placeholder = stringResource(R.string.select_arrival),
-                modifier = Modifier.weight(1f)
-            )
         }
     }
 }
 
 @Composable
 private fun BusSearchInput(
-    title: String,
     place: String,
     placeholder: String,
     modifier: Modifier = Modifier,
@@ -106,27 +183,24 @@ private fun BusSearchInput(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = title,
-            style = KoinTheme.typography.medium16,
-            color = KoinTheme.colors.primary500
-        )
-
         Box(
             modifier = Modifier
-                .background(if(isPlaceDetermined.not()) KoinTheme.colors.neutral100 else Color.Transparent)
-                .padding(vertical = 12.dp, horizontal = 39.dp),
+                .clip(RoundedCornerShape(4.dp))
+                .background(if (isPlaceDetermined.not()) KoinTheme.colors.neutral100 else Color.Transparent)
+                .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
             if (isPlaceDetermined.not())
                 Text(
                     text = placeholder,
+                    maxLines = 1,
                     style = KoinTheme.typography.regular14,
                     color = KoinTheme.colors.neutral400     // TODO neutral450 ?
                 )
             else {
                 Text(
                     text = place,
+                    maxLines = 1,
                     style = KoinTheme.typography.bold18,
                     color = KoinTheme.colors.neutral800
                 )
@@ -135,10 +209,26 @@ private fun BusSearchInput(
     }
 }
 
-@Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
 @Composable
 private fun BusSearchScreenPreview() {
-    BusSearchScreen(
+    Column(
         modifier = Modifier.fillMaxWidth()
-    )
+    ) {
+        KoinTopAppBar(
+            title = stringResource(R.string.title_bus_search),
+            onNavigationIconClick = { }
+        )
+
+        BusSearchContentView(
+            departure = "",
+            arrival = "도착지",
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 16.dp)
+                .padding(horizontal = 24.dp),
+            onSwapIconClicked = { }
+        )
+    }
 }
