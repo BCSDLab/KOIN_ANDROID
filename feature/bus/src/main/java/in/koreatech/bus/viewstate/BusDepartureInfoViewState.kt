c020aa36d19687ada251206f6a4658ee5c278d79
Thirfir <sangbbang31@gmail.com>
2024-11-09 00:16:11 +0900

    chore: 임시 데이터 포맷 수정

--- feature/bus/src/main/java/in/koreatech/bus/viewstate/BusDepartureInfoViewState.kt
+++ feature/bus/src/main/java/in/koreatech/bus/viewstate/BusDepartureInfoViewState.kt
@@ -5,5 +5,7 @@ import `in`.koreatech.bus.screen.timetable.type.BusType
 // TODO : 임시 데이터 타입
 data class BusDepartureInfoViewState(
     val type: BusType,
-    val departureTime: String,
+    val departureHour: Int,
+    val departureMinute: Int,
+    val remainingTime: Int, // 남은 시간(실제 현재 시간 기준)
 )
