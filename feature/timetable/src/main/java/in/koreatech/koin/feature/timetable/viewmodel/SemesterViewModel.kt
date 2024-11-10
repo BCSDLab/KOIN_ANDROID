a2d0df01284ab15c990216c95596ab8264f96ad6
DohyeokKim <hyeokdokim@gmail.com>
2024-11-10 15:46:41 +0900

    [Add] 학기 관련 유즈케이스 추가

--- feature/timetable/src/main/java/in/koreatech/koin/feature/timetable/viewmodel/SemesterViewModel.kt
+++ feature/timetable/src/main/java/in/koreatech/koin/feature/timetable/viewmodel/SemesterViewModel.kt
@@ -2,8 +2,14 @@ package `in`.koreatech.koin.feature.timetable.viewmodel
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import `in`.koreatech.koin.core.viewmodel.BaseViewModel
+import `in`.koreatech.koin.domain.usecase.timetable.GetSemestersUseCase
+import `in`.koreatech.koin.domain.usecase.timetable.GetUserSemestersUseCase
+import javax.inject.Inject
 
 @HiltViewModel
-class SemesterViewModel : BaseViewModel() {
+class SemesterViewModel @Inject constructor(
+    private val getUserSemestersUseCase: GetUserSemestersUseCase,
+    private val getSemestersUseCase: GetSemestersUseCase,
+) : BaseViewModel() {
 
 }
