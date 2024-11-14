ff0b681fd3a17a3e2737bd7b9eec99f00abbaba1
Jokwanhee <jonotch1@naver.com>
2024-11-14 21:41:42 +0900

    [fix] 학부 리스트 수정

--- koin/src/main/java/in/koreatech/koin/ui/timetablev2/TimetableActivity.kt
+++ koin/src/main/java/in/koreatech/koin/ui/timetablev2/TimetableActivity.kt
@@ -27,7 +27,7 @@ import `in`.koreatech.koin.core.designsystem.component.snackbar.showSnackBarWith
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 import `in`.koreatech.koin.core.util.KeyboardUtils
 import `in`.koreatech.koin.databinding.ActivityTimetableBinding
-import `in`.koreatech.koin.feature.timetable.model.dummyDepartment
+import `in`.koreatech.koin.feature.timetable.model.TimetableConstants.departments
 import `in`.koreatech.koin.feature.timetable.state.BottomSheetUI
 import `in`.koreatech.koin.feature.timetable.state.TimetableSideEffect
 import `in`.koreatech.koin.feature.timetable.utils.BitmapUtils
@@ -140,7 +140,7 @@ class TimetableActivity : KoinNavigationDrawerActivity() {
                 if (dialogState.isSelectDepartmentVisible) {
                     SelectDepartmentDialog(
                         department = searchEngineState.department,
-                        departments = dummyDepartment,
+                        departments = departments,
                         onConfirm = viewModel::updateDepartment,
                         onDismiss = viewModel::updateIsSelectDepartmentDialogVisible
                     )
