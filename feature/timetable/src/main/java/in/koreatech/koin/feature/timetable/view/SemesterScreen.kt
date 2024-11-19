8cfebaa77fe04e0dff107236ce1d67fab9ee2c00
DohyeokKim <hyeokdokim@gmail.com>
2024-11-20 03:08:48 +0900

    [Fix] 보여지는 시간표 이름 최대 길이 수정

--- feature/timetable/src/main/java/in/koreatech/koin/feature/timetable/view/SemesterScreen.kt
+++ feature/timetable/src/main/java/in/koreatech/koin/feature/timetable/view/SemesterScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
@@ -22,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import `in`.koreatech.koin.core.designsystem.component.icon.StableIcon
@@ -170,20 +173,26 @@ private fun LazyListScope.TimetableFrameBlock(
             verticalAlignment = Alignment.CenterVertically
         ) {
             Row(
-                horizontalArrangement = Arrangement.spacedBy(20.dp),
+                modifier = Modifier.weight(1f),
+                horizontalArrangement = Arrangement.spacedBy(0.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
+                    modifier = Modifier.weight(1f),
                     text = timetableFrame.timetableName,
                     style = KoinTheme.typography.medium18.copy(
                         KoinTheme.colors.neutral800
-                    )
+                    ),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
                 )
                 if (!isAnonymous && timetableFrame.isMain) {
+                    Spacer(modifier = Modifier.width(10.dp))
                     StableIcon(
                         drawableResId = R.drawable.ic_flag_main,
                         tint = Color.Unspecified
                     )
+                    Spacer(modifier = Modifier.width(16.dp))
                 }
             }
 
@@ -206,14 +215,14 @@ private fun SemesterScreenPreview() {
     SemesterScreen(
         userTimetables = mutableMapOf(
             SemesterModel(2024, SemesterType.Fall) to listOf(
-                TimetableFrame(0, "시간표1", true),
+                TimetableFrame(0, "시간표3시간표3시간표3시간표3시간표3시간표3시간표3시간표3시간표3시간표3시간표3", true),
                 TimetableFrame(1, "시간표2", false),
                 TimetableFrame(2, "시간표3", false),
             ),
             SemesterModel(2024, SemesterType.Spring) to listOf(TimetableFrame(0, "시간표1", true)),
             SemesterModel(2024, SemesterType.Winter) to listOf(
                 TimetableFrame(0, "시간표1", true),
-                TimetableFrame(2, "시간표3", false)
+                TimetableFrame(2, "시간표3시간표3시간표3시간표3시간표3시간표3시간표3시간표3시간표3시간표3시간표3", false)
             ),
         ),
         isAnonymous = false,
