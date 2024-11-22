3d2981973dcf2c935279ffae4dc94e17d68e8247
wateralsie <tnals08019@gmail.com>
2024-11-22 13:57:14 +0900

    add: 일주일마다 배너 텍스트 변경 구현

--- koin/src/main/java/in/koreatech/koin/ui/main/state/ArticleMainState.kt
+++ koin/src/main/java/in/koreatech/koin/ui/main/state/ArticleMainState.kt
@@ -1,6 +1,20 @@
 package `in`.koreatech.koin.ui.main.state
 
+import `in`.koreatech.koin.domain.model.article.ArticleHeader
+import `in`.koreatech.koin.domain.model.article.ArticleNoti
+
 sealed class ArticleMainState {
     data class Noti(val title: String, val sub: String): ArticleMainState()
     data class Content(val title: String, val id: Int, val boardId: Int): ArticleMainState()
 }
+
+fun ArticleHeader.toContent() = ArticleMainState.Content(
+    title = title,
+    id = id,
+    boardId = boardId
+)
+
+fun ArticleNoti.toNoti() = ArticleMainState.Noti(
+    title = title,
+    sub = sub
+)
