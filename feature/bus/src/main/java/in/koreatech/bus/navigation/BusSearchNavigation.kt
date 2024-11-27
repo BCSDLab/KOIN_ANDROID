f9a4c57b114610633cca57871cd001b725fc98af
Thirfir <sangbbang31@gmail.com>
2024-11-27 14:35:00 +0900

    chore: 패키지 정리

--- feature/bus/src/main/java/in/koreatech/bus/navigation/BusSearchNavigation.kt
+++ feature/bus/src/main/java/in/koreatech/bus/navigation/BusSearchNavigation.kt
@@ -9,7 +9,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import `in`.koreatech.bus.screen.search.composable.BusSearchResultScreen
+import `in`.koreatech.bus.screen.searchresult.composable.BusSearchResultScreen
 import `in`.koreatech.bus.screen.search.composable.BusSearchScreen
 
 @Composable
