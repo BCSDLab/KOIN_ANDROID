d9a2d11a5c1910d958b95160767c0060427db3f3
Thirfir <sangbbang31@gmail.com>
2024-11-28 15:54:07 +0900

    refactor: Date 유틸 분리

--- feature/bus/src/main/java/in/koreatech/bus/screen/search/viewmodel/BusSearchViewModel.kt
+++ feature/bus/src/main/java/in/koreatech/bus/screen/search/viewmodel/BusSearchViewModel.kt
@@ -3,13 +3,11 @@ package `in`.koreatech.bus.screen.search.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import `in`.koreatech.koin.domain.usecase.busv2.SearchBusV2UseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class BusSearchViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
-    private val searchBusV2UseCase: SearchBusV2UseCase
 ) : ViewModel() {
 
     val departure = savedStateHandle.getStateFlow(KEY_DEPARTURE, "")
