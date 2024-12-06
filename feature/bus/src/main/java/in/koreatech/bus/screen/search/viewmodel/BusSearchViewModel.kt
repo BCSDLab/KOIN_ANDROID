4b51d68acaa17b6f03fe42a7122813e07abd9b3a
Thirfir <sangbbang31@gmail.com>
2024-12-06 22:21:20 +0900

    chore: ComposeView Debug 시에만 보이게

--- feature/bus/src/main/java/in/koreatech/bus/screen/search/viewmodel/BusSearchViewModel.kt
+++ feature/bus/src/main/java/in/koreatech/bus/screen/search/viewmodel/BusSearchViewModel.kt
@@ -37,8 +37,7 @@ class BusSearchViewModel @Inject constructor(
                     NoticeViewState(
                 id = 1,
                 title = "[긴급] 9.27(금) 대학등교방향 천안셔틀버스 터미널 미정차 알림(천안역에서 승차바람)"
-            )
-                ))
+            )))
         else emit(BusNoticeUiState.NotShow)
     }.catch {
         emit(BusNoticeUiState.LoadFailed)
