98f84d5e72989835ce27f0c3aeec865730cb71f2
Thirfir <sangbbang31@gmail.com>
2024-12-07 15:23:20 +0900

    fix: Body -> Query

--- data/src/main/java/in/koreatech/koin/data/request/bus/ShuttleTimetableRequest.kt
+++
@@ -1,9 +0,0 @@
-package `in`.koreatech.koin.data.request.bus
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ShuttleTimetableRequest(
-    @SerialName("id") val id: String
-)
