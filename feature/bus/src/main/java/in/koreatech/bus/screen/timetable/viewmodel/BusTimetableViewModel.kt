dba43ed9a4868fc409e148e8f6c6e1c364112be5
Thirfir <sangbbang31@gmail.com>
2024-12-08 18:04:20 +0900

    refactor: timetable uistate 로직 변경

--- feature/bus/src/main/java/in/koreatech/bus/screen/timetable/viewmodel/BusTimetableViewModel.kt
+++ feature/bus/src/main/java/in/koreatech/bus/screen/timetable/viewmodel/BusTimetableViewModel.kt
@@ -1,5 +1,6 @@
 package `in`.koreatech.bus.screen.timetable.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -7,21 +8,24 @@ import `in`.koreatech.bus.busNoticeUiStateMock
 import `in`.koreatech.bus.mock.commonTimetableMock
 import `in`.koreatech.bus.mock.expressTimetableMock
 import `in`.koreatech.bus.mock.shuttleCoursesMock
-import `in`.koreatech.bus.state.ArrivalState
 import `in`.koreatech.bus.state.CommonTimetableState
 import `in`.koreatech.bus.state.BusNoticeState
 import `in`.koreatech.bus.state.ExpressTimetableState
 import `in`.koreatech.bus.state.ShuttleCoursesState
-import `in`.koreatech.bus.state.toBusNoticeViewState
+import `in`.koreatech.bus.state.toBusNoticeState
+import `in`.koreatech.bus.state.toExpressTimetableState
 import `in`.koreatech.bus.state.toShuttleCoursesState
+import `in`.koreatech.bus.type.CityBusNumberType
+import `in`.koreatech.bus.type.CommonDirectionType
+import `in`.koreatech.bus.util.withMock
 import `in`.koreatech.koin.core.onboarding.BuildConfig
 import `in`.koreatech.koin.core.onboarding.OnboardingManager
 import `in`.koreatech.koin.core.onboarding.OnboardingType
 import `in`.koreatech.koin.domain.repository.BusV2Repository
-import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.flow.stateIn
@@ -31,36 +35,49 @@ import javax.inject.Inject
 
 @HiltViewModel
 class BusTimetableViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
     private val onboardingManager: OnboardingManager,
     private val busRepository: BusV2Repository
 ) : ViewModel() {
 
-    /** 임시 데이터 모음 */
-    val timetableUiState = flow<BusTimetableUiState> {
-        val shuttleCourses = viewModelScope.async {
-            try {
-                busRepository.fetchShuttleCourses().getOrThrow().toShuttleCoursesState()
-            } catch (e: Exception) {
-                if (BuildConfig.DEBUG) shuttleCoursesMock else throw e
-            }
-        }
-        val expressTimetable = viewModelScope.async {
-            expressTimetableMock
-        }
-        val cityTimetable = viewModelScope.async {
-            commonTimetableMock
-        }
+    private val expressDirection = savedStateHandle.getStateFlow(KEY_EXPRESS_DIRECTION, CommonDirectionType.TO_BYEONGCHEON)
+    private val cityNumber = savedStateHandle.getStateFlow(KEY_CITY_NUMBER, CityBusNumberType.N400)
+    private val cityDirection = savedStateHandle.getStateFlow(KEY_CITY_DIRECTION, CommonDirectionType.TO_BYEONGCHEON)
 
-        emit(BusTimetableUiState.Success(
-            shuttleCourses.await(), expressTimetable.await(), cityTimetable.await()
-        ))
-    }.catch {
-        emit(BusTimetableUiState.LoadFailed)
-    }.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(5_000),
-        initialValue = BusTimetableUiState.Loading
-    )
+    private val shuttleCourses = flow {
+        emit(busRepository.fetchShuttleCourses().getOrThrow().toShuttleCoursesState())
+    }.withMock(shuttleCoursesMock)
+
+    private val expressTimetable = expressDirection.transform { direction ->
+        val directionQuery = when (direction) {
+            CommonDirectionType.TO_BYEONGCHEON -> "to"
+            CommonDirectionType.TO_CHEONAN -> "from"
+        }
+        emit(busRepository.fetchExpressTimetable(directionQuery).getOrThrow().toExpressTimetableState())
+    }.withMock(expressTimetableMock)
+
+    private val cityTimetable = combine(cityNumber, cityDirection) { number, direction ->
+        commonTimetableMock
+    }
+
+    val timetableUiState =
+        combine(
+            shuttleCourses,
+            expressTimetable,
+            cityTimetable
+        ) { shuttleCourses, expressTimetable, cityTimetable ->
+            BusTimetableUiState.Success(
+                shuttleCourses,
+                expressTimetable,
+                cityTimetable
+            )
+        }.catch {
+            BusTimetableUiState.LoadFailed
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = BusTimetableUiState.Loading
+        )
 
     private val shouldShowNotice = onboardingManager.getShouldOnboardFlow(
         OnboardingType.SHOW_BUS_HEAD_ARTICLE
@@ -70,17 +87,10 @@ class BusTimetableViewModel @Inject constructor(
     )
 
     val noticeUiState: StateFlow<BusNoticeUiState> = shouldShowNotice.transform { shouldShow ->
-        if (shouldShow) {
-            busRepository.fetchBusNotice().onSuccess {
-                emit(BusNoticeUiState.Show(it.toBusNoticeViewState()))
-            }.onFailure {
-                if (BuildConfig.DEBUG)  // TODO : Remove this line
-                    emit(busNoticeUiStateMock)
-                else emit(BusNoticeUiState.LoadFailed)
-            }
-        }
+        if (shouldShow)
+            emit(BusNoticeUiState.Show(busRepository.fetchBusNotice().getOrThrow().toBusNoticeState()))
         else emit(BusNoticeUiState.NotShow)
-    }.catch {
+    }.withMock(busNoticeUiStateMock).catch {
         emit(BusNoticeUiState.LoadFailed)
     }.stateIn(
         scope = viewModelScope,
@@ -92,6 +102,12 @@ class BusTimetableViewModel @Inject constructor(
         viewModelScope.launch {
             onboardingManager.updateShouldOnboard(OnboardingType.SHOW_BUS_HEAD_ARTICLE, false)
         }
+    }
+
+    companion object {
+        private const val KEY_EXPRESS_DIRECTION = "express_direction"
+        private const val KEY_CITY_NUMBER = "city_number"
+        private const val KEY_CITY_DIRECTION = "city_direction"
     }
 }
 
