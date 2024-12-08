20366d52ea0fd96a1e03c6f15541d064be37b975
Thirfir <sangbbang31@gmail.com>
2024-12-08 17:56:21 +0900

    fix: Kotlin Serialization -> Gson

--- data/src/main/java/in/koreatech/koin/data/response/bus/v2/ShuttleSemesterResponse.kt
+++ data/src/main/java/in/koreatech/koin/data/response/bus/v2/ShuttleSemesterResponse.kt
@@ -1,13 +1,13 @@
 package `in`.koreatech.koin.data.response.bus.v2
 
+import com.google.gson.annotations.SerializedName
 import `in`.koreatech.koin.domain.model.bus.v2.ShuttleSemester
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Serializable
 data class ShuttleSemesterResponse(
-    @SerialName("name") val name: String?,
-    @SerialName("term") val term: String?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("term") val term: String?,
 ) {
     fun toShuttleSemester() = ShuttleSemester(
         name = name.orEmpty(),
--- data/src/main/java/in/koreatech/koin/data/response/bus/v2/ShuttleTimetableNodeInfoResponse.kt
+++ data/src/main/java/in/koreatech/koin/data/response/bus/v2/ShuttleTimetableNodeInfoResponse.kt
@@ -1,13 +1,13 @@
 package `in`.koreatech.koin.data.response.bus.v2
 
+import com.google.gson.annotations.SerializedName
 import `in`.koreatech.koin.domain.model.bus.v2.ShuttleTimetableNodeInfo
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Serializable
 data class ShuttleTimetableNodeInfoResponse(
-    @SerialName("name") val name: String?,
-    @SerialName("detail") val detail: String?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("detail") val detail: String?,
 ) {
     fun toShuttleTimetableNodeInfo() = ShuttleTimetableNodeInfo(
         name = name.orEmpty(),
