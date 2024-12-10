5175499cd89fdc6039761e7237f69f848e78e1fc
Thirfir <sangbbang31@gmail.com>
2024-12-10 15:56:35 +0900

    refactor: 버스 긴급공지 로직 수정

--- feature/bus/src/main/java/in/koreatech/bus/mock/BusNoticeMock.kt
+++ feature/bus/src/main/java/in/koreatech/bus/mock/BusNoticeMock.kt
@@ -6,5 +6,11 @@ import `in`.koreatech.bus.state.BusNoticeState
 internal val busNoticeUiStateMock = BusNoticeUiState.Show(
     BusNoticeState(
         id = 17153,
-        title = "[긴급] 9.27(금) 대학등교방향 천안셔틀버스 터미널 미정차 알림(천안역에서 승차바람)")
+        title = "[긴급] 9.27(금) 대학등교방향 천안셔틀버스 터미널 미정차 알림(천안역에서 승차바람)"
+    )
+)
+
+internal val busNoticeMock = BusNoticeState(
+    id = 17153,
+    title = "[긴급] 9.27(금) 대학등교방향 천안셔틀버스 터미널 미정차 알림(천안역에서 승차바람)"
 )
