2d9a0d91f753a38938b504e40da95817940c2abc
Thirfir <sangbbang31@gmail.com>
2024-12-12 01:41:58 +0900

    add: BusType 칩

--- feature/bus/src/main/java/in/koreatech/bus/component/ShuttleBusOperationChip.kt
+++ feature/bus/src/main/java/in/koreatech/bus/component/ShuttleBusOperationChip.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import `in`.koreatech.bus.type.ShuttleBusOperationType
 import `in`.koreatech.koin.core.designsystem.component.chip.ReadOnlyTextChip
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
@@ -24,4 +25,22 @@ internal fun ShuttleBusOperationChip(
         },
         textStyle = KoinTheme.typography.regular12.copy(color = Color.White)
     )
+}
+
+@Preview
+@Composable
+private fun ShuttleBusOperationChipPreview() {
+    ShuttleBusOperationChip(operationType = ShuttleBusOperationType.WEEKEND)
+}
+
+@Preview
+@Composable
+private fun ShuttleBusOperationChipPreview2() {
+    ShuttleBusOperationChip(operationType = ShuttleBusOperationType.WEEKDAY)
+}
+
+@Preview
+@Composable
+private fun ShuttleBusOperationChipPreview3() {
+    ShuttleBusOperationChip(operationType = ShuttleBusOperationType.CIRCULATION)
 }
