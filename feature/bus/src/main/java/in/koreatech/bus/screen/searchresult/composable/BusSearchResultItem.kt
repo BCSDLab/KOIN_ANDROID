74662db0f90093a49529bde6bf603f39e92822b1
Thirfir <sangbbang31@gmail.com>
2024-12-12 01:45:17 +0900

    add: 검색 결과에 bus type 칩 적용

--- feature/bus/src/main/java/in/koreatech/bus/screen/searchresult/composable/BusSearchResultItem.kt
+++ feature/bus/src/main/java/in/koreatech/bus/screen/searchresult/composable/BusSearchResultItem.kt
@@ -9,13 +9,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import `in`.koreatech.bus.component.BusTypeChip
+import `in`.koreatech.bus.mock.busSearchResultMock1
 import `in`.koreatech.bus.state.BusSearchResultState
 import `in`.koreatech.bus.state.ImmutableLocalTime
 import `in`.koreatech.bus.util.formatBeforeTime
 import `in`.koreatech.bus.util.formatTime
 import `in`.koreatech.koin.core.designsystem.component.chip.ReadOnlyTextChip
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
+import java.time.LocalTime
 
 @Composable
 fun BusSearchResultItem(
@@ -31,25 +35,29 @@ fun BusSearchResultItem(
         Column(
             modifier = Modifier.weight(1f)
         ) {
-            ReadOnlyTextChip(
-                title = stringResource(result.busType.titleRes),   // TODO : 버스 종류
-                containerColor = Color(0xFFFBEBD7),
-                textStyle = KoinTheme.typography.regular12.copy(
-                    color = KoinTheme.colors.neutral600
-                )
-            )
+            BusTypeChip(busType = result.busType)
             Text(
                 modifier = Modifier.padding(top = 4.dp),
-                text = result.departureTime.formatTime(), // TODO : 출발 시간
+                text = result.departureTime.formatTime(),
                 style = KoinTheme.typography.bold20
             )
         }
         if (showBeforeTime)
             Text(
-                text = result.departureTime.formatBeforeTime(currentTime.localTime), // TODO : 남은 시간
+                text = result.departureTime.formatBeforeTime(currentTime.localTime),
                 style = KoinTheme.typography.bold16.copy(
                     color = KoinTheme.colors.info700
                 )
             )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BusSearchResultItemPreview() {
+    BusSearchResultItem(
+        result = busSearchResultMock1,
+        currentTime = ImmutableLocalTime(LocalTime.now()),
+        showBeforeTime = true
+    )
 }
