aaeea74b4671c0d25fd321a9ec4a450b7bb607fe
Thirfir <sangbbang31@gmail.com>
2024-12-13 01:43:28 +0900

    fix: bus type ALL 추가로 인한 버그 수정

--- feature/bus/src/main/java/in/koreatech/bus/type/BusType.kt
+++ feature/bus/src/main/java/in/koreatech/bus/type/BusType.kt
@@ -3,11 +3,16 @@ package `in`.koreatech.bus.type
 import androidx.annotation.StringRes
 import `in`.koreatech.koin.feature.bus.R
 
+// property 이름 바꾸면 안됨!!
 enum class BusType(
     @StringRes val titleRes: Int
 ) {
     ALL(R.string.all_bus_type),
     SHUTTLE(R.string.tab_shuttle),
     EXPRESS(R.string.tab_express),
-    CITY(R.string.tab_city),
+    CITY(R.string.tab_city);
+
+    companion object {
+        val entriesExceptAll = values().filter { it != ALL }
+    }
 }
