21b4e67e8c9e1aedc7102a337b4479a9952e45d6
Jokwanhee <jonotch1@naver.com>
2024-12-14 00:19:03 +0900

    Change Hilt KAPT to KSP plugin

--- build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -12,7 +12,6 @@ internal class AndroidLibraryConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("com.android.library")
-                apply("org.jetbrains.kotlin.kapt")
                 apply("org.jetbrains.kotlin.android")
                 apply("com.google.devtools.ksp")
             }
