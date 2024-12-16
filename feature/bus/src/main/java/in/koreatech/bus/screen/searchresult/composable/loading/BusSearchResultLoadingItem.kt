2d01e05fb25942e017b18d3aa7fd4d89e3426689
Thirfir <sangbbang31@gmail.com>
2024-12-16 15:11:26 +0900

    chore: 패키지 정리

--- feature/bus/src/main/java/in/koreatech/bus/screen/searchresult/composable/loading/BusSearchResultLoadingItem.kt
+++ feature/bus/src/main/java/in/koreatech/bus/screen/searchresult/composable/loading/BusSearchResultLoadingItem.kt
@@ -10,7 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import `in`.koreatech.bus.util.skeleton
+import `in`.koreatech.bus.animation.skeleton
 
 @Composable
 internal fun BusSearchResultLoadingItem(
