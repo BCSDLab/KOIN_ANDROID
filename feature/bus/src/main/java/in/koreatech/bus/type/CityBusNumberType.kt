d2185eb6fec8a20045c5772e8b5ec5d896dc3268
Thirfir <sangbbang31@gmail.com>
2024-12-21 01:28:09 +0900

    fix: 시내버스 번호

--- feature/bus/src/main/java/in/koreatech/bus/type/CityBusNumberType.kt
+++ feature/bus/src/main/java/in/koreatech/bus/type/CityBusNumberType.kt
@@ -8,9 +8,11 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 enum class CityBusNumberType(
     @StringRes val titleRes: Int,
-    val numberQuery: Int
+    val numberQuery: Int,
+    val toByeongcheonQuery: String,
+    val toCheonanQuery: String,
 ) : Parcelable {
-    N400(R.string.n400, 400),
-    N405(R.string.n405, 405),
-    N495(R.string.n495, 495),
+    N400(R.string.n400, 400, "병천3리", "종합터미널"),
+    N402(R.string.n402, 402, "황사동", "종합터미널"),
+    N405(R.string.n405, 405, "유관순열사사적지", "종합터미널"),
 }
