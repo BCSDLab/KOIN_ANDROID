597fa97fd9717d1899237956054aff898c0d3f05
Thirfir <sangbbang31@gmail.com>
2024-12-25 16:35:09 +0900

    add: 셔틀 등하교 UI 추가

--- feature/bus/src/main/java/in/koreatech/bus/screen/shuttle_timetable/composable/ShuttleTimetableScreenContent.kt
+++ feature/bus/src/main/java/in/koreatech/bus/screen/shuttle_timetable/composable/ShuttleTimetableScreenContent.kt
@@ -12,6 +12,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,25 +22,27 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEach
-import `in`.koreatech.bus.component.ShuttleBusOperationChip
-import `in`.koreatech.bus.component.CommonLoadingView
-import `in`.koreatech.bus.screen.shuttle_timetable.viewmodel.ShuttleTimetableUiState
-import `in`.koreatech.bus.mock.shuttleTimetableUiStateMock1
 import `in`.koreatech.bus.component.CommonFailureView
+import `in`.koreatech.bus.component.ShuttleBusOperationChip
+import `in`.koreatech.bus.mock.shuttleTimetableUiStateMock1
 import `in`.koreatech.bus.mock.shuttleTimetableUiStateMock2
 import `in`.koreatech.bus.screen.shuttle_timetable.composable.loading.ShuttleTimetableLoading
+import `in`.koreatech.bus.screen.shuttle_timetable.viewmodel.ShuttleTimetableUiState
 import `in`.koreatech.koin.core.designsystem.component.tab.KoinSurface
+import `in`.koreatech.koin.core.designsystem.component.tab.KoinTabRow
 import `in`.koreatech.koin.core.designsystem.component.topbar.KoinTopAppBar
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 import `in`.koreatech.koin.core.designsystem.util.getMeasuredKoreanHeightDp
 import `in`.koreatech.koin.feature.bus.R
 import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -49,6 +54,9 @@ fun ShuttleTimetableScreenContent(
 
     val nodeItemHeightDp =
         KoinTheme.typography.medium15.getMeasuredKoreanHeightDp() + KoinTheme.typography.regular12.getMeasuredKoreanHeightDp()
+
+    val pagerState = rememberPagerState { 2 }
+    val scope = rememberCoroutineScope()
 
     KoinSurface(
         modifier = modifier
@@ -84,52 +92,132 @@ fun ShuttleTimetableScreenContent(
                             )
                         }
 
-                        HorizontalDivider(
-                            color = KoinTheme.colors.neutral400
-                        )
+                        if (timetableUiState.timetable.showTabs()) {
+                            KoinTabRow(
+                                titles = listOf(
+                                    stringResource(R.string.tab_title_going),
+                                    stringResource(R.string.tab_title_return)
+                                ),
+                                selectedTabIndex = pagerState.currentPage,
+                                onTabSelected = {
+                                    scope.launch {
+                                        pagerState.animateScrollToPage(it)
+                                    }
+                                }
+                            )
+                            HorizontalPager(
+                                state = pagerState,
+                                modifier = Modifier.weight(1f),
+                                verticalAlignment = Alignment.Top,
+                                flingBehavior = PagerDefaults.flingBehavior(
+                                    state = pagerState,
+                                    snapPositionalThreshold = .1f,
+                                )
+                            ) { page ->
+                                Column(
+                                    modifier = Modifier.fillMaxSize()
+                                ) {
+                                    HorizontalDivider(
+                                        color = KoinTheme.colors.neutral400
+                                    )
 
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(14.dp)
-                                .background(color = KoinTheme.colors.neutral100)
-                        )
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(IntrinsicSize.Min),
-                        ) {
-                            ShuttleTimetableNodeItem(
-                                nodeItemHeightDp = nodeItemHeightDp,
-                                nodes = timetableUiState.timetable.nodeInfo.toPersistentList()
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .height(14.dp)
+                                            .background(color = KoinTheme.colors.neutral100)
+                                    )
+                                    Row(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .height(IntrinsicSize.Min),
+                                    ) {
+                                        ShuttleTimetableNodeItem(
+                                            nodeItemHeightDp = nodeItemHeightDp,
+                                            nodes = if (page == 0) timetableUiState.timetable.nodeInfo.toPersistentList()
+                                            else timetableUiState.timetable.nodeInfo.reversed().toPersistentList()
+                                        )
+
+                                        VerticalDivider(
+                                            modifier = Modifier.fillMaxHeight(),
+                                            color = KoinTheme.colors.neutral300
+                                        )
+
+                                        Row(
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .horizontalScroll(rememberScrollState())
+                                        ) {
+                                            ShuttleTimetableRouteItem(
+                                                route = if (page == 0) timetableUiState.timetable.routeInfo[page]
+                                                else timetableUiState.timetable.routeInfo[page].copy(
+                                                    arrivalTimes = timetableUiState.timetable.routeInfo[page].arrivalTimes.reversed()
+                                                ),
+                                                nodeItemHeightDp = nodeItemHeightDp,
+                                            )
+
+                                            /** 시간표가 화면을 못 채울 때, 상단의 회색 배경을 채우기 위함 (프리뷰 참조)
+                                             * 더 좋은 방법을 모르겠음... */
+                                            Spacer(
+                                                modifier = Modifier.weight(1f).height(
+                                                    KoinTheme.typography.regular14.getMeasuredKoreanHeightDp() + 16.dp
+                                                ).background(KoinTheme.colors.neutral100)
+                                            )
+                                        }
+                                    }
+                                    Spacer(modifier = Modifier.height(120.dp))
+                                }
+                            }
+                        } else {
+                            HorizontalDivider(
+                                color = KoinTheme.colors.neutral400
                             )
 
-                            VerticalDivider(
-                                modifier = Modifier.fillMaxHeight(),
-                                color = KoinTheme.colors.neutral300
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(14.dp)
+                                    .background(color = KoinTheme.colors.neutral100)
                             )
-
                             Row(
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .horizontalScroll(rememberScrollState())
+                                    .height(IntrinsicSize.Min),
                             ) {
-                                timetableUiState.timetable.routeInfo.fastForEach { route ->
-                                    ShuttleTimetableRouteItem(
-                                        route = route,
-                                        nodeItemHeightDp = nodeItemHeightDp,
+                                ShuttleTimetableNodeItem(
+                                    nodeItemHeightDp = nodeItemHeightDp,
+                                    nodes = timetableUiState.timetable.nodeInfo.toPersistentList()
+                                )
+
+                                VerticalDivider(
+                                    modifier = Modifier.fillMaxHeight(),
+                                    color = KoinTheme.colors.neutral300
+                                )
+
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .horizontalScroll(rememberScrollState())
+                                ) {
+                                    timetableUiState.timetable.routeInfo.fastForEach { route ->
+                                        ShuttleTimetableRouteItem(
+                                            route = route,
+                                            nodeItemHeightDp = nodeItemHeightDp,
+                                        )
+                                    }
+
+                                    /** 시간표가 화면을 못 채울 때, 상단의 회색 배경을 채우기 위함 (프리뷰 참조)
+                                     * 더 좋은 방법을 모르겠음... */
+                                    Spacer(
+                                        modifier = Modifier.weight(1f).height(
+                                            KoinTheme.typography.regular14.getMeasuredKoreanHeightDp() + 16.dp
+                                        ).background(KoinTheme.colors.neutral100)
                                     )
                                 }
-
-                                /** 시간표가 화면을 못 채울 때, 상단의 회색 배경을 채우기 위함 (프리뷰 참조)
-                                * 더 좋은 방법을 모르겠음... */
-                                Spacer(modifier = Modifier.weight(1f).height(
-                                    KoinTheme.typography.regular14.getMeasuredKoreanHeightDp() + 16.dp
-                                ).background(KoinTheme.colors.neutral100))
                             }
-                        }
 
-                        Spacer(modifier = Modifier.height(120.dp))
+                            Spacer(modifier = Modifier.height(120.dp))
+                        }
                     }
 
                     is ShuttleTimetableUiState.Loading -> {
