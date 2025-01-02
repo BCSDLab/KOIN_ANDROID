d70ece40b24db706d9c44de0905c93b99299abd5
Thirfir <sangbbang31@gmail.com>
2025-01-02 17:18:53 +0900

    add: 로깅 검수

--- feature/bus/src/main/java/in/koreatech/bus/screen/searchresult/composable/BusSearchResultScreenContent.kt
+++ feature/bus/src/main/java/in/koreatech/bus/screen/searchresult/composable/BusSearchResultScreenContent.kt
@@ -5,14 +5,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,7 +23,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -41,7 +38,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
@@ -299,7 +295,7 @@ internal fun BusSearchResultScreenContent(
             onComplete = { date, daytime, hour, minute ->
                 EventLogger.logCampusClickEvent(
                     "departure_time_setting_done",
-                    "시간설정 " + if (selectedDateIndex == date
+                    "시간설정: " + if (selectedDateIndex == date
                         && selectedDaytimeIndex == daytime
                         && selectedHourIndex == hour
                         && selectedMinuteIndex == minute
