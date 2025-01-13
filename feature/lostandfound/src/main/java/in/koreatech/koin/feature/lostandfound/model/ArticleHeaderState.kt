c8fd30efd1b813e886b8aaacda81753de25d3a83
WooJin Kong <kongwoojin03@gmail.com>
2025-01-13 21:48:13 +0900

    chore: Import ArticleHeaderState from main koin module

---
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/model/ArticleHeaderState.kt
@@ -0,0 +1,33 @@
+package `in`.koreatech.koin.feature.lostandfound.model
+
+import android.os.Parcelable
+import `in`.koreatech.koin.domain.model.article.ArticleHeader
+import `in`.koreatech.koin.feature.lostandfound.enums.ArticleBoardType
+import kotlinx.parcelize.Parcelize
+import java.time.LocalDate
+
+/**
+ * For hot articles
+ * from main koin module
+ * @see `in`.koreatech.koin.feature.main.model.ArticleHeaderState
+ */
+@Parcelize
+data class ArticleHeaderState(
+    val id: Int,
+    val board: ArticleBoardType,
+    val title: String,
+    val author: String,
+    val viewCount: Int,
+    val registeredAt: LocalDate,
+    val updatedAt: String,
+) : Parcelable
+
+fun ArticleHeader.toArticleHeaderState() = ArticleHeaderState(
+    id = id,
+    board = ArticleBoardType.entries.firstOrNull { it.id == boardId } ?: ArticleBoardType.ALL,
+    title = title,
+    author = author,
+    viewCount = viewCount,
+    registeredAt = LocalDate.parse(registeredAt),
+    updatedAt = updatedAt,
+)
