1a95c88d01e6b13440eb52c9eb43ac166ec0e1e7
WooJin Kong <kongwoojin03@gmail.com>
2025-01-13 20:30:54 +0900

    feat: Add ArticleLostAndFoundWriteFragment

--- koin/src/main/java/in/koreatech/koin/ui/article/ArticleActivity.kt
+++ koin/src/main/java/in/koreatech/koin/ui/article/ArticleActivity.kt
@@ -49,6 +49,7 @@ class ArticleActivity : ActivityBase() {
                 R.id.articleDetailFragment -> setToolbar(ArticleToolbarState.ARTICLE_DETAIL)
                 R.id.articleSearchFragment -> setToolbar(ArticleToolbarState.ARTICLE_SEARCH)
                 R.id.articleKeywordFragment -> setToolbar(ArticleToolbarState.ARTICLE_KEYWORD)
+                R.id.articleLostAndFoundWriteFragment -> setToolbar(ArticleToolbarState.ARTICLE_LOSTANDFOUND_FOUND_ITEM)
             }
         }
 
