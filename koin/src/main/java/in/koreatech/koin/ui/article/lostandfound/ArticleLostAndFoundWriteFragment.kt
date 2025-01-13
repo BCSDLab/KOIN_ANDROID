896a027a54966f4194846acb6132e5acb3004105
WooJin Kong <kongwoojin03@gmail.com>
2025-01-13 20:30:54 +0900

    feat: Add ArticleLostAndFoundWriteFragment

---
+++ koin/src/main/java/in/koreatech/koin/ui/article/lostandfound/ArticleLostAndFoundWriteFragment.kt
@@ -0,0 +1,26 @@
+package `in`.koreatech.koin.ui.article.lostandfound
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ArticleLostAndFoundWriteFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+
+            }
+        }
+    }
+}
