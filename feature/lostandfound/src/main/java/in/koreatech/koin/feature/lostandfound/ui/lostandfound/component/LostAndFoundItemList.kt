4ddfff51ea7cf7a3f3bd7dfb48255740a11d5609
WooJin Kong <kongwoojin03@gmail.com>
2025-01-14 20:07:25 +0900

    feat: Show empty articles text when no articles

--- feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/lostandfound/component/LostAndFoundItemList.kt
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/lostandfound/component/LostAndFoundItemList.kt
@@ -1,9 +1,14 @@
 package `in`.koreatech.koin.feature.lostandfound.ui.lostandfound.component
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import `in`.koreatech.koin.feature.lostandfound.R
 import `in`.koreatech.koin.feature.lostandfound.ui.lostandfound.LostAndFoundItemState
 
 @Composable
@@ -12,6 +17,13 @@ fun LostAndFoundItemList(
     modifier: Modifier = Modifier,
     onItemClick: (LostAndFoundItemState) -> Unit = {}
 ) {
+    if (data.isEmpty()) {
+        Text(
+            modifier = modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            text = stringResource(R.string.empty_articles)
+        )
+    }
     LazyColumn {
         items(data) {
             LostAndFoundItem(
