f00c48b8b14e16d58b590451a5a6dcf4d8821786
WooJin Kong <kongwoojin03@gmail.com>
2025-01-14 20:53:35 +0900

    chore: Adjust empty articles text padding and size

--- feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/lostandfound/component/LostAndFoundItemList.kt
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/lostandfound/component/LostAndFoundItemList.kt
@@ -1,6 +1,7 @@
 package `in`.koreatech.koin.feature.lostandfound.ui.lostandfound.component
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -8,6 +9,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import `in`.koreatech.koin.feature.lostandfound.R
 import `in`.koreatech.koin.feature.lostandfound.ui.lostandfound.LostAndFoundItemState
 
@@ -19,8 +22,9 @@ fun LostAndFoundItemList(
 ) {
     if (data.isEmpty()) {
         Text(
-            modifier = modifier.fillMaxWidth(),
+            modifier = modifier.fillMaxWidth().padding(top = 16.dp),
             textAlign = TextAlign.Center,
+            fontSize = 16.sp,
             text = stringResource(R.string.empty_articles)
         )
     }
