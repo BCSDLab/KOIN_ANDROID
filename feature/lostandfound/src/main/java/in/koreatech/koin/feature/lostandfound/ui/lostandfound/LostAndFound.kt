3c7c947172cca4371300813140ea89a45c08f209
WooJin Kong <kongwoojin03@gmail.com>
2025-01-16 01:57:33 +0900

    fix: Fix pagination not visible
    also make keyword group scroll with list

--- feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/lostandfound/LostAndFound.kt
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/lostandfound/LostAndFound.kt
@@ -1,15 +1,15 @@
 package `in`.koreatech.koin.feature.lostandfound.ui.lostandfound
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.calculateEndPadding
-import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -20,8 +20,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import `in`.koreatech.koin.core.analytics.AnalyticsConstant
 import `in`.koreatech.koin.core.analytics.EventLogger
@@ -30,7 +30,7 @@ import `in`.koreatech.koin.feature.lostandfound.R
 import `in`.koreatech.koin.feature.lostandfound.component.LoadingDialog
 import `in`.koreatech.koin.feature.lostandfound.ui.lostandfound.component.LostAndFoundDialog
 import `in`.koreatech.koin.feature.lostandfound.ui.lostandfound.component.LostAndFoundFAB
-import `in`.koreatech.koin.feature.lostandfound.ui.lostandfound.component.LostAndFoundItemList
+import `in`.koreatech.koin.feature.lostandfound.ui.lostandfound.component.LostAndFoundItem
 import `in`.koreatech.koin.feature.lostandfound.ui.lostandfound.component.LostAndFoundKeywordGroup
 import `in`.koreatech.koin.feature.lostandfound.ui.lostandfound.component.LostAndFoundPagination
 import org.orbitmvi.orbit.compose.collectAsState
@@ -137,26 +137,54 @@ fun LostAndFoundList(
                 modifier = modifier.padding(contentPadding)
                     .consumeWindowInsets(contentPadding)
             ) {
-                LostAndFoundKeywordGroup(
-                    keyWords = myKeywords,
-                    navigateToKeywordFragment = navigateToKeywordFragment
+                LazyColumn(
+                    modifier = modifier
                 ) {
-                    viewModel.selectKeyword(it)
-                }
-                /* TODO: 2차 배포
-                LostAndFoundDropdownGroup {}
-                */
-                LostAndFoundItemList(data = uiState.lostAndFoundList) {
-                    navigateToLostAndFoundDetail(it.id)
-                }
-                LostAndFoundPagination(
-                    modifier = Modifier
-                        .padding(vertical = 32.dp)
-                        .fillMaxWidth(),
-                    currentPage = uiState.currentPage,
-                    totalPage = uiState.totalPage
-                ) {
-                    viewModel.changePage(it)
+                    item {
+                        LostAndFoundKeywordGroup(
+                            keyWords = myKeywords,
+                            navigateToKeywordFragment = navigateToKeywordFragment
+                        ) {
+                            viewModel.selectKeyword(it)
+                        }
+                        /* TODO: 2차 배포
+                        LostAndFoundDropdownGroup {}
+                         */
+                    }
+                    if (uiState.lostAndFoundList.isEmpty()) {
+                        item {
+                            Text(
+                                modifier = modifier.fillMaxWidth().padding(top = 16.dp),
+                                textAlign = TextAlign.Center,
+                                fontSize = 16.sp,
+                                text = stringResource(R.string.empty_articles)
+                            )
+                        }
+                    } else {
+                        items(uiState.lostAndFoundList) {
+                            LostAndFoundItem(
+                                lostItemCategory = it.category,
+                                foundPlace = it.foundPlace,
+                                content = it.content,
+                                author = it.author,
+                                foundDate = it.foundDate,
+                                registeredAt = it.registeredAt,
+                            ) {
+                                navigateToLostAndFoundDetail(it.id)
+                            }
+                        }
+                        item {
+                            LostAndFoundPagination(
+                                modifier = Modifier
+                                    .padding(vertical = 32.dp)
+                                    .fillMaxWidth(),
+                                currentPage = uiState.currentPage,
+                                totalPage = uiState.totalPage
+                            ) {
+                                viewModel.changePage(it)
+                            }
+                        }
+                    }
                 }
 
                 if (isLoading) {
