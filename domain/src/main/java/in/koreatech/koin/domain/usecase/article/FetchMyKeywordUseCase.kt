10929a12d7fd9ffc4373f3965be914dfe51345d7
WooJin Kong <kongwoojin03@gmail.com>
2025-01-18 22:00:09 +0900

    fix: Retry 3 times when failed to fetch keyword

--- domain/src/main/java/in/koreatech/koin/domain/usecase/article/FetchMyKeywordUseCase.kt
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/article/FetchMyKeywordUseCase.kt
@@ -2,11 +2,18 @@ package `in`.koreatech.koin.domain.usecase.article
 
 import `in`.koreatech.koin.domain.repository.ArticleRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.retryWhen
 import javax.inject.Inject
 
 class FetchMyKeywordUseCase @Inject constructor(
     private val articleRepository: ArticleRepository
 ) {
     operator fun invoke(): Flow<List<String>> =
-        articleRepository.fetchMyKeyword()
+        articleRepository.fetchMyKeyword().retryWhen { cause, attempt ->
+            if (attempt < 3) {
+                true
+            } else {
+                throw cause
+            }
+        }
 }
