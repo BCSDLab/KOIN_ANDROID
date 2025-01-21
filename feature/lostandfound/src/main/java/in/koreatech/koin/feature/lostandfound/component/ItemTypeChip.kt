d56e0d8f45bda56d3748c2c60c82e9c9d93d708a
WooJin Kong <kongwoojin03@gmail.com>
2025-01-21 15:24:38 +0900

    fix: Fix padding on small screen

--- feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/component/ItemTypeChip.kt
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/component/ItemTypeChip.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -23,9 +24,9 @@ fun ItemTypeChip(
         showClickRipple = false,
         shape = RoundedCornerShape(50),
         contentPadding = PaddingValues(vertical = 8.dp, horizontal = 12.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
         chipColors = keywordChipColors(),
         modifier = modifier,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically)
     )
 }
