24054c923a02e9620e07956707fc161f3d64412a
WooJin Kong <kongwoojin03@gmail.com>
2025-02-05 14:39:18 +0900

    fix: Add missing Path annotation

--- data/src/main/java/in/koreatech/koin/data/api/auth/ChatAuthApi.kt
+++ data/src/main/java/in/koreatech/koin/data/api/auth/ChatAuthApi.kt
@@ -2,8 +2,9 @@ package `in`.koreatech.koin.data.api.auth
 
 import `in`.koreatech.koin.data.response.chat.ChatRoomResponse
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ChatAuthApi {
     @POST("chatroom/lost-item/{articleId}")
-    suspend fun getChatRoomFromArticleId(articleId: Int): ChatRoomResponse
+    suspend fun getChatRoomFromArticleId(@Path("articleId") articleId: Int): ChatRoomResponse
 }
