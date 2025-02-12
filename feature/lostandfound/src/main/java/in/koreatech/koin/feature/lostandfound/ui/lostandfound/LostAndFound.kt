8e50c7185345ea0ae477d91a1e16492ab2712565
WooJin Kong <kongwoojin03@gmail.com>
2025-02-12 09:43:55 +0900

    fix: Add extra padding on fab when scrolled to the end of list

--- feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/lostandfound/LostAndFound.kt
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/lostandfound/LostAndFound.kt
@@ -1,5 +1,6 @@
 package `in`.koreatech.koin.feature.lostandfound.ui.lostandfound
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
@@ -7,17 +8,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -56,6 +61,16 @@ fun LostAndFoundList(
         viewModel.fetchLostAndFoundList()
     }
 
+    val lazyListState = rememberLazyListState()
+    val firstItemPosition by remember { derivedStateOf { lazyListState.firstVisibleItemIndex } }
+    val fabBottomPadding: Dp by animateDpAsState(
+        if (lazyListState.isScrolledToTheEnd() && firstItemPosition != 0) {
+            64.dp
+        } else {
+            0.dp
+        }
+    )
+
     KoinTheme {
         Scaffold(
             modifier = Modifier.fillMaxSize(),
@@ -65,6 +80,7 @@ fun LostAndFoundList(
                 val fabFoundText = stringResource(R.string.fab_found)
                 val fabLostText = stringResource(R.string.fab_lost)
                 LostAndFoundFAB(
+                    modifier = Modifier.padding(bottom = fabBottomPadding),
                     isDialogExpanded = uiState.isFabDialogExpanded,
                     dialogExpandButtonText = fabWrite,
                     dialogExpandButtonPainter = painterResource(id = R.drawable.ic_fab_write),
@@ -112,7 +128,6 @@ fun LostAndFoundList(
             contentWindowInsets = WindowInsets(0, 0, 0, 0)
         ) { contentPadding ->
             val myKeywords = uiState.myKeywords
-            val lazyListState = rememberLazyListState()
             Column(
                 modifier = modifier
                     .padding(contentPadding)
@@ -234,3 +249,4 @@ fun handleSideEffect(
     }
 }
 
+fun LazyListState.isScrolledToTheEnd() = layoutInfo.visibleItemsInfo.lastOrNull()?.index == layoutInfo.totalItemsCount - 1
