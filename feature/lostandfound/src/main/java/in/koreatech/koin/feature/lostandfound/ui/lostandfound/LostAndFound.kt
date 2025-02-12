ca4edd27d43b1cbdb00625e961149db1f8b11a10
WooJin Kong <kongwoojin03@gmail.com>
2025-02-12 09:46:28 +0900

    fix: Use Spacer

--- feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/lostandfound/LostAndFound.kt
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/lostandfound/LostAndFound.kt
@@ -2,10 +2,12 @@ package `in`.koreatech.koin.feature.lostandfound.ui.lostandfound
 
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -194,15 +196,20 @@ fun LostAndFoundList(
                             }
                         }
                         item {
+                            Spacer(modifier = Modifier.height(32.dp))
+                        }
+                        item {
                             LostAndFoundPagination(
                                 modifier = Modifier
-                                    .padding(vertical = 32.dp)
                                     .fillMaxWidth(),
                                 currentPage = uiState.currentPage,
                                 totalPage = uiState.totalPage
                             ) {
                                 viewModel.changePage(it)
                             }
+                        }
+                        item {
+                            Spacer(modifier = Modifier.height(32.dp))
                         }
                     }
                 }
