dac749d3773e24b2893c851c0a91317a245583ac
WooJin Kong <kongwoojin03@gmail.com>
2025-02-15 14:11:32 +0900

    feat: Add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ plugins {
     alias(libs.plugins.google.service) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.ktlint) apply false
 }
 
 tasks.register<Delete>("clean") {
