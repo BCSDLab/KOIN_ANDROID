696ba8844fc6f0905d303f9730808ef7d26f274d
WooJin Kong <kongwoojin03@gmail.com>
2025-02-17 16:02:49 +0900

    fix: Use enableEdgeToEdgeWithLightStatusBar()

--- feature/chat/src/main/java/in/koreatech/koin/feature/chat/ui/list/ChatListActivity.kt
+++ feature/chat/src/main/java/in/koreatech/koin/feature/chat/ui/list/ChatListActivity.kt
@@ -4,9 +4,9 @@ import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
+import `in`.koreatech.koin.core.designsystem.util.enableEdgeToEdgeWithLightStatusBar
 import `in`.koreatech.koin.feature.chat.ui.room.ChatRoomActivity
 
 @AndroidEntryPoint
@@ -14,7 +14,7 @@ class ChatListActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdgeWithLightStatusBar()
         setContent {
             try {
                 requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
