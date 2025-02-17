ad8394b08f19f1bb498747f4eee6aeacb3ef5794
WooJin Kong <kongwoojin03@gmail.com>
2025-02-17 16:09:43 +0900

    fix: Use enableEdgeToEdgeWithDarkStatusBar()

--- koin/src/main/java/in/koreatech/koin/ui/article/lostandfound/LostAndFoundReportActivity.kt
+++ koin/src/main/java/in/koreatech/koin/ui/article/lostandfound/LostAndFoundReportActivity.kt
@@ -1,14 +1,12 @@
 package `in`.koreatech.koin.ui.article.lostandfound
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.core.os.bundleOf
 import dagger.hilt.android.AndroidEntryPoint
+import `in`.koreatech.koin.core.designsystem.util.enableEdgeToEdgeWithDarkStatusBar
 import `in`.koreatech.koin.feature.lostandfound.ui.report.LostAndFoundReport
 import `in`.koreatech.koin.ui.article.ArticleActivity
 import `in`.koreatech.koin.ui.article.ArticleActivity.Companion.BUNDLE_ARTICLE_EXTRA_KEY
@@ -18,7 +16,7 @@ import `in`.koreatech.koin.ui.article.ArticleBoardType
 class LostAndFoundReportActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge(statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT))
+        enableEdgeToEdgeWithDarkStatusBar()
         setContent {
             LostAndFoundReport(
                 articleId = intent.getIntExtra("article_id", 0),
