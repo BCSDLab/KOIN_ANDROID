de469e4336c8e41d63cddfe8bd7820017393ee28
DohyeokKim <hyeokdokim@gmail.com>
2025-03-01 16:58:42 +0900

    feat: add LoggerUserData and usecase

---
+++ domain/src/main/java/in/koreatech/koin/domain/model/user/LoggerUserData.kt
@@ -0,0 +1,15 @@
+package `in`.koreatech.koin.domain.model.user
+
+/**
+ * 로깅에 사용되는 유저 정보
+ * @param userId {학번 앞 6자리}_{User ID}
+ * @param gender 남성: "0", 여성: "1", 알수없음: ""
+ * @param major 학생의 전공
+ *
+ * @see User
+ */
+data class LoggerUserData(
+    val userId: String,
+    val gender: String,
+    val major: String
+)
---
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/user/GetLoggerUserDataUseCase.kt
@@ -0,0 +1,42 @@
+package `in`.koreatech.koin.domain.usecase.user
+
+import `in`.koreatech.koin.domain.model.user.Gender
+import `in`.koreatech.koin.domain.model.user.LoggerUserData
+import `in`.koreatech.koin.domain.model.user.User
+import `in`.koreatech.koin.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetLoggerUserDataUseCase @Inject constructor(
+    private val userRepository: UserRepository
+) {
+    operator fun invoke(): Flow<LoggerUserData> = userRepository.getUserInfoFlow().map { it.toLoggerUserData() ?: LoggerUserData(
+        userId = "",
+        gender = "",
+        major = ""
+    ) }
+}
+
+/**
+ * 로깅에 사용하는 유저 데이터로 변환하는 함수
+ *
+ * @See LoggerUserData
+ */
+private fun User.toLoggerUserData(): LoggerUserData? = when (this) {
+    is User.Anonymous -> null
+    is User.Student -> {
+        val id_prefix = studentNumber?.substring(0..5) ?: "anonymous"
+        val id_postfix = id
+
+        LoggerUserData(
+            userId = "${id_prefix}_${id_postfix}",
+            gender = when(gender) {
+                is Gender.Man -> "0"
+                is Gender.Woman -> "1"
+                is Gender.Unknown -> ""
+            },
+            major = major ?: ""
+        )
+    }
+}
