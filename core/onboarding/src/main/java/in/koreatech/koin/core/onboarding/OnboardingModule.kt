a74d1cf0d0c178c18fb2ddd6a0e3111c19d4cf45
WooJin Kong <kongwoojin03@gmail.com>
2025-03-03 20:55:07 +0900

    fix: Fix ktlint on core:onboarding module

--- core/onboarding/src/main/java/in/koreatech/koin/core/onboarding/OnboardingModule.kt
+++ core/onboarding/src/main/java/in/koreatech/koin/core/onboarding/OnboardingModule.kt
@@ -4,9 +4,7 @@ import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.android.scopes.ActivityScoped
 import dagger.hilt.components.SingletonComponent
 import `in`.koreatech.koin.domain.repository.OnboardingRepository
 import javax.inject.Singleton
@@ -14,12 +12,11 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object OnboardingModule {
-
     @Provides
     @Singleton
     fun provideOnboardingManager(
         onboardingRepository: OnboardingRepository,
-        @ApplicationContext context: Context
+        @ApplicationContext context: Context,
     ): OnboardingManager {
         return OnboardingManager(onboardingRepository, context)
     }
