5861a71dff0bb017aa74f211b704c47a10e925c1
DohyeokKim <hyeokdokim@gmail.com>
2025-03-08 22:27:53 +0900

    feat: add timetableLecture api v3

--- data/src/main/java/in/koreatech/koin/data/api/TimetableApi.kt
+++ data/src/main/java/in/koreatech/koin/data/api/TimetableApi.kt
@@ -1,6 +1,7 @@
 package `in`.koreatech.koin.data.api
 
 import `in`.koreatech.koin.data.response.timetable.LectureResponse
+import `in`.koreatech.koin.data.response.timetable.v3.LectureResponseV3
 import `in`.koreatech.koin.data.response.timetable.v3.SemesterResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,8 +10,15 @@ interface TimetableApi {
     @GET("/v3/semesters")
     suspend fun getSemestersV3(): List<SemesterResponse>
 
+    @Deprecated("use getLecturesBySemester instead")
     @GET("/lectures")
     suspend fun getLectures(
         @Query("semester_date") semesterDate: String,
     ): List<LectureResponse>
+
+    @GET("/v3/lectures")
+    suspend fun getLecturesBySemester(
+        @Query("year") year: Int,
+        @Query("term") term: String,
+    ): List<LectureResponseV3>
 }
