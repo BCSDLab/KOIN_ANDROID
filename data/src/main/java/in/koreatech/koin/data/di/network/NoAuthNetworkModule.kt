b15d3dfb52090e44a2991f6e7e46eeb13edb5cd7
WooJin Kong <kongwoojin03@gmail.com>
2025-03-28 15:11:38 +0900

    fix: Fix NoAuthRetrofit function's name is Auth

--- data/src/main/java/in/koreatech/koin/data/di/network/NoAuthNetworkModule.kt
+++ data/src/main/java/in/koreatech/koin/data/di/network/NoAuthNetworkModule.kt
@@ -31,7 +31,7 @@ object NoAuthNetworkModule {
     @NoAuth
     @Provides
     @Singleton
-    fun provideAuthOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+    fun provideNoAuthOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
         return OkHttpClient.Builder().apply {
             connectTimeout(10, TimeUnit.SECONDS)
             readTimeout(30, TimeUnit.SECONDS)
@@ -43,7 +43,7 @@ object NoAuthNetworkModule {
     @NoAuth
     @Provides
     @Singleton
-    fun provideAuthRetrofit(
+    fun provideNoAuthRetrofit(
         @ServerUrl baseUrl: String,
         @NoAuth okHttpClient: OkHttpClient
     ): Retrofit {
