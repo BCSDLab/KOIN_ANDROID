6a2d08e80477d4553992f0ae0206e8a4c1a9f277
WooJin Kong <kongwoojin03@gmail.com>
2025-03-28 19:36:56 +0900

    chore: Update version to 4.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
     }
 
     extra.apply {
-        set("versionName", "4.3.4")
-        set("versionCode", 40304)
+        set("versionName", "4.3.5")
+        set("versionCode", 40305)
         // 코인 버전 관리
 
         set("versionBusinessName", "1.0.1")
