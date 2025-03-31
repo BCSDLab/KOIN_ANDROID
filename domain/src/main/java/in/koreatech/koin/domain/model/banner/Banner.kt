f406341cb2eff3e75f36b0e4a0a37d8faaf04308
KYM02 <gmyeongmin@gmail.com>
2025-03-31 18:28:32 +0900

    feat: add banner title data in BannerResponse and Banner model
    feat: add banner title data in BannerResponse and Banner model

--- domain/src/main/java/in/koreatech/koin/domain/model/banner/Banner.kt
+++ domain/src/main/java/in/koreatech/koin/domain/model/banner/Banner.kt
@@ -2,6 +2,7 @@ package `in`.koreatech.koin.domain.model.banner
 
 data class Banner(
     val id: Int,
+    val title: String,
     val imageUrl: String,
     val redirectLink: String?,
     val version: String?
