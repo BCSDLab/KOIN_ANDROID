d0887d38be1fabbf0502ba2e6d748c8f0e7982eb
kimjusang <jusang3057@gmail.com>
2025-04-08 19:01:43 +0900

    lint 수정

--- data/src/main/java/in/koreatech/koin/data/api/UserApi.kt
+++ data/src/main/java/in/koreatech/koin/data/api/UserApi.kt
@@ -15,7 +15,6 @@ import `in`.koreatech.koin.data.response.user.RefreshResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
 
