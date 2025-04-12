f02e343fc6fec36c9a2b6698bbf06ee709407a2c
WooJin Kong <kongwoojin03@gmail.com>
2025-04-13 01:46:21 +0900

    add: Add SignUpActivity

---
+++ feature/signup/src/main/java/in/koreatech/koin/feature/signup/SignUpActivity.kt
@@ -0,0 +1,48 @@
+package `in`.koreatech.koin.feature.signup
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import `in`.koreatech.koin.core.designsystem.component.topbar.KoinTopAppBar
+import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
+import `in`.koreatech.koin.feature.signup.navigation.SignUpNavType
+import `in`.koreatech.koin.feature.signup.navigation.koinSignUpGraph
+
+@AndroidEntryPoint
+class SignUpActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KoinTheme {
+                val navController = rememberNavController()
+
+                Scaffold(
+                    topBar = {
+                        KoinTopAppBar(
+                            title = stringResource(R.string.sign_up_title),
+                            onNavigationIconClick = {
+                                navController.popBackStack()
+                            }
+                        )
+                    },
+                    containerColor = KoinTheme.colors.neutral0
+                ) { contentPadding ->
+
+                }
+            }
+        }
+    }
+}
