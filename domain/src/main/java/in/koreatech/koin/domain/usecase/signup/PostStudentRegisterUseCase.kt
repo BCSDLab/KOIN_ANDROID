2a27277ad657ee9a9d314cb67aeed340ef7524f2
WooJin Kong <kongwoojin03@gmail.com>
2025-04-24 22:45:40 +0900

    add: Add sign up related usecase

---
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/signup/PostStudentRegisterUseCase.kt
@@ -0,0 +1,32 @@
+package `in`.koreatech.koin.domain.usecase.signup
+
+import `in`.koreatech.koin.domain.repository.SignupRepository
+import javax.inject.Inject
+
+class PostStudentRegisterUseCase @Inject constructor(
+    private val signupRepository: SignupRepository
+) {
+    suspend operator fun invoke(
+        name: String,
+        phoneNumber: String,
+        userId: String,
+        password: String,
+        department: String,
+        studentNumber: String,
+        gender: String,
+        email: String,
+        nickname: String
+    ) {
+        signupRepository.postStudentRegister(
+            name = name,
+            phoneNumber = phoneNumber,
+            userId = userId,
+            password = password,
+            department = department,
+            studentNumber = studentNumber,
+            gender = gender,
+            email = email,
+            nickname = nickname
+        )
+    }
+}
