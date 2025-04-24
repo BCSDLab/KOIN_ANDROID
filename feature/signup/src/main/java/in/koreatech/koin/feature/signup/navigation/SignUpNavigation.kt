7f4a064150e87cdb3ec155abbf2ec2f247d8aa9d
WooJin Kong <kongwoojin03@gmail.com>
2025-04-24 18:17:18 +0900

    chore: Fix ktlint

--- feature/signup/src/main/java/in/koreatech/koin/feature/signup/navigation/SignUpNavigation.kt
+++ feature/signup/src/main/java/in/koreatech/koin/feature/signup/navigation/SignUpNavigation.kt
@@ -2,15 +2,7 @@ package `in`.koreatech.koin.feature.signup.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavType
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
-import `in`.koreatech.koin.domain.model.user.Gender
-import `in`.koreatech.koin.feature.signup.ui.term.SignUpTermScreen
-import `in`.koreatech.koin.feature.signup.ui.userinfo.general.SignUpGeneralUserInfo
-import `in`.koreatech.koin.feature.signup.ui.userinfo.student.SignUpStudentUserInfo
-import `in`.koreatech.koin.feature.signup.ui.usertype.SignUpUserType
-import `in`.koreatech.koin.feature.signup.ui.verification.SignUpVerification
 
 fun NavGraphBuilder.koinSignUpGraph(
     navController: NavController
@@ -18,34 +10,27 @@ fun NavGraphBuilder.koinSignUpGraph(
     composable(
         route = SignUpNavType.Term.route
     ) {
-
     }
     composable(
         route = SignUpNavType.Verification.route
     ) {
-
     }
     composable(
         route = SignUpNavType.UserType.route
     ) {
-
     }
     composable(
         route = SignUpNavType.GeneralUserInfo.route
     ) {
-
     }
 
     composable(
         route = SignUpNavType.StudentUserInfo.route
     ) {
-
     }
 
     composable(
         route = SignUpNavType.SignUpComplete.route
     ) {
-
     }
 }
-
