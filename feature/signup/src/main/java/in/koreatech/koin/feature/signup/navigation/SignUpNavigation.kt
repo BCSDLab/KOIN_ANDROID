574a63cd5ff892cd7ede916a7d71b19031dbadbb
WooJin Kong <kongwoojin03@gmail.com>
2025-04-25 17:07:35 +0900

    feat: Add SignUpVerification page to navigation

--- feature/signup/src/main/java/in/koreatech/koin/feature/signup/navigation/SignUpNavigation.kt
+++ feature/signup/src/main/java/in/koreatech/koin/feature/signup/navigation/SignUpNavigation.kt
@@ -2,8 +2,11 @@ package `in`.koreatech.koin.feature.signup.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import `in`.koreatech.koin.feature.signup.ui.term.SignUpTermScreen
+import `in`.koreatech.koin.feature.signup.ui.verification.SignUpVerification
 
 fun NavGraphBuilder.koinSignUpGraph(
     navController: NavController
@@ -18,18 +21,36 @@ fun NavGraphBuilder.koinSignUpGraph(
     composable(
         route = SignUpNavType.Verification.route
     ) {
+        SignUpVerification { name, phoneNumber, gender ->
+            navController.navigate("${SignUpNavType.UserType.route}/$name/$phoneNumber/$gender")
+        }
     }
     composable(
-        route = SignUpNavType.UserType.route
+        route = "${SignUpNavType.UserType.route}/{$NAME}/{$PHONE_NUMBER}/{$GENDER}",
+        arguments = listOf(
+            navArgument(NAME) { type = NavType.StringType },
+            navArgument(PHONE_NUMBER) { type = NavType.StringType },
+            navArgument(GENDER) { type = NavType.StringType }
+        )
     ) {
     }
     composable(
-        route = SignUpNavType.GeneralUserInfo.route
+        route = "${SignUpNavType.GeneralUserInfo.route}/{$NAME}/{$PHONE_NUMBER}/{$GENDER}",
+        arguments = listOf(
+            navArgument(NAME) { type = NavType.StringType },
+            navArgument(PHONE_NUMBER) { type = NavType.StringType },
+            navArgument(GENDER) { type = NavType.StringType }
+        )
     ) {
     }
 
     composable(
-        route = SignUpNavType.StudentUserInfo.route
+        route = "${SignUpNavType.StudentUserInfo.route}/{$NAME}/{$PHONE_NUMBER}/{$GENDER}",
+        arguments = listOf(
+            navArgument(NAME) { type = NavType.StringType },
+            navArgument(PHONE_NUMBER) { type = NavType.StringType },
+            navArgument(GENDER) { type = NavType.StringType }
+        )
     ) {
     }
 
@@ -38,3 +59,7 @@ fun NavGraphBuilder.koinSignUpGraph(
     ) {
     }
 }
+
+const val PHONE_NUMBER = "phoneNumber"
+const val NAME = "name"
+const val GENDER = "gender"
