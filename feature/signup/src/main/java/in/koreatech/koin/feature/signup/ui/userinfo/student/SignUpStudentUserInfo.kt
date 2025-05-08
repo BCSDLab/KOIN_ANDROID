19363b42559fe398ef6e0d70c5e2eabc7fbc6a77
WooJin Kong <kongwoojin03@gmail.com>
2025-05-08 23:54:31 +0900

    fix: Student number text field should use number keypad

--- feature/signup/src/main/java/in/koreatech/koin/feature/signup/ui/userinfo/student/SignUpStudentUserInfo.kt
+++ feature/signup/src/main/java/in/koreatech/koin/feature/signup/ui/userinfo/student/SignUpStudentUserInfo.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -376,6 +377,7 @@ private fun SignUpStudentUserInfoNickNameEmailStep(
         value = studentNumber,
         onValueChange = { onStudentNumberChange(it) },
         keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number,
             imeAction = ImeAction.Next
         )
     )
