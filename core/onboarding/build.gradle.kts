3558764b5b183d55094fdc209540f9b7735a374c
WooJin Kong <kongwoojin03@gmail.com>
2025-05-09 14:56:48 +0900

    add: Add Balloon compose

--- core/onboarding/build.gradle.kts
+++ core/onboarding/build.gradle.kts
@@ -5,6 +5,14 @@ plugins {
 
 android {
     namespace = "in.koreatech.koin.core.onboarding"
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.10"
+    }
+
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
@@ -12,6 +20,9 @@ dependencies {
 
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose.m3)
     implementation(libs.timber)
     implementation(libs.balloon)
+    implementation(libs.balloon.compose)
 }
