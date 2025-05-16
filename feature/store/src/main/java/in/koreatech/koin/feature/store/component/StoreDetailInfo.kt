afff52e7083ef329076eabb497bf7cfbba716cc9
skdud0629 <skdud4081@gmail.com>
2025-05-16 12:02:15 +0900

    Add: StoreDetailInfo

---
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/component/StoreDetailInfo.kt
@@ -0,0 +1,59 @@
+package `in`.koreatech.koin.feature.store.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import `in`.koreatech.koin.domain.model.store.StoreReview
+import `in`.koreatech.koin.domain.model.store.StoreWithMenu
+import `in`.koreatech.koin.feature.store.R
+
+@Composable
+fun StoreDetailInfo(
+    storeInfo: StoreWithMenu,
+    storeReview: StoreReview,
+) {
+    Column(modifier = Modifier.padding(horizontal = 24.dp, vertical = 12.dp)) {
+        Text(modifier = Modifier.padding(vertical = 4.dp), text = storeInfo.name, fontWeight = FontWeight.Bold, fontSize = 20.sp)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    modifier = Modifier.size(25.dp),
+                    painter = painterResource(R.drawable.ic_star),
+                    contentDescription = null,
+                    tint = Color(0xFFFFC107)
+                )
+                Text(modifier = Modifier.padding(start = 5.dp), text = storeReview.statistics.averageRating.toString(), fontSize = 14.sp, fontWeight = FontWeight.SemiBold)
+                Text(text = stringResource(R.string.review_count, storeReview.totalCount), fontSize = 12.sp, fontWeight = FontWeight.Bold)
+                Icon(painter = painterResource(R.drawable.ic_right_arrow), contentDescription = null, modifier = Modifier.size(9.dp))
+            }
+            OriginInfoChips()
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        AvailableChips(storeInfo)
+        Spacer(modifier = Modifier.height(16.dp))
+        StoreDetailInfoCard(storeInfo)
+    }
+}
