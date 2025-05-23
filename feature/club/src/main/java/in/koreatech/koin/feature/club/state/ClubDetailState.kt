2d4d2ca3bb807affe5bdda1c80fc9b21e738a165
KYM02 <gmyeongmin@gmail.com>
2025-05-23 22:17:38 +0900

    feature: add Detail State
    feature: add Detail State

--- feature/club/src/main/java/in/koreatech/koin/feature/club/state/ClubDetailState.kt
+++ feature/club/src/main/java/in/koreatech/koin/feature/club/state/ClubDetailState.kt
@@ -1,8 +1,10 @@
 package `in`.koreatech.koin.feature.club.state
 
 import `in`.koreatech.koin.domain.model.club.ClubDetails
+import `in`.koreatech.koin.domain.model.club.ClubQnasInfo
 
 data class ClubDetailState(
     val isLogin: Boolean = false,
-    val clubDetails: ClubDetails? = null
+    val clubDetails: ClubDetails? = null,
+    val clubQnasInfo: ClubQnasInfo? = null
 )
