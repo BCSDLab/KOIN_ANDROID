c22c5ec86a23519a9eb02d476a407e4e351c7960
KYM02 <gmyeongmin@gmail.com>
2025-05-23 17:33:24 +0900

    fix: feat last branch data
    fix: feat last branch data

--- feature/club/src/main/java/in/koreatech/koin/feature/club/ui/ClubActivity.kt
+++ feature/club/src/main/java/in/koreatech/koin/feature/club/ui/ClubActivity.kt
@@ -1,5 +1,4 @@
 package `in`.koreatech.koin.feature.club.ui
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,14 +7,13 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
-import `in`.koreatech.koin.core.designsystem.util.enableEdgeToEdgeWithLightStatusBar
 import `in`.koreatech.koin.feature.club.navigation.ClubNavType
 import `in`.koreatech.koin.feature.club.navigation.koinClubGraph
 
 class ClubActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdgeWithLightStatusBar()
+        enableEdgeToEdge()
         setContent {
             KoinTheme {
                 val navController = rememberNavController()
@@ -23,7 +21,7 @@ class ClubActivity : ComponentActivity() {
                 NavHost(
                     modifier = Modifier,
                     navController = navController,
-                    startDestination = ClubNavType.ClubDetail.route
+                    startDestination = ClubNavType.ClubList.route
                 ) {
                     koinClubGraph(
                         navController = navController
--- feature/club/src/main/java/in/koreatech/koin/feature/club/ui/detail/type/DetailTabType.kt
+++
@@ -1,8 +0,0 @@
-package `in`.koreatech.koin.feature.club.ui.detail.type
-
-enum class DetailTabType (
-    val title: String
-){
-    DETAIL_INTRO("상세 소개"),
-    QNA("Q&A")
-}
