0a8ff12dd7c97c6e7cd7e3674b2c04231452a13e
WooJin Kong <kongwoojin03@gmail.com>
2025-05-24 01:17:31 +0900

    chore: Change main activity deep link

--- feature/signin/src/main/java/in/koreatech/koin/feature/signin/Constant.kt
+++ feature/signin/src/main/java/in/koreatech/koin/feature/signin/Constant.kt
@@ -1,6 +1,6 @@
 package `in`.koreatech.koin.feature.signin
 
-const val DEEPLINK_MAIN = "koin://home/home"
+const val DEEPLINK_MAIN = "koin://main/activity"
 const val DEEPLINK_TIMETABLE = "koin://timetable/activity"
 const val DEEPLINK_ARTICLE = "koin://article/activity"
 const val DEEPLINK_FIND_PASSWORD = "koin://findpassword/activity"
