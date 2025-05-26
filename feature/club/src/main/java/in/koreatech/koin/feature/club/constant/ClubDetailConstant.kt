5994c9cc11fc836da8dcdc91287e98301d2ed498
KYM02 <gmyeongmin@gmail.com>
2025-05-26 19:49:50 +0900

    feature: add club detail constant
    feature: add club detail constant

---
+++ feature/club/src/main/java/in/koreatech/koin/feature/club/constant/ClubDetailConstant.kt
@@ -0,0 +1,6 @@
+package `in`.koreatech.koin.feature.club.constant
+
+const val KOIN_WEB_URL = "https://koreatech.in/"
+const val KOIN_WEB_STAGE_URL = "https://stage.koreatech.in/"
+
+const val LOGIN_ACTIVITY_URL = "koin://login/login"
