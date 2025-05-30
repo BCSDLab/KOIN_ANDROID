9c126eaa75118c162d5fa5808d6486e79c0109e8
KYM02 <gmyeongmin@gmail.com>
2025-05-30 22:46:27 +0900

    fix: delete import
    fix: delete import

--- feature/club/src/main/java/in/koreatech/koin/feature/club/ui/detail/qna/ClubDetailQna.kt
+++ feature/club/src/main/java/in/koreatech/koin/feature/club/ui/detail/qna/ClubDetailQna.kt
@@ -5,10 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
