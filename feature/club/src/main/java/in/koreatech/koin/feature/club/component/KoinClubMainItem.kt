abaab0ffc884f5b4d0965ab6a0ac44cc6fc7a142
WooJin Kong <kongwoojin03@gmail.com>
2025-06-03 05:56:12 +0900

    chore: Fix ktlint

--- feature/club/src/main/java/in/koreatech/koin/feature/club/component/KoinClubMainItem.kt
+++ feature/club/src/main/java/in/koreatech/koin/feature/club/component/KoinClubMainItem.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowRight
 import androidx.compose.material3.Icon
