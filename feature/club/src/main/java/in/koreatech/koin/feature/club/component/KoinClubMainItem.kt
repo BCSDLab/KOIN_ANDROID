dff18e7faa4c3b514576079b3f9faa4abda4e310
WooJin Kong <kongwoojin03@gmail.com>
2025-06-03 05:02:11 +0900

    fix: Hide padding on onclick effect

--- feature/club/src/main/java/in/koreatech/koin/feature/club/component/KoinClubMainItem.kt
+++ feature/club/src/main/java/in/koreatech/koin/feature/club/component/KoinClubMainItem.kt
@@ -3,6 +3,7 @@ package `in`.koreatech.koin.feature.club.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,7 +15,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowRight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -36,9 +39,9 @@ fun KoinClubMainItem(
     Row(
         modifier = modifier
             .clip(RoundedCornerShape(8.dp))
+            .clickable { onClick() }
             .background(color = KoinTheme.colors.neutral50)
-            .padding(horizontal = 16.dp, vertical = 12.dp)
-            .clickable { onClick() },
+            .padding(horizontal = 16.dp, vertical = 12.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
