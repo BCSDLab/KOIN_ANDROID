8aa8c8bbcb972adbaa4956a901cce645c8f967c4
KYM02 <gmyeongmin@gmail.com>
2025-06-05 01:28:01 +0900

    fix: answer qna delete available
    fix: answer qna delete available

--- feature/club/src/main/java/in/koreatech/koin/feature/club/ui/clubdetail/component/qnabox/DetailQnaBox.kt
+++ feature/club/src/main/java/in/koreatech/koin/feature/club/ui/clubdetail/component/qnabox/DetailQnaBox.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
@@ -169,7 +168,7 @@ fun DetailQnaBox(
                                     .size(20.dp)
                                     .padding(end = 4.dp)
                                     .clickable {
-                                        showDeleteQnaDialog = Pair(true, qnaId)
+                                        showDeleteQnaDialog = Pair(true, answerQnaId)
                                     }
                             )
                         }
