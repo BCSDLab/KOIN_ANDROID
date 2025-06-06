f4b144c7ea27955507f3136cd8ff22c9127579f7
WooJin Kong <kongwoojin03@gmail.com>
2025-06-07 02:12:51 +0900

    refactor: UserResponse to StudentUserResponse

--- data/src/main/java/in/koreatech/koin/data/request/user/StudentUserRequest.kt
+++ data/src/main/java/in/koreatech/koin/data/request/user/StudentUserRequest.kt
@@ -2,14 +2,13 @@ package `in`.koreatech.koin.data.request.user
 
 import com.google.gson.annotations.SerializedName
 
-data class UserRequest(
+data class StudentUserRequest(
     @SerializedName("nickname") val nickname: String?,
     @SerializedName("name") val name: String?,
     @SerializedName("student_number") val studentNumber: String?,
     @SerializedName("major") val major: String?,
-    @SerializedName("identity") val identity: Int,
-    @SerializedName("is_graduated") val isGraduated: Boolean,
     @SerializedName("phone_number") val phoneNumber: String?,
     @SerializedName("gender") val gender: Int?,
-    @SerializedName("password") val hashedPassword: String?
+    @SerializedName("password") val hashedPassword: String?,
+    @SerializedName("email") val email: String?
 )
