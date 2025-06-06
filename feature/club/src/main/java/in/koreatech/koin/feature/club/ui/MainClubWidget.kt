663ca3d599437a917b7a64440d5918eb3c02d7ca
WooJin Kong <kongwoojin03@gmail.com>
2025-06-07 00:05:13 +0900

    add: Add arrow icon

--- feature/club/src/main/java/in/koreatech/koin/feature/club/ui/MainClubWidget.kt
+++ feature/club/src/main/java/in/koreatech/koin/feature/club/ui/MainClubWidget.kt
@@ -11,6 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowRight
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,6 +25,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
+import `in`.koreatech.koin.core.designsystem.noRippleClickable
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 import `in`.koreatech.koin.feature.club.R
 import `in`.koreatech.koin.feature.club.component.KoinClubCategoryItem
@@ -41,11 +45,28 @@ fun MainClubWidgetA(
             .fillMaxWidth()
             .padding(horizontal = 24.dp)
     ) {
-        Text(
-            text = stringResource(R.string.club_main_activity_widget_title),
-            style = KoinTheme.typography.bold18.copy(fontSize = 15.sp),
-            color = KoinTheme.colors.primary500
-        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(R.string.club_main_activity_widget_title),
+                style = KoinTheme.typography.bold18.copy(fontSize = 15.sp),
+                color = KoinTheme.colors.primary500
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Icon(
+                modifier = Modifier.noRippleClickable {
+                    Intent(context, ClubActivity::class.java).apply {
+                        context.startActivity(this)
+                    }
+                },
+                imageVector = Icons.AutoMirrored.Rounded.KeyboardArrowRight,
+                tint = KoinTheme.colors.primary500,
+                contentDescription = stringResource(R.string.club_main_activity_widget_title)
+            )
+        }
 
         Spacer(modifier = Modifier.height(12.dp))
 
