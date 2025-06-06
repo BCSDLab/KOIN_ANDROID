7ba6e24541d35ccecbfda06c7e4686e21983156f
WooJin Kong <kongwoojin03@gmail.com>
2025-06-07 04:15:25 +0900

    del: Remove old user info screen

--- koin/src/main/java/in/koreatech/koin/ui/userinfo/PwdVerificationState.kt
+++
@@ -1,8 +0,0 @@
-package `in`.koreatech.koin.ui.userinfo
-
-import `in`.koreatech.koin.common.UiStatus
-
-data class PwdVerificationState(
-    val status: UiStatus = UiStatus.Init,
-    val isEdited: Boolean
-)
--- koin/src/main/java/in/koreatech/koin/ui/userinfo/UserInfoState.kt
+++
@@ -1,7 +0,0 @@
-package `in`.koreatech.koin.ui.userinfo
-
-import `in`.koreatech.koin.common.UiStatus
-
-data class UserInfoState(
-    val status: UiStatus = UiStatus.Init
-)
