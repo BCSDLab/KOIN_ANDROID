367346756e38c731ed83a7c331a98d2860b8f75d
WooJin Kong <kongwoojin03@gmail.com>
2025-06-08 23:03:21 +0900

    fix: Add ime padding

--- feature/club/src/main/java/in/koreatech/koin/feature/club/ui/clubcreate/ClubCreateScreen.kt
+++ feature/club/src/main/java/in/koreatech/koin/feature/club/ui/clubcreate/ClubCreateScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -86,6 +87,7 @@ fun ClubCreateScreen(
     }
 
     Scaffold(
+        modifier = modifier.imePadding(),
         containerColor = KoinTheme.colors.neutral0,
         topBar = {
             KoinTopAppBar(
