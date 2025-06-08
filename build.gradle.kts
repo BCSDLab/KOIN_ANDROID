a99600ff447d110d9b63f2605046e0862261dd64
WooJin Kong <kongwoojin03@gmail.com>
2025-06-09 00:34:11 +0900

    release: v4.4.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
     }
 
     extra.apply {
-        set("versionName", "4.4.2")
-        set("versionCode", 40402)
+        set("versionName", "4.4.3")
+        set("versionCode", 40403)
         // 코인 버전 관리
 
         set("versionBusinessName", "1.0.1")
