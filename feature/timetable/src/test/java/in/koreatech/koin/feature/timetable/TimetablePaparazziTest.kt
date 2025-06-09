f40873fb587e5e8b9af97bc1d2839b39d99ec657
skdud0629 <skdud4081@gmail.com>
2025-06-09 16:15:07 +0900

    Add: custom test device

--- feature/timetable/src/test/java/in/koreatech/koin/feature/timetable/TimetablePaparazziTest.kt
+++ feature/timetable/src/test/java/in/koreatech/koin/feature/timetable/TimetablePaparazziTest.kt
@@ -7,6 +7,15 @@ import `in`.koreatech.koin.domain.model.timetable.response.TimetableLecture
 import org.junit.Rule
 import org.junit.Test
 
+val FOLDABLE_DEVICE = DeviceConfig(
+    screenWidth = 1800,
+    screenHeight = 2200,
+    xdpi = 480,
+    ydpi = 480,
+    fontScale = 1.0f,
+    softButtons = false,
+)
+
 class StoreDetailSnapshotTestPIXEL_6_PRO {
     @get:Rule
     val paparazzi = Paparazzi(
@@ -40,6 +49,36 @@ class StoreDetailSnapshotTestPIXEL_4 {
     @get:Rule
     val paparazzi = Paparazzi(
         deviceConfig = DeviceConfig.PIXEL_4.copy(softButtons = false)
+    )
+
+    @Test
+    fun snapshot_store_detail_FOLDABLE() {
+        paparazzi.snapshot(name = "store_detail_FOLDABLE") {
+            KoinTheme {
+                TimetableLecture(
+                    id = 1,
+                    lectureId = 1,
+                    code = "HRD011",
+                    classTitle = "직업능력개발훈련평가",
+                    professor = "우성민",
+                    grades = "2",
+                    lectureClass = "01",
+                    regularNumber = "40",
+                    department = "HRD학과",
+                    target = "전기3",
+                    classInfos = emptyList(),
+                    designScore = "0"
+                )
+            }
+        }
+    }
+}
+
+
+class StoreDetailSnapshotTestFoldable {
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = FOLDABLE_DEVICE.copy(softButtons = false)
     )
 
     @Test
