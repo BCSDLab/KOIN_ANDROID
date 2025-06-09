0de6681fa0cf013f3b681f40de71135b374f6ec3
skdud0629 <skdud4081@gmail.com>
2025-06-09 16:01:57 +0900

    Add: paparazzi test code

---
+++ feature/timetable/src/test/java/in/koreatech/koin/feature/timetable/TimetablePaparazziTest.kt
@@ -0,0 +1,69 @@
+package `in`.koreatech.koin.feature.timetable
+
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
+import `in`.koreatech.koin.domain.model.timetable.response.TimetableLecture
+import org.junit.Rule
+import org.junit.Test
+
+class StoreDetailSnapshotTestPIXEL_6_PRO {
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = DeviceConfig.PIXEL_6_PRO.copy(softButtons = false),
+    )
+
+    @Test
+    fun snapshot_store_detail_FOLDABLE() {
+        paparazzi.snapshot(name = "store_detail_FOLDABLE") {
+            KoinTheme {
+                TimetableLecture(
+                    id = 1,
+                    lectureId = 1,
+                    code = "HRD011",
+                    classTitle = "직업능력개발훈련평가",
+                    professor = "우성민",
+                    grades = "2",
+                    lectureClass = "01",
+                    regularNumber = "40",
+                    department = "HRD학과",
+                    target = "전기3",
+                    classInfos = emptyList(),
+                    designScore = "0"
+                )
+
+            }
+        }
+    }
+}
+
+
+class StoreDetailSnapshotTestPIXEL_4 {
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = DeviceConfig.PIXEL_4.copy(softButtons = false),
+    )
+
+    @Test
+    fun snapshot_store_detail_FOLDABLE() {
+        paparazzi.snapshot(name = "store_detail_FOLDABLE") {
+            KoinTheme {
+                TimetableLecture(
+                    id = 1,
+                    lectureId = 1,
+                    code = "HRD011",
+                    classTitle = "직업능력개발훈련평가",
+                    professor = "우성민",
+                    grades = "2",
+                    lectureClass = "01",
+                    regularNumber = "40",
+                    department = "HRD학과",
+                    target = "전기3",
+                    classInfos = emptyList(),
+                    designScore = "0"
+                )
+
+            }
+        }
+    }
+}
