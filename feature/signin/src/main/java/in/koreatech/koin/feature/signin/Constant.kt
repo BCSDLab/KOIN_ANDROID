733bc1d88169e0a8280770c23e7b38ad0dbf3bd6
WooJin Kong <kongwoojin03@gmail.com>
2025-06-14 16:00:39 +0900

    add: Add find id deeplink

--- feature/signin/src/main/java/in/koreatech/koin/feature/signin/Constant.kt
+++ feature/signin/src/main/java/in/koreatech/koin/feature/signin/Constant.kt
@@ -3,5 +3,6 @@ package `in`.koreatech.koin.feature.signin
 const val DEEPLINK_MAIN = "koin://main/activity"
 const val DEEPLINK_TIMETABLE = "koin://timetable/activity"
 const val DEEPLINK_ARTICLE = "koin://article/activity"
+const val DEEPLINK_FIND_ID = "koin://findid/activity"
 const val DEEPLINK_FIND_PASSWORD = "koin://findpassword/activity"
 const val DEEPLINK_SIGN_UP = "koin://signup/activity"
