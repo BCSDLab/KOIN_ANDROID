ff434d5d6e0890c696062e0596b66ec64fb9400e
WooJin Kong <kongwoojin03@gmail.com>
2025-06-15 01:26:21 +0900

    chore: Fix ktlint

--- feature/club/src/main/java/in/koreatech/koin/feature/club/ui/clubmodify/ClubModifyScreen.kt
+++ feature/club/src/main/java/in/koreatech/koin/feature/club/ui/clubmodify/ClubModifyScreen.kt
@@ -11,12 +11,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
