5786a84a26400671f5dbb0d0c855d97864d61ec0
WooJin Kong <kongwoojin03@gmail.com>
2025-06-19 08:07:40 +0900

    refactor: Add support edge to edge on splash

--- koin/src/main/java/in/koreatech/koin/ui/splash/SplashActivity.kt
+++ koin/src/main/java/in/koreatech/koin/ui/splash/SplashActivity.kt
@@ -4,8 +4,13 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup.MarginLayoutParams
 import androidx.activity.viewModels
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.os.bundleOf
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.lifecycle.lifecycleScope
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.google.android.play.core.install.model.UpdateAvailability
@@ -13,6 +18,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import `in`.koreatech.koin.BuildConfig
 import `in`.koreatech.koin.R
 import `in`.koreatech.koin.core.activity.ActivityBase
+import `in`.koreatech.koin.core.designsystem.util.enableEdgeToEdgeWithLightStatusBar
 import `in`.koreatech.koin.core.navigation.Navigator
 import `in`.koreatech.koin.core.navigation.NavigatorType
 import `in`.koreatech.koin.core.navigation.utils.EXTRA_ARTICLE_ID
@@ -57,6 +63,7 @@ class SplashActivity : ActivityBase() {
     private val createdTime = System.currentTimeMillis()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdgeWithLightStatusBar()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
@@ -68,8 +75,16 @@ class SplashActivity : ActivityBase() {
     private fun initView() {
         splashViewModel.checkUpdate()
         firebasePerformanceUtil.start()
-        SystemBarsUtils(this).apply {
-            setImmersiveMode(window)
+        val constraintLayoutSplashRoot = findViewById<ConstraintLayout>(R.id.constraint_layout_splash_root)
+        ViewCompat.setOnApplyWindowInsetsListener(constraintLayoutSplashRoot) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            view.updateLayoutParams<MarginLayoutParams> {
+                topMargin = systemBars.top
+                bottomMargin = systemBars.bottom
+            }
+
+            WindowInsetsCompat.CONSUMED
         }
     }
 
