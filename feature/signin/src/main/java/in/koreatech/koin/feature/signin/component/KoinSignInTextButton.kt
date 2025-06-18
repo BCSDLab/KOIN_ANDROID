ec748392b4541427f057d71f716580dcfaeee430
WooJin Kong <kongwoojin03@gmail.com>
2025-06-19 06:32:46 +0900

    chore: Fix ktlint

--- feature/signin/src/main/java/in/koreatech/koin/feature/signin/component/KoinSignInTextButton.kt
+++ feature/signin/src/main/java/in/koreatech/koin/feature/signin/component/KoinSignInTextButton.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import `in`.koreatech.koin.core.designsystem.noRippleClickable
