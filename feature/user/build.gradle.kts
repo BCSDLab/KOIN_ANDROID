7a86aa390ab5626f8aba3d923a4df883628f325c
WooJin Kong <kongwoojin03@gmail.com>
2025-06-20 14:58:37 +0900

    add: Add UI test dependency

--- feature/user/build.gradle.kts
+++ feature/user/build.gradle.kts
@@ -44,4 +44,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.compose.ui.test)
+    androidTestImplementation(libs.compose.ui.test.manifest)
 }
