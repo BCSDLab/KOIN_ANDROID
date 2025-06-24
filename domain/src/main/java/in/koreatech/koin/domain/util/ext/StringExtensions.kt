fbb378eccff818c1da926a6e5a3a6561b9157287
KYM02 <gmyeongmin@gmail.com>
2025-06-24 23:30:17 +0900

    fix: delete duplicate functions
    fix: delete duplicate functions

--- domain/src/main/java/in/koreatech/koin/domain/util/ext/StringExtensions.kt
+++ domain/src/main/java/in/koreatech/koin/domain/util/ext/StringExtensions.kt
@@ -2,22 +2,8 @@ package `in`.koreatech.koin.domain.util.ext
 
 import `in`.koreatech.koin.domain.constant.INSTAGRAM_URL
 import `in`.koreatech.koin.domain.util.regex.PasswordUtil
-import java.util.Calendar
 
 fun String.toSHA256() = PasswordUtil().generateSHA256(this)
-
-val String.isValidStudentId: Boolean
-    get() {
-        if (this.trim().length != 10) {
-            return false
-        }
-
-        val year: Int = this.trim().substring(0..3).toInt()
-        return year in 1992..Calendar.getInstance().get(Calendar.YEAR)
-    }
-
-val String.isValidPhoneNumber: Boolean get() =
-    this.trim().matches(Regex("""^(01[016789]{1})-?([0-9]{3,4})-?([0-9]{4})$"""))
 
 fun String.toUnderlineForHtml() = "<u>$this</u>"
 
@@ -26,14 +12,6 @@ fun String.toColorForHtml(color: String) = "<font color = '#${color.substring(3)
 fun String.formatInstagramUrlForm() = "${INSTAGRAM_URL}/$this"
 
 fun String.formatInstagramLinkForm() = "@${this.removePrefix("${INSTAGRAM_URL}/").removeSuffix("/")}"
-
-fun String.isNameFormat(): Boolean = this.matches(Regex("""^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$"""))
-
-fun String.isNicknameFormat(): Boolean = this.matches(Regex("""^[ㄱ-ㅎ가-힣a-zA-Z0-9]+${'$'}"""))
-
-fun String.formatPhoneNumber(): String = this.replace(Regex("(\\d{3})-?(\\d{4})-?(\\d{4})"), "$1-$2-$3")
-
-fun String.formatBusinessNumber(): String = this.replace(Regex("(\\d{3})(\\d{2})(\\d{5})"), "$1-$2-$3")
 
 fun Int.formatTime(): String {
     val time = this
