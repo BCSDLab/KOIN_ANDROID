e86010bb2efd7a162cecbca328b151ef967a4d07
KYM02 <gmyeongmin@gmail.com>
2025-06-26 22:25:49 +0900

    refactor: add endpoint constant
    refactor: add endpoint constant

--- data/src/main/java/in/koreatech/koin/data/constant/URLConstant.kt
+++ data/src/main/java/in/koreatech/koin/data/constant/URLConstant.kt
@@ -13,15 +13,8 @@ object URLConstant {
     const val OWNER_URL_STAGE = "https://owner.stage.koreatech.in/"
     const val OWNER_URL_PRODUCTION = "https://owner.koreatech.in/"
 
-    const val ADMIN = "admin/"
     const val VERSION = "version"
-    const val FAQ = "faqs"
-    const val LECTURE = "lectures"
-    const val TIMETABLE = "timetable"
-    const val TIMETABLES = "timetables"
-    const val SEMESTERS = "semesters"
     const val LAND = "lands"
-    const val TERM = "term"
     const val COOPSHOP = "coopshop"
 
     object DINING {
@@ -35,33 +28,53 @@ object URLConstant {
 
     object SHOPS {
         const val OWNERSHOPS = "owner/shops"
+        const val ID = "{id}"
+        const val MENUID = "{menuId}"
+        const val SHOPID = "{shopId}"
+        const val INFO = "$OWNERSHOPS/$ID"
+        const val MENUS = "$OWNERSHOPS/menus"
+        const val POST_MENU = "$OWNERSHOPS/$ID/menus"
+        const val MODIFY_MENU = "$OWNERSHOPS/menus/$MENUID"
+        const val MENU_INFO = "$OWNERSHOPS/menus/$MENUID"
+        const val MODIFY_INFO = "$OWNERSHOPS/$SHOPID"
+        const val GET_EVENTS = "$OWNERSHOPS/$SHOPID/event"
+        const val DELETE_EVENTS = "$OWNERSHOPS/$SHOPID/events/{eventId}"
+        const val POST_EVENT = "$OWNERSHOPS/$ID/event"
         const val SHOPS = "shops"
+        const val REVIEWID = "{reviewId}"
         const val SHOPS_V2 = "/v2/shops"
         const val EVENTS = "$SHOPS/events"
         const val CATERGORIES = "$SHOPS/categories"
+        const val GET_REVIEWS = "$SHOPS/$ID/reviews"
+        const val WRITE_REVIEWS = "$SHOPS/$SHOPID/reviews"
+        const val DELETE_REVIEWS = "$SHOPS/$SHOPID/reviews/$REVIEWID"
+        const val MODIFY_REVIEWS = "$SHOPS/$SHOPID/reviews/$REVIEWID"
+        const val REPORTS = "$SHOPS/$SHOPID/reviews/$REVIEWID/reports"
+        const val SHOP_NOTIFICATION = "$SHOPS/$SHOPID/call-notification"
     }
 
     object USER {
         const val USER = "user"
         const val LOGIN: String = "$USER/login"
-        const val LOGOUT: String = "$USER/logout"
-        const val REGISTER: String = "$USER/register"
         const val FINDPASSWORD: String = "$USER/find/password"
         const val ME: String = "$USER/student/me"
         const val REFRESH: String = "$USER/refresh"
         const val CHECKNICKNAME: String = "$USER/check/nickname"
         const val CHECKEMAIL: String = "$USER/check/email"
         const val CHECKPASSWORD: String = "$USER/check/password"
-        const val PROFILEUPLOAD: String = "$USER/profile/upload"
         const val CHECKPHONE: String = "$USER/check/phone"
         const val CHECKNICKNAME_V2: String = "$USER/check/nickname"
         const val SMSSEND: String = "$USER/verification/send"
         const val SMSVERIFY: String = "$USER/verification/verify"
         const val SMSCOUNT: String = "$USER/verification/count"
         const val AUTH: String = "$USER/auth"
-        const val ID = "portal_account"
         const val EMAIL = "email"
         const val PW = "password"
+        const val NICKNAME = "{nickname}"
+        const val AUTH_CHECKNICKNAME = "$CHECKNICKNAME/$NICKNAME"
+        const val NOTIFICATION = "/notification"
+        const val SUBSCRIBE = "$NOTIFICATION/subscribe"
+        const val DETAIL = "$NOTIFICATION/subscribe/detail"
 
         const val VERSION: String = "v2"
         object STUDENT {
@@ -94,7 +107,6 @@ object URLConstant {
         const val CHANGEPASSWORDSMS = "$OWNERS/$PASSWORD/$RESET/sms"
         const val CODE_SMS = "$OWNERS/$VERIFICATION/code/sms"
         const val SMS = "$OWNERS/$VERIFICATION/sms"
-        const val PW = "password"
         const val SHOPS = "$OWNER/shops"
         const val EXISTS_ACCOUNT = "$OWNERS/exists/account"
     }
@@ -114,9 +126,9 @@ object URLConstant {
 
     object CLUBS {
         const val CLUBS = "clubs"
+        const val CLUBID = "{clubId}"
         const val CATEGORIES = "$CLUBS/categories"
         const val HOT = "$CLUBS/hot"
-        const val CLUBID = "{clubId}"
         const val QNA = "$CLUBS/$CLUBID/qna"
         const val DELETE_QNA = "$QNA/{qnaId}"
         const val DETAILS = "$CLUBS/$CLUBID"
@@ -124,5 +136,60 @@ object URLConstant {
         const val LIKE = "$CLUBS/$CLUBID/like"
         const val CANCEL_LIKE = "$LIKE/cancel"
         const val EMPOWERMENT = "$CLUBS/empowerment"
+    }
+
+    object ARTICLES {
+        const val ARTICLES = "articles"
+        const val ID = "{id}"
+        const val KEYWORD = "$ARTICLES/keyword"
+        const val FETCH_KEYWORD = "$KEYWORD/me"
+        const val DELETE_KEYWORD = "$KEYWORD/$ID"
+        const val SUGGESTIONS = "$KEYWORD/suggestions"
+        const val LOSTANDFOUND = "$ARTICLES/lost-item"
+        const val DELETE_LOSTANDFOUND = "$LOSTANDFOUND/$ID"
+        const val REPORT_LOSTANDFOUND = "$LOSTANDFOUND/$ID/reports"
+    }
+
+    object CHAT {
+        const val CHATROOM = "chatroom/lost-item"
+        const val ARTICLEID = "{articleId}"
+        const val CHATROOMID = "{chat_room_id}"
+        const val CHATROOM_ARTICLEID = "$CHATROOM/$ARTICLEID"
+        const val CHATROOM_ARTICLEID_ROOMID = "$CHATROOM/$ARTICLEID/$CHATROOMID"
+        const val MESSAGES = "$CHATROOM/$ARTICLEID/$CHATROOMID/messages"
+        const val BLOCK = "$CHATROOM/$ARTICLEID/$CHATROOMID/block"
+    }
+
+    object TIMETABLE {
+        const val V2 = "v2"
+        const val V3 = "v3"
+        const val TIMETABLE = "timetables"
+        const val ID = "{id}"
+        const val FRAMEID = "{frameId}"
+        const val LECTUREID = "{lectureId}"
+        const val SEMESTERS = "/$V3/semesters/check"
+        const val V2_LECTURE = "/$V2/$TIMETABLE/lecture"
+        const val V3_LECTURE = "/$V2/$TIMETABLE/lecture"
+        const val REGULER = "/$V3/$TIMETABLE/lecture/regular"
+        const val CUSTOM = "/$V3/$TIMETABLE/lecture/custom"
+        const val V2_FRAME = "/$V2/$TIMETABLE/frame/$ID"
+        const val V3_FRAME = "/$V3/$TIMETABLE/frame/$ID"
+        const val V2_DELETE_FRAME = "/$V2/$TIMETABLE/frame"
+        const val V2_DELETE_FRAMES = "/$V2/all/$TIMETABLE/frame"
+        const val V3_DELETE_FRAME = "/$V3/$TIMETABLE/frames"
+        const val V2_GET_FRAME = "/$V2/$TIMETABLE/frames"
+        const val V3_GET_FRAME = "/$V3/$TIMETABLE/frame"
+        const val V3_GET_FRAMES = "/$V3/$TIMETABLE/frames"
+        const val V2_ROLLBACK = "/$V2/$TIMETABLE/frame/rollback"
+        const val V3_ROLLBACK = "/$V3/$TIMETABLE/frame/rollback"
+        const val V2_DELETE_LECTURE = "/$V2/$TIMETABLE/lecture/$ID"
+        const val V2_DELETE_FRAME_LECTURE = "/$V2/$TIMETABLE/frame/$FRAMEID/lecture/$LECTUREID"
+        const val V2_DELETE_LECTURES = "/$V2/$TIMETABLE/lectures"
+    }
+
+    object ABTEST {
+        const val ABTEST = "abtest"
+        const val UPDATE = "$ABTEST/assign/token"
+        const val ASSIGN = "$ABTEST/assign"
     }
 }
