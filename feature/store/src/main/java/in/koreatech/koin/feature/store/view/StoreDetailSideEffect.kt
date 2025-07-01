80bc1287d49924dab88157855f7d9342302a79c9
skdud0629 <skdud4081@gmail.com>
2025-07-01 20:42:52 +0900

    Add: StoreDetailSideEffect

---
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/StoreDetailSideEffect.kt
@@ -0,0 +1,9 @@
+package `in`.koreatech.koin.feature.store.view
+
+sealed class StoreDetailSideEffect {
+    data object NavigateToBack: StoreDetailSideEffect()
+    data object NavigateToShoppingCart : StoreDetailSideEffect()
+    data object NavigateToReview: StoreDetailSideEffect()
+    data object NavigateToDeliveryInfo : StoreDetailSideEffect()
+    data object NavigateToDetailInfo : StoreDetailSideEffect()
+}
