316dd6d008d68c6a917f5d20e3f8a49021d2c9a8
skdud0629 <skdud4081@gmail.com>
2025-07-01 20:26:53 +0900

    Add: StoreDetailState

---
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/StoreDetailState.kt
@@ -0,0 +1,18 @@
+package `in`.koreatech.koin.feature.store.view
+
+import `in`.koreatech.koin.domain.model.store.ShopMenus
+import `in`.koreatech.koin.domain.model.store.StoreDetailScrollType
+import `in`.koreatech.koin.domain.model.store.StoreMenu
+import `in`.koreatech.koin.domain.model.store.StoreReview
+import `in`.koreatech.koin.domain.model.store.StoreWithMenu
+
+
+data class StoreDetailState(
+    val store: StoreWithMenu = StoreWithMenu.empty(),
+    val categories: StoreMenu = StoreMenu(menuCategories = emptyList()),
+    val storeMenu: List<ShopMenus> = emptyList(),
+    val storeReview: StoreReview = StoreReview.empty(),
+    val scrollUp: StoreDetailScrollType = StoreDetailScrollType.NONE,
+    val isLogin: Boolean = false,
+    val isLoading: Boolean = true,
+)
