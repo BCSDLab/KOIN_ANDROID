74a1e0a0d0b98b4ed8c6c0a8c0e77bb1f1ee5af5
skdud0629 <skdud4081@gmail.com>
2025-07-06 14:08:54 +0900

    Del: StoreDetailSideEffect value

--- feature/store/src/main/java/in/koreatech/koin/feature/store/view/StoreDetailSideEffect.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/StoreDetailSideEffect.kt
@@ -1,8 +1,4 @@
 package `in`.koreatech.koin.feature.store.view
 
 sealed class StoreDetailSideEffect {
-    data object NavigateToBack : StoreDetailSideEffect()
-    data object NavigateToShoppingCart : StoreDetailSideEffect()
-    data object NavigateToReview : StoreDetailSideEffect()
-    data object NavigateToDetailInfo : StoreDetailSideEffect()
 }
