1dc9b63b43f1307201c546bc90114b20a2e6b745
skdud0629 <skdud4081@gmail.com>
2025-07-07 12:11:08 +0900

    Fix: ShoppingCartScreen

--- feature/store/src/main/java/in/koreatech/koin/feature/store/view/ShoppingCartScreen.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/ShoppingCartScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -20,9 +21,11 @@ import `in`.koreatech.koin.core.designsystem.component.topbar.KoinTopAppBar
 import `in`.koreatech.koin.core.designsystem.noRippleClickable
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 import `in`.koreatech.koin.core.designsystem.theme.RebrandKoinTheme
+import `in`.koreatech.koin.domain.model.cart.Cart
 import `in`.koreatech.koin.domain.model.owner.StoreDetailInfo
 import `in`.koreatech.koin.feature.store.R
 import `in`.koreatech.koin.feature.store.viewmodel.ShoppingCartViewModel
+import org.orbitmvi.orbit.compose.collectAsState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,10 +33,9 @@ fun ShoppingCartScreen(
     viewModel: ShoppingCartViewModel = hiltViewModel(),
     onBackClick: () -> Unit,
     onDeleteAllClick: () -> Unit,
-    storeInfo: StoreDetailInfo,
-    isEmpty: Boolean
 ) {
-    viewModel.getCart()
+    val uiState by viewModel.collectAsState()
+
     Scaffold(
         topBar = {
             KoinTopAppBar(
@@ -44,18 +46,22 @@ fun ShoppingCartScreen(
                 onNavigationIconClick = onBackClick,
                 actions = {
                     Text(
-                        color = RebrandKoinTheme.colors.primary500,
+                        color = if(uiState.cart.items.isEmpty()) RebrandKoinTheme.colors.primary300 else  RebrandKoinTheme.colors.primary500,
                         fontWeight = SemiBold,
                         text = stringResource(R.string.delete_all),
                         modifier = Modifier
                             .padding(end = 10.dp)
-                            .noRippleClickable { onDeleteAllClick() }
+                            .noRippleClickable {
+                                if(uiState.cart.items.isEmpty())
+                                    return@noRippleClickable
+                                onDeleteAllClick()
+                            }
                     )
                 }
             )
         }
     ) { innerPadding ->
-        if (isEmpty) {
+        if (uiState.cart.items.isEmpty()) {
             ShoppingCartEmptyContent(
                 modifier = Modifier
                     .padding(innerPadding)
@@ -67,7 +73,7 @@ fun ShoppingCartScreen(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize(),
-            storeInfo = storeInfo
+            cart = uiState.cart,
         )
     }
 }
@@ -80,23 +86,6 @@ private fun ShoppingCartItem() {
             ShoppingCartScreen(
                 onBackClick = {},
                 onDeleteAllClick = {},
-                storeInfo = StoreDetailInfo(
-                    address = "서울시 강남구 역삼동 123-45",
-                    name = "테스트 가게",
-                    description = "테스트 가게 설명",
-                    imageUrls = listOf("https://example.com/image.jpg"),
-                    mainCategoryId = 1,
-                    categoryIds = listOf(1, 2, 3),
-                    isBankOk = true,
-                    isCardOk = true,
-                    isDeliveryOk = true,
-                    phone = "010-1234-5678",
-                    accountNumber = "123-456-7890",
-                    deliveryPrice = 3000,
-                    operatingTime = null,
-                    bank = "우리은행"
-                ),
-                isEmpty = false
             )
         }
     }
