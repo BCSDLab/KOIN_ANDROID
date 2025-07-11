73b748cef61db89e7c0647456e3718548bb4e782
WooJin Kong <kongwoojin03@gmail.com>
2025-07-11 13:47:52 +0900

    chore: Fix ktlint

--- feature/club/src/main/java/in/koreatech/koin/feature/club/ui/clublist/ClubListScreen.kt
+++ feature/club/src/main/java/in/koreatech/koin/feature/club/ui/clublist/ClubListScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
