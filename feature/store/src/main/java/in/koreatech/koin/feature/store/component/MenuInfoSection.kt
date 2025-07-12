0559f9ed02d646b5aec10f20ff6e80ad21878223
WooJin Kong <kongwoojin03@gmail.com>
2025-07-12 12:59:58 +0900

    chore: Add missing string

--- feature/store/src/main/java/in/koreatech/koin/feature/store/component/MenuInfoSection.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/component/MenuInfoSection.kt
@@ -8,8 +8,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import `in`.koreatech.koin.R
 import `in`.koreatech.koin.core.designsystem.theme.RebrandKoinTheme
+import `in`.koreatech.koin.feature.store.R
 
 @Composable
 fun MenuInfoSection(
