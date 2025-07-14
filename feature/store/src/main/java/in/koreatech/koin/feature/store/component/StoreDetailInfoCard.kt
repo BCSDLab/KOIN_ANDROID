5f712501ac6266921a7574e63c1d8b927ad803cd
skdud0629 <skdud4081@gmail.com>
2025-07-14 19:35:38 +0900

    Fix: wrong code

--- feature/store/src/main/java/in/koreatech/koin/feature/store/component/StoreDetailInfoCard.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/component/StoreDetailInfoCard.kt
@@ -95,7 +95,7 @@ fun DeliveryInfoCard(
                 Row {
                     Text(text = stringResource(R.string.delivery_fee), fontSize = 12.sp, lineHeight = 18.sp)
                     Spacer(modifier = Modifier.width(8.dp))
-                    Text(stringResource(R.string.price_with_won,storeInfo.minimumDeliveryTip?:""), fontSize = 12.sp, lineHeight = 18.sp, color = KoinTheme.colors.neutral500)
+                    Text(stringResource(R.string.price_with_won,storeInfo.minimumDeliveryTip?:0), fontSize = 12.sp, lineHeight = 18.sp, color = KoinTheme.colors.neutral500)
                 }
             }
             Icon(painter = painterResource(id = R.drawable.ic_delivery_arrow_right), contentDescription = null, modifier = Modifier.size(10.dp))
