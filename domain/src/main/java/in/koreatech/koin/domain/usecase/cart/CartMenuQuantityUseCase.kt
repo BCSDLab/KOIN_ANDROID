3b39733a6def17ef5ff3081a45d0200c45aaa67e
skdud0629 <skdud4081@gmail.com>
2025-07-14 14:00:20 +0900

    Fix: change emitAll to suspend operator function

--- domain/src/main/java/in/koreatech/koin/domain/usecase/cart/CartMenuQuantityUseCase.kt
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/cart/CartMenuQuantityUseCase.kt
@@ -1,15 +1,11 @@
 package `in`.koreatech.koin.domain.usecase.cart
 
 import `in`.koreatech.koin.domain.repository.CartRepository
-import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
 class CartMenuQuantityUseCase @Inject constructor(
     private val cartRepository: CartRepository
 ) {
-    operator fun invoke(cartMenuItemId: Int, quantity: Int): Flow<Unit> = flow {
-        emitAll(cartRepository.cartMenuQuantity(cartMenuItemId, quantity))
-    }
+    suspend operator fun invoke(cartMenuItemId: Int, quantity: Int): Flow<Unit> = cartRepository.cartMenuQuantity(cartMenuItemId, quantity)
 }
--- domain/src/main/java/in/koreatech/koin/domain/usecase/cart/DeleteCartMenuItemUseCase.kt
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/cart/DeleteCartMenuItemUseCase.kt
@@ -1,15 +1,11 @@
 package `in`.koreatech.koin.domain.usecase.cart
 
 import `in`.koreatech.koin.domain.repository.CartRepository
-import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
 class DeleteCartMenuItemUseCase @Inject constructor(
     private val cartRepository: CartRepository
 ) {
-    operator fun invoke(cartMenuItemId: Int): Flow<Unit> = flow {
-        emitAll(cartRepository.deleteCartMenuItem(cartMenuItemId))
-    }
+    suspend operator fun invoke(cartMenuItemId: Int): Flow<Unit> = cartRepository.deleteCartMenuItem(cartMenuItemId)
 }
