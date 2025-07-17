dc8577fc1763121266938a2cbf035f2dde3a4f36
KYM02 <gmyeongmin@gmail.com>
2025-07-17 17:35:57 +0900

    fix: delete unused import
    fix: delete unused import

--- feature/club/src/main/java/in/koreatech/koin/feature/club/ui/clubeventmodify/ClubEventModifyScreen.kt
+++ feature/club/src/main/java/in/koreatech/koin/feature/club/ui/clubeventmodify/ClubEventModifyScreen.kt
@@ -17,14 +17,12 @@ import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.HorizontalPager
