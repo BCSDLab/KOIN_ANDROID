1fce70d277da9892d8084df1a68500fe1905349f
WooJin Kong <kongwoojin03@gmail.com>
2025-07-18 02:50:12 +0900

    feat: Implement no store screen

--- feature/store/src/main/java/in/koreatech/koin/feature/store/view/main/home/StoreHomeScreen.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/main/home/StoreHomeScreen.kt
@@ -1,5 +1,6 @@
 package `in`.koreatech.koin.feature.store.view.main.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -20,6 +22,7 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Text
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarDefaults
@@ -30,9 +33,11 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
@@ -258,16 +263,46 @@ private fun StoreHomeScreen(
                     .padding(horizontal = 24.dp),
                 verticalArrangement = Arrangement.spacedBy(12.dp)
             ) {
-                items(storeList) {
-                    KoinStoreCard(
-                        modifier = Modifier.fillMaxWidth(),
-                        storeName = it.name,
-                        storeAverageRating = it.ratingAverage.toString(),
-                        storeReviewCount = it.reviewCount,
-                        storeDeliveryFee = it.minimumDeliveryTip.toString(),
-                        storeImageUrl = it.imageUrls.firstOrNull() ?: "",
-                        isOpen = it.isOpen
-                    )
+                if (!isLoading && storeList.isEmpty()) {
+                    item {
+                        Column(
+                            modifier = Modifier.aspectRatio(1f),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Image(
+                                imageVector = ImageVector.vectorResource(R.drawable.ic_store_no_store),
+                                contentDescription = null
+                            )
+
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            Text(
+                                text = stringResource(R.string.store_list_empty),
+                                style = RebrandKoinTheme.typography.bold18.copy(
+                                    color = RebrandKoinTheme.colors.primary500
+                                )
+                            )
+                            Text(
+                                text = stringResource(R.string.store_list_empty_description),
+                                style = RebrandKoinTheme.typography.regular14.copy(
+                                    color = RebrandKoinTheme.colors.neutral600
+                                )
+                            )
+                        }
+                    }
+                } else {
+                    items(storeList) {
+                        KoinStoreCard(
+                            modifier = Modifier.fillMaxWidth(),
+                            storeName = it.name,
+                            storeAverageRating = it.ratingAverage.toString(),
+                            storeReviewCount = it.reviewCount,
+                            storeDeliveryFee = it.minimumDeliveryTip.toString(),
+                            storeImageUrl = it.imageUrls.firstOrNull() ?: "",
+                            isOpen = it.isOpen
+                        )
+                    }
                 }
             }
 
