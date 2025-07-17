114decd0951574cf2868e5763e6d558ef5c43eb1
WooJin Kong <kongwoojin03@gmail.com>
2025-07-18 02:37:37 +0900

    feat: Implement store main

---
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/main/home/StoreHomeState.kt
@@ -0,0 +1,21 @@
+package `in`.koreatech.koin.feature.store.view.main.home
+
+import `in`.koreatech.koin.feature.store.enums.MinimumPriceOption
+import `in`.koreatech.koin.feature.store.enums.OrderOption
+import `in`.koreatech.koin.feature.store.enums.StoreFilter
+import `in`.koreatech.koin.feature.store.model.LocalShop
+import `in`.koreatech.koin.feature.store.model.LocalStoreCategories
+
+data class StoreHomeState(
+    val isLoading: Boolean = true,
+    val categoryId: Int = -1,
+    val showSearch: Boolean = false,
+    val query: String = "",
+    val storeCategories: List<LocalStoreCategories> = listOf(),
+    val orderableShops: List<LocalShop> = listOf(),
+    val showOrderOptions: Boolean = false,
+    val showMinimumPriceOptions: Boolean = false,
+    val selectedOrderOption: OrderOption = OrderOption.NONE,
+    val selectedStoreFilter: List<StoreFilter> = listOf(StoreFilter.IS_OPEN),
+    val selectedMinimumPriceOption: MinimumPriceOption = MinimumPriceOption.ALL
+)
---
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/main/home/StoreHomeViewModel.kt
@@ -0,0 +1,133 @@
+package `in`.koreatech.koin.feature.store.view.main.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import `in`.koreatech.koin.domain.usecase.store.GetOrderableShopsUseCase
+import `in`.koreatech.koin.domain.usecase.store.GetStoreCategoriesUseCase
+import `in`.koreatech.koin.feature.store.enums.MinimumPriceOption
+import `in`.koreatech.koin.feature.store.enums.OrderOption
+import `in`.koreatech.koin.feature.store.enums.StoreFilter
+import `in`.koreatech.koin.feature.store.model.toLocalShop
+import `in`.koreatech.koin.feature.store.model.toLocalStoreCategories
+import javax.inject.Inject
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.blockingIntent
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
+import org.orbitmvi.orbit.viewmodel.container
+
+@HiltViewModel
+class StoreHomeViewModel @Inject constructor(
+    private val getStoreCategoriesUseCase: GetStoreCategoriesUseCase,
+    private val getOrderableShopsUseCase: GetOrderableShopsUseCase
+) : ViewModel(), ContainerHost<StoreHomeState, StoreHomeSideEffect> {
+    override val container = container<StoreHomeState, StoreHomeSideEffect>(StoreHomeState())
+
+    init {
+        intent {
+            getStoreCategoriesUseCase().let {
+                reduce {
+                    state.copy(
+                        storeCategories = it.map { it.toLocalStoreCategories() }
+                    )
+                }
+            }
+        }
+    }
+
+    fun fetchData() = intent {
+        reduce {
+            state.copy(
+                isLoading = true
+            )
+        }
+        getOrderableShopsUseCase(
+            sorter = state.selectedOrderOption.name,
+            filter = state.selectedStoreFilter.map { it.name },
+            categoryId = state.categoryId,
+            minimumOrderAmount = state.selectedMinimumPriceOption.price
+        ).onSuccess {
+            reduce {
+                state.copy(
+                    orderableShops = it.map {
+                        it.toLocalShop()
+                    },
+                    isLoading = false
+                )
+            }
+        }
+    }
+
+    fun onShowOrderOptionsChange(showOrderOptions: Boolean) = intent {
+        reduce {
+            state.copy(
+                showOrderOptions = showOrderOptions
+            )
+        }
+    }
+
+    fun onSelectedOrderOptionChange(orderOption: OrderOption) = intent {
+        reduce {
+            state.copy(
+                selectedOrderOption = orderOption
+            )
+        }
+    }
+
+    fun onSelectedStoreFilterChange(selectedStoreFilter: StoreFilter) = intent {
+        reduce {
+            state.copy(
+                selectedStoreFilter = if (state.selectedStoreFilter.contains(selectedStoreFilter)) {
+                    state.selectedStoreFilter - selectedStoreFilter
+                } else {
+                    state.selectedStoreFilter + selectedStoreFilter
+                }
+            )
+        }
+    }
+
+    fun onShowMinimumPriceOptionsChange(showMinimumPriceOptions: Boolean) = intent {
+        reduce {
+            state.copy(
+                showMinimumPriceOptions = showMinimumPriceOptions
+            )
+        }
+    }
+
+    fun onSelectedMinimumPriceOptionChange(selectedMinimumPriceOption: MinimumPriceOption) = intent {
+        reduce {
+            state.copy(
+                selectedMinimumPriceOption = selectedMinimumPriceOption
+            )
+        }
+    }
+
+    fun onShowSearchChange(showSearch: Boolean) = intent {
+        reduce {
+            state.copy(
+                showSearch = showSearch
+            )
+        }
+    }
+
+    fun onCategoryChange(categoryId: Int) = intent {
+        if (categoryId == state.categoryId) return@intent
+        reduce {
+            state.copy(
+                categoryId = categoryId
+            )
+        }
+    }
+
+    fun onQueryChange(query: String) = blockingIntent {
+        reduce {
+            state.copy(
+                query = query
+            )
+        }
+    }
+
+    fun onSearch() = intent {
+        // TODO
+    }
+}
