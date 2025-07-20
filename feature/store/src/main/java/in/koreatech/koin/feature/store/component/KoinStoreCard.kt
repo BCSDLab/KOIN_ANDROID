1d62064bfc1e4cb9b7c42031ffd92a6c77cb75e0
WooJin Kong <kongwoojin03@gmail.com>
2025-07-20 15:21:36 +0900

    fix: Make badge scrollable

--- feature/store/src/main/java/in/koreatech/koin/feature/store/component/KoinStoreCard.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/component/KoinStoreCard.kt
@@ -2,6 +2,7 @@ package `in`.koreatech.koin.feature.store.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.CircularProgressIndicator
@@ -149,6 +151,7 @@ fun KoinStoreCard(
                     Spacer(modifier = Modifier.height(8.dp))
 
                     Row(
+                        modifier = Modifier.horizontalScroll(rememberScrollState()),
                         verticalAlignment = Alignment.CenterVertically,
                         horizontalArrangement = Arrangement.spacedBy(8.dp)
                     ) {
