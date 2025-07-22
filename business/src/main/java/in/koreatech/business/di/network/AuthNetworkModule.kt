3c557495fd4da896a89c654b0de34e25eb6d04db
skdud0629 <skdud4081@gmail.com>
2025-07-22 12:16:01 +0900

    Fix: wrong code

--- business/src/main/java/in/koreatech/business/di/network/AuthNetworkModule.kt
+++ business/src/main/java/in/koreatech/business/di/network/AuthNetworkModule.kt
@@ -24,6 +24,7 @@ import `in`.koreatech.koin.data.api.PreSignedUrlApi
 import `in`.koreatech.koin.data.api.UploadUrlApi
 import `in`.koreatech.koin.data.api.UserApi
 import `in`.koreatech.koin.data.api.auth.OwnerAuthApi
+import `in`.koreatech.koin.data.api.auth.StoreAuthApi
 import `in`.koreatech.koin.data.api.auth.UserAuthApi
 import `in`.koreatech.koin.data.source.local.TokenLocalDataSource
 import java.util.concurrent.TimeUnit
@@ -120,6 +121,13 @@ object AuthNetworkModule {
     ): UserAuthApi {
         return retrofit.create(UserAuthApi::class.java)
     }
+
+    @Provides
+    @Singleton
+    fun provideStoreAuthApi(@Auth retrofit: Retrofit): StoreAuthApi {
+        return retrofit.create(StoreAuthApi::class.java)
+    }
+
 }
 
 @Module
