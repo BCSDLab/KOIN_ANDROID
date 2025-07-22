ccb5ca85b1669a9fbd7c7d6cf9da070baac27c7d
WooJin Kong <kongwoojin03@gmail.com>
2025-07-23 04:47:24 +0900

    feat: Implement basic StorePaymentScreen

---
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/payment/StorePaymentViewModel.kt
@@ -0,0 +1,32 @@
+package `in`.koreatech.koin.feature.store.view.payment
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import `in`.koreatech.koin.domain.model.user.AuthToken
+import `in`.koreatech.koin.domain.usecase.user.GetTokensUseCase
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+@HiltViewModel
+class StorePaymentViewModel @Inject constructor(
+    private val getTokensUseCase: GetTokensUseCase
+) : ViewModel() {
+    private val _authToken: MutableStateFlow<AuthToken> = MutableStateFlow(AuthToken("", "", ""))
+    val authToken: StateFlow<AuthToken> get() = _authToken
+
+    private val _isLoading: MutableStateFlow<Boolean> = MutableStateFlow(true)
+    val isLoading: StateFlow<Boolean> get() = _isLoading
+
+    init {
+        viewModelScope.launch {
+            getTokensUseCase().let {
+                _authToken.emit(it)
+                _isLoading.emit(false)
+            }
+        }
+    }
+}
