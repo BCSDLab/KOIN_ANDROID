d6670efe3a448ecdb63b48e3af9162711ce68d75
KYM02 <gmyeongmin@gmail.com>
2025-07-22 20:42:02 +0900

    fix: match develop branch related to club
    fix: match develop branch related to club

--- feature/club/src/main/java/in/koreatech/koin/feature/club/ui/clubdetail/ClubDetail.kt
+++ feature/club/src/main/java/in/koreatech/koin/feature/club/ui/clubdetail/ClubDetail.kt
@@ -70,17 +70,19 @@ import `in`.koreatech.koin.core.designsystem.component.button.FilledButtonColors
 import `in`.koreatech.koin.core.designsystem.component.topbar.KoinTopAppBar
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 import `in`.koreatech.koin.core.toast.ToastUtil
-import `in`.koreatech.koin.domain.constant.HTTPS_URL
 import `in`.koreatech.koin.domain.constant.KOIN_WEB_STAGE_URL
 import `in`.koreatech.koin.domain.constant.KOIN_WEB_URL
 import `in`.koreatech.koin.domain.constant.LOGIN_ACTIVITY_URL
-import `in`.koreatech.koin.domain.util.ext.formatInstagramLinkForm
-import `in`.koreatech.koin.domain.util.ext.formatInstagramUrlForm
 import `in`.koreatech.koin.domain.util.ext.formatPhoneNumber
 import `in`.koreatech.koin.domain.util.ext.isValidGoogleFormUrl
 import `in`.koreatech.koin.domain.util.ext.isValidInstagramUrl
 import `in`.koreatech.koin.domain.util.ext.isValidOpenChatUrl
 import `in`.koreatech.koin.domain.util.ext.isValidPhoneNumber
+import `in`.koreatech.koin.domain.util.ext.isValidUrlScheme
+import `in`.koreatech.koin.domain.util.ext.removeUrlScheme
+import `in`.koreatech.koin.domain.util.ext.toHttpsUrl
+import `in`.koreatech.koin.domain.util.ext.toInstagramLink
+import `in`.koreatech.koin.domain.util.ext.toInstagramUrl
 import `in`.koreatech.koin.feature.club.BuildConfig
 import `in`.koreatech.koin.feature.club.R
 import `in`.koreatech.koin.feature.club.component.DetailDialog
@@ -587,19 +589,26 @@ fun ClubDetail(
                                         onClick = { showMore.value = !showMore.value }
                                     }
                                     DETAIL_INSTAGRAM -> {
-                                        linkUrl = if (it.isValidInstagramUrl()) it else it.formatInstagramUrlForm()
+                                        val url = if (it.isValidUrlScheme()) it else it.toHttpsUrl()
+                                        linkUrl = if (url.isValidInstagramUrl()) url else url.toInstagramUrl()
                                         onClick = { viewModel.openUrl(linkUrl) }
-                                        outputText = it.formatInstagramLinkForm()
+                                        outputText = it.toInstagramLink()
                                     }
                                     DETAIL_GOOGLE_FORM -> {
-                                        outputText = it.removePrefix(HTTPS_URL)
+                                        val url = if (it.isValidUrlScheme()) it else it.toHttpsUrl()
+                                        linkUrl = if (url.isValidGoogleFormUrl()) url else ""
+                                        onClick = { viewModel.openUrl(linkUrl) }
+                                        outputText = it.removeUrlScheme()
                                         icon = R.drawable.icon_club_copy
-                                        onIconClick = { clipboard.setText(AnnotatedString(it)) }
+                                        onIconClick = { clipboard.setText(AnnotatedString(linkUrl)) }
                                     }
                                     DETAIL_OPEN_CHAT -> {
-                                        outputText = it.removePrefix(HTTPS_URL)
+                                        val url = if (it.isValidUrlScheme()) it else it.toHttpsUrl()
+                                        linkUrl = if (url.isValidOpenChatUrl()) url else ""
+                                        onClick = { viewModel.openUrl(linkUrl) }
+                                        outputText = it.removeUrlScheme()
                                         icon = R.drawable.icon_club_copy
-                                        onIconClick = { clipboard.setText(AnnotatedString(it)) }
+                                        onIconClick = { clipboard.setText(AnnotatedString(linkUrl)) }
                                     }
                                     DETAIL_PHONE_NUMBER -> {
                                         outputText = if (it.isValidPhoneNumber) it.formatPhoneNumber() else it
@@ -630,7 +639,7 @@ fun ClubDetail(
                                     style = KoinTheme.typography.medium18,
                                     color = if (linkUrl.isEmpty()) KoinTheme.colors.neutral800 else KoinTheme.colors.info700,
                                     overflow = TextOverflow.Ellipsis,
-                                    modifier = Modifier.clickable { onClick() }
+                                    modifier = Modifier.clickable { onClick() }.weight(1f, fill = false)
                                 )
                                 if (icon != -1) {
                                     Spacer(Modifier.width(8.dp))
@@ -853,8 +862,12 @@ suspend fun handleSideEffect(
             )
         }
         is ClubDetailSideEffect.OpenUrl -> {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(sideEffect.url))
-            context.startActivity(intent)
+            try {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(sideEffect.url))
+                context.startActivity(intent)
+            } catch (e: Exception) {
+                ToastUtil.getInstance().makeShort(context.getString(R.string.detail_error_incorrect_link))
+            }
         }
         is ClubDetailSideEffect.UnauthorizedError -> {
             ToastUtil.getInstance().makeShort(sideEffect.messageResId)
