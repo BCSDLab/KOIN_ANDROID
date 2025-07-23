5dc6fa3d38b5c3e7d25e1379eb1a81cf5e428346
skdud0629 <skdud4081@gmail.com>
2025-07-23 22:21:58 +0900

    Fix: AddMenuScreen

--- feature/store/src/main/java/in/koreatech/koin/feature/store/view/menu/AddMenuScreen.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/menu/AddMenuScreen.kt
@@ -1,6 +1,5 @@
 package `in`.koreatech.koin.feature.store.view.menu
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -19,10 +18,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,25 +40,22 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import `in`.koreatech.feature.store.util.CustomClosingToolbarScreenDefaults
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 import `in`.koreatech.koin.core.designsystem.theme.RebrandKoinTheme
-import `in`.koreatech.koin.domain.model.store.CartItemEdit.CartItemEditOptionGroup
 import `in`.koreatech.koin.feature.store.R
 import `in`.koreatech.koin.feature.store.component.MenuOptionCard
+import `in`.koreatech.koin.feature.store.component.MenuPriceOptionCard
 import `in`.koreatech.koin.feature.store.scroll.storeCollapsingToolbarConnection
 import `in`.koreatech.koin.feature.store.state.currentToolbarHeightDp
 import `in`.koreatech.koin.feature.store.state.progress
 import `in`.koreatech.koin.feature.store.state.rememberCollapsingToolbarState
-import `in`.koreatech.koin.feature.store.viewmodel.StoreDetailViewModel
+import `in`.koreatech.koin.feature.store.viewmodel.AddMenuViewModel
 import org.orbitmvi.orbit.compose.collectAsState
 import kotlin.math.roundToInt
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun AddMenuScreen(
-    viewModel: StoreDetailViewModel = hiltViewModel(),
+    viewModel: AddMenuViewModel = hiltViewModel(),
     navigateToCart: () -> Unit = {},
     navigateToBack: () -> Unit = {},
-    navigateToDetailInfo: () -> Unit = {},
-    navigateToReview: () -> Unit = {}
 ) {
     val uiState by viewModel.collectAsState()
 
@@ -73,148 +69,88 @@ fun AddMenuScreen(
         minHeightPx = rememberState.minHeightPx
     )
     val currentToolbarHeightDp = rememberState.currentToolbarHeightDp()
-    val coroutineScope = rememberCoroutineScope()
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-
-            .background(color = colorResource(id = R.color.store_detail_background))
-            .nestedScroll(nestedScrollConnection)
-    ) {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 24.dp)
-                .navigationBarsPadding()
-                .padding(
-                    top = currentToolbarHeightDp + CustomClosingToolbarScreenDefaults.windowInsets
-                        .asPaddingValues()
-                        .calculateTopPadding()
-                ),
-            state = rememberState.listState
-        ) {
-            item {
-                Text(
-                    text = "메뉴 이름",
-                    style = RebrandKoinTheme.typography.bold18,
-                    color = RebrandKoinTheme.colors.neutral800
-                )
-                Text(
-                    modifier = Modifier.padding(vertical = 3.dp),
-                    text = "25000원",
-                    style = RebrandKoinTheme.typography.bold18,
-                    color = RebrandKoinTheme.colors.primary500
-                )
-                Text(
-                    modifier = Modifier.padding(bottom = 4.dp),
-                    text = "메뉴세부사항등등등",
-                    style = RebrandKoinTheme.typography.medium12,
-                    color = RebrandKoinTheme.colors.neutral500
-                )
-            }
-            item{
-                MenuOptionCard(
-                    shopMenuOption = CartItemEditOptionGroup(
-                        id = 0,
-                        name = "옵션 없음",
-                        description = "옵션이 없습니다.",
-                        isRequired = false,
-                        minSelect = 0,
-                        maxSelect = 0,
-                        options = listOf(
-                            CartItemEditOptionGroup.CartItemEditOption(
-                                id = 0,
-                                name = "옵션 없음",
-                                price = 0,
-                                isSelected = false
-                            ),
-                            CartItemEditOptionGroup.CartItemEditOption(
-                                id = 1,
-                                name = "옵션 없음",
-                                price = 1000,
-                                isSelected = true
-                            ),
-                            CartItemEditOptionGroup.CartItemEditOption(
-                                id = 1,
-                                name = "옵션 없음",
-                                price = 1000,
-                                isSelected = true
-                            ),
-
-                        ),
-                    ),
-
-                )
-            }
-            item{
-                MenuOptionCard(
-                    shopMenuOption = CartItemEditOptionGroup(
-                        id = 0,
-                        name = "옵션 없음",
-                        description = "옵션이 없습니다.",
-                        isRequired = false,
-                        minSelect = 0,
-                        maxSelect = 0,
-                        options = listOf(
-                            CartItemEditOptionGroup.CartItemEditOption(
-                                id = 0,
-                                name = "옵션 없음",
-                                price = 0,
-                                isSelected = false
-                            ),
-                            CartItemEditOptionGroup.CartItemEditOption(
-                                id = 1,
-                                name = "옵션 없음",
-                                price = 1000,
-                                isSelected = true
-                            ),
-                            CartItemEditOptionGroup.CartItemEditOption(
-                                id = 1,
-                                name = "옵션 없음",
-                                price = 1000,
-                                isSelected = true
-                            ),
-
-                            ),
-                    ),
-
-                    )
-            }
-
-        }
-        Row(
-            modifier = Modifier
-                .statusBarsPadding()
-                .fillMaxWidth()
-                .zIndex(2f),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            IconButton(onClick = {
-                navigateToBack()
-            }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_arrow_left),
-                    contentDescription = null,
-                    tint = lerp(
-                        KoinTheme.colors.neutral800,
-                        KoinTheme.colors.neutral0,
-                        1f - overlayAlpha
-                    )
-                )
-            }
-            Text(
-                text = uiState.store.name,
-                fontWeight = Bold,
-                color = KoinTheme.colors.neutral800.copy(alpha = overlayAlpha)
+    Scaffold(
+        bottomBar = {
+            AddMenuBottomCard(
+                price = uiState.totalPrice,
+                onClick = { viewModel.onAddMenuClick() },
             )
-            Box(contentAlignment = Alignment.TopEnd) {
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .background(color = colorResource(id = R.color.store_detail_background))
+                .nestedScroll(nestedScrollConnection)
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 24.dp)
+                    .navigationBarsPadding()
+                    .padding(
+                        top = currentToolbarHeightDp + CustomClosingToolbarScreenDefaults.windowInsets
+                            .asPaddingValues()
+                            .calculateTopPadding()
+                    ),
+                state = rememberState.listState
+            ) {
+                item {
+                    Text(
+                        text = uiState.cartItemEdit?.name ?: "",
+                        style = RebrandKoinTheme.typography.bold18,
+                        color = RebrandKoinTheme.colors.neutral800
+                    )
+                    Text(
+                        modifier = Modifier.padding(vertical = 3.dp),
+                        text = uiState.cartItemEdit?.prices?.getOrNull(0)?.price.toString() + "원",
+                        style = RebrandKoinTheme.typography.bold18,
+                        color = RebrandKoinTheme.colors.primary500
+                    )
+                    Text(
+                        modifier = Modifier.padding(bottom = 4.dp),
+                        text = uiState.cartItemEdit?.description ?: "",
+                        style = RebrandKoinTheme.typography.medium12,
+                        color = RebrandKoinTheme.colors.neutral500
+                    )
+                }
+                item {
+                    MenuPriceOptionCard(
+                        prices = uiState.cartItemEdit?.prices ?: emptyList(),
+                        onChangeOption = { priceId ->
+                            viewModel.changeCartPriceEdit(priceId)
+                        }
+                    )
+
+                }
+                item {
+                    uiState.cartItemEdit?.optionGroups?.forEach { optionGroup ->
+                        MenuOptionCard(
+                            shopMenuOption = optionGroup,
+                            onChangeOption = { optionGroupId, optionId ->
+                                viewModel.changeCartOptionEdit(
+                                    optionGroupId = optionGroupId,
+                                    optionId = optionId
+                                )
+                            }
+                        )
+                    }
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .statusBarsPadding()
+                    .fillMaxWidth()
+                    .zIndex(2f),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 IconButton(onClick = {
-                    navigateToCart()
+                    navigateToBack()
                 }) {
                     Icon(
-                        modifier = Modifier.size(25.dp),
-                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_shopping_cart),
+                        painter = painterResource(id = R.drawable.ic_arrow_left),
                         contentDescription = null,
                         tint = lerp(
                             KoinTheme.colors.neutral800,
@@ -223,33 +159,54 @@ fun AddMenuScreen(
                         )
                     )
                 }
-                Box(
-                    modifier = Modifier
-                        .offset(x = (-5).dp, y = 5.dp)
-                        .size(16.dp)
-                        .background(Color.Magenta, CircleShape)
-                ) {
-                    Text(
-                        text = "3",
-                        fontSize = 10.sp,
-                        lineHeight = 11.sp,
-                        color = KoinTheme.colors.neutral0,
-                        modifier = Modifier.padding(start = 4.dp, bottom = 0.5.dp)
-                    )
+                Text(
+                    text = uiState.cartItemEdit?.name ?: "",
+                    fontWeight = Bold,
+                    color = KoinTheme.colors.neutral800.copy(alpha = overlayAlpha)
+                )
+                Box(contentAlignment = Alignment.TopEnd) {
+                    IconButton(onClick = {
+                        navigateToCart()
+                    }) {
+                        Icon(
+                            modifier = Modifier.size(25.dp),
+                            imageVector = ImageVector.vectorResource(id = R.drawable.ic_shopping_cart),
+                            contentDescription = null,
+                            tint = lerp(
+                                KoinTheme.colors.neutral800,
+                                KoinTheme.colors.neutral0,
+                                1f - overlayAlpha
+                            )
+                        )
+                    }
+                    Box(
+                        modifier = Modifier
+                            .offset(x = (-5).dp, y = 5.dp)
+                            .size(16.dp)
+                            .background(Color.Magenta, CircleShape)
+                    ) {
+                        Text(
+                            text = "3",
+                            fontSize = 10.sp,
+                            lineHeight = 11.sp,
+                            color = KoinTheme.colors.neutral0,
+                            modifier = Modifier.padding(start = 4.dp, bottom = 0.5.dp)
+                        )
+                    }
                 }
             }
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(rememberState.toolbarMaxHeight)
+                    .offset { IntOffset(0, rememberState.toolbarOffsetPx.floatValue.roundToInt()) }
+                    .zIndex(1f),
+                painter = painterResource(id = R.drawable.ic_delivery),
+                contentDescription = null,
+                alpha = 1 - overlayAlpha,
+                alignment = Alignment.Center,
+            )
         }
-        Image(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(rememberState.toolbarMaxHeight)
-                .offset { IntOffset(0, rememberState.toolbarOffsetPx.floatValue.roundToInt()) }
-                .zIndex(1f),
-            painter = painterResource(id = R.drawable.ic_delivery),
-            contentDescription = null,
-            alpha = 1 - overlayAlpha,
-            alignment = Alignment.Center,
-        )
     }
 }
 
