80987df280abc012861bed7e7471313454182961
skdud0629 <skdud4081@gmail.com>
2025-07-31 02:20:33 +0900

    Add: clip

--- feature/store/src/main/java/in/koreatech/koin/feature/store/component/MenuCategoryChips.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/component/MenuCategoryChips.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.colorResource
@@ -80,6 +81,7 @@ fun MenuCategoryChip(
                 color = borderColor,
                 shape = RoundedCornerShape(50)
             )
+            .clip(RoundedCornerShape(50))
             .clickable {
                 onCategoryClicked(menuCategory.menuGroupId)
             }
--- feature/store/src/main/java/in/koreatech/koin/feature/store/view/StoreDetailInfo.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/StoreDetailInfo.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -68,6 +70,7 @@ fun StoreDetailInfo(
             OriginInfoChips(
                 modifier = Modifier
                     .defaultMinSize(minHeight = 28.dp)
+                    .clip(RoundedCornerShape(50))
                     .clickable {
                         navigateToDetailInfo()
                     }
