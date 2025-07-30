acfcca2ec1839b9207df434ebc2d50448b49e403
skdud0629 <skdud4081@gmail.com>
2025-07-31 01:00:40 +0900

    Fix: back arrow icon

--- feature/store/src/main/java/in/koreatech/koin/feature/store/view/ShopOriginInfoScreen.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/ShopOriginInfoScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.sharp.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -63,7 +64,7 @@ fun ShopOriginInfoScreen(
                 navigationIcon = {
                     IconButton(onClick = onBackClick) {
                         Icon(
-                            imageVector = Icons.Default.ArrowBack,
+                            imageVector = Icons.AutoMirrored.Sharp.KeyboardArrowLeft,
                             contentDescription = ""
                         )
                     }
