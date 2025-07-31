1f8d308d720f205f2c0ec991ae12b0172f3775eb
WooJin Kong <kongwoojin03@gmail.com>
2025-08-01 01:07:32 +0900

    fix: Fix cart item count not updated

--- feature/store/src/main/java/in/koreatech/koin/feature/store/view/StoreDetailScreen.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/StoreDetailScreen.kt
@@ -58,8 +58,8 @@ import `in`.koreatech.koin.feature.store.viewmodel.ShoppingCartViewModel
 import `in`.koreatech.koin.feature.store.viewmodel.StoreDetailViewModel
 import kotlin.math.roundToInt
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -108,10 +108,12 @@ fun StoreDetailScreen(
     }
 
     LaunchedEffect(Unit) {
-        snapshotFlow {
-            uiState.isLogin
-        }.filter { it }.onEach {
-            viewModel.getCartItemsCount()
+        snapshotFlow { uiState.isLogin }
+            .distinctUntilChanged()
+            .collectLatest {
+            if (uiState.isLogin) {
+                viewModel.getCartItemsCount()
+            }
         }
     }
 
