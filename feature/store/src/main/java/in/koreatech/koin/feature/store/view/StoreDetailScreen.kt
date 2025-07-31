355c91800e0a6af0b9d0c4ec783c08c527eed320
skdud0629 <skdud4081@gmail.com>
2025-07-31 14:57:47 +0900

    Fix: StoreDetailScreen

--- feature/store/src/main/java/in/koreatech/koin/feature/store/view/StoreDetailScreen.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/StoreDetailScreen.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -18,6 +21,7 @@ import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -39,27 +43,33 @@ import `in`.koreatech.koin.core.designsystem.theme.RebrandKoinTheme
 import `in`.koreatech.koin.feature.store.R
 import `in`.koreatech.koin.feature.store.component.KoinStoreTopAppBar
 import `in`.koreatech.koin.feature.store.component.MenuCategoryChips
+import `in`.koreatech.koin.feature.store.component.OrderBottomBar
 import `in`.koreatech.koin.feature.store.component.menuListSection
+import `in`.koreatech.koin.feature.store.enums.CartValidation
 import `in`.koreatech.koin.feature.store.scroll.storeCollapsingToolbarConnection
 import `in`.koreatech.koin.feature.store.state.collapseToolbar
 import `in`.koreatech.koin.feature.store.state.rememberCollapsingToolbarState
+import `in`.koreatech.koin.feature.store.viewmodel.ShoppingCartViewModel
 import `in`.koreatech.koin.feature.store.viewmodel.StoreDetailViewModel
-import kotlin.math.roundToInt
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.compose.collectAsState
+import kotlin.math.roundToInt
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun StoreDetailScreen(
     viewModel: StoreDetailViewModel = hiltViewModel(),
+    cartViewModel: ShoppingCartViewModel = hiltViewModel(),
     navigateToCart: () -> Unit = {},
     navigateToBack: () -> Unit = {},
     navigateToDetailInfo: () -> Unit = {},
     navigateToReview: () -> Unit = {},
-    navigateToMenuInfo: (menuId: Int) -> Unit = {}
+    navigateToMenuInfo: (menuId: Int) -> Unit = {},
+    modifier: Modifier = Modifier
 ) {
     val uiState by viewModel.collectAsState()
+    val cartUiState by cartViewModel.collectAsState()
     val pagerState = rememberPagerState(0, 0f) { 1 }
 
     val rememberState = rememberCollapsingToolbarState(
@@ -74,133 +84,156 @@ fun StoreDetailScreen(
     )
     val currentToolbarHeightDp = rememberState.currentToolbarHeightDp()
     val coroutineScope = rememberCoroutineScope()
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = colorResource(id = R.color.store_detail_background))
-            .nestedScroll(nestedScrollConnection)
+
+    Scaffold(
+        modifier = modifier.imePadding(),
+        bottomBar = {
+            if (cartUiState.cart.items.isNotEmpty())
+                OrderBottomBar(
+                    totalPrice = cartUiState.cart.totalAmount,
+                    isOrderEnabled = cartUiState.cartValidation == CartValidation.VALID,
+                    orderableMessage = cartUiState.isValidateCart.message,
+                    navigateToCart = navigateToCart
+                )
+
+        }
+
     ) {
-        LazyColumn(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .navigationBarsPadding()
-                .padding(bottom = rememberState.toolbarMinHeight * 2)
-                .offset {
-                    IntOffset(
-                        0,
-                        currentToolbarHeightDp.value.toPx().roundToInt() + rememberState.toolbarMinHeight.toPx().roundToInt()
-                    )
-                },
-            state = rememberState.listState
+                .background(color = colorResource(id = R.color.store_detail_background))
+                .nestedScroll(nestedScrollConnection)
         ) {
-            item {
-                Column {
-                    StoreDetailInfo(
-                        storeInfo = uiState.store,
-                        storeReview = uiState.storeReview,
-                        storeDescriptionModel = uiState.shopDescription,
-                        navigateToReview = { navigateToReview() },
-                        navigateToDetailInfo = { navigateToDetailInfo() }
-                    )
-                    Divider(
-                        modifier = Modifier.padding(vertical = 8.dp),
-                        color = KoinTheme.colors.neutral100,
-                        thickness = 8.dp
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .navigationBarsPadding()
+                    .padding(bottom = rememberState.toolbarMinHeight * 2)
+                    .offset {
+                        IntOffset(
+                            0,
+                            currentToolbarHeightDp.value
+                                .toPx()
+                                .roundToInt() + rememberState.toolbarMinHeight
+                                .toPx()
+                                .roundToInt()
+                        )
+                    },
+                state = rememberState.listState
+            ) {
+                item {
+                    Column {
+                        StoreDetailInfo(
+                            storeInfo = uiState.store,
+                            storeReview = uiState.storeReview,
+                            storeDescriptionModel = uiState.shopDescription,
+                            navigateToReview = { navigateToReview() },
+                            navigateToDetailInfo = { navigateToDetailInfo() }
+                        )
+                        Divider(
+                            modifier = Modifier.padding(vertical = 8.dp),
+                            color = KoinTheme.colors.neutral100,
+                            thickness = 8.dp
+                        )
+                    }
+                }
+                stickyHeader {
+                    MenuCategoryChips(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .heightIn(min = 66.dp),
+                        menuCategories = uiState.categories,
+                        onCategoryClicked = { categoryId, stickyHeaderHeight ->
+                            viewModel.clickMenuCategory(categoryId)
+                            rememberState.collapseToolbar(
+                                state = rememberState
+                            )
+                            CoroutineScope(coroutineScope.coroutineContext).launch {
+                                rememberState.listState.scrollToItem(uiState.categories.indexOfFirst { it.menuGroupId == categoryId } + 2, -stickyHeaderHeight)
+                            }
+                        }
                     )
                 }
-            }
-            stickyHeader {
-                MenuCategoryChips(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .heightIn(min = 66.dp),
-                    menuCategories = uiState.categories,
-                    onCategoryClicked = { categoryId, stickyHeaderHeight ->
-                        viewModel.clickMenuCategory(categoryId)
-                        rememberState.collapseToolbar(
-                            state = rememberState
-                        )
-                        CoroutineScope(coroutineScope.coroutineContext).launch {
-                            rememberState.listState.scrollToItem(uiState.categories.indexOfFirst { it.menuGroupId == categoryId } + 2, -stickyHeaderHeight)
+                uiState.categories.forEach { category ->
+                    menuListSection(
+                        category = category.menuGroupName,
+                        menus = category.menus,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                    ) {
+                        if (uiState.isOrderableShop) {
+                            navigateToMenuInfo(it)
                         }
                     }
-                )
-            }
-            uiState.categories.forEach { category ->
-                menuListSection(
-                    category = category.menuGroupName,
-                    menus = category.menus,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp)
-                ) {
-                    if (uiState.isOrderableShop) {
-                        navigateToMenuInfo(it)
-                    }
                 }
+                item{
+                    Spacer(modifier = Modifier.height(100.dp))
+                }
+            }
+
+            KoinStoreTopAppBar(
+                title = uiState.store.name,
+                onNavigationIconClick = {
+                    navigateToBack()
+                },
+                actions = {
+                    Box(contentAlignment = Alignment.TopEnd) {
+                        IconButton(onClick = {
+                            navigateToCart()
+                        }) {
+                            Icon(
+                                modifier = Modifier.size(25.dp),
+                                imageVector = ImageVector.vectorResource(id = R.drawable.ic_shopping_cart),
+                                contentDescription = null
+                            )
+                        }
+                        IconButton(onClick = {
+                            navigateToCart()
+                        }) {
+                            Icon(
+                                modifier = Modifier.size(25.dp),
+                                imageVector = ImageVector.vectorResource(id = R.drawable.ic_shopping_cart),
+                                contentDescription = null
+                            )
+                        }
+                        Box(
+                            modifier = Modifier
+                                .offset(x = (-5).dp, y = 5.dp)
+                                .size(16.dp)
+                                .background(RebrandKoinTheme.colors.primary500, CircleShape),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = "",
+                                style = RebrandKoinTheme.typography.medium12.copy(
+                                    color = RebrandKoinTheme.colors.neutral0,
+                                    lineHeightStyle = LineHeightStyle(
+                                        trim = LineHeightStyle.Trim.Both,
+                                        alignment = LineHeightStyle.Alignment.Center
+                                    )
+                                )
+                            )
+                        }
+                    }
+                },
+                overlayAlpha = overlayAlpha
+            ) {
+                StoreDetailImage(
+                    modifier = Modifier
+                        .heightIn(rememberState.toolbarMinHeight, rememberState.toolbarMinHeight + rememberState.toolbarMaxHeight)
+                        .fillMaxWidth()
+                        .graphicsLayer {
+                            clip = true
+                            translationY = -(rememberState.toolbarMaxHeight.toPx() - currentToolbarHeightDp.value.toPx())
+                            alpha = 1f - overlayAlpha.value
+                        },
+                    imageUrls = uiState.store.imageUrls ?: emptyList(),
+                    pagerState = pagerState
+                )
             }
         }
 
-        KoinStoreTopAppBar(
-            modifier = Modifier.zIndex(2f),
-            title = uiState.store.name,
-            onNavigationIconClick = {
-                navigateToBack()
-            },
-            actions = {
-                Box(contentAlignment = Alignment.TopEnd) {
-                    IconButton(onClick = {
-                        navigateToCart()
-                    }) {
-                        Icon(
-                            modifier = Modifier.size(25.dp),
-                            imageVector = ImageVector.vectorResource(id = R.drawable.ic_shopping_cart),
-                            contentDescription = null
-                        )
-                    }
-                    IconButton(onClick = {
-                        navigateToCart()
-                    }) {
-                        Icon(
-                            modifier = Modifier.size(25.dp),
-                            imageVector = ImageVector.vectorResource(id = R.drawable.ic_shopping_cart),
-                            contentDescription = null
-                        )
-                    }
-                    Box(
-                        modifier = Modifier
-                            .offset(x = (-5).dp, y = 5.dp)
-                            .size(16.dp)
-                            .background(RebrandKoinTheme.colors.primary500, CircleShape),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = "",
-                            style = RebrandKoinTheme.typography.medium12.copy(
-                                color = RebrandKoinTheme.colors.neutral0,
-                                lineHeightStyle = LineHeightStyle(
-                                    trim = LineHeightStyle.Trim.Both,
-                                    alignment = LineHeightStyle.Alignment.Center
-                                )
-                            )
-                        )
-                    }
-                }
-            },
-            overlayAlpha = overlayAlpha
-        ) {
-            StoreDetailImage(
-                modifier = Modifier
-                    .heightIn(rememberState.toolbarMinHeight, rememberState.toolbarMinHeight + rememberState.toolbarMaxHeight)
-                    .fillMaxWidth()
-                    .graphicsLayer {
-                        clip = true
-                        translationY = -(rememberState.toolbarMaxHeight.toPx() - currentToolbarHeightDp.value.toPx())
-                        alpha = 1f - overlayAlpha.value
-                    },
-                imageUrls = uiState.store.imageUrls ?: emptyList(),
-                pagerState = pagerState
-            )
-        }
     }
 }
