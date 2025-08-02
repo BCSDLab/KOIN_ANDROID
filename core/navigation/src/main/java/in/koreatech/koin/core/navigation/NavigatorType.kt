148fb1dc3ebd2c08403e81752434aafde00ebbf7
WooJin Kong <kongwoojin03@gmail.com>
2025-08-02 23:30:43 +0900

    refactor: Move SchemeType to koin

--- core/navigation/src/main/java/in/koreatech/koin/core/navigation/NavigatorType.kt
+++ core/navigation/src/main/java/in/koreatech/koin/core/navigation/NavigatorType.kt
@@ -6,14 +6,3 @@ enum class NavigatorType(
     MAIN("main"),
     DETAIL("detail")
 }
-
-enum class SchemeType(
-    val type: String
-) {
-    SHOP("shop"),
-    DINING("dining"),
-    ARTICLE("keyword"),
-    CHAT("chat"),
-    CLUB_RECRUIT("club-recruitment"),
-    CLUB("club")
-}
---
+++ koin/src/main/java/in/koreatech/koin/navigation/SchemeType.kt
@@ -0,0 +1,12 @@
+package `in`.koreatech.koin.navigation
+
+enum class SchemeType(
+    val type: String
+) {
+    SHOP("shop"),
+    DINING("dining"),
+    ARTICLE("keyword"),
+    CHAT("chat"),
+    CLUB_RECRUIT("club-recruitment"),
+    CLUB("club")
+}
