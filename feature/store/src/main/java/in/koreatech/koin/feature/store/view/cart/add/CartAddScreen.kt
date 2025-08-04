0a41658cddef4872741af5751f40606bd238a22d
WooJin Kong <kongwoojin03@gmail.com>
2025-08-05 04:47:09 +0900

    fix: Set icon color as black if image not exist

--- feature/store/src/main/java/in/koreatech/koin/feature/store/view/cart/add/CartAddScreen.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/cart/add/CartAddScreen.kt
@@ -24,14 +24,17 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.LineHeightStyle
@@ -190,7 +193,13 @@ fun CartAddScreen(
                         }
                     }
                 },
-                overlayAlpha = overlayAlpha
+                overlayAlpha = overlayAlpha,
+                expandedColors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.Transparent,
+                    navigationIconContentColor = RebrandKoinTheme.colors.neutral0,
+                    titleContentColor = RebrandKoinTheme.colors.neutral0,
+                    actionIconContentColor = if (uiState.menuImageUrls.firstOrNull() == null) RebrandKoinTheme.colors.neutral800 else RebrandKoinTheme.colors.neutral0
+                )
             ) {
                 Column(
                     modifier = Modifier
