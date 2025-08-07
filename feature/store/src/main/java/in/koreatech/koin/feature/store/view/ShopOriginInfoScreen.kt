82e241e53013725d70719b66febff7c268fc8a3e
skdud0629 <skdud4081@gmail.com>
2025-08-07 11:36:11 +0900

    Add: loading to ShopOriginInfoScreen

--- feature/store/src/main/java/in/koreatech/koin/feature/store/view/ShopOriginInfoScreen.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/view/ShopOriginInfoScreen.kt
@@ -31,17 +31,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import `in`.koreatech.koin.core.designsystem.noRippleClickable
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 import `in`.koreatech.koin.core.designsystem.theme.RebrandKoinTheme
 import `in`.koreatech.koin.feature.store.R
+import `in`.koreatech.koin.feature.store.component.KoinStoreProgressIndicator
 import `in`.koreatech.koin.feature.store.model.DeliveryTipModel
 import `in`.koreatech.koin.feature.store.viewmodel.StoreDetailViewModel
 import org.orbitmvi.orbit.compose.collectAsState
@@ -55,6 +58,25 @@ fun ShopOriginInfoScreen(
     navigateToShoppingCart: () -> Unit = {}
 ) {
     val uiState by storeDetailViewModel.collectAsState()
+    if (uiState.isLoading) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .zIndex(2f)
+                .pointerInput(Unit) {
+                    awaitPointerEventScope {
+                        while (true) {
+                            awaitPointerEvent()
+                        }
+                    }
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            KoinStoreProgressIndicator(
+                modifier = Modifier.size(150.dp)
+            )
+        }
+    }
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
