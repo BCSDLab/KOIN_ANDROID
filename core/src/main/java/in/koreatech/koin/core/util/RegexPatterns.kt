6dba5551b0f46923da67ea2b0460d34129614039
jusang3057 <jusang3057@gmail.com>
2025-08-07 15:11:42 +0900

    Update RegexPatterns.kt
    안쓰는 Regex제거

--- core/src/main/java/in/koreatech/koin/core/util/RegexPatterns.kt
+++ core/src/main/java/in/koreatech/koin/core/util/RegexPatterns.kt
@@ -5,6 +5,5 @@ object RegexPatterns {
     val rgb = """rgb\(([0-9]+), ([0-9]+), ([0-9]+)\)""".toRegex()
 
     val fileSize = """[0-9]+\s?[KMG]?B""".toRegex()
-
-    val phone = """(010)-?([0-9]{4})-?([0-9]{4})""".toRegex()
+    
 }
