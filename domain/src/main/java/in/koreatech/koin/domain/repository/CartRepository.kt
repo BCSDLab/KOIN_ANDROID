f5e2e0187aab49e3c1d33f28bad73c595a8ea619
WooJin Kong <kongwoojin03@gmail.com>
2025-08-09 03:35:55 +0900

    del: Remove duplicated api

--- domain/src/main/java/in/koreatech/koin/domain/repository/CartRepository.kt
+++ domain/src/main/java/in/koreatech/koin/domain/repository/CartRepository.kt
@@ -1,12 +1,9 @@
 package `in`.koreatech.koin.domain.repository
 
-import `in`.koreatech.koin.domain.model.cart.Cart
-import `in`.koreatech.koin.domain.model.cart.CartType
 import `in`.koreatech.koin.domain.model.cart.CartValidate
 import kotlinx.coroutines.flow.Flow
 
 interface CartRepository {
-    suspend fun getCart(type: CartType): Flow<Cart>
     suspend fun getCartValidate(): Flow<CartValidate>
     suspend fun cartMenuQuantity(cartMenuItemId: Int, quantity: Int): Flow<Unit>
     suspend fun resetCart(): Flow<Unit>
