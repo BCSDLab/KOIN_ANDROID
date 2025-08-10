40237d9f87bfd0a70d7d6916dbcfa29827de3090
WooJin Kong <kongwoojin03@gmail.com>
2025-08-11 00:42:30 +0900

    add: Add compose dependency on core:navigation

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -5,9 +5,19 @@ plugins {
 
 android {
     namespace = "in.koreatech.koin.core.navigation"
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.10"
+    }
+
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose.m3)
 }
