e8924900c140438e7149a4059fd603476cb8bf2a
WooJin Kong <kongwoojin03@gmail.com>
2025-08-16 20:21:04 +0900

    chore: Bump missing targetSdk to 35

--- build-logic/convention/src/main/java/in/koreatech/convention/AndroidLibrary.kt
+++ build-logic/convention/src/main/java/in/koreatech/convention/AndroidLibrary.kt
@@ -9,15 +9,15 @@ internal fun Project.configureAndroidLibrary(
     commonExtension: CommonExtension<*, *, *, *, *, *>,
 ) {
     (commonExtension as? LibraryExtension)?.let {
-        it.defaultConfig.targetSdk = 34
+        it.defaultConfig.targetSdk = 35
     }
 
     commonExtension.apply {
         (this as? LibraryExtension)?.let {
-            it.defaultConfig.targetSdk = 34
+            it.defaultConfig.targetSdk = 35
         }
 
-        compileSdk = 34
+        compileSdk = 35
 
         defaultConfig {
             minSdk = 28
