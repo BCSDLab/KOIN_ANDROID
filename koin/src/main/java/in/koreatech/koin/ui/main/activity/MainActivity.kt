fdb8f20cfd7a24cc33f19ca362973bb7c06543a8
WooJin Kong <kongwoojin03@gmail.com>
2025-09-03 02:35:04 +0900

    chore: Fix ktlint

--- koin/src/main/java/in/koreatech/koin/ui/main/activity/MainActivity.kt
+++ koin/src/main/java/in/koreatech/koin/ui/main/activity/MainActivity.kt
@@ -3,7 +3,6 @@ package `in`.koreatech.koin.ui.main.activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
 import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
