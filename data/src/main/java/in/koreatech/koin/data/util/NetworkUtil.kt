65be7e52d0f7adc78569f7d64e3389e6f4f2a9b6
KYM02 <gmyeongmin@gmail.com>
2025-09-08 22:49:47 +0900

    feature: Create custom runCatching, which handles NetworkUnavailableException
    Create custom runCatching, which handles NetworkUnavailableException

--- data/src/main/java/in/koreatech/koin/data/util/NetworkUtil.kt
+++ data/src/main/java/in/koreatech/koin/data/util/NetworkUtil.kt
@@ -3,6 +3,7 @@ package `in`.koreatech.koin.data.util
 import com.google.gson.Gson
 import `in`.koreatech.koin.data.response.ErrorResponse
 import `in`.koreatech.koin.domain.error.KoinUnknownErrorException
+import `in`.koreatech.koin.domain.error.network.KoinNetworkException
 import retrofit2.HttpException
 
 fun HttpException.getErrorResponse(): ErrorResponse {
@@ -11,4 +12,18 @@ fun HttpException.getErrorResponse(): ErrorResponse {
 
 fun ErrorResponse.toKoinUnknownErrorException(): KoinUnknownErrorException {
     return KoinUnknownErrorException(this.code, this.message, this.errorTraceId)
+}
+
+inline fun <T> runCatchingWithNetwork(
+    block: () -> T
+): Result<T> {
+    return runCatching(block).onFailure { exception ->
+        return Result.failure(
+            if(exception.cause is KoinNetworkException.NetworkUnavailableException) {
+                KoinNetworkException.NetworkUnavailableException()
+            } else {
+                exception
+            }
+        )
+    }
 }
