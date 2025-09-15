b147e62030b2fb1a544ef22872efb1e3f06c45fa
WooJin Kong <kongwoojin03@gmail.com>
2025-09-16 07:44:27 +0900

    fix: Set isLoading to false when early return

--- feature/store/src/main/java/in/koreatech/koin/feature/store/orderhistory/StoreOrderHistoryViewModel.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/orderhistory/StoreOrderHistoryViewModel.kt
@@ -62,7 +62,14 @@ class StoreOrderHistoryViewModel @Inject constructor(
             type = state.filters.orderType?.name ?: "NONE",
             query = state.searchQuery
         ).onSuccess {
-            if (state.currentPage > it.currentPage) return@intent
+            if (state.currentPage > it.currentPage) {
+                reduce {
+                    state.copy(
+                        isLoading = false
+                    )
+                }
+                return@intent
+            }
             reduce {
                 state.copy(
                     isLoading = false,
