eecc3891c53fca15db769edec278818329d652d0
unknown <172613798+JaeYoung290@users.noreply.github.com>
2025-09-25 12:40:21 +0900

    fix: Apply PR comments

--- feature/dining/src/main/java/in/koreatech/koin/feature/dining/component/abTeset/DiningAbTestFloatingButton.kt
+++ feature/dining/src/main/java/in/koreatech/koin/feature/dining/component/abTeset/DiningAbTestFloatingButton.kt
@@ -9,13 +9,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 
@@ -45,23 +48,24 @@ fun DiningAbTestFloatingButton(
             color = KoinTheme.colors.primary500,
             style = KoinTheme.typography.medium14,
             maxLines = 1,
-            overflow = TextOverflow.Clip
+            overflow = TextOverflow.Ellipsis
         )
 
-        Button(
-            onClick = onClick,
-            modifier = Modifier.defaultMinSize(minHeight = 1.dp),
-            shape = KoinTheme.shapes.small,
-            colors = ButtonDefaults.buttonColors(
-                containerColor = KoinTheme.colors.primary500
-            ),
-            contentPadding = PaddingValues(horizontal = 8.dp, vertical = 4.dp)
-        ) {
-            Text(
-                text = buttonText,
-                color = KoinTheme.colors.neutral0,
-                style = KoinTheme.typography.bold14
-            )
+        CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides Dp.Unspecified) {
+            Button(
+                onClick = onClick,
+                shape = KoinTheme.shapes.small,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = KoinTheme.colors.primary500
+                ),
+                contentPadding = PaddingValues(horizontal = 8.dp, vertical = 4.dp)
+            ) {
+                Text(
+                    text = buttonText,
+                    color = KoinTheme.colors.neutral0,
+                    style = KoinTheme.typography.bold14
+                )
+            }
         }
     }
 }
