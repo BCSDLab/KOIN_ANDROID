31ac512b3e4b5475a56068d1bbb2992b633e15e6
unknown <172613798+JaeYoung290@users.noreply.github.com>
2025-09-25 12:43:21 +0900

    chore: Apply ktlint formatting

--- feature/dining/src/main/java/in/koreatech/koin/feature/dining/component/abTeset/DiningAbTestFloatingButton.kt
+++ feature/dining/src/main/java/in/koreatech/koin/feature/dining/component/abTeset/DiningAbTestFloatingButton.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
