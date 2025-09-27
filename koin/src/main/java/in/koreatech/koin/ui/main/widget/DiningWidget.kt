9476637654f8846cd876ca2b67a90246f43305cb
unknown <172613798+JaeYoung290@users.noreply.github.com>
2025-09-27 17:47:12 +0900

    chore: Apply ktlint formatting

--- koin/src/main/java/in/koreatech/koin/ui/main/widget/DiningWidget.kt
+++ koin/src/main/java/in/koreatech/koin/ui/main/widget/DiningWidget.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.pager.HorizontalPager
