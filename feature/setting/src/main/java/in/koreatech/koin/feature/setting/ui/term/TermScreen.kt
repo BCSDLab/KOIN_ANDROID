7a9ac83592efbeddb0ce225cbaba33a07c469093
KYM-P <k_ym02@naver.com>
2025-10-07 13:45:11 +0900

    fix: Add systembar padding in snackbarhost
    Add systembar padding in snackbarhost

--- feature/setting/src/main/java/in/koreatech/koin/feature/setting/ui/term/TermScreen.kt
+++ feature/setting/src/main/java/in/koreatech/koin/feature/setting/ui/term/TermScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -88,7 +89,10 @@ fun TermScreen(
             )
         },
         snackbarHost = {
-            SnackbarHost(hostState = snackbarHostState)
+            SnackbarHost(
+                hostState = snackbarHostState,
+                modifier = Modifier.systemBarsPadding()
+            )
         },
         contentWindowInsets = WindowInsets(0, 0, 0, 0)
     ) { contentPadding ->
