ab4e617d84e42614460cc0b72c76ec6fae28dce5
KYM-P <k_ym02@naver.com>
2025-10-09 01:27:30 +0900

    chore: Feat ktlint
    Feat ktlint

--- core/src/main/java/in/koreatech/koin/core/util/ContextUtil.kt
+++ core/src/main/java/in/koreatech/koin/core/util/ContextUtil.kt
@@ -3,7 +3,6 @@ package `in`.koreatech.koin.core.util
 import android.content.Context
 import android.content.Intent
 import androidx.core.net.toUri
-import androidx.core.os.bundleOf
 
 fun Context.goToContactUrl() = startActivity(Intent(Intent.ACTION_VIEW, KOIN_ASK_FORM.toUri()))
 
--- feature/setting/src/main/java/in/koreatech/koin/feature/setting/ui/SettingScreen.kt
+++ feature/setting/src/main/java/in/koreatech/koin/feature/setting/ui/SettingScreen.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
