2f682410c364d3c227baa8d974aa38624b07772a
KYM-P <k_ym02@naver.com>
2025-10-13 00:05:25 +0900

    chore: Delete URLConstant object
    Delete URLConstant object

---
+++ feature/setting/src/main/java/in/koreatech/koin/feature/setting/constant/Constant.kt
@@ -0,0 +1,3 @@
+package `in`.koreatech.koin.feature.setting.constant
+
+val ARTICLE_KEYWORD_URL = "koin://article/activity?fragment=article_keyword"
--- feature/setting/src/main/java/in/koreatech/koin/feature/setting/constant/URLConstant.kt
+++
@@ -1,5 +0,0 @@
-package `in`.koreatech.koin.feature.setting.constant
-
-object URLConstant {
-    val articleKeyword = "koin://article/activity?fragment=article_keyword"
-}
