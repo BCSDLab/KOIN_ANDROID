a97dbc2aa50617edc216620fc380a23c4d9a0ec7
WooJin Kong <kongwoojin03@gmail.com>
2025-10-12 21:55:58 +0900

    fix: Return if articles is empty

--- koin/src/main/java/in/koreatech/koin/ui/main/compose/ArticleMain.kt
+++ koin/src/main/java/in/koreatech/koin/ui/main/compose/ArticleMain.kt
@@ -49,6 +49,7 @@ fun HotArticlePager(
     onNotiClick: (ArticleMainState.Noti) -> Unit = {},
     onArticleClick: (ArticleMainState.Content) -> Unit = {}
 ) {
+    if (articles.isEmpty()) return
     val pagerState = rememberPagerState { articles.size }
 
     LaunchedEffect(pagerState.settledPage, articles.size) {
