045bd1795b78da2b66dd5e46f51bc382d73c9735
KYM-P <k_ym02@naver.com>
2025-10-13 00:18:44 +0900

    chore: Feat ktlint
    Feat ktlint

--- feature/setting/src/main/java/in/koreatech/koin/feature/setting/util/NotificationInfoUtil.kt
+++ feature/setting/src/main/java/in/koreatech/koin/feature/setting/util/NotificationInfoUtil.kt
@@ -4,13 +4,13 @@ import `in`.koreatech.koin.domain.model.notification.Subscribes
 import `in`.koreatech.koin.domain.model.notification.SubscribesDetailType
 import `in`.koreatech.koin.domain.model.notification.SubscribesType
 
-fun List<Subscribes>.isTypePermitted(type : SubscribesType): Boolean {
+fun List<Subscribes>.isTypePermitted(type: SubscribesType): Boolean {
     this.forEach { subscribe ->
         if (subscribe.type == type) return subscribe.isPermit
     }
     return false
 }
-fun List<Subscribes>.isDetailTypePermitted(type : SubscribesDetailType): Boolean {
+fun List<Subscribes>.isDetailTypePermitted(type: SubscribesDetailType): Boolean {
     this.forEach { subscribe ->
         subscribe.detailSubscribes.forEach { subscribeDetail ->
             if (subscribeDetail.type == type) return subscribeDetail.isPermit
