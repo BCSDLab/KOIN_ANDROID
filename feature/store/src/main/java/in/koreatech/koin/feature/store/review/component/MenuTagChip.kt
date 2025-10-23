358454bf53a43965609e20f2785e7685b594c414
unknown <172613798+JaeYoung290@users.noreply.github.com>
2025-10-24 08:27:30 +0900

    refactor: Replace MenuTagChip with KoinStoreChip

--- feature/store/src/main/java/in/koreatech/koin/feature/store/review/component/MenuTagChip.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/review/component/MenuTagChip.kt
@@ -1,81 +1,21 @@
 package `in`.koreatech.koin.feature.store.review.component
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import `in`.koreatech.koin.core.designsystem.theme.RebrandKoinTheme
 import `in`.koreatech.koin.feature.store.R
-
-data class MenuTagChipStyle(
-    val textColor: Color,
-    val textStyle: TextStyle,
-    val containerColor: Color,
-    val borderWidth: Dp,
-    val borderColor: Color,
-    val shape: Shape,
-    val paddingValues: PaddingValues
-)
-
-data class MenuTagChipIconStyle(
-    val iconColor: Color,
-    val iconSize: Dp
-)
-
-object MenuTagChipDefaults {
-    @Composable
-    fun menuTagChipStyle(
-        textColor: Color = RebrandKoinTheme.colors.primary300,
-        textStyle: TextStyle = RebrandKoinTheme.typography.regular12,
-        containerColor: Color = RebrandKoinTheme.colors.neutral0,
-        borderWidth: Dp = 1.dp,
-        borderColor: Color = RebrandKoinTheme.colors.primary300,
-        shape: Shape = RebrandKoinTheme.shapes.extraSmall.copy(all = CornerSize(5.dp)),
-        paddingValues: PaddingValues = PaddingValues(horizontal = 10.dp, vertical = 3.dp)
-    ): MenuTagChipStyle = MenuTagChipStyle(
-        textColor = textColor,
-        textStyle = textStyle,
-        containerColor = containerColor,
-        borderWidth = borderWidth,
-        borderColor = borderColor,
-        shape = shape,
-        paddingValues = paddingValues
-    )
-
-    @Composable
-    fun menuTagChipIconStyle(
-        iconColor: Color = RebrandKoinTheme.colors.primary300,
-        iconSize: Dp = 14.dp
-    ): MenuTagChipIconStyle = MenuTagChipIconStyle(
-        iconColor = iconColor,
-        iconSize = iconSize
-    )
-}
+import `in`.koreatech.koin.feature.store.component.KoinStoreChip
+import `in`.koreatech.koin.feature.store.component.KoinStoreChipIconStyle
+import `in`.koreatech.koin.feature.store.component.KoinStoreChipStyle
 
 @Composable
 fun MenuTagChipGroup(
@@ -83,6 +23,26 @@ fun MenuTagChipGroup(
     modifier: Modifier = Modifier,
     onClick: (Int) -> Unit = { }
 ) {
+    val menuTagChipIcon = painterResource(id = R.drawable.ic_close_thin)
+    val menuTagStyle = KoinStoreChipStyle(
+        textColor = RebrandKoinTheme.colors.primary300,
+        textStyle = RebrandKoinTheme.typography.regular12,
+        containerColor = RebrandKoinTheme.colors.neutral0,
+        elevation = 0.dp,
+        borderWidth = 1.dp,
+        borderColor = RebrandKoinTheme.colors.primary300,
+        shape = RebrandKoinTheme.shapes.extraSmall.copy(all = CornerSize(5.dp)),
+        paddingValues = PaddingValues(horizontal = 10.dp, vertical = 3.dp),
+        ambientColor = Color.Transparent,
+        spotColor = Color.Transparent,
+        innerSpace = 4.dp
+    )
+
+    val menuTagIconStyle = KoinStoreChipIconStyle(
+        iconColor = RebrandKoinTheme.colors.primary300,
+        iconSize = 14.dp
+    )
+
     FlowRow(
         modifier = modifier
             .padding(vertical = 24.dp),
@@ -90,53 +50,13 @@ fun MenuTagChipGroup(
         verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
         menuTags.forEachIndexed { i, tag ->
-            MenuTagChip(
+            KoinStoreChip(
                 text = tag,
-                trailingIcon = painterResource(id = R.drawable.ic_close_thin),
+                chipStyle = menuTagStyle,
+                trailingIcon = menuTagChipIcon,
+                trailingIconStyle = menuTagIconStyle,
                 onClick = { onClick(i) }
             )
-        }
-    }
-}
-
-@Composable
-fun MenuTagChip(
-    text: String,
-    modifier: Modifier = Modifier,
-    chipStyle: MenuTagChipStyle = MenuTagChipDefaults.menuTagChipStyle(),
-    trailingIcon: Painter? = null,
-    trailingIconStyle: MenuTagChipIconStyle = MenuTagChipDefaults.menuTagChipIconStyle(),
-    onClick: () -> Unit = { }
-) {
-    Box(
-        modifier = modifier
-            .wrapContentWidth()
-            .border(width = chipStyle.borderWidth, color = chipStyle.borderColor, shape = chipStyle.shape)
-            .clip(shape = chipStyle.shape)
-            .background(color = chipStyle.containerColor, shape = chipStyle.shape)
-            .clickable { onClick() },
-        contentAlignment = Alignment.Center
-    ) {
-        Row(
-            modifier = Modifier.padding(chipStyle.paddingValues),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = text,
-                style = chipStyle.textStyle,
-                color = chipStyle.textColor
-            )
-            Spacer(modifier = Modifier.width(4.dp))
-            if (trailingIcon != null) {
-                Box(
-                    modifier = Modifier
-                        .size(trailingIconStyle.iconSize)
-                        .paint(
-                            painter = trailingIcon,
-                            colorFilter = ColorFilter.tint(trailingIconStyle.iconColor)
-                        )
-                )
-            }
         }
     }
 }
