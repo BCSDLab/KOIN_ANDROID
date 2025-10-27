8f04f161738fe52db62f36e4ef8767c4d584f868
KYM-P <k_ym02@naver.com>
2025-10-27 11:24:33 +0900

    feature: Apply fastForEach
    Apply fastForEach

--- feature/setting/src/main/java/in/koreatech/koin/feature/setting/util/NotificationInfoUtil.kt
+++ feature/setting/src/main/java/in/koreatech/koin/feature/setting/util/NotificationInfoUtil.kt
@@ -1,17 +1,18 @@
 package `in`.koreatech.koin.feature.setting.util
 
+import androidx.compose.ui.util.fastForEach
 import `in`.koreatech.koin.domain.model.notification.Subscribes
 import `in`.koreatech.koin.domain.model.notification.SubscribesDetailType
 import `in`.koreatech.koin.domain.model.notification.SubscribesType
 
 fun List<Subscribes>.isTypePermitted(type: SubscribesType): Boolean {
-    this.forEach { subscribe ->
+    this.fastForEach { subscribe ->
         if (subscribe.type == type) return subscribe.isPermit
     }
     return false
 }
 fun List<Subscribes>.isDetailTypePermitted(type: SubscribesDetailType): Boolean {
-    this.forEach { subscribe ->
+    this.fastForEach { subscribe ->
         subscribe.detailSubscribes.forEach { subscribeDetail ->
             if (subscribeDetail.type == type) return subscribeDetail.isPermit
         }
