45497748b2499390eb103a20ff4d7b1943a58a16
WooJin Kong <kongwoojin03@gmail.com>
2025-10-27 16:40:04 +0900

    add: Add ReviewCheckbox

--- feature/store/src/main/java/in/koreatech/koin/feature/store/review/component/ReviewCheckbox.kt
+++ feature/store/src/main/java/in/koreatech/koin/feature/store/review/component/ReviewCheckbox.kt
@@ -1,2 +1,40 @@
-package in.koreatech.koin.feature.store.review.component
+package `in`.koreatech.koin.feature.store.review.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
+import `in`.koreatech.koin.core.designsystem.noRippleClickable
+import `in`.koreatech.koin.feature.store.R
+
+@Composable
+fun ReviewCheckbox(
+    checked: Boolean,
+    modifier: Modifier = Modifier,
+    onCheckedChange: (Boolean) -> Unit = {}
+) {
+    Image(
+        modifier = modifier
+            .noRippleClickable {
+                onCheckedChange(!checked)
+            }
+            .size(20.dp)
+            .padding(1.dp)
+            .semantics {
+                this.role = Role.Checkbox
+            },
+        imageVector = if (checked) {
+            ImageVector.vectorResource(R.drawable.ic_store_checkbox_checked)
+        } else {
+            ImageVector.vectorResource(R.drawable.ic_store_checkbox_unchecked)
+        },
+        contentDescription = "Checkbox"
+    )
+}
