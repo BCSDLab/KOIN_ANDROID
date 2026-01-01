658ac8cc03cfe9c83c50d83bd1389c0b2d03e6b9
KYM-P <k_ym02@naver.com>
2026-01-01 16:26:32 +0900

    refactor: Change to non-constant in Dining

--- data/src/main/java/in/koreatech/koin/data/api/DiningApi.kt
+++ data/src/main/java/in/koreatech/koin/data/api/DiningApi.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface DiningApi {
-    @GET(URLConstant.DINING.DININGS)
+    @GET("dinings")
     suspend fun getDining(
         @Query("date") date: String
     ): List<DiningResponse>
