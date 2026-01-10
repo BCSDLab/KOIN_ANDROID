bb6b724716c3bc325a1d540ee4f82e8d024cea4b
WooJin Kong <kongwoojin03@gmail.com>
2026-01-10 23:52:46 +0900

    fix: Fallback to deeplink first, then MainActivity if type not defined in SchemeType

--- core/navigation/src/main/java/in/koreatech/koin/core/navigation/utils/Extensions.kt
+++ core/navigation/src/main/java/in/koreatech/koin/core/navigation/utils/Extensions.kt
@@ -3,6 +3,7 @@ package `in`.koreatech.koin.core.navigation.utils
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import androidx.core.net.toUri
 import androidx.core.os.bundleOf
 
 inline fun <reified T : Activity> Context.buildIntent(vararg argument: Pair<String, Any?>) =
@@ -14,6 +15,14 @@ fun Context.buildIntent(className: Class<*>, vararg argument: Pair<String, Any?>
     Intent(this, className).apply {
         putExtras(bundleOf(*argument))
     }
+
+fun buildDeepLinkIntent(type: String?) = Intent(Intent.ACTION_VIEW).apply {
+    data = "koin://$type/activity".toUri()
+}
+
+fun Context.isValidDeepLink(type: String?): Boolean {
+    return buildDeepLinkIntent(type).resolveActivity(packageManager) != null
+}
 
 inline fun <reified T : Activity> Context.goToActivity(vararg argument: Pair<String, Any?>) {
     startActivity(buildIntent<T>(*argument))
