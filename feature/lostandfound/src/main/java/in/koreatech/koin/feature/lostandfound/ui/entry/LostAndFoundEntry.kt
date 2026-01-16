c4505e544ce6e14eb9e7cf36abbbf63c2be00d72
KYM-P <k_ym02@naver.com>
2026-01-16 20:41:46 +0900

    chore: Feat ktlint

--- feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/entry/LostAndFoundEntry.kt
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/entry/LostAndFoundEntry.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 import `in`.koreatech.koin.feature.lostandfound.R
 import `in`.koreatech.koin.feature.lostandfound.component.SlideUpText
