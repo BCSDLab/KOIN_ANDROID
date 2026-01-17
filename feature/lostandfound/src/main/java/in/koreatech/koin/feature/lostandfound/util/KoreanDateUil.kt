fc94d1b80e27b5559952dd93a7dd3307830059d5
TTRR1007 <erickim2765@gmail.com>
2026-01-18 07:21:53 +0900

    chore: add lost and found

---
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/util/KoreanDateUil.kt
@@ -0,0 +1,21 @@
+package `in`.koreatech.koin.feature.lostandfound.util
+
+import java.time.DayOfWeek
+import java.time.LocalDate
+
+fun LocalDate.getKoreanDayOfWeekName(): String {
+    return when (dayOfWeek) {
+        DayOfWeek.MONDAY -> "월요일"
+        DayOfWeek.TUESDAY -> "화요일"
+        DayOfWeek.WEDNESDAY -> "수요일"
+        DayOfWeek.THURSDAY -> "목요일"
+        DayOfWeek.FRIDAY -> "금요일"
+        DayOfWeek.SATURDAY -> "토요일"
+        DayOfWeek.SUNDAY -> "일요일"
+        else -> throw IllegalArgumentException("Invalid day of week")
+    }
+}
+
+fun LocalDate.getKoreanDayOfWeekShortName(): String {
+    return this.getKoreanDayOfWeekName().substring(0, 1)
+}
