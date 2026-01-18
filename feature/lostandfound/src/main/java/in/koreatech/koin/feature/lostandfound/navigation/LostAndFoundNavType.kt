078181551feb6009fbba831091a8e630dbd05acf
KYM-P <k_ym02@naver.com>
2026-01-18 21:00:38 +0900

    chore: Delete useless space in text

--- feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/navigation/LostAndFoundNavType.kt
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/navigation/LostAndFoundNavType.kt
@@ -4,5 +4,5 @@ sealed class LostAndFoundNavType(val route: String) {
     data object LostAndFoundList : LostAndFoundNavType("lost_and_found_list")
     data object LostAndFoundDetail : LostAndFoundNavType("lost_and_found_detail")
     data object LostAndFoundReport : LostAndFoundNavType("lost_and_found_report")
-    data object LostAndFoundWrite : LostAndFoundNavType("lost_and_found_write ")
+    data object LostAndFoundWrite : LostAndFoundNavType("lost_and_found_write")
 }
