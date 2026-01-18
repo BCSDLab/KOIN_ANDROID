0c98532b3d8ea8c1145ecdd81d54a1ec32d93d1a
KYM-P <k_ym02@naver.com>
2026-01-18 20:27:20 +0900

    chore: Feat ktlint

--- feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/LostAndFoundActivity.kt
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/LostAndFoundActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 import `in`.koreatech.koin.core.designsystem.util.enableEdgeToEdgeWithLightStatusBar
-import `in`.koreatech.koin.feature.lostandfound.ui.detail.LostAndFoundDetail
 
 @AndroidEntryPoint
 class LostAndFoundActivity : ComponentActivity() {
