89915b5cf21169e131dcdd53d6985fe7e746f130
KYM-P <k_ym02@naver.com>
2026-01-18 21:20:07 +0900

    feature: Add detail navigate code

--- feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/navigation/Navigation.kt
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/navigation/Navigation.kt
@@ -1,17 +1,20 @@
 package `in`.koreatech.koin.feature.lostandfound.navigation
 
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import `in`.koreatech.koin.core.navigation.Navigator
 import `in`.koreatech.koin.feature.lostandfound.ui.detail.LostAndFoundDetail
 import `in`.koreatech.koin.feature.lostandfound.ui.list.LostAndFoundList
 import `in`.koreatech.koin.feature.lostandfound.ui.report.LostAndFoundReport
 
 
 fun NavGraphBuilder.koinLostAndFoundGraph(
-    navController: NavController
+    navController: NavController,
+    navigator: Navigator
 ) {
     composable(
         route = LostAndFoundNavType.LostAndFoundList.route,
@@ -25,9 +28,24 @@ fun NavGraphBuilder.koinLostAndFoundGraph(
             navArgument(ARTICLE_ID) { type = NavType.IntType }
         )
     ) {
+        val context = LocalContext.current
         LostAndFoundDetail(
+            navigateToArticleList = {
+                navController.navigate(LostAndFoundNavType.LostAndFoundList.route)
+            },
             onTopbarBackClick = {
                 navController.navigateUp()
+            },
+            navigateToChatRoom = { articleId ->
+                val intent = navigator.navigateToChatRoom(context)
+                intent.putExtra(ARTICLE_ID, articleId)
+                context.startActivity(intent)
+            },
+            navigateToRecentArticle = { articleId ->
+                navController.navigate("${LostAndFoundNavType.LostAndFoundDetail.route}/$articleId")
+            },
+            navigateToReport = { articleId ->
+                navController.navigate("${LostAndFoundNavType.LostAndFoundReport.route}/$articleId")
             }
         )
     }
@@ -53,4 +71,4 @@ fun NavGraphBuilder.koinLostAndFoundGraph(
 
 }
 
-const val ARTICLE_ID = "articleId"
+const val ARTICLE_ID = "article_id"
