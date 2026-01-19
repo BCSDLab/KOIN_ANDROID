3f3b19a9dde96f72c6d49ffb63900abb3e2dbf9d
KYM-P <k_ym02@naver.com>
2026-01-19 12:26:14 +0900

    feature: Recover lostandfound-ui branch

---
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/write/LostAndFoundWriteArticleItemState.kt
@@ -0,0 +1,33 @@
+package `in`.koreatech.koin.feature.lostandfound.ui.write
+
+import android.os.Parcelable
+import `in`.koreatech.koin.domain.model.article.ArticleLostAndFoundUpload
+import `in`.koreatech.koin.feature.lostandfound.enums.LostItemCategory
+import `in`.koreatech.koin.feature.lostandfound.enums.LostItemCategory.Companion.getCategoryKoreanWord
+import `in`.koreatech.koin.feature.lostandfound.enums.LostOrFoundType
+import java.time.LocalDate
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class LostAndFoundWriteArticleItemState(
+    val lostOrFoundType: LostOrFoundType = LostOrFoundType.FOUND,
+    val itemTypeRequired: Boolean = false,
+    val locationRequired: Boolean = false,
+    val dateRequired: Boolean = false,
+    val category: LostItemCategory = LostItemCategory.NONE,
+    val foundPlace: String = "",
+    val foundDate: LocalDate? = null,
+    val content: String? = null,
+    val images: List<String> = emptyList()
+) : Parcelable
+
+fun LostAndFoundWriteArticleItemState.toArticleLostAndFoundUpload(): ArticleLostAndFoundUpload {
+    return ArticleLostAndFoundUpload(
+        type = lostOrFoundType.name,
+        category = category.getCategoryKoreanWord(),
+        foundPlace = foundPlace.trim(),
+        foundDate = foundDate.toString(),
+        content = content,
+        images = images.map { it }
+    )
+}
---
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/write/LostAndFoundWriteArticleState.kt
@@ -0,0 +1,12 @@
+package `in`.koreatech.koin.feature.lostandfound.ui.write
+
+import android.os.Parcelable
+import `in`.koreatech.koin.feature.lostandfound.enums.LostOrFoundType
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class LostAndFoundWriteArticleState(
+    val lostOrFoundType: LostOrFoundType = LostOrFoundType.FOUND,
+    val itemList: List<LostAndFoundWriteArticleItemState> = emptyList(),
+    val isAllFieldValid: Boolean = false
+) : Parcelable
