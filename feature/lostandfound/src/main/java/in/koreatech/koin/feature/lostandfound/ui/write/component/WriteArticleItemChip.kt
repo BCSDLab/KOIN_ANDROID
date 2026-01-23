85c3a3c816027c5e1a8c27e9d94322ccae111270
KYM-P <k_ym02@naver.com>
2026-01-23 18:11:04 +0900

    feature: Add LostAndFounWrite item delete option

--- feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/write/component/WriteArticleItemChip.kt
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/write/component/WriteArticleItemChip.kt
@@ -3,14 +3,20 @@ package `in`.koreatech.koin.feature.lostandfound.ui.write.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import `in`.koreatech.koin.core.designsystem.noRippleClickable
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
 import `in`.koreatech.koin.feature.lostandfound.R
 import `in`.koreatech.koin.feature.lostandfound.enums.LostOrFoundType
@@ -47,6 +53,20 @@ fun WriteArticleItemChip(
                 color = KoinTheme.colors.primary600,
                 style = KoinTheme.typography.medium14,
                 modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp)
+            )
+        }
+        if (shouldShowDelete) {
+            Icon(
+                modifier =
+                    Modifier
+                        .width(36.dp)
+                        .height(28.dp)
+                        .padding(vertical = 4.dp, horizontal = 8.dp)
+                        .noRippleClickable {
+                            onDeleteItemClick()
+                        },
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_item_delete),
+                contentDescription = null
             )
         }
     }
