2f4946743725d377042ccec172360ab08bfc9756
WooJin Kong <kongwoojin03@gmail.com>
2026-01-29 00:59:57 +0900

    add: Add CacheMetadata Database

---
+++ data/src/main/java/in/koreatech/koin/data/constant/DBConstant.kt
@@ -0,0 +1,5 @@
+package `in`.koreatech.koin.data.constant
+
+object DBConstant {
+    const val CACHE_METADATA = "cache_metadata"
+}
--- data/src/main/java/in/koreatech/koin/data/db/AppDatabase.kt
+++ data/src/main/java/in/koreatech/koin/data/db/AppDatabase.kt
@@ -2,7 +2,10 @@ package `in`.koreatech.koin.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import `in`.koreatech.koin.data.dao.CacheMetadataDao
+import `in`.koreatech.koin.data.entity.CacheMetadataEntity
 
-@Database(entities = [], version = 1)
+@Database(entities = [CacheMetadataEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
+    abstract fun cacheMetadataDao(): CacheMetadataDao
 }
