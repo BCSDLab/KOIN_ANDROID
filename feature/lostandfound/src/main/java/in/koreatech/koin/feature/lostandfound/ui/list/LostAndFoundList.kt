e46c34c68070a038d8f18f3dbdf56285779b9a9f
KYM-P <k_ym02@naver.com>
2026-01-29 21:45:07 +0900

    feature: FAB button moved more bottom

--- feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/list/LostAndFoundList.kt
+++ feature/lostandfound/src/main/java/in/koreatech/koin/feature/lostandfound/ui/list/LostAndFoundList.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
@@ -145,6 +146,7 @@ fun LostAndFoundList(
         },
         floatingActionButton = {
             LostAndFoundFAB(
+                modifier = Modifier.offset(y = 10.dp),
                 onClick = {
                     if (uiState.isLoggedIn) {
                         viewModel.setShowWriteBottomSheet(true)
