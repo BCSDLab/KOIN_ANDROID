6316ef2b89af1b831b936adc09c767a02c58fb79
WooJin Kong <kongwoojin03@gmail.com>
2026-02-15 22:38:34 +0900

    add: Add Room schemeDirectory

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.room) apply false
 }
 
 tasks.register<Delete>("clean") {
