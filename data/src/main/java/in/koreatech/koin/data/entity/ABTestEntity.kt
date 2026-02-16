f97339c8f8f92f1896cf8cfeca76e3bcb9a971ad
WooJin Kong <kongwoojin03@gmail.com>
2026-02-16 17:32:51 +0900

    fix: Use access_history_id as primary key too

--- data/src/main/java/in/koreatech/koin/data/entity/ABTestEntity.kt
+++ data/src/main/java/in/koreatech/koin/data/entity/ABTestEntity.kt
@@ -2,12 +2,14 @@ package `in`.koreatech.koin.data.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 import `in`.koreatech.koin.data.constant.DBConstant
 
-@Entity(tableName = DBConstant.ABTEST)
+@Entity(
+    tableName = DBConstant.ABTEST,
+    primaryKeys = ["title", "access_history_id"]
+)
 data class ABTestEntity(
-    @PrimaryKey val title: String,
+    @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "access_history_id") val accessHistoryId: String,
     @ColumnInfo(name = "variable_name") val variableName: String
 )
