ce68f89256c4d14ea1f6b2b883e1e1398d8dd83c
WooJin Kong <kongwoojin03@gmail.com>
2026-02-21 15:16:59 +0900

    feat: Add callvan main entry point

---
+++ feature/callvan/src/main/java/in/koreatech/koin/feature/callvan/CallvanEntry.kt
@@ -0,0 +1,102 @@
+package `in`.koreatech.koin.feature.callvan
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowRight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import `in`.koreatech.koin.core.designsystem.noRippleClickable
+import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
+import `in`.koreatech.koin.core.designsystem.theme.RebrandKoinTheme
+
+@Composable
+fun CallvanEntry(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.callvan_entry_title),
+            style = KoinTheme.typography.bold18.copy(fontSize = 15.sp),
+            color = KoinTheme.colors.primary500
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Row {
+            CallvanEntryCard(
+                modifier = Modifier.weight(1f),
+                title = stringResource(R.string.callvan_entry_find_title),
+                description = stringResource(R.string.callvan_entry_find_description),
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            CallvanEntryCard(
+                modifier = Modifier.weight(1f),
+                title = stringResource(R.string.callvan_entry_recruit_title),
+                description = stringResource(R.string.callvan_entry_recruit_description)
+            )
+        }
+    }
+}
+
+@Composable
+private fun CallvanEntryCard(
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    Row(
+        modifier = modifier
+            .clip(KoinTheme.shapes.small)
+            .noRippleClickable(onClick = onClick)
+            .background(color = KoinTheme.colors.neutral50)
+            .padding(horizontal = 16.dp, vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column {
+            Text(
+                text = title,
+                style = KoinTheme.typography.medium14
+            )
+            Text(
+                text = description,
+                style = KoinTheme.typography.regular12
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Icon(
+            imageVector = Icons.AutoMirrored.Rounded.KeyboardArrowRight,
+            contentDescription = title
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CallvanEntryPreview() {
+    KoinTheme {
+        CallvanEntry()
+    }
+}
