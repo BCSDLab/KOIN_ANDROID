2c7216309a1a494db2cd78b7fa50d9d4e1113781
WooJin Kong <kongwoojin03@gmail.com>
2026-02-21 14:43:41 +0900

    add: Add basic navigation

--- feature/callvan/src/main/java/in/koreatech/koin/feature/callvan/CallvanActivity.kt
+++ feature/callvan/src/main/java/in/koreatech/koin/feature/callvan/CallvanActivity.kt
@@ -3,8 +3,15 @@ package `in`.koreatech.koin.feature.callvan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import `in`.koreatech.koin.core.designsystem.theme.RebrandKoinTheme
 import `in`.koreatech.koin.core.designsystem.util.enableEdgeToEdgeWithLightStatusBar
+import `in`.koreatech.koin.feature.callvan.navigation.CallvanNavType
+import `in`.koreatech.koin.feature.callvan.navigation.koinCallvanGraph
 
 class CallvanActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +19,17 @@ class CallvanActivity : ComponentActivity() {
         enableEdgeToEdgeWithLightStatusBar()
         setContent {
             RebrandKoinTheme {
+                val navController = rememberNavController()
 
+                Scaffold { innerPadding ->
+                    NavHost(
+                        modifier = Modifier.padding(innerPadding),
+                        navController = navController,
+                        startDestination = CallvanNavType.CallvanCreate::class
+                    ) {
+                        koinCallvanGraph(navController = navController)
+                    }
+                }
             }
         }
     }
---
+++ feature/callvan/src/main/java/in/koreatech/koin/feature/callvan/navigation/Navigation.kt
@@ -0,0 +1,33 @@
+package `in`.koreatech.koin.feature.callvan.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+
+fun NavGraphBuilder.koinCallvanGraph(
+    navController: NavController
+) {
+    composable<CallvanNavType.CallvanMain> {
+
+    }
+
+    composable<CallvanNavType.CallvanDetail> {
+
+    }
+
+    composable<CallvanNavType.CallvanCreate> {
+
+    }
+
+    composable<CallvanNavType.CallvanChat> {
+
+    }
+
+    composable<CallvanNavType.CallvanNotifications> {
+
+    }
+
+    composable<CallvanNavType.CallvanReport> {
+
+    }
+}
