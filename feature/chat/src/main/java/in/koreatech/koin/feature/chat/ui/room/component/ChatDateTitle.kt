230554d3ee18e7c79bacd9ee6f42cc56a68e59ba
WooJin Kong <kongwoojin03@gmail.com>
2026-02-23 22:10:55 +0900

    add: Add Rebrand colors for ChatDateTitle

--- feature/chat/src/main/java/in/koreatech/koin/feature/chat/ui/room/component/ChatDateTitle.kt
+++ feature/chat/src/main/java/in/koreatech/koin/feature/chat/ui/room/component/ChatDateTitle.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import `in`.koreatech.koin.core.designsystem.theme.KoinTheme
+import `in`.koreatech.koin.core.designsystem.theme.RebrandKoinTheme
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -21,6 +22,15 @@ object ChatDateTitleDefaults {
     fun colors(
         containerColor: Color = KoinTheme.colors.neutral200.copy(alpha = 0.8f),
         contentColor: Color = KoinTheme.colors.primary600
+    ): ChatDateTitleColors = ChatDateTitleColors(
+        containerColor = containerColor,
+        contentColor = contentColor
+    )
+
+    @Composable
+    fun purpleColors(
+        containerColor: Color = RebrandKoinTheme.colors.neutral100,
+        contentColor: Color = RebrandKoinTheme.colors.primary600
     ): ChatDateTitleColors = ChatDateTitleColors(
         containerColor = containerColor,
         contentColor = contentColor
