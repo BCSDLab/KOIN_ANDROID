6cd29f784f287c1f708304baf21c638ddef70aa2
WooJin Kong <kongwoojin03@gmail.com>
2026-03-07 18:43:43 +0900

    fix: Merge android lint

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,8 +54,8 @@ sonar {
     properties {
         property("sonar.projectKey", "BCSDLab_KOIN_ANDROID")
         property("sonar.organization", "bcsdlab")
-        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/kover/report.xml")
-        property("sonar.androidLint.reportPaths", "**/build/reports/lint-results*.xml")
+        property("sonar.coverage.jacoco.xmlReportPaths", "${layout.buildDirectory}/reports/kover/report.xml")
+        property("sonar.androidLint.reportPaths", "koin/build/reports/lint-results*.xml")
     }
 }
 
