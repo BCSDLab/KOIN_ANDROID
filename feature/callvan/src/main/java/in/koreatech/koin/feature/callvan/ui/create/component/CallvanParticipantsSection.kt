4deb0bc80629b962b81b555040a6d114b4df17e9
JaeYoung290 <172613798+JaeYoung290@users.noreply.github.com>
2026-03-16 12:51:58 +0900

    refactor: use RebrandKoinTheme instead fontweight

--- feature/callvan/src/main/java/in/koreatech/koin/feature/callvan/ui/create/component/CallvanParticipantsSection.kt
+++ feature/callvan/src/main/java/in/koreatech/koin/feature/callvan/ui/create/component/CallvanParticipantsSection.kt
@@ -24,9 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,7 +56,7 @@ fun CallvanParticipantsSection(
             )
             Text(
                 text = buildAnnotatedString {
-                    withStyle(SpanStyle(fontWeight = FontWeight.SemiBold)) {
+                    withStyle(RebrandKoinTheme.typography.bold12.toSpanStyle()) {
                         append(stringResource(R.string.callvan_create_participants_hint_bold))
                     }
                     append(stringResource(R.string.callvan_create_participants_hint_normal))
