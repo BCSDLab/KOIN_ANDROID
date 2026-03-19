caa4a69e59a0613287cce428815897a7fa6c1d87
KYM-P <k_ym02@naver.com>
2026-03-20 00:12:28 +0900

    Revert "Merge branch 'feature/#1283-callvan-create-screen' of https://github.com/BCSDLab/KOIN_ANDROID into feature/#1283-callvan-list-viewmodel"
    This reverts commit 07e1a4b51c7e175525be024f3da1a7289eb9a3a1, reversing
    changes made to a354c20819745944a122a5754c0ecfaaa0e2ad79.

--- feature/callvan/src/main/java/in/koreatech/koin/feature/callvan/ui/create/CallvanCreateSideEffect.kt
+++
@@ -1,6 +0,0 @@
-package `in`.koreatech.koin.feature.callvan.ui.create
-
-sealed interface CallvanCreateSideEffect {
-    data class NavigateToDetail(val postId: Int) : CallvanCreateSideEffect
-    data object ShowSubmitError : CallvanCreateSideEffect
-}
