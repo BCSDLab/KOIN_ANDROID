7defb1ea761c36887e7720b159633f04bff10859
WooJin Kong <kongwoojin03@gmail.com>
2026-04-15 20:13:54 +0900

    fix: Fix DiningPlace order

--- domain/src/main/java/in/koreatech/koin/domain/usecase/dining/GetNotOperationFilteredDiningUseCase.kt
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/dining/GetNotOperationFilteredDiningUseCase.kt
@@ -1,6 +1,7 @@
 package `in`.koreatech.koin.domain.usecase.dining
 
 import `in`.koreatech.koin.domain.model.dining.Dining
+import `in`.koreatech.koin.domain.model.dining.DiningPlace
 import `in`.koreatech.koin.domain.repository.DiningRepository
 import javax.inject.Inject
 
@@ -9,7 +10,17 @@ class GetNotOperationFilteredDiningUseCase @Inject constructor(
 ) {
     suspend operator fun invoke(date: String): Result<List<Dining>> {
         return runCatching {
-            diningRepository.getDining(date).filter { dining -> dining.menu.isNotEmpty() && dining.menu.first() != "미운영" }
+            diningRepository.getDining(date)
+                .filter { dining -> dining.menu.isNotEmpty() && dining.menu.first() != "미운영" }
+                .sortedBy { diningOrder[it.place] ?: Int.MAX_VALUE }
         }
     }
 }
+
+private val diningOrder = mapOf(
+    DiningPlace.CornerA.place to 0,
+    DiningPlace.CornerB.place to 1,
+    DiningPlace.CornerC.place to 2,
+    DiningPlace.Nungsu.place to 3,
+    DiningPlace.Campus2.place to 4
+)
