c6053689006121f9547b1415fde052ece8a81796
WooJin Kong <kongjak@kongjak.dev>
2026-05-29 22:06:01 +0900

    feat: Add callvan and store api

--- data/src/main/java/in/koreatech/koin/data/api/StoreApi.kt
+++ data/src/main/java/in/koreatech/koin/data/api/StoreApi.kt
@@ -11,8 +11,10 @@ import `in`.koreatech.koin.data.response.store.ShopRelatedListResponse
 import `in`.koreatech.koin.data.response.store.ShopResponse
 import `in`.koreatech.koin.data.response.store.ShopSearchRelatedResponse
 import `in`.koreatech.koin.data.response.store.ShopSummaryResponse
+import `in`.koreatech.koin.data.response.store.StoreEventCountResponse
 import `in`.koreatech.koin.data.response.store.StoreBenefitResponse
 import `in`.koreatech.koin.data.response.store.StoreCategoriesResponse
+import `in`.koreatech.koin.data.response.store.StoreCountResponse
 import `in`.koreatech.koin.data.response.store.StoreDetailEventResponse
 import `in`.koreatech.koin.data.response.store.StoreEventResponse
 import `in`.koreatech.koin.data.response.store.StoreItemWithMenusResponse
@@ -127,6 +129,12 @@ interface StoreApi {
         @Query("filter") filter: List<String>?,
         @Query("query") query: String?
     ): StoreResponse
+
+    @GET("/shops/count")
+    suspend fun getShopsCount(): StoreCountResponse
+
+    @GET("/shops/events/count")
+    suspend fun getShopsEventCount(): StoreEventCountResponse
 
     @GET("/order/shop/{orderableShopId}/summary")
     suspend fun getOrderableShopSummary(
--- data/src/main/java/in/koreatech/koin/data/repository/CallvanRepositoryImpl.kt
+++ data/src/main/java/in/koreatech/koin/data/repository/CallvanRepositoryImpl.kt
@@ -87,6 +87,27 @@ class CallvanRepositoryImpl @Inject constructor(
         }
     }
 
+    override suspend fun getRecruitingCallvanCount(): Result<Int> {
+        return suspendRunCatching {
+            callvanRemoteDataSource.getCallvanPosts(
+                author = null,
+                departures = null,
+                departureKeyword = null,
+                arrivals = null,
+                arrivalKeyword = null,
+                statuses = listOf("RECRUITING"),
+                title = null,
+                sort = null,
+                joined = false,
+                page = null,
+                limit = null
+            ).totalCount.toInt()
+        }.mapHttpFailure {
+            on(401) throws KoinCallvanException.UnauthorizedUserException()
+            on(404) throws KoinCallvanException.NotFoundUserException()
+        }
+    }
+
     override suspend fun sendMessage(
         postId: Int,
         isImage: Boolean,
