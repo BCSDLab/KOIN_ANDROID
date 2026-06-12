302b3e31e3fa03ef16d3116a14113a2066737787
WooJin Kong <kongjak@kongjak.dev>
2026-06-12 09:45:46 +0900

    fix: Use suspendRunCatching

--- data/src/main/java/in/koreatech/koin/data/repository/WeatherRepositoryImpl.kt
+++ data/src/main/java/in/koreatech/koin/data/repository/WeatherRepositoryImpl.kt
@@ -3,6 +3,7 @@ package `in`.koreatech.koin.data.repository
 import `in`.koreatech.koin.data.mapper.toWeather
 import `in`.koreatech.koin.data.source.remote.WeatherRemoteDataSource
 import `in`.koreatech.koin.data.util.mapHttpFailure
+import `in`.koreatech.koin.data.util.suspendRunCatching
 import `in`.koreatech.koin.domain.error.weather.KoinWeatherException
 import `in`.koreatech.koin.domain.model.weather.Weather
 import `in`.koreatech.koin.domain.repository.WeatherRepository
@@ -12,7 +13,7 @@ class WeatherRepositoryImpl @Inject constructor(
     private val weatherRemoteDataSource: WeatherRemoteDataSource
 ) : WeatherRepository {
     override suspend fun getWeather(): Result<Weather> {
-        return runCatching {
+        return suspendRunCatching {
             weatherRemoteDataSource.getWeather().toWeather()
         }.mapHttpFailure {
             on(500, "EXTERNAL_API_ERROR") throws KoinWeatherException.ExternalApiErrorException()
