ffbdffc6c56b2ad758a1113442b9c101a0cbbab6
WooJin Kong <kongjak@kongjak.dev>
2026-06-12 13:35:42 +0900

    chore: Remove unused weatherId

--- feature/home/src/main/java/in/koreatech/koin/feature/home/model/LocalWeather.kt
+++ feature/home/src/main/java/in/koreatech/koin/feature/home/model/LocalWeather.kt
@@ -5,13 +5,11 @@ import `in`.koreatech.koin.domain.model.weather.Weather
 data class LocalWeather(
     val temperature: Int,
     val weather: String,
-    val weatherId: Int,
     val weatherIconUrl: String
 )
 
 internal fun Weather.toLocalWeather() = LocalWeather(
     temperature = temperature,
     weather = weather,
-    weatherId = weatherId,
     weatherIconUrl = weatherIconUrl
 )
