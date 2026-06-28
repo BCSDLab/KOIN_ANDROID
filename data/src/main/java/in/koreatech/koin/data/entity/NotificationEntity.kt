923fa4162616c44fde3957fe60f8eb1fbfef3257
WooJin Kong <kongjak@kongjak.dev>
2026-06-29 04:05:21 +0900

    fix: Make originUrl unique

--- data/src/main/java/in/koreatech/koin/data/entity/NotificationEntity.kt
+++ data/src/main/java/in/koreatech/koin/data/entity/NotificationEntity.kt
@@ -2,11 +2,15 @@ package `in`.koreatech.koin.data.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import `in`.koreatech.koin.data.constant.DBConstant
 import java.time.LocalDateTime
 
-@Entity(tableName = DBConstant.NOTIFICATION)
+@Entity(
+    tableName = DBConstant.NOTIFICATION,
+    indices = [Index(value = ["originUrl"], unique = true)]
+)
 data class NotificationEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo val type: String,
