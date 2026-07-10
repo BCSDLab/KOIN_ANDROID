bb02deecf343d99c2f80296473815e81caf88920
WooJin Kong <kongjak@kongjak.dev>
2026-07-10 09:51:25 +0900

    feat: Update LargeHomeCard design

--- feature/home/src/main/java/in/koreatech/koin/feature/home/component/LargeHomeCard.kt
+++ feature/home/src/main/java/in/koreatech/koin/feature/home/component/LargeHomeCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -71,6 +72,10 @@ fun LargeHomeCard(
             .padding(contentPadding),
         verticalAlignment = Alignment.CenterVertically
     ) {
+        icon()
+
+        Spacer(modifier = Modifier.width(20.dp))
+
         Column(
             modifier = Modifier
         ) {
@@ -86,8 +91,6 @@ fun LargeHomeCard(
         }
 
         Spacer(modifier = Modifier.weight(1f))
-
-        icon()
 
         Icon(
             modifier = Modifier.size(20.dp),
