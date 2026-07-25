a1343b3d13c1ab00c694248259545880524391b1
TTRR1007 <erickim2765@gmail.com>
2026-07-25 20:43:06 +0900

    chore: kt lint

--- feature/department/src/main/java/in/koreatech/koin/feature/department/component/DepartmentSearchField.kt
+++ feature/department/src/main/java/in/koreatech/koin/feature/department/component/DepartmentSearchField.kt
@@ -14,8 +14,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
