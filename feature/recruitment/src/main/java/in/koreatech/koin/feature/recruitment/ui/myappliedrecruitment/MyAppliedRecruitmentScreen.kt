ea52d306cb2edf3a868b7e41b444597ec0c3575d
KYM-P <k_ym02@naver.com>
2026-08-25 23:26:36 +0900

    chore: Feat ktlint

--- feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/ui/myappliedrecruitment/MyAppliedRecruitmentScreen.kt
+++ feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/ui/myappliedrecruitment/MyAppliedRecruitmentScreen.kt
@@ -29,8 +29,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
