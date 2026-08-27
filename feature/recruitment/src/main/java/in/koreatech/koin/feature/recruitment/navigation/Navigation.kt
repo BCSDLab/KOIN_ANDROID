98e9104dd08df1a2cff59ebba59d768ae3990e33
WooJin Kong <kongjak@kongjak.dev>
2026-08-27 09:14:07 +0900

    add: Add Navigation

--- feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/navigation/Navigation.kt
+++ feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/navigation/Navigation.kt
@@ -2,9 +2,18 @@ package `in`.koreatech.koin.feature.recruitment.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import `in`.koreatech.koin.feature.recruitment.ui.notification.RecruitmentNotificationScreen
 
-@Suppress("EmptyFunctionBlock")
 fun NavGraphBuilder.koinRecruitmentGraph(
     navController: NavController
 ) {
+    composable<RecruitmentNavType.Notification> {
+        RecruitmentNotificationScreen(
+            onBack = { navController.popBackStack() },
+            onNavigateToPost = {
+                // TODO: 모집글 상세 화면 라우트가 추가되면 postId 로 이동하도록 연결한다.
+            }
+        )
+    }
 }
