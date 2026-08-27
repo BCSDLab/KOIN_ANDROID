b91b267023c746c83919f4de353ad3367bbd43a3
WooJin Kong <kongjak@kongjak.dev>
2026-08-27 11:29:08 +0900

    add: Add chat navigation

--- feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/navigation/Navigation.kt
+++ feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/navigation/Navigation.kt
@@ -2,9 +2,18 @@ package `in`.koreatech.koin.feature.recruitment.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import `in`.koreatech.koin.feature.recruitment.ui.chat.directchat.RecruitmentDirectChatScreen
+import `in`.koreatech.koin.feature.recruitment.ui.chat.groupchat.RecruitmentGroupChatScreen
 
-@Suppress("EmptyFunctionBlock")
 fun NavGraphBuilder.koinRecruitmentGraph(
     navController: NavController
 ) {
+    composable<RecruitmentNavType.RecruitmentGroupChat> {
+        RecruitmentGroupChatScreen()
+    }
+
+    composable<RecruitmentNavType.RecruitmentDirectChat> {
+        RecruitmentDirectChatScreen()
+    }
 }
--- feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/navigation/RecruitmentNavType.kt
+++ feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/navigation/RecruitmentNavType.kt
@@ -3,4 +3,20 @@ package `in`.koreatech.koin.feature.recruitment.navigation
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed class RecruitmentNavType
+sealed class RecruitmentNavType {
+    @Serializable
+    data class RecruitmentGroupChat(
+        val postId: Int,
+        val title: String,
+        val currentMemberCount: Int,
+        val maxMemberCount: Int,
+        val date: String
+    ) : RecruitmentNavType()
+
+    @Serializable
+    data class RecruitmentDirectChat(
+        val postId: Int,
+        val partnerNickname: String,
+        val date: String
+    ) : RecruitmentNavType()
+}
