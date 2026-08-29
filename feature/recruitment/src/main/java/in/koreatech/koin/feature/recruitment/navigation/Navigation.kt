70983c7831068bdd7ca25036333b59edc940c0b7
TTRR1007 <erickim2765@gmail.com>
2026-08-30 01:47:26 +0900

    chore: kt lint

--- feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/navigation/Navigation.kt
+++ feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/navigation/Navigation.kt
@@ -3,10 +3,6 @@ package `in`.koreatech.koin.feature.recruitment.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import `in`.koreatech.koin.feature.recruitment.ui.profile.ProfileScreen
-import `in`.koreatech.koin.feature.recruitment.ui.profilecreate.ProfileCreateScreen
-import `in`.koreatech.koin.feature.recruitment.ui.recruitmentapply.RecruitmentApplyScreen
-import `in`.koreatech.koin.feature.recruitment.ui.recruitmentcreate.RecruitmentCreateScreen
 import androidx.navigation.toRoute
 import `in`.koreatech.koin.feature.recruitment.ui.applicantdetail.ApplicantDetailScreen
 import `in`.koreatech.koin.feature.recruitment.ui.applicantmanagement.ApplicantManagementScreen
@@ -14,6 +10,10 @@ import `in`.koreatech.koin.feature.recruitment.ui.chat.directchat.RecruitmentDir
 import `in`.koreatech.koin.feature.recruitment.ui.chat.groupchat.RecruitmentGroupChatScreen
 import `in`.koreatech.koin.feature.recruitment.ui.myrecruitment.MyRecruitmentScreen
 import `in`.koreatech.koin.feature.recruitment.ui.notification.RecruitmentNotificationScreen
+import `in`.koreatech.koin.feature.recruitment.ui.profile.ProfileScreen
+import `in`.koreatech.koin.feature.recruitment.ui.profilecreate.ProfileCreateScreen
+import `in`.koreatech.koin.feature.recruitment.ui.recruitmentapply.RecruitmentApplyScreen
+import `in`.koreatech.koin.feature.recruitment.ui.recruitmentcreate.RecruitmentCreateScreen
 
 fun NavGraphBuilder.koinRecruitmentGraph(
     navController: NavController
@@ -44,7 +44,8 @@ fun NavGraphBuilder.koinRecruitmentGraph(
         ProfileCreateScreen(
             onNavigateUp = { navController.navigateUp() },
             onSaveSuccess = { navController.navigateUp() }
-            onNavigateToMyAppliedRecruitment = { }
+        )
+    }
     composable<RecruitmentNavType.RecruitmentGroupChat> {
         RecruitmentGroupChatScreen()
     }
