0848e2ac7af0a7a4c4f468923fde3d41bf3be6f3
TTRR1007 <erickim2765@gmail.com>
2026-08-30 00:25:42 +0900

    chore: kt lint

--- feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/ui/profile/component/ProfileEmptyState.kt
+++ feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/ui/profile/component/ProfileEmptyState.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,7 +17,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Text
 import `in`.koreatech.koin.core.designsystem.theme.RebrandKoinTheme
 import `in`.koreatech.koin.feature.recruitment.R
 import `in`.koreatech.koin.feature.recruitment.ui.component.RecruitmentOutlinedActionButton
