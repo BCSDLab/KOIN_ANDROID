e62b901b3e956bea470c2a5c4a53da59479cc655
TTRR1007 <erickim2765@gmail.com>
2026-08-30 02:00:06 +0900

    chore: kt lint

--- feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/navigation/Navigation.kt
+++ feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/navigation/Navigation.kt
@@ -3,7 +3,6 @@ package `in`.koreatech.koin.feature.recruitment.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import `in`.koreatech.koin.feature.recruitment.ui.recruitmentcreate.RecruitmentCreateScreen
 import androidx.navigation.toRoute
 import `in`.koreatech.koin.feature.recruitment.ui.applicantdetail.ApplicantDetailScreen
 import `in`.koreatech.koin.feature.recruitment.ui.applicantmanagement.ApplicantManagementScreen
@@ -11,6 +10,7 @@ import `in`.koreatech.koin.feature.recruitment.ui.chat.directchat.RecruitmentDir
 import `in`.koreatech.koin.feature.recruitment.ui.chat.groupchat.RecruitmentGroupChatScreen
 import `in`.koreatech.koin.feature.recruitment.ui.myrecruitment.MyRecruitmentScreen
 import `in`.koreatech.koin.feature.recruitment.ui.notification.RecruitmentNotificationScreen
+import `in`.koreatech.koin.feature.recruitment.ui.recruitmentcreate.RecruitmentCreateScreen
 
 fun NavGraphBuilder.koinRecruitmentGraph(
     navController: NavController
@@ -19,6 +19,8 @@ fun NavGraphBuilder.koinRecruitmentGraph(
         RecruitmentCreateScreen(
             onNavigateUp = { navController.navigateUp() },
             onRecruitmentCreated = { navController.navigateUp() }
+        )
+    }
     composable<RecruitmentNavType.RecruitmentGroupChat> {
         RecruitmentGroupChatScreen()
     }
