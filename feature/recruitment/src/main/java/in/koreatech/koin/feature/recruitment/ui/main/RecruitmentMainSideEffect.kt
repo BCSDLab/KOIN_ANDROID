50151d97ee72d2af28b304bd5029fc3a184f0298
TTRR1007 <erickim2765@gmail.com>
2026-09-16 19:56:00 +0900

    fix: block profile access for unauthenticated users

--- feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/ui/main/RecruitmentMainSideEffect.kt
+++ feature/recruitment/src/main/java/in/koreatech/koin/feature/recruitment/ui/main/RecruitmentMainSideEffect.kt
@@ -2,7 +2,9 @@ package `in`.koreatech.koin.feature.recruitment.ui.main
 
 sealed interface RecruitmentMainSideEffect {
     data object ShowError : RecruitmentMainSideEffect
+    data object ShowLoginRequiredToast : RecruitmentMainSideEffect
     data object NavigateToWrite : RecruitmentMainSideEffect
+    data object NavigateToProfile : RecruitmentMainSideEffect
     data object NavigateToNotification : RecruitmentMainSideEffect
     data object NavigateToLogin : RecruitmentMainSideEffect
 }
