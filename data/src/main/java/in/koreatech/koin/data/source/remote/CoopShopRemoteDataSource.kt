18fecdfb29acbe12118ac327b4d369944f2da15f
WooJin Kong <kongjak@kongjak.dev>
2026-09-16 22:28:37 +0900

    fix: Fix CoopShop Response not correct

--- data/src/main/java/in/koreatech/koin/data/source/remote/CoopShopRemoteDataSource.kt
+++ data/src/main/java/in/koreatech/koin/data/source/remote/CoopShopRemoteDataSource.kt
@@ -1,13 +1,14 @@
 package `in`.koreatech.koin.data.source.remote
 
 import `in`.koreatech.koin.data.api.CoopShopApi
+import `in`.koreatech.koin.data.response.coopshop.CoopShopAllResponse
 import `in`.koreatech.koin.data.response.coopshop.CoopShopResponse
 import javax.inject.Inject
 
 class CoopShopRemoteDataSource @Inject constructor(
     private val coopShopApi: CoopShopApi
 ) {
-    suspend fun getCoopShopAll(): List<CoopShopResponse> {
+    suspend fun getCoopShopAll(): CoopShopAllResponse {
         return coopShopApi.getCoopShopAll()
     }
 
