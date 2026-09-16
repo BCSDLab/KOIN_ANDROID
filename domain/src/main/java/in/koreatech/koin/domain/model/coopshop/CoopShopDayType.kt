a4be4561cec38e071cef1151a0bdbc43ca668d53
WooJin Kong <kongjak@kongjak.dev>
2026-09-16 22:35:17 +0900

    fix: Fix when unknown date type responsed from API

--- domain/src/main/java/in/koreatech/koin/domain/model/coopshop/CoopShopDayType.kt
+++ domain/src/main/java/in/koreatech/koin/domain/model/coopshop/CoopShopDayType.kt
@@ -5,13 +5,25 @@ enum class CoopShopDayType(
 ) {
     Weekday("평일"),
     Weekend("주말"),
-    Saturday("토요일")
+    Monday("월요일"),
+    Tuesday("화요일"),
+    Wednesday("수요일"),
+    Thursday("목요일"),
+    Friday("금요일"),
+    Saturday("토요일"),
+    Sunday("일요일")
 }
 
 val String.toCoopShopDayType
     get() = when (this) {
         "평일" -> CoopShopDayType.Weekday
         "주말" -> CoopShopDayType.Weekend
+        "월요일" -> CoopShopDayType.Monday
+        "화요일" -> CoopShopDayType.Tuesday
+        "수요일" -> CoopShopDayType.Wednesday
+        "목요일" -> CoopShopDayType.Thursday
+        "금요일" -> CoopShopDayType.Friday
         "토요일" -> CoopShopDayType.Saturday
+        "일요일" -> CoopShopDayType.Sunday
         else -> throw IllegalArgumentException("Invalid day type")
     }
