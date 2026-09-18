a73e889b230d052ab54347b3b86aae4ccbf1a024
WooJin Kong <kongjak@kongjak.dev>
2026-09-19 01:30:06 +0900

    fix: Use suspendRunCatching

--- domain/src/main/java/in/koreatech/koin/domain/usecase/home/SyncHomeUseCase.kt
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/home/SyncHomeUseCase.kt
@@ -4,13 +4,14 @@ import `in`.koreatech.koin.domain.repository.DiningRepository
 import `in`.koreatech.koin.domain.repository.WeatherRepository
 import `in`.koreatech.koin.domain.util.DiningUtil
 import `in`.koreatech.koin.domain.util.TimeUtil
+import `in`.koreatech.koin.domain.util.suspendRunCatching
 import javax.inject.Inject
 
 class SyncHomeUseCase @Inject constructor(
     private val diningRepository: DiningRepository,
     private val weatherRepository: WeatherRepository
 ) {
-    suspend operator fun invoke(): Result<Unit> = runCatching {
+    suspend operator fun invoke(): Result<Unit> = suspendRunCatching {
         weatherRepository.getWeather(forceRefresh = true).getOrThrow()
         diningRepository.fetchDining(TimeUtil.dateFormatToYYMMDD(DiningUtil.getCurrentDate())).getOrThrow()
     }
