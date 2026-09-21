35e0324a52d37e473ce2276e2d5c040ec1b8391b
WooJin Kong <kongjak@kongjak.dev>
2026-09-21 09:45:45 +0900

    fix: Use async to fetch weather and dining

--- domain/src/main/java/in/koreatech/koin/domain/usecase/home/SyncHomeUseCase.kt
+++ domain/src/main/java/in/koreatech/koin/domain/usecase/home/SyncHomeUseCase.kt
@@ -6,13 +6,20 @@ import `in`.koreatech.koin.domain.util.DiningUtil
 import `in`.koreatech.koin.domain.util.TimeUtil
 import `in`.koreatech.koin.domain.util.suspendRunCatching
 import javax.inject.Inject
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 
 class SyncHomeUseCase @Inject constructor(
     private val diningRepository: DiningRepository,
     private val weatherRepository: WeatherRepository
 ) {
     suspend operator fun invoke(): Result<Unit> = suspendRunCatching {
-        weatherRepository.getWeather(forceRefresh = true).getOrThrow()
-        diningRepository.fetchDining(TimeUtil.dateFormatToYYMMDD(DiningUtil.getCurrentDate())).getOrThrow()
+        coroutineScope {
+            val weather = async { weatherRepository.getWeather(forceRefresh = true).getOrThrow() }
+            val dining = async { diningRepository.fetchDining(TimeUtil.dateFormatToYYMMDD(DiningUtil.getCurrentDate())).getOrThrow() }
+
+            weather.await()
+            dining.await()
+        }
     }
 }
